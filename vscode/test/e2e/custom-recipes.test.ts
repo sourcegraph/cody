7cdbae6c82ca8adf48fd8941532f11c8231afd4f
Beatrix <beatrix@sourcegraph.com>
2023-07-25 08:33:10 -0700

    update test

--- vscode/test/e2e/custom-recipes.test.ts
+++ vscode/test/e2e/custom-recipes.test.ts
@@ -3,7 +3,7 @@ import { expect } from '@playwright/test'
 import { sidebarSignin } from './common'
 import { test } from './helpers'
 
-test('open the custom recipes in sidebar and quick pick menu', async ({ page, sidebar }) => {
+test('open the custom recipes in sidebar', async ({ page, sidebar }) => {
     // Sign into Cody
     await sidebarSignin(page, sidebar)
 
@@ -17,10 +17,8 @@ test('open the custom recipes in sidebar and quick pick menu', async ({ page, si
     // Check if default recipes are present
     await expect(sidebar.getByText('Generate a unit test')).toBeVisible()
 
-    // Check if the Custom Recipes UI shows up
-    await expect(sidebar.locator('.codicon-tools')).toBeVisible()
-
     // TODO: Open the quick pick menu
+    // await expect(sidebar.locator('.codicon-tools')).toBeVisible()
     // await sidebar.locator('.codicon-tools').click()
     // await page.getByRole('option', { name: 'Create a New User Recipe, recipes manager' }).locator('a').click()
     // await expect(page.getByText('Cody Custom Recipes - New Recipe')).toBeVisible()
