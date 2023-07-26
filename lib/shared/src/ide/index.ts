c20abe40675e8bc2ec2683f760d2054608eba513
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-07-26 21:24:08 +0200

    WIP

--- lib/shared/src/ide/index.ts
+++ lib/shared/src/ide/index.ts
@@ -1,6 +1,7 @@
 import type * as vscode from 'vscode'
 
-export let ide: typeof vscode = 'IDE is not initialized. To fix this problem, call the `setIDE` function from @sourcegraph/cody-shared/src/ide.' as unknown as typeof vscode
+export let ide: typeof vscode =
+    'IDE is not initialized. To fix this problem, call the `setIDE` function from @sourcegraph/cody-shared/src/ide.' as unknown as typeof vscode
 export function setIDE(newIDE: Partial<typeof vscode>): void {
     ide = newIDE as typeof vscode
 }
