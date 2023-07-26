9f34cbb3c6f8c0e9e6d9bbea8ba461272d8944d2
Beatrix <beatrix@sourcegraph.com>
2023-07-26 13:47:50 -0700

    update e2e test

--- vscode/test/e2e/custom-recipes.test.ts
+++ vscode/test/e2e/custom-recipes.test.ts
@@ -3,7 +3,7 @@ import { expect } from '@playwright/test'
 import { sidebarSignin } from './common'
 import { test } from './helpers'
 
-test('open the custom recipes in sidebar', async ({ page, sidebar }) => {
+test('open the custom recipes in sidebar and add new user recipe', async ({ page, sidebar }) => {
     // Sign into Cody
     await sidebarSignin(page, sidebar)
 
@@ -15,11 +15,19 @@ test('open the custom recipes in sidebar', async ({ page, sidebar }) => {
     await expect(sidebar.getByText('Custom Recipes')).toBeVisible()
 
     // Check if default recipes are present
-    await expect(sidebar.getByText('Generate a unit test')).toBeVisible()
+    await expect(sidebar.getByText('Generate Unit Tests')).toBeVisible()
 
     // TODO: Open the quick pick menu
-    // await expect(sidebar.locator('.codicon-tools')).toBeVisible()
-    // await sidebar.locator('.codicon-tools').click()
-    // await page.getByRole('option', { name: 'Create a New User Recipe, recipes manager' }).locator('a').click()
-    // await expect(page.getByText('Cody Custom Recipes - New Recipe')).toBeVisible()
+    await expect(sidebar.getByTitle('Open Custom Recipes Menu')).toBeVisible()
+    await sidebar.getByTitle('Open Custom Recipes Menu').click()
+    await page.getByText('Cody: Custom Recipes (Experimental)').click()
+    await page.locator('a').filter({ hasText: 'Add User Recipe' }).click()
+    await page.getByPlaceholder('e,g. Vulnerability Scanner').click()
+    await page.getByPlaceholder('e,g. Vulnerability Scanner').fill('Test Recipes')
+    await page.getByPlaceholder('e,g. Vulnerability Scanner').press('Enter')
+    await page.getByPlaceholder("e,g. 'Create five different test cases for the selected code''").fill('this is a test')
+    await page.getByPlaceholder("e,g. 'Create five different test cases for the selected code''").press('Enter')
+    await page.getByLabel('NoneExclude all types of context.').check()
+    await page.getByRole('button', { name: 'OK' }).click()
+    await sidebar.getByRole('button', { name: 'Test Recipes' }).click()
 })
