787055f243cabed815658d7c4140d9014c105ab4
Quinn Slack <quinn@slack.org>
2023-07-28 00:51:54 -0700

    full mock of vscode.TextDocument

--- vscode/test/completions/run-code-completions-on-dataset.ts
+++ vscode/test/completions/run-code-completions-on-dataset.ts
@@ -5,7 +5,7 @@ import fs from 'fs'
 import path from 'path'
 
 import * as vscode from 'vscode'
-import { URI } from 'vscode-uri'
+import { TextDocument } from 'vscode-languageserver-textdocument'
 
 import { NoopEditor } from '@sourcegraph/cody-shared/src/editor'
 import { SourcegraphNodeCompletionsClient } from '@sourcegraph/cody-shared/src/sourcegraph-api/completions/nodeClient'
@@ -18,11 +18,11 @@ import { createProviderConfig } from '../../src/completions/providers/createProv
 import { getFullConfig } from '../../src/configuration'
 import { configureExternalServices } from '../../src/external-services'
 import { InMemorySecretStorage } from '../../src/services/SecretStorageProvider'
+import { wrapVSCodeTextDocument } from '../../src/testutils/textDocument'
 
 import { completionsDataset, CURSOR, Sample } from './completions-dataset'
 import { ENVIRONMENT_CONFIG } from './environment-config'
 import { findSubstringPosition } from './utils'
-import { TextDocument } from './vscode-text-document'
 
 let didLogConfig = false
 
@@ -85,7 +85,7 @@ function prepareTextDocument(
 
     // Remove CURSOR marks from the code before processing it further.
     const completionReadyCode = code.replaceAll(CURSOR, '')
-    const textDocument = new TextDocument(URI.parse('file:///' + fileName), languageId, completionReadyCode)
+    const textDocument = TextDocument.create('file:///' + fileName, languageId, 0, completionReadyCode)
 
     return { textDocument, position }
 }
@@ -126,7 +126,7 @@ async function generateCompletionsForDataset(codeSamples: Sample[]): Promise<voi
 
             const completionsProvider = await initCompletionsProvider(context)
             const completionItems = await completionsProvider.provideInlineCompletionItems(
-                textDocument,
+                wrapVSCodeTextDocument(textDocument),
                 position,
                 {
                     triggerKind: 1,
