0619956cee6430c7f86ce1cce24cd7fbd33de92b
Philipp Spiess <hello@philippspiess.com>
2023-07-28 11:58:37 +0200

    Autocomplete: Add fireworks provider

--- vscode/src/completions/providers/createProvider.ts
+++ vscode/src/completions/providers/createProvider.ts
@@ -4,6 +4,7 @@ import { SourcegraphNodeCompletionsClient } from '@sourcegraph/cody-shared/src/s
 import { createProviderConfig as createAnthropicProviderConfig } from './anthropic'
 import { ProviderConfig } from './provider'
 import { createProviderConfig as createUnstableCodeGenProviderConfig } from './unstable-codegen'
+import { createProviderConfig as createUnstableFireworksProviderConfig } from './unstable-fireworks'
 import { createProviderConfig as createUnstableHuggingFaceProviderConfig } from './unstable-huggingface'
 
 export function createProviderConfig(
@@ -37,6 +38,20 @@ export function createProviderConfig(
 
             onError(
                 'Provider `unstable-huggingface` can not be used without configuring `cody.autocomplete.advanced.serverEndpoint`. Falling back to `anthropic`.'
+            )
+            break
+        }
+        case 'unstable-fireworks': {
+            if (config.autocompleteAdvancedServerEndpoint !== null) {
+                providerConfig = createUnstableFireworksProviderConfig({
+                    serverEndpoint: config.autocompleteAdvancedServerEndpoint,
+                    accessToken: config.autocompleteAdvancedAccessToken,
+                })
+                break
+            }
+
+            onError(
+                'Provider `unstable-fireworks` can not be used without configuring `cody.autocomplete.advanced.serverEndpoint`. Falling back to `anthropic`.'
             )
             break
         }
