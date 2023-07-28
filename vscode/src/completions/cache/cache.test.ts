73c2b6cd3ef2b0394637942fc3a0c513c6a346ed
Quinn Slack <quinn@slack.org>
2023-07-28 03:18:44 -0700

    wip CC pass

--- vscode/src/completions/cache/cache.test.ts
+++ vscode/src/completions/cache/cache.test.ts
@@ -7,7 +7,9 @@ function docState(
     other?: Partial<Omit<CompletionsCacheDocumentState, 'prefix'>>
 ): CompletionsCacheDocumentState {
     return {
+        uri: 'file:///f',
         prefix,
+        position: prefix.length,
         suffix: ';',
         languageId: 'javascript',
         ...other,
@@ -29,11 +31,6 @@ describe('CompletionsCache', () => {
         const cache = new CompletionsCache()
         cache.add('id1', docState('foo\n'), [{ content: 'bar' }])
 
-        expect(cache.__stateForTestsOnly).toEqual<CompletionsCache['__stateForTestsOnly']>({
-            'javascript<|>foo\n<|>;': { logId: 'id1', completions: [{ content: 'bar' }] },
-            'javascript<|>foo\nb<|>;': { logId: 'id1', completions: [{ content: 'ar' }] },
-            'javascript<|>foo\nba<|>;': { logId: 'id1', completions: [{ content: 'r' }] },
-        })
         expect(cache.get({ documentState: docState('foo\nb') })).toEqual({
             logId: 'id1',
             completions: [{ content: 'ar' }],
@@ -48,46 +45,13 @@ describe('CompletionsCache', () => {
         const cache = new CompletionsCache()
         cache.add('id1', docState('p', { suffix: 's' }), [{ content: 'c' }])
 
-        expect(cache.__stateForTestsOnly).toEqual<CompletionsCache['__stateForTestsOnly']>({
-            'javascript<|>p<|>s': { logId: 'id1', completions: [{ content: 'c' }] },
-        })
         expect(cache.get({ documentState: docState('foo\nb', { suffix: 's2' }) })).toEqual(undefined)
-    })
-
-    it('trims trailing whitespace on empty line', () => {
-        const cache = new CompletionsCache()
-        cache.add('id1', docState('foo \n  '), [{ content: 'bar' }])
-
-        expect(cache.__stateForTestsOnly).toEqual<CompletionsCache['__stateForTestsOnly']>({
-            'javascript<|>foo \n<|>;': { logId: 'id1', completions: [{ content: 'bar' }] },
-            'javascript<|>foo \n  <|>;': { logId: 'id1', completions: [{ content: 'bar' }] },
-            'javascript<|>foo \nb<|>;': { logId: 'id1', completions: [{ content: 'ar' }] },
-            'javascript<|>foo \nba<|>;': { logId: 'id1', completions: [{ content: 'r' }] },
-        })
-        expect(cache.get({ documentState: docState('foo \n  ') })).toEqual({
-            logId: 'id1',
-            completions: [{ content: 'bar' }],
-        })
-        expect(cache.get({ documentState: docState('foo \n ') })).toEqual({
-            logId: 'id1',
-            completions: [{ content: 'bar' }],
-        })
-        expect(cache.get({ documentState: docState('foo \n') })).toEqual({
-            logId: 'id1',
-            completions: [{ content: 'bar' }],
-        })
-        expect(cache.get({ documentState: docState('foo ') })).toEqual(undefined)
     })
 
     it('does not trim trailing whitespace on non-empty line', () => {
         const cache = new CompletionsCache()
         cache.add('id1', docState('foo'), [{ content: 'bar' }])
 
-        expect(cache.__stateForTestsOnly).toEqual<CompletionsCache['__stateForTestsOnly']>({
-            'javascript<|>foo<|>;': { logId: 'id1', completions: [{ content: 'bar' }] },
-            'javascript<|>foob<|>;': { logId: 'id1', completions: [{ content: 'ar' }] },
-            'javascript<|>fooba<|>;': { logId: 'id1', completions: [{ content: 'r' }] },
-        })
         expect(cache.get({ documentState: docState('foo') })).toEqual({
             logId: 'id1',
             completions: [{ content: 'bar' }],
@@ -97,6 +61,31 @@ describe('CompletionsCache', () => {
         expect(cache.get({ documentState: docState('foo \n') })).toEqual(undefined)
         expect(cache.get({ documentState: docState('foo\n') })).toEqual(undefined)
         expect(cache.get({ documentState: docState('foo\t') })).toEqual(undefined)
+    })
+
+    it('caches through deletions of prefix', () => {
+        const cache = new CompletionsCache()
+        cache.add('1', docState('a\nbc '), [{ content: 'd' }])
+
+        expect(cache.get({ documentState: docState('a\nbc ') })).toEqual({
+            logId: '1',
+            completions: [{ content: 'd' }],
+        })
+        expect(cache.get({ documentState: docState('a\nbc') })).toEqual({
+            logId: '1',
+            completions: [{ content: ' d' }],
+        })
+        expect(cache.get({ documentState: docState('a\nb') })).toEqual({
+            logId: '1',
+            completions: [{ content: 'c d' }],
+        })
+        expect(cache.get({ documentState: docState('a\n') })).toEqual({
+            logId: '1',
+            completions: [{ content: 'bc d' }],
+        })
+
+        // But stop serving from cache when the user deletes the newline.
+        expect(cache.get({ documentState: docState('a') })).toEqual(undefined)
     })
 
     it('has a lookup function for untrimmed prefixes', () => {
--- vscode/src/completions/cache/cache.ts
+++ vscode/src/completions/cache/cache.ts
@@ -1,7 +1,6 @@
 import { LRUCache } from 'lru-cache'
 
 import { Completion } from '..'
-import { trimEndOnLastLineIfWhitespaceOnly } from '../text-processing'
 
 export interface CachedCompletions {
     logId: string
@@ -13,9 +12,19 @@ export interface CachedCompletions {
  */
 export interface CompletionsCacheDocumentState {
     /**
+     * The document URI.
+     */
+    uri: string
+
+    /**
      * The prefix (up to the cursor) of the source file where the completion request was triggered.
      */
     prefix: string
+
+    /**
+     * The cursor position in the source file where the completion request was triggered.
+     */
+    position: number
 
     /**
      * The suffix (after the cursor) of the source file where the completion request was triggered.
@@ -41,92 +50,114 @@ export interface CacheRequest {
     isExactPrefixOnly?: boolean
 }
 
-const CACHE_KEY_DOCUMENT_CONTENT_PREFIX_SUFFIX_LENGTH = 200
-
 /*
- * Return the cache key used for a given document state.
- *
  * Only the first {@link CACHE_KEY_DOCUMENT_CONTENT_SUFFIX_LENGTH} characters of the prefix and
  * suffix are used to distinguish cache keys (because an edit that is sufficiently far away from the
  * cursor can be considered to not invalidate the relevant cache entries).
  **/
-function cacheKey({ prefix, suffix, languageId }: CompletionsCacheDocumentState): string {
-    return `${languageId}<|>${prefix.slice(-CACHE_KEY_DOCUMENT_CONTENT_PREFIX_SUFFIX_LENGTH)}<|>${suffix.slice(
-        0,
-        CACHE_KEY_DOCUMENT_CONTENT_PREFIX_SUFFIX_LENGTH
-    )}`
-}
+const CACHE_KEY_DOCUMENT_CONTENT_PREFIX_SUFFIX_LENGTH = 200
 
+/**
+ * A completions cache for multiple documents.
+ */
 export class CompletionsCache {
-    private cache = new LRUCache<string, CachedCompletions>({
-        max: 500, // Maximum input prefixes in the cache.
+    private documentCaches = new LRUCache<string, DocumentCompletionsCache>({
+        max: 5, // Only maintain caches for the 5 most recent documents.
     })
 
-    public clear(): void {
-        this.cache.clear()
+    private documentCache({ uri, languageId }: CompletionsCacheDocumentState): DocumentCompletionsCache {
+        const key = `${languageId}:${uri}`
+        let cache = this.documentCaches.get(key)
+        if (!cache) {
+            cache = new DocumentCompletionsCache()
+            this.documentCaches.set(key, cache)
+        }
+        return cache
     }
 
-    public get({
-        documentState: { prefix, suffix, languageId },
-        isExactPrefixOnly,
-    }: CacheRequest): CachedCompletions | undefined {
-        const trimmedPrefix = isExactPrefixOnly ? prefix : trimEndOnLastLineIfWhitespaceOnly(prefix)
-        const key = cacheKey({ prefix: trimmedPrefix, suffix, languageId })
-        return this.cache.get(key)
+    public get(request: CacheRequest): CachedCompletions | undefined {
+        return this.documentCache(request.documentState).get(request)
     }
 
     public add(logId: string, documentState: CompletionsCacheDocumentState, completions: Completion[]): void {
-        const trimmedPrefix = trimEndOnLastLineIfWhitespaceOnly(documentState.prefix)
+        return this.documentCache(documentState).add(logId, documentState, completions)
+    }
+}
 
-        for (const completion of completions) {
-            // Cache the exact prefix first and then append characters from the
-            // completion one after the other until the first line is exceeded.
-            //
-            // If the completion starts with a `\n`, this logic will append the
-            // second line instead.
-            let maxCharsAppended = completion.content.indexOf('\n', completion.content.at(0) === '\n' ? 1 : 0)
-            if (maxCharsAppended === -1) {
-                maxCharsAppended = completion.content.length - 1
+interface CacheEntry extends CachedCompletions {
+    documentState: CompletionsCacheDocumentState
+}
+
+/**
+ * A completions cache for a single document.
+ *
+ * TODO(sqs): maybe take advantage of the fact that the range can be arbitrary and not JUST an insertion?
+ */
+class DocumentCompletionsCache {
+    private seq = 0
+    private cache = new LRUCache<number, CacheEntry>({ max: 50 })
+
+    public get(req: CacheRequest): Pick<CachedCompletions, 'logId' | 'completions'> | undefined {
+        for (const e of this.cache.values() as Generator<CacheEntry>) {
+            const exactMatch =
+                req.documentState.position === e.documentState.position &&
+                req.documentState.prefix === e.documentState.prefix &&
+                req.documentState.suffix === e.documentState.suffix
+            if (exactMatch) {
+                return toResult(e)
+            }
+            if (req.isExactPrefixOnly) {
+                return undefined
             }
 
-            // We also cache the completion with the exact (= untrimmed) prefix for the separate
-            // lookup mode used for deletions.
-            const prefixHasTrailingWhitespaceOnLastLine = trimmedPrefix !== documentState.prefix
-            if (prefixHasTrailingWhitespaceOnLastLine) {
-                this.insertCompletion(cacheKey(documentState), logId, completion)
+            const offset = req.documentState.position - e.documentState.position
+            if (offset > 0) {
+                // Request is after the cached position.
+                const addedText = req.documentState.prefix.slice(-offset)
+
+                // Find completions that start with the addedText and would still be valid.
+                const validCompletions = e.completions.filter(c => c.content.startsWith(addedText))
+
+                // Trim the addedText from the valid completions because the addedText is already present in the document.
+                const trimmedCompletions = validCompletions.map(c => ({
+                    ...c,
+                    content: c.content.slice(addedText.length),
+                }))
+
+                if (trimmedCompletions.length > 0) {
+                    return { logId: e.logId, completions: trimmedCompletions }
+                }
+
+                // TODO(sqs): check suffix?
             }
+            if (offset < 0) {
+                // Request is before the cached position.
+                const deletedText = e.documentState.prefix.slice(offset)
 
-            for (let i = 0; i <= maxCharsAppended; i++) {
-                const completionPrefixToAppend = completion.content.slice(0, i)
-                const partialCompletionContent = completion.content.slice(i)
-                const appendedPrefix = trimmedPrefix + completionPrefixToAppend
-                const key = cacheKey({
-                    ...documentState,
-                    prefix: appendedPrefix,
-                })
-                this.insertCompletion(key, logId, { content: partialCompletionContent })
+                // Do not reuse cache entries across starting lines.
+                if (deletedText.includes('\n')) {
+                    continue
+                }
+
+                // Find completions that came after the deletedText, and prepend the deletedText to
+                // them.
+                const prependedCompletions = e.completions.map(c => ({ ...c, content: deletedText + c.content }))
+
+                if (prependedCompletions.length > 0) {
+                    return { logId: e.logId, completions: prependedCompletions }
+                }
             }
         }
+
+        return undefined // cache miss
     }
 
-    private insertCompletion(key: string, logId: string, completion: Completion): void {
-        let existingCompletions: Completion[] = []
-        if (this.cache.has(key)) {
-            existingCompletions = this.cache.get(key)!.completions
-        }
-
-        const cachedCompletion: CachedCompletions = {
-            logId,
-            completions: existingCompletions.concat(completion),
-        }
-
-        this.cache.set(key, cachedCompletion)
+    public add(logId: string, documentState: CompletionsCacheDocumentState, completions: Completion[]): void {
+        // TODO(sqs): merge identical completions?
+        this.cache.set(++this.seq, { logId, completions, documentState })
     }
+}
 
-    /**
-     * For use by tests only.
-     */
-    public get __stateForTestsOnly(): { [key: string]: CachedCompletions } {
-        return Object.fromEntries(this.cache.entries())
-    }
+function toResult(e: CacheEntry): CachedCompletions {
+    return { logId: e.logId, completions: e.completions }
 }
