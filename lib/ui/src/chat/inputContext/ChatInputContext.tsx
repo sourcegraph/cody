6198a1521f1bed35d6b5c2bda669faefae5fa73d
Tim Lucas <t@toolmantim.com>
2023-08-01 06:32:46 +1000

    Layout cleanups: smaller header and single line message input (#449)

--- lib/ui/src/chat/inputContext/ChatInputContext.tsx
+++ lib/ui/src/chat/inputContext/ChatInputContext.tsx
@@ -44,20 +44,24 @@ export const ChatInputContext: React.FunctionComponent<{
             ) : contextStatus.mode && contextStatus.connection ? (
                 <CodebaseState
                     codebase={contextStatus.codebase}
-                    tooltip="Repository is indexed and has embeddings"
+                    tooltip={`Repository ${contextStatus.codebase} is indexed and has embeddings`}
                     icon={mdiDatabaseCheckOutline}
                 />
             ) : (
                 <CodebaseState
                     codebase={contextStatus.codebase}
-                    tooltip="Repository is not indexed and has no embeddings"
+                    tooltip={`Repository ${contextStatus.codebase} is not indexed and has no embeddings`}
                     icon={mdiDatabaseRemoveOutline}
                     iconClassName={styles.warningColor}
                 />
             )}
-            {contextStatus.filePath && (
+            {(contextStatus.filePath && (
                 <p className={styles.file} title={contextStatus.filePath}>
                     {formatFilePath(contextStatus.filePath, contextStatus.selectionRange)}
+                </p>
+            )) || (
+                <p className={styles.file} title={contextStatus.filePath}>
+                    No file selected
                 </p>
             )}
         </div>
@@ -72,8 +76,5 @@ const CodebaseState: React.FunctionComponent<{
 }> = ({ tooltip, iconClassName, icon, codebase }) => (
     <h3 title={tooltip} className={styles.codebase}>
         <Icon svgPath={icon} className={classNames(styles.codebaseIcon, iconClassName)} />
-        {codebase && (
-            <span className={styles.codebaseLabel}>{basename(codebase.replace(/^(github|gitlab)\.com\//, ''))}</span>
-        )}
     </h3>
 )
