286e88a3e3d3fa838d4c3d13bf71de38ca7222bf
Philipp Spiess <hello@philippspiess.com>
2023-08-01 16:26:37 +0200

    Also add vue since it's basically the same as JSX?

--- vscode/src/completions/language.ts
+++ vscode/src/completions/language.ts
@@ -16,6 +16,7 @@ export function getLanguageConfig(languageId: string): LanguageConfig | null {
         case 'javascriptreact':
         case 'typescript':
         case 'typescriptreact':
+        case 'vue':
         case 'php':
             return {
                 blockStart: '{',
