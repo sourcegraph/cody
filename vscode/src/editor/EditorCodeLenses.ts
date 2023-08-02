49535572abd1e1f249ec4d0912fae360867aa549
Beatrix <beatrix@sourcegraph.com>
2023-08-02 15:30:58 -0700

    remove Variable

--- vscode/src/editor/EditorCodeLenses.ts
+++ vscode/src/editor/EditorCodeLenses.ts
@@ -102,8 +102,7 @@ export class EditorCodeLenses implements vscode.CodeLensProvider {
                 symbol.kind === vscode.SymbolKind.Function ||
                 symbol.kind === vscode.SymbolKind.Class ||
                 symbol.kind === vscode.SymbolKind.Method ||
-                symbol.kind === vscode.SymbolKind.Constructor ||
-                symbol.kind === vscode.SymbolKind.Variable
+                symbol.kind === vscode.SymbolKind.Constructor
         )
 
         // Add code lenses for each symbol
