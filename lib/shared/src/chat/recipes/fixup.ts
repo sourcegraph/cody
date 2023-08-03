a10fcb43d95da4eec66c337637e495db97650925
Tom Ross <tom@umpox.com>
2023-08-03 13:42:12 +0100

    Use quick pick for instruction

--- lib/shared/src/chat/recipes/fixup.ts
+++ lib/shared/src/chat/recipes/fixup.ts
@@ -9,7 +9,7 @@ import { Interaction } from '../transcript/interaction'
 import { getContextMessagesFromSelection } from './helpers'
 import { Recipe, RecipeContext, RecipeID } from './recipe'
 
-export type FixupIntent = 'add' | 'edit' | 'delete' | 'fix' | 'test' | 'document'
+export type FixupIntent = 'add' | 'edit' | 'remove' | 'fix' | 'test' | 'document'
 const FixupIntentClassification: IntentClassificationOption<FixupIntent>[] = [
     {
         id: 'edit',
@@ -17,8 +17,8 @@ const FixupIntentClassification: IntentClassificationOption<FixupIntent>[] = [
         examplePrompts: ['Edit this code', 'Change this code', 'Update this code'],
     },
     {
-        id: 'delete',
-        description: 'Delete a part of the selection code',
+        id: 'remove',
+        description: 'Remove a part of the selected code',
         examplePrompts: ['Delete these comments', 'Remove log statements'],
     },
     {
@@ -35,7 +35,7 @@ const FixupIntentClassification: IntentClassificationOption<FixupIntent>[] = [
 
 const PromptIntentInstruction: Record<Exclude<FixupIntent, 'add'>, string> = {
     edit: 'The user wants you to replace parts of the selected code by following their instructions.',
-    delete: 'The user wants you to remove parts of the selected code by following their instructions.',
+    remove: 'The user wants you to remove parts of the selected code by following their instructions.',
     fix: 'The user wants you to correct a problem in the selected code by following their instructions.',
     document:
         'The user wants you to add documentation or comments to the selected code by following their instructions.',
--- vscode/src/non-stop/FixupTypingUI.ts
+++ vscode/src/non-stop/FixupTypingUI.ts
@@ -3,11 +3,68 @@ import * as vscode from 'vscode'
 import { FixupTask } from './FixupTask'
 import { FixupTaskFactory } from './roles'
 
+const FixupCommands = new Map([
+    ['/fix', 'Fix a problem in the selected code'],
+    ['/add', 'Suggest new code'],
+    ['/edit', 'Edit the selected code'],
+    ['/document', 'Generate documentation or comments for the selected code'],
+    ['/remove', 'Remove parts of the selected code'],
+    ['/test', 'Generate a test for the selected code'],
+])
+const FixupQuickPickItems = [...FixupCommands].map(([command, description]) => ({ label: command, description }))
+
 /**
  * The UI for creating non-stop fixup tasks by typing instructions.
  */
 export class FixupTypingUI {
     constructor(private readonly taskFactory: FixupTaskFactory) {}
+
+    private async getInstructionFromQuickPick({
+        title = 'Cody',
+        placeholder = "Ask Cody to do something, or type ' / ' for commands",
+        fallback = '',
+    } = {}): Promise<string> {
+        const quickPick = vscode.window.createQuickPick()
+        quickPick.title = title
+        quickPick.placeholder = placeholder
+        quickPick.buttons = [{ tooltip: 'Cody', iconPath: new vscode.ThemeIcon('cody-logo-heavy') }]
+        quickPick.ignoreFocusOut = true
+        quickPick.onDidTriggerButton(() => {
+            console.log('triggeredeed')
+            void vscode.commands.executeCommand('cody.focus')
+            quickPick.hide()
+        })
+
+        quickPick.onDidChangeValue(value => {
+            if (value.startsWith('/')) {
+                quickPick.items = FixupQuickPickItems
+            } else {
+                // We show no items by default
+                quickPick.items = []
+            }
+        })
+
+        quickPick.show()
+
+        return new Promise(resolve =>
+            quickPick.onDidAccept(() => {
+                const selectedItem = quickPick.selectedItems[0]?.label
+                const command = FixupCommands.get(selectedItem)
+                if (command) {
+                    return resolve(
+                        this.getInstructionFromQuickPick({
+                            title: `Cody - ${selectedItem}`,
+                            placeholder: command,
+                            fallback: `${selectedItem} ${command}`,
+                        })
+                    )
+                }
+
+                quickPick.hide()
+                return resolve(quickPick.value.trim() || fallback)
+            })
+        )
+    }
 
     public async show(): Promise<FixupTask | null> {
         const editor = vscode.window.activeTextEditor
@@ -22,16 +79,11 @@ export class FixupTypingUI {
             return null
         }
 
-        const CHAT_COMMAND = '/chat'
-        const CHAT_RE = /^\/chat(|\s.*)$/
-        const instruction = (
-            await vscode.window.showInputBox({
-                title: `Ask Cody to edit your code, or use ${CHAT_COMMAND} to ask a question`,
-            })
-        )?.trim()
+        const instruction = (await this.getInstructionFromQuickPick())?.trim()
         if (!instruction) {
             return null
         }
+        const CHAT_RE = /^\/chat(|\s.*)$/
         const match = instruction.match(CHAT_RE)
         if (match?.[1]) {
             // If we got here, we have a selection; start chat with match[1].
