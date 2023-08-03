58204044a0b3aa32cc1897845ec4d27187867356
Quinn Slack <quinn@slack.org>
2023-08-02 22:47:59 -0700

    disable commands code lens and editor title icon by default (#550)
    I personally found these distracting to have on by default (although I
    am super happy to see the commands stuff in!!), and I bet users would
    agree (especially users who haven't started using the features yet). I
    am sure they help with feature discovery, but let's run with them
    disabled-by-default for a bit until we can find a better way for
    discovery and/or until this feature is really lovable.

--- vscode/src/configuration.ts
+++ vscode/src/configuration.ts
@@ -68,7 +68,7 @@ export function getConfiguration(config: ConfigGetter): Configuration {
         inlineChat: config.get(CONFIG_KEY.inlineChatEnabled, true),
         experimentalGuardrails: config.get(CONFIG_KEY.experimentalGuardrails, isTesting),
         experimentalNonStop: config.get('cody.experimental.nonStop' as any, isTesting),
-        experimentalCommandLenses: config.get(CONFIG_KEY.experimentalCommandLenses, true),
+        experimentalCommandLenses: config.get(CONFIG_KEY.experimentalCommandLenses, false),
         autocompleteAdvancedProvider,
         autocompleteAdvancedServerEndpoint: config.get<string | null>(
             CONFIG_KEY.autocompleteAdvancedServerEndpoint,
