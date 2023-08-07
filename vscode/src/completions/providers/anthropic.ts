1ddf7abc18528ebb1cebe1ed5a826953567b46c0
Philipp Spiess <hello@philippspiess.com>
2023-08-07 18:11:09 +0200

    Autocomplete: Improve Python and Ruby quality (#597)
    While looking into
    https://github.com/sourcegraph/cody/discussions/358#discussioncomment-6658645
    I noticed some really bad completions. The completions was regularly
    containing invalid code.
    
    After fixing this line in the prompt, the quality seems to have improved
    a bit 😬
    
    ## Test plan
    
    <img width="1329" alt="Screenshot 2023-08-07 at 18 01 38"
    src="https://github.com/sourcegraph/cody/assets/458591/d17c624e-ebaa-4b91-ab23-df30fb6d8bad">
    <img width="1265" alt="Screenshot 2023-08-07 at 18 01 25"
    src="https://github.com/sourcegraph/cody/assets/458591/c04fab3e-9b4f-41bf-b443-b03ff8cfb343">
    
    <!-- Required. See
    https://docs.sourcegraph.com/dev/background-information/testing_principles.
    -->

--- vscode/src/completions/providers/anthropic.ts
+++ vscode/src/completions/providers/anthropic.ts
@@ -61,7 +61,13 @@ export class AnthropicProvider extends Provider {
         const prefixMessages: Message[] = [
             {
                 speaker: 'human',
-                text: `You are a code completion AI that writes high-quality code like a senior engineer. You are looking at ${this.options.fileName}. You write code in between tags like this: ${OPENING_CODE_TAG}/* Code goes here */${CLOSING_CODE_TAG}.`,
+                text: `You are a code completion AI that writes high-quality code like a senior engineer. You are looking at ${
+                    this.options.fileName
+                }. You write code in between tags like this: ${OPENING_CODE_TAG}${
+                    this.options.languageId === 'python' || this.options.languageId === 'ruby'
+                        ? '# Code goes here'
+                        : '/* Code goes here */'
+                }${CLOSING_CODE_TAG}.`,
             },
             {
                 speaker: 'assistant',
