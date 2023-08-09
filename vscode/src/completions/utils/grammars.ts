d27a9ea0fff6b8872f4f6c119f15d0f9d63688c8
Vova Kulikov <vova@sourcegraph.com>
2023-08-09 12:21:28 -0300

    Inline grammar lexem values

--- vscode/src/completions/utils/grammars.ts
+++ vscode/src/completions/utils/grammars.ts
@@ -37,84 +37,6 @@ export enum GenericLexem {
     MethodCall,
 }
 
-enum StandardLexem {
-    IfStatement = 'if_statement',
-    ElseClause = 'else_clause',
-    StatementBlock = 'statement_block',
-    CallExpression = 'call_expression',
-    Comment = 'comment',
-}
-
-enum JavaLexemType {
-    IfStatement = 'if_statement',
-    ElseClause = 'else',
-    StatementBlock = 'block',
-    MethodInvocation = 'method_invocation',
-    Comment = 'comment',
-}
-
-enum GoLexemType {
-    IfStatement = 'if_statement',
-    ElseClause = 'else',
-    StatementBlock = 'block',
-    CallExpression = 'call_expression',
-    MethodCall = 'call_expression',
-    Comment = 'comment',
-}
-
-enum PythonLexemType {
-    IfStatement = 'if_statement',
-    ElseClause = 'else',
-    StatementBlock = 'block',
-    CallExpression = 'call',
-    MethodCall = 'call',
-    Comment = 'comment',
-}
-
-enum DartLexemType {
-    Comment = 'comment',
-    IfStatement = 'if_statement',
-    ElseClause = 'else',
-    StatementBlock = 'block',
-    CallExpression = 'expression_statement',
-    MethodCall = 'call_expression',
-}
-
-enum CLexemType {
-    IfStatement = 'if_statement',
-    ElseClause = 'else_clause',
-    StatementBlock = 'compound_statement',
-    CallExpression = 'call_expression',
-    Comment = 'comment',
-}
-
-enum CppLexemType {
-    Comment = 'comment',
-    IfStatement = 'if_statement',
-    ElseClause = 'else_clause',
-    StatementBlock = 'compound_statement',
-    CallExpression = 'call_expression',
-    MethodCall = 'call_expression',
-}
-
-enum CSharpLexemType {
-    Comment = 'comment',
-    IfStatement = 'if_statement',
-    ElseClause = 'else',
-    StatementBlock = 'block',
-    CallExpression = 'invocation_expression',
-    MethodCall = 'invocation_expression',
-}
-
-enum PhpLexemType {
-    Comment = 'comment',
-    IfStatement = 'if_statement',
-    ElseClause = 'else_clause',
-    StatementBlock = 'compound_statement',
-    CallExpression = 'function_call_expression',
-    MethodCall = 'member_call_expression',
-}
-
 export type LEXEME_DICTIONARY = Record<GenericLexem, string | null>
 
 export function getLanguageLexems(language: SupportedLanguage): LEXEME_DICTIONARY | null {
@@ -124,102 +46,102 @@ export function getLanguageLexems(language: SupportedLanguage): LEXEME_DICTIONAR
         case SupportedLanguage.TSX:
         case SupportedLanguage.TypeScript:
             return {
-                [GenericLexem.IfStatement]: StandardLexem.IfStatement,
-                [GenericLexem.ElseClause]: StandardLexem.ElseClause,
-                [GenericLexem.StatementBlock]: StandardLexem.StatementBlock,
-                [GenericLexem.CallExpression]: StandardLexem.CallExpression,
-                [GenericLexem.Comment]: StandardLexem.Comment,
-                [GenericLexem.MethodCall]: StandardLexem.CallExpression,
+                [GenericLexem.IfStatement]: 'if_statement',
+                [GenericLexem.ElseClause]: 'else_clause',
+                [GenericLexem.StatementBlock]: 'statement_block',
+                [GenericLexem.CallExpression]: 'call_expression',
+                [GenericLexem.MethodCall]: 'call_expression',
+                [GenericLexem.Comment]: 'comment',
             }
 
         case SupportedLanguage.Java:
             return {
-                [GenericLexem.IfStatement]: JavaLexemType.IfStatement,
-                [GenericLexem.ElseClause]: JavaLexemType.ElseClause,
-                [GenericLexem.StatementBlock]: JavaLexemType.StatementBlock,
-                [GenericLexem.CallExpression]: JavaLexemType.MethodInvocation,
-                [GenericLexem.Comment]: StandardLexem.Comment,
-                [GenericLexem.MethodCall]: JavaLexemType.MethodInvocation,
+                [GenericLexem.IfStatement]: 'if_statement',
+                [GenericLexem.ElseClause]: 'else',
+                [GenericLexem.StatementBlock]: 'block',
+                [GenericLexem.CallExpression]: 'method_invocation',
+                [GenericLexem.Comment]: 'comment',
+                [GenericLexem.MethodCall]: 'method_invocation',
             }
 
         case SupportedLanguage.Go:
             return {
-                [GenericLexem.IfStatement]: GoLexemType.IfStatement,
-                [GenericLexem.ElseClause]: GoLexemType.ElseClause,
-                [GenericLexem.StatementBlock]: GoLexemType.StatementBlock,
-                [GenericLexem.CallExpression]: GoLexemType.CallExpression,
-                [GenericLexem.Comment]: StandardLexem.Comment,
-                [GenericLexem.MethodCall]: GoLexemType.MethodCall,
+                [GenericLexem.IfStatement]: 'if_statement',
+                [GenericLexem.ElseClause]: 'else',
+                [GenericLexem.StatementBlock]: 'block',
+                [GenericLexem.CallExpression]: 'call_expression',
+                [GenericLexem.Comment]: 'comment',
+                [GenericLexem.MethodCall]: 'call_expression',
             }
 
         case SupportedLanguage.Python:
             return {
-                [GenericLexem.IfStatement]: PythonLexemType.IfStatement,
-                [GenericLexem.ElseClause]: PythonLexemType.ElseClause,
-                [GenericLexem.StatementBlock]: PythonLexemType.StatementBlock,
-                [GenericLexem.CallExpression]: PythonLexemType.CallExpression,
-                [GenericLexem.Comment]: StandardLexem.Comment,
-                [GenericLexem.MethodCall]: PythonLexemType.MethodCall,
+                [GenericLexem.IfStatement]: 'if_statement',
+                [GenericLexem.ElseClause]: 'else',
+                [GenericLexem.StatementBlock]: 'block',
+                [GenericLexem.CallExpression]: 'call',
+                [GenericLexem.Comment]: 'comment',
+                [GenericLexem.MethodCall]: 'call',
             }
 
         case SupportedLanguage.Dart:
             return {
-                [GenericLexem.IfStatement]: DartLexemType.IfStatement,
-                [GenericLexem.ElseClause]: DartLexemType.ElseClause,
-                [GenericLexem.StatementBlock]: DartLexemType.StatementBlock,
-                [GenericLexem.CallExpression]: DartLexemType.CallExpression,
-                [GenericLexem.Comment]: StandardLexem.Comment,
-                [GenericLexem.MethodCall]: DartLexemType.MethodCall,
+                [GenericLexem.IfStatement]: 'if_statement',
+                [GenericLexem.ElseClause]: 'else',
+                [GenericLexem.StatementBlock]: 'block',
+                [GenericLexem.CallExpression]: 'expression_statement',
+                [GenericLexem.Comment]: 'comment',
+                [GenericLexem.MethodCall]: 'call_expression',
             }
 
         case SupportedLanguage.C:
             return {
-                [GenericLexem.IfStatement]: CLexemType.IfStatement,
-                [GenericLexem.ElseClause]: CLexemType.ElseClause,
-                [GenericLexem.StatementBlock]: CLexemType.StatementBlock,
-                [GenericLexem.CallExpression]: CLexemType.CallExpression,
-                [GenericLexem.Comment]: StandardLexem.Comment,
+                [GenericLexem.IfStatement]: 'if_statement',
+                [GenericLexem.ElseClause]: 'else_clause',
+                [GenericLexem.StatementBlock]: 'compound_statement',
+                [GenericLexem.CallExpression]: 'call_expression',
+                [GenericLexem.Comment]: 'comment',
                 // C doesn't support class or methods
                 [GenericLexem.MethodCall]: null,
             }
 
         case SupportedLanguage.Cpp:
             return {
-                [GenericLexem.IfStatement]: CppLexemType.IfStatement,
-                [GenericLexem.ElseClause]: CppLexemType.ElseClause,
-                [GenericLexem.StatementBlock]: CppLexemType.StatementBlock,
-                [GenericLexem.CallExpression]: CppLexemType.CallExpression,
-                [GenericLexem.Comment]: StandardLexem.Comment,
-                [GenericLexem.MethodCall]: CppLexemType.MethodCall,
+                [GenericLexem.IfStatement]: 'if_statement',
+                [GenericLexem.ElseClause]: 'else_clause',
+                [GenericLexem.StatementBlock]: 'compound_statement',
+                [GenericLexem.CallExpression]: 'call_expression',
+                [GenericLexem.Comment]: 'comment',
+                [GenericLexem.MethodCall]: 'call_expression',
             }
 
         case SupportedLanguage.CSharp:
             return {
-                [GenericLexem.IfStatement]: CSharpLexemType.IfStatement,
-                [GenericLexem.ElseClause]: CSharpLexemType.ElseClause,
-                [GenericLexem.StatementBlock]: CSharpLexemType.StatementBlock,
-                [GenericLexem.CallExpression]: CSharpLexemType.CallExpression,
-                [GenericLexem.Comment]: StandardLexem.Comment,
-                [GenericLexem.MethodCall]: CSharpLexemType.MethodCall,
+                [GenericLexem.IfStatement]: 'if_statement',
+                [GenericLexem.ElseClause]: 'else',
+                [GenericLexem.StatementBlock]: 'block',
+                [GenericLexem.CallExpression]: 'invocation_expression',
+                [GenericLexem.Comment]: 'comment',
+                [GenericLexem.MethodCall]: 'invocation_expression',
             }
 
         case SupportedLanguage.Php:
             return {
-                [GenericLexem.IfStatement]: PhpLexemType.IfStatement,
-                [GenericLexem.ElseClause]: PhpLexemType.ElseClause,
-                [GenericLexem.StatementBlock]: PhpLexemType.StatementBlock,
-                [GenericLexem.CallExpression]: PhpLexemType.CallExpression,
-                [GenericLexem.Comment]: StandardLexem.Comment,
-                [GenericLexem.MethodCall]: PhpLexemType.MethodCall,
+                [GenericLexem.IfStatement]: 'if_statement',
+                [GenericLexem.ElseClause]: 'else_clause',
+                [GenericLexem.StatementBlock]: 'compound_statement',
+                [GenericLexem.CallExpression]: 'function_call_expression',
+                [GenericLexem.Comment]: 'comment',
+                [GenericLexem.MethodCall]: 'member_call_expression',
             }
 
         default:
             return {
-                [GenericLexem.IfStatement]: StandardLexem.IfStatement,
-                [GenericLexem.ElseClause]: StandardLexem.ElseClause,
-                [GenericLexem.StatementBlock]: StandardLexem.StatementBlock,
-                [GenericLexem.CallExpression]: StandardLexem.CallExpression,
-                [GenericLexem.Comment]: StandardLexem.Comment,
+                [GenericLexem.IfStatement]: 'if_statement',
+                [GenericLexem.ElseClause]: 'else_clause',
+                [GenericLexem.StatementBlock]: 'statement_block',
+                [GenericLexem.CallExpression]: 'call_expression',
+                [GenericLexem.Comment]: 'comment',
                 [GenericLexem.MethodCall]: null,
             }
     }
