4538a7ea3a2907560f468f70f0b30ce0cc8c533d
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-08-11 18:52:55 +0200

    Fix build failures

--- agent/src/AgentTabGroups.ts
+++ agent/src/AgentTabGroups.ts
@@ -1,10 +1,12 @@
 import type * as vscode from 'vscode'
 
+import { emptyEvent } from '../../vscode/src/testutils/mocks'
+
 export class AgentTabGroups implements vscode.TabGroups {
     all: vscode.TabGroup[] = []
-    activeTabGroup: vscode.TabGroup
-    public onDidChangeTabGroups: vscode.Event<vscode.TabGroupChangeEvent>
-    public onDidChangeTabs: vscode.Event<vscode.TabChangeEvent>
+    activeTabGroup: vscode.TabGroup = { activeTab: undefined, isActive: true, tabs: [], viewColumn: 1 }
+    public onDidChangeTabGroups: vscode.Event<vscode.TabGroupChangeEvent> = emptyEvent()
+    public onDidChangeTabs: vscode.Event<vscode.TabChangeEvent> = emptyEvent()
     public close(): Thenable<boolean> {
         throw new Error('Method not implemented.')
     }
