7e81e547d076b2c3e231098ab7289f461dd150e3
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-08-11 16:53:10 +0200

    Remove unnecessary esbuild-options
    It's not necessary because vitest supports `resolve.alias`.

--- agent/src/esbuild.mjs
+++ agent/src/esbuild.mjs
@@ -1,7 +1,23 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import { build } from 'esbuild';
-import { esbuildOptions } from './esbuild-options.mjs';
+import path from 'path'
+import process from 'process'
 
-(async () => {
-  const res = await build(esbuildOptions);
-})();
+import { build } from 'esbuild'
+import { aliasPath } from 'esbuild-plugin-alias-path'
+
+;(async () => {
+    /** @type {import('esbuild').BuildOptions} */
+    const esbuildOptions = {
+        entryPoints: ['./src/index.ts'],
+        bundle: true,
+        outfile: './dist/index.js',
+        platform: 'node',
+        format: 'cjs',
+        plugins: [
+            aliasPath({
+                alias: { vscode: path.resolve(process.cwd(), './src/vscode-shim.ts') },
+            }),
+        ],
+    }
+    const res = await build(esbuildOptions)
+})()
--- agent/vitest.config.ts
+++ agent/vitest.config.ts
@@ -4,12 +4,9 @@ import path from 'path'
 
 import { defineConfig } from 'vite'
 
-import { esbuildOptions } from './src/esbuild-options'
-
 export default defineConfig({
     logLevel: 'warn',
     test: {},
-    optimizeDeps: { esbuildOptions },
     resolve: {
         alias: { vscode: path.resolve(process.cwd(), 'src', 'vscode-shim') },
     },
