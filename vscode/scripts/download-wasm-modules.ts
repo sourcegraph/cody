7fd7767add2be63832309d0a7f3d052901cbb436
Vova Kulikov <vova@sourcegraph.com>
2023-08-13 22:55:03 -0300

    Cody completion: Add initial tree-sitter utils (#538)
    This is an early draft of tree-sitter adoption. This PR doesn't change
    anything about the current logic but tries to add some initial parsers
    API for our post-text procession flow.
    
    ## Todo 
    - [x] Add initial logic 
    - [x] Add custom wasm loader script
    
    Note that we have to use wasm modules for tree sitter logic, and this
    adds some complexity, in case if you
    have a better idea of how we can bring parsers into VSCode runtime, let
    me know, please 😉
    
    ## Test plan
    - CI is green
    <!-- Required. See
    https://docs.sourcegraph.com/dev/background-information/testing_principles.
    -->

---
+++ vscode/scripts/download-wasm-modules.ts
@@ -0,0 +1,118 @@
+import fs, { copyFileSync, existsSync, mkdirSync, readdirSync, WriteStream } from 'fs'
+import http from 'https'
+import path from 'path'
+
+import ProgressBar from 'progress'
+
+const DIST_DIRECTORY = path.join(path.resolve(__dirname), '..', 'dist')
+const WASM_DIRECTORY = path.join(path.resolve(__dirname), '..', 'resources', 'wasm')
+
+const urls = [
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-javascript.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-typescript.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-tsx.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-c_sharp.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-cpp.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-go.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-python.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-ruby.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-rust.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-java.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-dart.wasm',
+    'https://storage.googleapis.com/sourcegraph-assets/cody-wasm/tree-sitter-php.wasm',
+]
+
+export async function main(): Promise<void> {
+    const hasStoreDir = existsSync(WASM_DIRECTORY)
+
+    if (!hasStoreDir) {
+        mkdirSync(WASM_DIRECTORY)
+    }
+
+    const filesToDownload = getMissingFiles(urls)
+
+    if (filesToDownload.length === 0) {
+        copyFilesToDistDir()
+        console.log('All wasm modules are in place, have a good day!')
+        return
+    }
+
+    console.log(`We are missing ${filesToDownload.length} files.`)
+
+    try {
+        await Promise.all(filesToDownload.map(downloadFile))
+        copyFilesToDistDir()
+        console.log('All files were successful downloaded, check resources/wasm directory')
+    } catch (error) {
+        console.error('Some error occurred', error)
+        process.exit(1)
+    }
+}
+
+void main()
+
+function copyFilesToDistDir(): void {
+    const hasDistDir = existsSync(DIST_DIRECTORY)
+
+    if (!hasDistDir) {
+        mkdirSync(DIST_DIRECTORY)
+    }
+
+    const files = readdirSync(WASM_DIRECTORY)
+
+    for (const file of files) {
+        copyFileSync(path.join(WASM_DIRECTORY, file), path.join(DIST_DIRECTORY, file))
+    }
+}
+
+function getMissingFiles(urls: string[]): string[] {
+    const missingFiles = []
+
+    for (const url of urls) {
+        const filePath = getFilePathFromURL(url)
+        if (!existsSync(path.resolve(WASM_DIRECTORY, filePath))) {
+            missingFiles.push(url)
+        }
+    }
+
+    return missingFiles
+}
+
+function getFilePathFromURL(url: string): string {
+    const parts = url.split('/')
+    return parts[parts.length - 1]
+}
+
+function downloadFile(url: string): Promise<WriteStream> {
+    const fileName = getFilePathFromURL(url)
+
+    const downloadDirectory = path.resolve(__dirname, '..', 'resources', 'wasm')
+    const file = fs.createWriteStream(path.join(downloadDirectory, fileName))
+
+    return new Promise((resolve, reject) => {
+        http.get(url).on('response', res => {
+            const contentLength = res.headers?.['content-length'] ?? '0'
+            const totalLength = parseInt(contentLength, 10)
+
+            const progress = new ProgressBar(`-> ${fileName} [:bar] :rate/bps :percent :etas`, {
+                width: 40,
+                complete: '=',
+                incomplete: ' ',
+                renderThrottle: 1,
+                total: totalLength,
+            })
+
+            res.on('data', (chunk): void => {
+                file.write(chunk)
+                progress.tick(chunk.length)
+            })
+                .on('end', () => {
+                    resolve(file.end())
+                })
+                .on('error', err => {
+                    console.log('\n')
+                    reject(err)
+                })
+        })
+    })
+}
