7f3e3780ecd0d0f52ad5bee1dd9bb035fa16b40f
Vova Kulikov <vova@sourcegraph.com>
2023-08-17 23:23:19 -0300

    Don't trigger multiline completion in case of function or method invocation (#726)
    Fixes
    https://github.com/sourcegraph/cody/discussions/358#discussioncomment-6519606
    
    As it is described in GH discussion, we shouldn't make multiline
    completion for cases like function or method
    invocation (but we should do this in case of method or function
    declaration).
    
    So this PR gates multiline completion behind this regexp check (regexp
    isn't ideal, but this is what we have to
    work with until we get parsers working) You can check this regexp
    [here](https://regex101.com/r/k85odc/1)
    
    ## Test plan
    - Check that completion doesn't try to do multiline complete if your
    cursor is within arguments
    position of function or method invocation/call statement 
    
    <!-- Required. See
    https://docs.sourcegraph.com/dev/background-information/testing_principles.
    -->

--- vscode/src/completions/getInlineCompletions.test.ts
+++ vscode/src/completions/getInlineCompletions.test.ts
@@ -583,6 +583,42 @@ describe('getInlineCompletions', () => {
             expect(requests[0].stopSequences).not.toContain('\n')
         })
 
+        test('does not trigger a multi-line completion at a function call', async () => {
+            const requests: CompletionParameters[] = []
+            await getInlineCompletions(
+                params('bar(█)', [], {
+                    onNetworkRequest(request) {
+                        requests.push(request)
+                    },
+                })
+            )
+            expect(requests).toHaveLength(1)
+        })
+
+        test('does not trigger a multi-line completion at a method call', async () => {
+            const requests: CompletionParameters[] = []
+            await getInlineCompletions(
+                params('foo.bar(█)', [], {
+                    onNetworkRequest(request) {
+                        requests.push(request)
+                    },
+                })
+            )
+            expect(requests).toHaveLength(1)
+        })
+
+        test('trigger a multi-line completion at a method declarations', async () => {
+            const requests: CompletionParameters[] = []
+            await getInlineCompletions(
+                params('method.hello () { █', [], {
+                    onNetworkRequest(request) {
+                        requests.push(request)
+                    },
+                })
+            )
+            expect(requests).toHaveLength(1)
+        })
+
         test('uses an indentation based approach to cut-off completions', async () => {
             const items = await getInlineCompletionsInsertText(
                 params(
