ef990f22ef494bf2e3920fb0a04f2c93476b44f0
Alex Bildner <alex.bildner@sourcegraph.com>
2023-08-18 14:09:00 -0400

    removed inline comments

--- e2e/src/test-cases/numpy.ts
+++ e2e/src/test-cases/numpy.ts
@@ -1,32 +1,24 @@
 import { addTestCase } from '.'
 
 addTestCase('Vector dot product', {
-    // Codebase to test against.
     codebase: 'github.com/numpy/numpy',
-    // Context type.
     context: 'embeddings',
     transcript: [
         {
             question: 'How do I calculate the dot product of two vectors in NumPy?',
-            // Facts we want to see in the response.
             facts: ['Dot product'],
-            // Summary of the desired response. LLM judge uses this to evaluate correctness.
             answerSummary: 'Explanation of how to calculate dot product of two vectors in NumPy',
         },
     ],
 })
 
 addTestCase('Softmax', {
-    // Codebase to test against.
     codebase: 'github.com/numpy/numpy',
-    // Context type.
     context: 'embeddings',
     transcript: [
         {
             question: 'How do I calculate the softmax of a vector in NumPy?',
-            // Facts we want to see in the response.
             facts: ['Softmax'],
-            // Summary of the desired response. LLM judge uses this to evaluate correctness.
             answerSummary: 'Sample code of how to calculate softmax of a vector in NumPy',
         },
         {
@@ -40,16 +32,12 @@ addTestCase('Softmax', {
 })
 
 addTestCase('Ufunc explanation', {
-    // Codebase to test against.
     codebase: 'github.com/numpy/numpy',
-    // Context type.
     context: 'embeddings',
     transcript: [
         {
             question: 'Please provide a detailed explanation of ufuncs in NumPy',
-            // Facts we want to see in the response.
             facts: ['Ufunc'],
-            // Summary of the desired response. LLM judge uses this to evaluate correctness.
             answerSummary:
                 'An explanatin of NumPy ufucs including what they are, common examples, and how they work under the hood',
         },
--- e2e/src/test-cases/pytorch.ts
+++ e2e/src/test-cases/pytorch.ts
@@ -1,16 +1,12 @@
 import { addTestCase } from '.'
 
 addTestCase('PyTorch Model Training', {
-    // Codebase to test against.
     codebase: 'github.com/pytorch/pytorch',
-    // Context type.
     context: 'embeddings',
     transcript: [
         {
             question: 'How do I train a model in PyTorch?',
-            // Facts we want to see in the response.
             facts: ['Model training, backpropagation, loss functions, optimizers'],
-            // Summary of the desired response. LLM judge uses this to evaluate correctness.
             answerSummary: 'Explanation of how to train a model in PyTorch',
         },
         {
@@ -22,16 +18,12 @@ addTestCase('PyTorch Model Training', {
 })
 
 addTestCase('PyTorch Learning Rate Scheduling', {
-    // Codebase to test against.
     codebase: 'github.com/pytorch/pytorch',
-    // Context type.
     context: 'embeddings',
     transcript: [
         {
             question: 'What learning rate schedulers are available in PyTorch?',
-            // Facts we want to see in the response.
             facts: ['learning rate schedulers', 'StepLR', 'MultiStepLR', 'ExponentialLR', 'ReduceLROnPlateau'],
-            // Summary of the desired response. LLM judge uses this to evaluate correctness.
             answerSummary: 'List of learning rate schedulers available in PyTorch',
         },
     ],
