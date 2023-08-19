7f447b00d449ccf7dc9977dbc1cda9c81865a527
Tim Lucas <t@toolmantim.com>
2023-08-19 23:58:01 +1000

    Comment GH release link

--- vscode/webviews/Notices/UpdateNotice.tsx
+++ vscode/webviews/Notices/UpdateNotice.tsx
@@ -11,6 +11,9 @@ export const UpdateNotice: React.FunctionComponent<UpdateNoticeProps> = ({ versi
     <Notice
         icon={<Icon />}
         title={`Cody updated to ${version}!`}
+        /* At the top of each GitHub release notes we include a link to the
+           release's blog post for that point release (e.g. 0.8.x). So even
+           if they update from 0.7.1 -> 0.8.3 they'll have a blog post link handy */
         linkHref={`https://github.com/sourcegraph/cody/releases/tag/vscode-v${version}`}
         linkText="See what’s new →"
         linkTarget="_blank"
