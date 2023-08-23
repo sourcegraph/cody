815dff7a5536b6451bc58e3fb306857ff61aac79
Eric Fritz <eric@eric-fritz.com>
2023-08-23 11:03:40 -0500

    Intelligent Cody: Fetch locations multiple hops in the graph (#778)

--- vscode/src/chat/GraphContextProvider.ts
+++ vscode/src/chat/GraphContextProvider.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode'
 import { URI } from 'vscode-uri'
 
-import { PreciseContext } from '@sourcegraph/cody-shared'
+import { isDefined, PreciseContext } from '@sourcegraph/cody-shared'
 import { ActiveTextEditorSelectionRange, Editor } from '@sourcegraph/cody-shared/src/editor'
 import { GraphContextFetcher } from '@sourcegraph/cody-shared/src/graph-context'
 
@@ -12,6 +12,8 @@ export class GraphContextProvider implements GraphContextFetcher {
         return getGraphContextFromEditor(this.editor)
     }
 }
+
+const recursionLimit = 1
 
 /**
  * Return the definitions of symbols that occur within the editor's active document. If there is
@@ -26,36 +28,70 @@ export const getGraphContextFromEditor = async (editor: Editor): Promise<Precise
     }
 
     // Debuggin'
-    const label = 'precise context - vscode api'
+    const label = 'precise context from editor'
     performance.mark(label)
 
-    // Construct vscode.URI for the open file to interface with LSP queries
-    const activeEditorFileUri = workspaceRootUri.with({ path: activeEditor.filePath })
+    const uri = workspaceRootUri.with({ path: activeEditor.filePath })
+    const contexts = await getGraphContextFromSelection(
+        [{ uri, range: activeEditor.selectionRange }],
+        new Map([[uri.fsPath, activeEditor.content.split('\n')]]),
+        recursionLimit
+    )
 
-    // Split content of active editor into lines (we slice this many times array below)
-    const activeEditorLines = activeEditor.content.split('\n')
+    const nonLocalContexts = contexts.filter(({ filePath }) => filePath !== uri.fsPath)
+
+    // Debuggin'
+    console.debug(`Retrieved ${nonLocalContexts.length} non-file-local context snippets`)
+    performance.mark(label)
+    return nonLocalContexts
+}
+
+interface Selection {
+    uri: URI
+    range?: ActiveTextEditorSelectionRange
+}
+
+/**
+ * Return the definitions of symbols that occur within the given selection ranges. If a selection has
+ * a defined range, we will cull the symbols to those referenced in intersecting document symbol ranges.
+ */
+const getGraphContextFromSelection = async (
+    selections: Selection[],
+    contentMap: Map<string, string[]>,
+    recursionLimit: number = 0
+): Promise<PreciseContext[]> => {
+    // Debuggin'
+    const label = 'precise context from selection'
+    performance.mark(label)
 
     // Get the document symbols in the current file and extract their definition range
-    const relevantDocumentSymbolRanges = await extractRelevantDocumentSymbolRanges(
-        activeEditorFileUri,
-        activeEditor.selectionRange
-    )
+    const definitionSelections = await extractRelevantDocumentSymbolRanges(selections)
 
     // Extract identifiers from the relevant document symbol ranges and request their definitions
-    const definitionMatches = await gatherDefinitions(
-        activeEditorFileUri,
-        activeEditorLines,
-        relevantDocumentSymbolRanges
+    const definitionMatches = await gatherDefinitions(definitionSelections, contentMap)
+
+    // Open each URI referenced by a definition match in the current workspace, and make the document
+    // content retrievable by filepath by adding it to the shared content map.
+    //
+    // NOTE: Before asking for data about a document it must be opened in the workspace. This forces
+    // a resolution so that the following queries that require the document context will not fail with
+    // an unknown document.
+
+    const unseenDefinitionUris = dedupeWith(
+        definitionMatches.map(({ locations }) => locations.map(({ uri }) => uri)).flat(),
+        uri => uri.fsPath
+    ).filter(uri => !contentMap.has(uri.fsPath))
+
+    const newContentMap = new Map(
+        unseenDefinitionUris.map(uri => [
+            uri.fsPath,
+            vscode.workspace.openTextDocument(uri.fsPath).then(document => document.getText().split('\n')),
+        ])
     )
 
-    // Resolve, extract, and deduplicate the URIs distinct from the active editor file
-    const uris = dedupeWith(
-        definitionMatches
-            .map(({ locations }) => locations.map(({ uri }) => uri))
-            .flat()
-            .filter(uri => uri.fsPath !== activeEditorFileUri.fsPath),
-        uri => uri.fsPath
-    )
+    for (const [fsPath, lines] of await unwrapThenableMap(newContentMap)) {
+        contentMap.set(fsPath, lines)
+    }
 
     // Resolve, extract, and deduplicate the symbol and location match pairs from the definition matches
     const matches = dedupeWith(
@@ -65,46 +101,82 @@ export const getGraphContextFromEditor = async (editor: Editor): Promise<Precise
         ({ location }) => locationKeyFn(location)
     )
 
-    // Open each URI in the current workspace, and make the document content retrievable by filepath
-    const contentMap = new Map(
-        uris.map(uri => [
-            uri.fsPath,
-            vscode.workspace.openTextDocument(uri.fsPath).then(document => document.getText().split('\n')),
-        ])
-    )
-
-    // NOTE: Before asking for data about a document it must be opened in the workspace. Force a resolution
-    // here otherwise the following queries that require the document context will fail non-deterministically.
-    await Promise.all([...contentMap.values()])
-
     // Extract definition text from our matches
-    const contexts = await extractDefinitionContexts(matches, await unwrapThenableMap(contentMap))
+    const contexts = await extractDefinitionContexts(matches, contentMap)
 
     // Debuggin'
-    console.debug(`Retrieved ${contexts.length} non-file-local context snippets`)
+    console.debug(`Retrieved ${contexts.length} context snippets`)
     performance.mark(label)
+
+    if (recursionLimit > 0) {
+        contexts.push(
+            ...(await getGraphContextFromSelection(
+                contexts.map(c => ({
+                    uri: URI.file(c.filePath),
+                    range: c.range
+                        ? new vscode.Range(
+                              c.range.startLine,
+                              c.range.startCharacter,
+                              c.range.endLine,
+                              c.range.endCharacter
+                          )
+                        : undefined,
+                })),
+                contentMap,
+                recursionLimit - 1
+            ))
+        )
+    }
+
     return contexts
 }
 
 /**
- * Get the document symbols in the current file and extract their definition range. This
- * will give us indication of where the user selection and cursor is located, which we
- * assume to be the most relevant code to the current question.
+ * Get the document symbols in files indicated by the given selections and extract the symbol ranges.
+ * This will give us indication of where either the user selection and cursor is located or the range
+ * of a relevant definition we've fetched in a previous iteration, which we assume to be the most
+ * relevant code to the current question.
  */
 export const extractRelevantDocumentSymbolRanges = async (
-    activeEditorFileUri: URI,
-    selectionRange?: ActiveTextEditorSelectionRange,
+    selections: Selection[],
     getDocumentSymbolRanges: typeof defaultGetDocumentSymbolRanges = defaultGetDocumentSymbolRanges
-): Promise<vscode.Range[]> => {
-    const documentSymbolRanges = await getDocumentSymbolRanges(activeEditorFileUri)
+): Promise<Selection[]> => {
+    const rangeMap = await unwrapThenableMap(
+        new Map(
+            dedupeWith(
+                selections.map(({ uri }) => uri),
+                uri => uri.fsPath
+            ).map(uri => [uri.fsPath, getDocumentSymbolRanges(uri)])
+        )
+    )
 
-    // Filter the document symbol ranges to just those whose range intersects the selection.
-    // If no selection exists, keep all symbols, we'll utilize all document symbol ranges.
-    return selectionRange
-        ? documentSymbolRanges.filter(
-              ({ start, end }) => start.line <= selectionRange.end.line && selectionRange.start.line <= end.line
-          )
-        : documentSymbolRanges
+    const pathsByUri = new Map<string, (ActiveTextEditorSelectionRange | undefined)[]>()
+    for (const { uri, range } of selections) {
+        pathsByUri.set(uri.fsPath, [...(pathsByUri.get(uri.fsPath) ?? []), range])
+    }
+
+    const combinedRanges: Selection[] = []
+    for (const [fsPath, ranges] of pathsByUri.entries()) {
+        const documentSymbolRanges = rangeMap.get(fsPath)
+        if (!documentSymbolRanges) {
+            continue
+        }
+
+        // Filter the document symbol ranges to just those whose range intersects the selection.
+        // If no selection exists (if we have an undefined in the ranges list), keep all symbols,
+        // we'll utilize all document symbol ranges.
+        const definedRanges = ranges.filter(isDefined)
+        combinedRanges.push(
+            ...(definedRanges.length < ranges.length
+                ? documentSymbolRanges
+                : documentSymbolRanges.filter(({ start, end }) =>
+                      definedRanges.some(range => start.line <= range.end.line && range.start.line <= end.line)
+                  )
+            ).map(range => ({ uri: URI.file(fsPath), range }))
+        )
+    }
+
+    return combinedRanges
 }
 
 const identifierPattern = /[$A-Z_a-z][\w$]*/g
@@ -211,39 +283,66 @@ interface ResolvedSymbolDefinitionMatches {
 }
 
 /**
- * Search the given lines of code for identifier definitions matching an a common identifier pattern
- * and filter out common keywords. Each matching symbol is queried for definitions which are resolved
- * in parallel before return.
+ * Search the given ranges identifier definitions matching an a common identifier pattern and filter out
+ * common keywords. Each matching symbol is queried for definitions which are resolved in parallel before
+ * return.
  */
 export const gatherDefinitions = async (
-    activeEditorFileUri: URI,
-    activeEditorLines: string[],
-    relevantDocumentSymbolRanges: vscode.Range[],
+    selections: Selection[],
+    contentMap: Map<string, string[]>,
     getDefinitions: typeof defaultGetDefinitions = defaultGetDefinitions
 ): Promise<ResolvedSymbolDefinitionMatches[]> => {
     // Construct a list of symbol and definition location pairs by querying the LSP server
     // with all identifiers (heuristically chosen via regex) in the relevant code ranges.
     const definitionMatches: SymbolDefinitionMatches[] = []
-    for (const { start, end } of relevantDocumentSymbolRanges) {
-        for (const [lineIndex, line] of activeEditorLines.slice(start.line, end.line + 1).entries()) {
-            for (const match of line.matchAll(identifierPattern)) {
-                if (match.index === undefined || commonKeywords.has(match[0])) {
-                    continue
-                }
 
-                definitionMatches.push({
-                    symbolName: match[0],
-                    locations: getDefinitions(
-                        activeEditorFileUri,
-                        new vscode.Position(start.line + lineIndex, match.index + 1)
-                    ),
-                })
+    for (const selection of selections) {
+        const { uri, range } = selection
+        const lines = contentMap.get(uri.fsPath)
+        if (!range || !lines) {
+            continue
+        }
+
+        for (const { start, end } of [range]) {
+            for (const [lineIndex, line] of lines.slice(start.line, end.line + 1).entries()) {
+                for (const match of line.matchAll(identifierPattern)) {
+                    if (match.index === undefined || commonKeywords.has(match[0])) {
+                        continue
+                    }
+
+                    definitionMatches.push({
+                        symbolName: match[0],
+                        locations: getDefinitions(uri, new vscode.Position(start.line + lineIndex, match.index + 1)),
+                    })
+                }
             }
         }
     }
 
-    return Promise.all(
+    // Resolve all in-flight promises in parallel
+    const resolvedDefinitionMatches = await Promise.all(
         definitionMatches.map(async ({ symbolName, locations }) => ({ symbolName, locations: await locations }))
+    )
+
+    return (
+        resolvedDefinitionMatches
+            // Remove definition ranges that exist within one fo the input definition selections
+            // These are locals and don't give us any additional information in the context window.
+            .map(({ symbolName, locations }) => ({
+                symbolName,
+                locations: locations.filter(
+                    ({ uri, range }) =>
+                        !selections.some(
+                            ({ uri: selectionUri, range: selectionRange }) =>
+                                uri.fsPath === selectionUri.fsPath &&
+                                (selectionRange === undefined ||
+                                    (selectionRange.start.line <= range.start.line &&
+                                        range.end.line <= selectionRange.end.line))
+                        )
+                ),
+            }))
+            // Remove empty locations
+            .filter(({ locations }) => locations.length !== 0)
     )
 }
 
@@ -260,7 +359,9 @@ export const extractDefinitionContexts = async (
     // Retrieve document symbols for each of the open documents, which we will use to extract the relevant
     // definition "bounds" given the range of the definition symbol (which is contained within the range).
     const documentSymbolsMap = new Map(
-        [...contentMap.keys()].map(fsPath => [fsPath, getDocumentSymbolRanges(vscode.Uri.file(fsPath))])
+        [...contentMap.keys()]
+            .filter(fsPath => matches.some(({ location }) => location.uri.fsPath === fsPath))
+            .map(fsPath => [fsPath, getDocumentSymbolRanges(vscode.Uri.file(fsPath))])
     )
 
     // NOTE: In order to make sure the loop below is unblocked we'll also force resolve the entirety
@@ -372,6 +473,9 @@ const locationKeyFn = (location: vscode.Location): string =>
  * Convert a mapping from K -> Thenable<V> to a map of K -> V.
  */
 const unwrapThenableMap = async <K, V>(m: Map<K, Thenable<V>>): Promise<Map<K, V>> => {
+    // Force resolution so that the await in the loop below is unblocked.
+    await Promise.all(m.values())
+
     const resolved = new Map<K, V>()
     for (const [k, v] of [...m.entries()]) {
         resolved.set(k, await v)
