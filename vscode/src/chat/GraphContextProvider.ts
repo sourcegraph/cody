c4469fcca8a82bdc1dae8a63538858e1f972f552
Eric Fritz <eric@eric-fritz.com>
2023-08-29 08:46:28 -0500

    Deduplicate request queue by symbol name.

--- vscode/src/chat/GraphContextProvider.ts
+++ vscode/src/chat/GraphContextProvider.ts
@@ -304,19 +304,37 @@ export const gatherDefinitions = async (
             continue
         }
 
+        const requestQueue: { symbolName: string; position: vscode.Position }[] = []
         for (const { start, end } of [range]) {
             for (const [lineIndex, line] of lines.slice(start.line, end.line + 1).entries()) {
-                for (const match of line.matchAll(identifierPattern)) {
+                // NOTE: pretty hacky - strip out C-style line comments and find everything
+                // that might look like it could be an identifier. If we end up running a
+                // VSCode provider over this cursor position and it's not a symbol we can
+                // use, we'll just get back an empty location list.
+                const identifierMatches = line.replace(/\/\/.*$/, '').matchAll(identifierPattern)
+
+                for (const match of identifierMatches) {
                     if (match.index === undefined || commonKeywords.has(match[0])) {
                         continue
                     }
 
-                    definitionMatches.push({
+                    requestQueue.push({
                         symbolName: match[0],
-                        locations: getDefinitions(uri, new vscode.Position(start.line + lineIndex, match.index + 1)),
+                        position: new vscode.Position(start.line + lineIndex, match.index + 1),
                     })
                 }
             }
+        }
+
+        // NOTE: deduplicating here will save duplicate queries that are _likely_ to point to the
+        // same definition, but we may be culling aggressively here for some edge cases. I don't
+        // currently think that these are likely to be make-or-break a quality response on any
+        // significant segment of real world questions, though.
+        for (const { symbolName, position } of dedupeWith(requestQueue, ({ symbolName }) => symbolName)) {
+            definitionMatches.push({
+                symbolName,
+                locations: getDefinitions(uri, position),
+            })
         }
     }
 
