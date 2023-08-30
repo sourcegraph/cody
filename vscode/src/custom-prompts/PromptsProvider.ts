7ee7fb62cabf5a79c9982b2fb4358f949af3e076
Taras Yemets <yemets.taras@gmail.com>
2023-08-30 10:22:19 +0300

    update copies casing

--- vscode/src/custom-prompts/PromptsProvider.ts
+++ vscode/src/custom-prompts/PromptsProvider.ts
@@ -3,7 +3,7 @@ import { CodyPrompt, getDefaultCommandsMap } from '@sourcegraph/cody-shared/src/
 import { debug } from '../log'
 
 // Manage default commands created by the prompts in prompts.json
-const editorCommands = [{ name: 'Request a code edit', prompt: '/edit', slashCommand: '/edit' }]
+const editorCommands = [{ name: 'Request a Code Edit', prompt: '/edit', slashCommand: '/edit' }]
 export class PromptsProvider {
     // The default prompts
     private defaultPromptsMap = getDefaultCommandsMap(editorCommands)
