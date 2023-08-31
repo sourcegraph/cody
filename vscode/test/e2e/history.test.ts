c0bf6bd57f880d5215a1766c5231456972644097
Beatrix <beatrix@sourcegraph.com>
2023-08-31 12:11:37 -0500

    update history test

--- vscode/test/e2e/history.test.ts
+++ vscode/test/e2e/history.test.ts
@@ -1,12 +1,18 @@
 import { expect } from '@playwright/test'
 
-import { sidebarSignin } from './common'
+import { sidebarExplorer, sidebarSignin } from './common'
 import { test } from './helpers'
 
-test.skip('checks for the chat history and new session', async ({ page, sidebar }) => {
+test('checks if clear chat history button clears history and current session', async ({ page, sidebar }) => {
     // Sign into Cody
     await sidebarSignin(page, sidebar)
+    // Open the File Explorer view from the sidebar
+    await sidebarExplorer(page).click()
+    // Open the index.html file from the tree view
+    await page.getByRole('treeitem', { name: 'index.html' }).locator('a').dblclick()
 
+    // Bring the cody sidebar to the foreground
+    await page.click('[aria-label="Cody"]')
     await page.click('[aria-label="Chat History"]')
     await expect(sidebar.getByText('Chat History')).toBeVisible()
 
@@ -17,10 +23,26 @@ test.skip('checks for the chat history and new session', async ({ page, sidebar 
 
     await sidebar.getByRole('textbox', { name: 'Chat message' }).fill('Hello')
     await sidebar.locator('vscode-button').getByRole('img').click()
+
+    await page.click('[aria-label="Start a New Chat Session"]')
+    await sidebar.getByRole('textbox', { name: 'Chat message' }).fill('Hey')
+    await sidebar.locator('vscode-button').getByRole('img').click()
+
+    await page.getByRole('button', { name: 'Chat History' }).click()
+
+    await expect(sidebar.getByText('Hey')).toBeVisible()
     await expect(sidebar.getByText('Hello')).toBeVisible()
-    await page.getByRole('button', { name: 'Chat History' }).click()
-    await sidebar.locator('vscode-button').filter({ hasText: 'Hello' }).click()
-    await page.getByRole('button', { name: 'Chat History' }).click()
-    await sidebar.locator('vscode-button').filter({ hasText: 'Hello' }).locator('i').click()
+
+    await sidebar.locator('vscode-button').filter({ hasText: 'Clear' }).click()
+    await expect(sidebar.getByText('Hey')).not.toBeVisible()
     await expect(sidebar.getByText('Hello')).not.toBeVisible()
+
+    // Open the Cody Commands palette and run a command
+    await page.getByRole('button', { name: 'Commands (⌥C)' }).click()
+    await page.keyboard.type('/explain')
+    await page.keyboard.press('Enter')
+
+    await expect(sidebar.getByText("Hello! I'm Cody. I can write code and answer questions for you.")).toBeVisible()
+    await expect(sidebar.getByText('/explain')).toBeVisible()
+    await expect(sidebar.getByText('Hey')).not.toBeVisible()
 })
