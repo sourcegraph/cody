bdc021673d3b92ca2326529a82cf1df6998ac97a
Philipp Spiess <hello@philippspiess.com>
2023-08-31 16:43:59 +0200

    Work around VSCode default behavior

--- vscode/src/fetch.node.ts
+++ vscode/src/fetch.node.ts
@@ -1,19 +1,59 @@
-import { Agent as HTTPAgent } from 'http'
-import { Agent as HTTPSAgent } from 'https'
+import http from 'http'
+import https from 'https'
 
 import { agent } from './fetch'
+
+const nodeModules = '_VSCODE_NODE_MODULES'
+const proxyAgentPath = '@vscode/proxy-agent/out/agent'
+const proxyAgent = 'PacProxyAgent'
 
 export function initializeNetworkAgent(): void {
     /**
      * We use keepAlive agents here to avoid excessive SSL/TLS handshakes for autocomplete requests.
      */
-    const httpAgent = new HTTPAgent({ keepAlive: true })
-    const httpsAgent = new HTTPSAgent({ keepAlive: true })
+    const httpAgent = new http.Agent({ keepAlive: true })
+    const httpsAgent = new https.Agent({ keepAlive: true })
 
-    agent.current = (parsedURL: URL): HTTPAgent => {
-        if (parsedURL.protocol === 'http:') {
+    const customAgent = ({ protocol }: Pick<URL, 'protocol'>): http.Agent => {
+        if (protocol === 'http:') {
             return httpAgent
         }
         return httpsAgent
+    }
+
+    agent.current = customAgent
+
+    /**
+     * This works around an issue in the default VS Code proxy agent code. When `http.proxySupport`
+     * is set to its default value and no proxy setting is being used, the proxy library does not
+     * properly reuse the agent set on the http(s) method and is instead always using a new agent
+     * per request.
+     *
+     * To work around this, we patch the default proxy agent method and overwrite the
+     * `originalAgent` value before invoking it for requests that want to keep their connection
+     * alive (as documented by the `Connection: keep-alive` header).
+     *
+     * c.f. https://github.com/microsoft/vscode/issues/173861
+     */
+    try {
+        const PacProxyAgent = (globalThis as any)?.[nodeModules]?.[proxyAgentPath]?.[proxyAgent] ?? undefined
+        if (PacProxyAgent) {
+            const originalConnect = PacProxyAgent.prototype.connect
+            PacProxyAgent.prototype.connect = function (req: http.ClientRequest, opts: { protocol: string }): any {
+                try {
+                    const connectionHeader = req.getHeader('connection')
+                    if (Array.isArray(connectionHeader) && connectionHeader.includes('keep-alive')) {
+                        this.opts.originalAgent = customAgent(opts)
+                        return originalConnect.call(this, req, opts)
+                    }
+                    return originalConnect.call(this, req, opts)
+                } catch {
+                    return originalConnect.call(this, req, opts)
+                }
+            }
+        }
+    } catch (error) {
+        // Ignore any errors in the patching logic
+        void error
     }
 }
