be11d0401713c9ce6c8e042fd3ac2e6ca9cc65c3
Philipp Spiess <hello@philippspiess.com>
2023-09-01 13:29:09 +0200

    VSCode: Add Sentry to extension host process (#882)
    This PR adds a simple setup for Sentry to be used inside the extension
    host thread. It does not set up any default integration and we will
    instead need to wrap top-level entry points with calls to
    `Setnry.captureException`.
    
    This also wires up autocomplete errors to be routed to Sentry so we have
    an easy service in place to understand the reliability of the product.
    
    Note: When the user is connected to an instance other than dotcom, this
    does not send any events.
    
    ## Test plan
    
    - Add a throw case into the anthropic handler
    - Force the env to be production
    - See that the error is logged to Sentry
    
    <img width="1100" alt="Screenshot 2023-08-31 at 14 03 13"
    src="https://github.com/sourcegraph/cody/assets/458591/99febc73-f79f-4b70-951f-e4e2a3ec0bb8">
    
    I have also changed the backend to S2 and observed that no error is
    logged.
    
    <!-- Required. See
    https://docs.sourcegraph.com/dev/background-information/testing_principles.
    -->

---
+++ vscode/src/services/sentry/sentry.ts
@@ -0,0 +1,59 @@
+import type { init as browserInit } from '@sentry/browser'
+import type { init as nodeInit } from '@sentry/node'
+
+import { Configuration } from '@sourcegraph/cody-shared/src/configuration'
+import { isDotCom } from '@sourcegraph/cody-shared/src/sourcegraph-api/environments'
+
+import { extensionDetails } from '../EventLogger'
+
+export * from '@sentry/core'
+export const SENTRY_DSN = 'https://f565373301c9c7ef18448a1c60dfde8d@o19358.ingest.sentry.io/4505743319564288'
+
+export type SentryOptions = NonNullable<Parameters<typeof nodeInit | typeof browserInit>[0]>
+
+export abstract class SentryService {
+    constructor(protected config: Pick<Configuration, 'serverEndpoint' | 'isRunningInsideAgent'>) {
+        this.prepareReconfigure()
+    }
+
+    public onConfigurationChange(newConfig: Pick<Configuration, 'serverEndpoint'>): void {
+        this.config = newConfig
+        this.prepareReconfigure()
+    }
+
+    private prepareReconfigure(): void {
+        const isProd = process.env.NODE_ENV === 'production'
+        const options: SentryOptions = {
+            dsn: SENTRY_DSN,
+            release: extensionDetails.version,
+            environment: this.config.isRunningInsideAgent
+                ? 'agent'
+                : typeof process !== 'undefined'
+                ? 'vscode-node'
+                : 'vscode-web',
+
+            // In dev mode, have Sentry log extended debug information to the console.
+            debug: !isProd,
+
+            // Only send errors when connected to dotcom
+            beforeSend: event => {
+                if (!isDotCom(this.config.serverEndpoint) && isProd) {
+                    return null
+                }
+                return event
+            },
+
+            // The extension host is shared across other extensions, so listening on the default
+            // unhandled error listeners would not be helpful in case other extensions or VS Code
+            // throw. Instead, use the manual `captureException` API.
+            //
+            // When running inside Agent, we control the whole Node environment so we can safely
+            // listen to unhandled errors/rejections.
+            ...(this.config.isRunningInsideAgent ? {} : { defaultIntegrations: false }),
+        }
+
+        this.reconfigure(options)
+    }
+
+    protected abstract reconfigure(options: Parameters<typeof nodeInit | typeof browserInit>[0]): void
+}
