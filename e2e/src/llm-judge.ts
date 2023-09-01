8cf6c01275e384af711adf93426ab8cc1ea7fc2f
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2023-09-01 13:15:54 -0500

    Azure / e2e Cody suite (#873)
    Part of https://github.com/sourcegraph/sourcegraph/issues/56182
    
    Here's how to get this running:
    
    1. Azure keys
    * https://portal.azure.com > Azure OpenAI > sourcegraph-test-oai > Keys
    and Endpoint
       * Copy the key and `export AZURE_API_KEY="<paste the key here>"`
    1. `export SRC_ENDPOINT=https://sourcegraph.sourcegraph.com`
    1. `export SRC_ACCESS_TOKEN="<s2 access token goes here>"`
    1. Run like so: `pnpm run start --label="Beam search"
    --output="$HOME/test.results" --provider=azure`
       * single beam search test runs faster than total # test cases
    * output file actually allows you to drill into the LLM interaction
    details
    
    ## Test plan
    
    Manually verified:
    
    ### success scenario with Azure OpenAI and
    
    ```
    ➜  e2e git:(cb/s/56182-azure) ✗ pnpm run start --label="Beam search" --output="$HOME/azure.results" --provider=azure
    
    > @sourcegraph/cody-e2e@0.0.1 start /Users/cbart/cody/e2e
    > pnpm run --silent build && node dist/e2e "--label=Beam search" "--output=/Users/cbart/azure.results" "--provider=azure"
    
    Run 1 of 1
    
    Testing (1/1): Beam search (github.com/huggingface/transformers)
    Incorrect answers (LLM judge): 0/2 (0%)
    Incorrect or partial answers (LLM judge): 1/2 (50%)
    Missing facts: 5/9 (56%)
    Hallucinated entities: 9/9 (100%)
    
    Run 1 of 1 summary:
    Incorrect answers (LLM judge): 0/2 (0%)
    Incorrect or partial answers (LLM judge): 1/2 (50%)
    Missing facts: 5/9 (56%)
    Hallucinated entities: 9/9 (100%)
    ```
    
    ### fast fail when not enough env vars provided
    
    ```
    ➜  e2e git:(cb/s/56182-azure) unset AZURE_DEPLOYMENT_ID
    ➜  e2e git:(cb/s/56182-azure) ✗ pnpm run start --label="Beam search" --output="$HOME/azure.results" --provider=azure
    
    > @sourcegraph/cody-e2e@0.0.1 start /Users/cbart/cody/e2e
    > pnpm run --silent build && node dist/e2e "--label=Beam search" "--output=/Users/cbart/azure.results" "--provider=azure"
    
    Missing required environment variables: AZURE_DEPLOYMENT_ID
    1. https://portal.azure.com > Azure OpenAI > sourcegraph-test-oai > Keys and Endpoint
    2. Go to Model Deployments > Manage Deployments. Find the deployment name and:
       export AZURE_DEPLOYMENT_ID="<paste deployment name here>"
     ELIFECYCLE  Command failed with exit code 1.
    ```

--- e2e/src/llm-judge.ts
+++ e2e/src/llm-judge.ts
@@ -1,4 +1,7 @@
 import Anthropic from '@anthropic-ai/sdk'
+import { AzureKeyCredential, OpenAIClient } from '@azure/openai'
+
+import { CLIOptions } from '.'
 
 export interface LLMJudgement {
     answerMatchesSummary: 'yes' | 'no' | 'partial' | 'unknown'
@@ -22,7 +25,10 @@ function serializeConversationTranscript(transcript: { question: string; answer:
     return `Conversation transcript:\n${transcriptSerialized}\n`
 }
 
+const maxTokensToSample = 300
+
 export async function llmJudge(
+    provider: CLIOptions['provider'],
     transcript: { question: string; answer: string }[],
     question: string,
     answerSummary: string,
@@ -43,15 +49,73 @@ export async function llmJudge(
         'Think step by step. Conclude your thought process with YES, NO, or PARTIAL in all capital letters as the final judgement.',
     ].join('')
 
+    switch (provider) {
+        case 'anthropic':
+            return anthropicJudge(instructions)
+        case 'azure':
+            return azureJudge(instructions)
+    }
+}
+
+async function anthropicJudge(instructions: string): Promise<LLMJudgement> {
     const completion = await anthropic.completions.create({
         model: 'claude-2',
-        max_tokens_to_sample: 300,
+        max_tokens_to_sample: maxTokensToSample,
         prompt: `${Anthropic.HUMAN_PROMPT}${instructions}${Anthropic.AI_PROMPT}Thought:`,
     })
-
     return {
         answerMatchesSummary: doesAnswerMatchSummary(completion.completion),
         answerMatchesSummaryJudgement: completion.completion,
+    }
+}
+
+// failFastIfAzureEnvVarsNotSet validates the environment variables needed
+// to run e2e suite against Azure OpenAI. It exists the process early
+// if any of the variables are not present.
+export const failFastIfAzureEnvVarsNotSet = (): void => {
+    const requiredVars = ['AZURE_API_KEY', 'AZURE_API_ENDPOINT', 'AZURE_DEPLOYMENT_ID']
+    const missingVars = requiredVars.filter(varName => !(varName in process.env))
+    if (missingVars.length > 0) {
+        console.log(`Missing required environment variables: ${missingVars.join(', ')}`)
+        console.log('1. https://portal.azure.com > Azure OpenAI > sourcegraph-test-oai > Keys and Endpoint')
+        let nextBulletPoint = 2
+        if (!process.env.AZURE_API_KEY) {
+            console.log(`${nextBulletPoint}. Copy the key and export AZURE_API_KEY="<paste the key here>"`)
+            nextBulletPoint++
+        }
+        if (!process.env.AZURE_API_ENDPOINT) {
+            console.log(
+                `${nextBulletPoint}. Copy the endpoint and export AZURE_API_ENDPOINT="<paste the endpoint here>"`
+            )
+        }
+        if (!process.env.AZURE_DEPLOYMENT_ID) {
+            console.log(
+                `${nextBulletPoint}. Go to Model Deployments > Manage Deployments. Find the deployment name and:`
+            )
+            console.log('   export AZURE_DEPLOYMENT_ID="<paste deployment name here>"')
+        }
+        process.exit(1)
+    }
+}
+
+async function azureJudge(instructions: string): Promise<LLMJudgement> {
+    // 1. https://portal.azure.com > Azure OpenAI > sourcegraph-test-oai > Keys and Endpoint
+    // 2. Copy the key and export AZURE_API_KEY="<paste the key here>"
+    // 3. Copy the endpoint and export AZURE_API_ENDPOINT="<paste the endpoint here>"
+    // 4. Go to Model Deployments > Manage Deployments. Find the deployment name and:
+    //    export AZURE_DEPLOYMENT_ID="<paste deployment name here>"
+    const azureApiKey = process.env.AZURE_API_KEY as string
+    const endpoint = process.env.AZURE_API_ENDPOINT as string
+
+    const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey))
+    const deploymentId = process.env.AZURE_DEPLOYMENT_ID as string
+    const result = await client.getCompletions(deploymentId, [instructions], { maxTokens: maxTokensToSample })
+    // Pick the first choice. Probably we can do something better here?
+    const [response] = result.choices
+
+    return {
+        answerMatchesSummary: doesAnswerMatchSummary(response.text),
+        answerMatchesSummaryJudgement: response.text,
     }
 }
 
