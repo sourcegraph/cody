125696185bae822995c3933190a49ab0f0b62076
Philipp Spiess <hello@philippspiess.com>
2023-09-06 16:20:06 +0200

    Add dev flag to open output console on startup

--- vscode/src/dev/helpers.ts
+++ vscode/src/dev/helpers.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode'
 
+import { outputChannel } from '../log'
+
 /**
  * A development helper that runs on activation to make the edit-debug loop easier.
  *
@@ -7,11 +9,16 @@ import * as vscode from 'vscode'
  * configuration JSON Schema, so they will not validate in your VS Code user settings file.)
  *
  * - `cody.dev.openAutocompleteTraceView`: boolean
+ * - `cody.dev.openOutputConsole`: boolean
  */
 export function onActivationDevelopmentHelpers(): void {
     const settings = vscode.workspace.getConfiguration('cody.dev')
 
     if (settings.get('openAutocompleteTraceView')) {
         void vscode.commands.executeCommand('cody.autocomplete.openTraceView')
+    }
+
+    if (settings.get('openOutputConsole')) {
+        outputChannel.show()
     }
 }
