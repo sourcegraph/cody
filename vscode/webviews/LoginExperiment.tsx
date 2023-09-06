69a0f99f464902579f0c67f91eb42a1b697cfc48
Dominic Cooney <dominic.cooney@sourcegraph.com>
2023-09-06 15:10:14 +0900

    notes

--- vscode/webviews/LoginExperiment.tsx
+++ vscode/webviews/LoginExperiment.tsx
@@ -1,8 +1,7 @@
 import { VSCodeButton } from '@vscode/webview-ui-toolkit/react'
 
+import { DOTCOM_URL } from '@sourcegraph/cody-shared/src/sourcegraph-api/environments'
 import { TelemetryService } from '@sourcegraph/cody-shared/src/telemetry'
-
-import { DOTCOM_URL } from '../src/chat/protocol'
 
 import styles from './LoginExperiment.module.css'
 
@@ -23,15 +22,33 @@ export const LoginSimplified: React.FunctionComponent<React.PropsWithoutRef<Logi
     telemetryService,
     uriScheme,
 }) => {
+    const signInRedirect = uriScheme + '://sourcegraph.cody-ai/code='
+
+    // Create the turducken of redirects:
+    // 1. Specific login page (GitLab, GitHub, etc.) redirects to the post-sign up survey
+    // 2. Post-sign up survery redirects to the new token page
+    // 3. New token page redirects back to the extension with the new token
+    //
+    // Works w/ sg start dotcom & sourcegraph.test when the survey is done
+    // open question if that will survive the login handler
+    // TODO: the sign up form needs to also follow this redirect instead of hardcoding it
+    // TODO: URL encoding it also seems fine
+    // https://sourcegraph.test:3443/post-sign-up?returnTo=/user/settings/tokens/new/callback?requestFrom=CODY
+    // Email sign *in* (but maybe we need a "sign up" here?)
+    // https://sourcegraph.test:3443/sign-in?showMore=&returnTo=/post-sign-up?returnTo=/user/settings/tokens/new/callback?requestFrom=CODY
     const isInsiders = uriScheme === 'vscode-insiders'
     const referralCode = isInsiders ? 'CODY_INSIDERS' : 'CODY'
-    const signInRedirect = uriScheme + '://sourcegraph.cody-ai/code='
+    const newTokenUrl = `/user/settings/tokens/new/callback?requestFrom=${referralCode}`
+    const postSignUpSurveyUrl = `/post-sign-up?returnTo=${encodeURIComponent(newTokenUrl)}`
+    const site = 'https://sourcegraph.test:3443' || DOTCOM_URL.toString()
+    const gitHubLoginUrl = `${site}/.auth/github/login?pc=https%3A%2F%2Fgithub.com%2F%3A%3Ae917b2b7fa9040e1edd4&redirect=${encodeURIComponent(
+        postSignUpSurveyUrl
+    )}`
 
     return (
         <div>
-            <a
-                href={`https://sourcegraph.com/.auth/github/login?pc=https%3A%2F%2Fgithub.com%2F%3A%3Ae917b2b7fa9040e1edd4&redirect=/user/settings/tokens/new/callback%3frequestFrom=${referralCode}`}
-            >
+            <a href={gitHubLoginUrl}>
+                <p>{gitHubLoginUrl}</p>
                 <VSCodeButton
                     className={styles.button}
                     type="button"
