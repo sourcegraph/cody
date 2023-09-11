e643b0ae140c901f2b46a5818c5c75e9d3757c89
Tim Lucas <t@toolmantim.com>
2023-09-11 10:19:40 +1000

    Rely on docs for examples

--- vscode/src/custom-prompts/utils/helpers.ts
+++ vscode/src/custom-prompts/utils/helpers.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode'
 
-import { CodyPromptType, ConfigFileName } from '@sourcegraph/cody-shared/src/chat/prompts'
+import { ConfigFileName } from '@sourcegraph/cody-shared/src/chat/prompts'
 
 export function constructFileUri(fileName: string, rootDirPath?: string): vscode.Uri | undefined {
     if (!rootDirPath) {
@@ -15,34 +15,23 @@ export function constructFileUri(fileName: string, rootDirPath?: string): vscode
 // Create a .vscode/cody.json file in the root directory of the workspace or user's home directory using the sample files
 export async function createJSONFile(
     extensionPath: string,
-    configFileUri: vscode.Uri,
-    isUserType: boolean
+    configFileUri: vscode.Uri
 ): Promise<void> {
-    const sampleFileName = isUserType ? 'user-cody.json' : 'workspace-cody.json'
-    const codyJsonPath = constructFileUri('resources/samples/' + sampleFileName, extensionPath)
-    if (!configFileUri || !codyJsonPath) {
-        void vscode.window.showErrorMessage('Failed to create cody.json file.')
-        return
-    }
-    const decoded = await getFileContentText(codyJsonPath)
-    await saveJSONFile(decoded, configFileUri)
+    await saveJSONFile({ commands: [] }, configFileUri)
 }
 
 // Add context from the sample files to the .vscode/cody.json file
-export async function saveJSONFile(context: string, filePath: vscode.Uri, isSaveMode = false): Promise<void> {
+export async function saveJSONFile(data: unknown, filePath: vscode.Uri): Promise<void> {
     try {
         const workspaceEditor = new vscode.WorkspaceEdit()
         // Clear the file before writing to it
         workspaceEditor.deleteFile(filePath, { ignoreIfNotExists: true })
-        workspaceEditor.createFile(filePath, { ignoreIfExists: isSaveMode })
-        workspaceEditor.insert(filePath, new vscode.Position(0, 0), context)
+        workspaceEditor.createFile(filePath, { ignoreIfExists: true })
+        workspaceEditor.insert(filePath, new vscode.Position(0, 0), JSON.stringify(data, null, 2))
         await vscode.workspace.applyEdit(workspaceEditor)
         // Save the file
         const doc = await vscode.workspace.openTextDocument(filePath)
         await doc.save()
-        if (!isSaveMode) {
-            await vscode.window.showTextDocument(filePath)
-        }
     } catch (error) {
         throw new Error(`Failed to save your Custom Commands to a JSON file: ${error}`)
     }
