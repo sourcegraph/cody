7222d759fcd0faf4c37fcfd0590f908bfc4e70da
Philipp Spiess <hello@philippspiess.com>
2023-09-12 15:19:06 +0200

    Autocomplete: Add minimum latency

--- lib/shared/src/experimentation/FeatureFlagProvider.ts
+++ lib/shared/src/experimentation/FeatureFlagProvider.ts
@@ -12,6 +12,7 @@ export enum FeatureFlag {
     CodyAutocompleteStarCoder7B = 'cody-autocomplete-default-starcoder-7b',
     CodyAutocompleteStarCoder16B = 'cody-autocomplete-default-starcoder-16b',
     CodyAutocompleteClaudeInstantInfill = 'cody-autocomplete-claude-instant-infill',
+    CodyAutocompleteMinimumLatency = 'cody-autocomplete-minimum-latency',
 }
 
 const ONE_HOUR = 60 * 60 * 1000
