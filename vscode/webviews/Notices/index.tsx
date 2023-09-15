ab500d427a675445e7e506f7abf83c162d48758c
Tim Lucas <t@toolmantim.com>
2023-09-15 11:30:46 +1000

    Add autocomplete onboarding notice

--- vscode/webviews/Notices/index.tsx
+++ vscode/webviews/Notices/index.tsx
@@ -10,5 +10,6 @@ interface NoticesProps {
 export const Notices: React.FunctionComponent<NoticesProps> = ({ extensionVersion, probablyNewInstall }) => (
     <div className={styles.notices}>
         <VersionUpdatedNotice version={extensionVersion} probablyNewInstall={probablyNewInstall} />
+        {/* <OnboardingAutocompleteNotice /> */}
     </div>
 )
