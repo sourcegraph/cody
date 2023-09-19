f20fdfeb95588a8408491df325fb33079dc39828
Beatrix <beatrix@sourcegraph.com>
2023-09-19 14:14:38 -0700

    remove class range from getCursorFoldingRange

--- vscode/src/editor/utils.ts
+++ vscode/src/editor/utils.ts
@@ -42,17 +42,46 @@ export async function getCursorFoldingRange(
     uri: vscode.Uri,
     activeCursor: number
 ): Promise<vscode.Selection | undefined> {
-    const ranges = await getFoldingRange(uri).then(r => r.filter(r => r.kind !== 2 && r.kind !== 1))
-    // Get the folding range containing the active cursor
-    for (const range of ranges) {
-        if (range.start <= activeCursor && range.end + 2 >= activeCursor) {
-            return new vscode.Selection(range.start, 0, range.end + 2, 0)
+    const symbols = await getSymbolRanges(uri).then(r => r.filter(s => s.kind === vscode.SymbolKind.Class))
+    // Get the folding ranges for the document,
+    const ranges = await getFoldingRange(uri).then(r => r.filter(r => r.kind !== 2 && r.kind !== 1 && r.kind !== 3))
+    // remove ranges that match the symbols from ranges
+    for (const s of symbols) {
+        const symbolRange = s.location.range
+        for (let i = 0; i < ranges.length; i++) {
+            const r = ranges[i]
+            if (r.start === symbolRange.start.line && r.end === symbolRange.end.line) {
+                ranges.splice(i, 1)
+                i--
+            }
         }
     }
 
+    // Filter to keep folding ranges that contained other folding ranges
+    const filteredRanges = ranges.filter(r => !ranges.some(r2 => r2 !== r && r2.start <= r.start && r2.end >= r.end))
+    // Get the folding range containing the active cursor
+    const cursorRange = filteredRanges.find(r => r.start <= activeCursor && r.end >= activeCursor)
+    if (cursorRange) {
+        return new vscode.Selection(cursorRange.start, 0, cursorRange.end + 2, 0)
+    }
     return undefined
 }
 
 export function addSelectionToPrompt(prompt: string, code: string): string {
     return prompt + '\nHere is the code: \n<Code>' + code + '</Code>'
+}
+
+/**
+ * Gets the symbol information ranges for the given document URI.
+ *
+ * @param uri - The URI of the document to get symbol ranges for
+ * @returns A promise resolving to an array of SymbolInformation objects
+ * representing the symbols in the document.
+ */
+export async function getSymbolRanges(uri: vscode.Uri): Promise<vscode.SymbolInformation[]> {
+    const ranges = await vscode.commands.executeCommand<vscode.SymbolInformation[]>(
+        'vscode.executeDocumentSymbolProvider',
+        uri
+    )
+    return ranges
 }
