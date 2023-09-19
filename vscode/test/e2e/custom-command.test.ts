f2e96e7ac3eeae3160f36453432a3d6b9605ce48
Aditya Kalia <32119652+akalia25@users.noreply.github.com>
2023-09-19 15:51:13 -0400

    when clickling 'Configure Custom Commands' command:menu:opened is logged again

--- vscode/test/e2e/custom-command.test.ts
+++ vscode/test/e2e/custom-command.test.ts
@@ -13,19 +13,16 @@ const expectedOrderedEvent = [
     'CodyVSCodeExtension:auth:selectSigninMenu',
     'CodyVSCodeExtension:auth:fromToken',
     'CodyVSCodeExtension:Auth:connected',
-    'CodyVSCodeExtension:command:menu:opened',
     'CodyVSCodeExtension:chat:submitted',
+    'CodyVSCodeExtension:command:menu:opened',
     'CodyVSCodeExtension:command:menu:opened',
 ]
 test('open the Custom Commands in sidebar and add new user recipe', async ({ page, sidebar }) => {
     // Sign into Cody
     await sidebarSignin(page, sidebar)
-
     await expect(sidebar.getByText("Hello! I'm Cody.")).toBeVisible()
-
     await sidebar.getByRole('textbox', { name: 'Chat message' }).fill('/')
     await sidebar.locator('vscode-button').getByRole('img').click()
-
     // Create Command via UI
     const recipeName = 'ATestRecipes'
     await page.getByText('Configure Custom Commands...').click()
