c53f267b7dab90956ca2bb4a8727ee34e8938ba5
Beyang Liu <beyang.liu@gmail.com>
2023-09-21 17:51:10 -0700

    wip: fix downlaod

--- vscode/src/local-context/download-symf.ts
+++ vscode/src/local-context/download-symf.ts
@@ -1,9 +1,12 @@
 import * as os from 'os'
-import * as vscode from 'vscode'
-import { promises as fs } from 'fs'
 
-import { getApi, FileDownloader } from '@microsoft/vscode-file-downloader-api';
+import * as vscode from 'vscode'
+import * as path from 'path'
+import { promises as fspromises, createWriteStream } from 'fs'
 import { logDebug } from '../log'
+
+// import * as unzip from 'unzip'
+import { request } from 'http'
 
 const symfVersion = 'v0.0.0'
 
@@ -11,58 +14,103 @@ const symfVersion = 'v0.0.0'
  * Get the path to `symf`. If the symf binary is not found, download it.
  */
 export async function getSymfPath(context: vscode.ExtensionContext): Promise<string | null> {
+    console.log('# getSymfPath')
     // If user-specified symf path is set, use that
     const config = vscode.workspace.getConfiguration()
     const userSymfPath = config.get<string>('experimentalSymfPath')
     if (userSymfPath) {
-        logDebug('symf', `using user symfPath ${userSymfPath}`)
+        logDebug('symf', `using user symf: ${userSymfPath}`)
         return userSymfPath
     }
 
-    // Otherwise, download and return the path to the downloaded symf binary
-    const fileDownloader: FileDownloader = await getApi()
-
-    const symfPath = await fileDownloader.tryGetItem('symf', context)
-    if (symfPath) {
-        logDebug('symf', `using symf at ${symfPath.fsPath}`)
-        return symfPath.fsPath
-    }
-
-    return downloadSymf(context, fileDownloader)
-}
-
-async function downloadSymf(context: vscode.ExtensionContext, fileDownloader: FileDownloader): Promise<string | null> {
     const osArch = getOSArch()
     if (!osArch) {
         return null
     }
-
     const { platform, arch } = osArch
+    
+    const symfPath = path.join(context.globalStorageUri.fsPath, `symf-${symfVersion}-${arch}-${platform}`)
+    if (await fileExists(symfPath)) {
+        return symfPath
+    }
+
+    // NEXT
+
     const symfURL = `https://github.com/sourcegraph/symf/releases/download/${symfVersion}/symf-${arch}-${platform}`
     logDebug('symf', `downloading symf from ${symfURL}`)
 
     // Download symf binary with vscode progress api
-    const file = await vscode.window.withProgress({
+    await vscode.window.withProgress({
         location: vscode.ProgressLocation.Notification,
-        title: 'Downloading symf tool for semantic code search',
-        cancellable: true
-    }, async (progress, token) => {
-        const file: vscode.Uri = await fileDownloader.downloadFile(
-            vscode.Uri.parse(symfURL),
-            'symf',
-            context,
-            token,
-            undefined,
-            { shouldUnzip: false },
-        )
-        return file
-    })
-    logDebug('symf', `downloaded symf to ${file.fsPath}`)
-    // Make the file permissions executable
-    await fs.chmod(file.fsPath, 0o755)
+        title: 'Downloading semantic code search utility, symf',
+        cancellable: false
+    }, async (progress) => {
+        progress.report({ message: 'Downloading symf and extracting symf' })
 
-    return file.fsPath
+        const symfPathTmp = symfPath + '.tmp'
+        request(symfURL).pipe(createWriteStream(symfPathTmp + '.zip'))
+        console.log('# symfPathTmp', symfPathTmp)
+
+
+
+        // request(symfURL).pipe(unzip.Extract({ path: symfPathTmp }));
+        // logDebug('symf', `downloaded symf to ${symfPathTmp}`)
+        // await fs.chmod(symfPathTmp, 0o755)
+        // await fs.rename(symfPathTmp, symfPath)
+    })
+
+    // TODO: clean up old symf binaries
+
+
+    console.log('############ HERE')
+
+    return symfPath
+
+
+    // // Otherwise, download and return the path to the downloaded symf binary
+    // const fileDownloader: FileDownloader = await getApi()
+
+    // const symfPath = await fileDownloader.tryGetItem('symf', context)
+    // if (symfPath) {
+    //     logDebug('symf', `using symf at ${symfPath.fsPath}`)
+    //     return symfPath.fsPath
+    // }
+
+    // return downloadSymf(context, fileDownloader)
 }
+
+// async function downloadSymf(context: vscode.ExtensionContext, fileDownloader: FileDownloader): Promise<string | null> {
+//     const osArch = getOSArch()
+//     if (!osArch) {
+//         return null
+//     }
+
+//     const { platform, arch } = osArch
+//     const symfURL = `https://github.com/sourcegraph/symf/releases/download/${symfVersion}/symf-${arch}-${platform}`
+//     logDebug('symf', `downloading symf from ${symfURL}`)
+
+//     // Download symf binary with vscode progress api
+//     const file = await vscode.window.withProgress({
+//         location: vscode.ProgressLocation.Notification,
+//         title: 'Downloading symf tool for semantic code search',
+//         cancellable: true
+//     }, async (progress, token) => {
+//         const file: vscode.Uri = await fileDownloader.downloadFile(
+//             vscode.Uri.parse(symfURL),
+//             'symf',
+//             context,
+//             token,
+//             undefined,
+//             { shouldUnzip: false },
+//         )
+//         return file
+//     })
+//     logDebug('symf', `downloaded symf to ${file.fsPath}`)
+//     // Make the file permissions executable
+//     await fs.chmod(file.fsPath, 0o755)
+
+//     return file.fsPath
+// }
 
 function getOSArch(): { platform: string; arch: string } | null {
     const nodePlatformToPlatform: { [key: string]: string } = {
@@ -88,5 +136,14 @@ function getOSArch(): { platform: string; arch: string } | null {
     return {
         platform,
         arch,
+    }
+}
+
+async function fileExists(path: string): Promise<boolean> {
+    try {
+        await fspromises.access(path)
+        return true
+    } catch {
+        return false
     }
 }
