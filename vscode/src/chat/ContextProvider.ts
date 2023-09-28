51510a18ab164f5c42e1bb047238060cdfd356cf
Dominic Cooney <dominic.cooney@sourcegraph.com>
2023-09-28 17:12:19 -0400

    Simplified Onboarding: Pick up the app token if available after sign-in (#1210)
    LocalAppDetector stops monitoring app once the user has signed in.
    Simplified Onboarding needs to pick up the app token, even if the user
    has logged into dotcom, so that we can use app for local embeddings.
    This makes simplified onboarding's LocalAppWatcher ping the
    LocalAppDetector to retrieve the app token, if it detects a change in
    the app.json file, even after the user has logged in.
    
    Part of #632.
    
    ## Test plan
    
    0. Open Cody App, add a repo, and add embeddings for a private local
    repository which has a remote.
    1. Quit Cody App if it is running. Open Keychain and delete any
    vscodesourcegraph.cody-ai tokens. Delete ~/Library/Application
    Support/com.sourcegraph.cody/app.json . This means VScode will not have
    a cached app token. (Some uncertainty around where VScode stores secrets
    across versions. You can set a breakpoint in the extension
    LocalAppWatcher and run `secretStorage.deleteToken(LOCAL_APP_URL.href)`
    to clear the token. Alternatively, logging into App and signing out
    should also clear that token.)
    2. Run VScode, edit User Settings JSON and add
    `"testing.simplified-onboarding": true`. Restart VScode.
    3. Log into dotcom using GitHub, etc. if necessary.
    4. Open the repository from step 0, open a file from the repository,
    click on the chat input status indicator. You should see a pop-up to
    open app.
    5. Open app and wait up to 20 seconds. Chat input status indicator
    should flip to having a checkmark indicating VScode has detected app is
    running, picked up the token from app.json, and found app has embeddings
    for the repository.
    6. Close app and wait up to 20 seconds. The chat input status indicator
    should flip to red/open app popup.

--- vscode/src/chat/ContextProvider.ts
+++ vscode/src/chat/ContextProvider.ts
@@ -317,13 +317,13 @@ async function getCodebaseContext(
     }
 
     // Find an embeddings client
-    const embeddingsSearch = await EmbeddingsDetector.newEmbeddingsSearchClient(embeddingsClientCandidates, codebase)
+    let embeddingsSearch = await EmbeddingsDetector.newEmbeddingsSearchClient(embeddingsClientCandidates, codebase)
     if (isError(embeddingsSearch)) {
         logDebug(
             'ContextProvider:getCodebaseContext',
             `Cody could not find embeddings for '${codebase}' on your Sourcegraph instance`
         )
-        return null
+        embeddingsSearch = undefined
     }
 
     return new CodebaseContext(
