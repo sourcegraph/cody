91ba8f89fe0947725f5398cf85a63dbd21758639
Beatrix <beatrix@sourcegraph.com>
2023-09-28 20:55:14 -0400

    add doc behind feature flag

--- vscode/src/custom-prompts/CommandRunner.ts
+++ vscode/src/custom-prompts/CommandRunner.ts
@@ -6,6 +6,8 @@ import { getCursorFoldingRange } from '../editor/utils'
 import { logDebug } from '../log'
 import { telemetryService } from '../services/telemetry'
 
+const release = false
+
 /**
  * CommandRunner class implements disposable interface.
  * Manages executing a Cody command and optional fixup.
@@ -58,7 +60,7 @@ export class CommandRunner implements vscode.Disposable {
         }
 
         // Run fixup if this is a edit command
-        const insertMode = command.mode === 'insert'
+        const insertMode = command.mode === 'insert' && release
         const fixupMode = command.mode === 'edit' || instruction?.startsWith('/edit ')
         this.isFixupRequest = isFixupRequest || fixupMode || insertMode
         if (this.isFixupRequest) {
@@ -139,8 +141,8 @@ export class CommandRunner implements vscode.Disposable {
                 range,
                 instruction,
                 document: doc,
-                auto: true,
-                insertMode,
+                auto: release,
+                insertMode: release,
             },
             source
         )
