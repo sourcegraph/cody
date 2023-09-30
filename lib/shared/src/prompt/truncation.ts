f5416adfd060324ffcade5ac2fe586ff778cd079
arafatk <arafat.da.khan@gmail.com>
2023-09-30 12:25:36 -0600

    Removing Extra example of Terminal commands from the prompt just to make sure that the existing results are affected the least

--- lib/shared/src/prompt/truncation.ts
+++ lib/shared/src/prompt/truncation.ts
@@ -25,30 +25,10 @@ export function getTerminalCommandContextMessages(): ContextMessage[] {
             speaker: 'assistant',
             text: 'Assistant: To list all files in your current directory, you can use the following command: `ls`.',
         },
-        { speaker: 'human', text: 'Human: How do I check my current directory?' },
-        {
-            speaker: 'assistant',
-            text: 'Assistant: To check your current directory, you can use the following command: `pwd`.',
-        },
-        { speaker: 'human', text: 'Human: How do I create a new directory?' },
-        {
-            speaker: 'assistant',
-            text: 'Assistant: To create a new directory, you can use the following command: `mkdir <directory_name>`.',
-        },
         { speaker: 'human', text: 'Human: List all the files where $SYMBOL being used?' },
         {
             speaker: 'assistant',
             text: 'Assistant: To find where `$SYMBOL` is being used in your current directory and its subdirectories, you can use the following command: `grep -r "$SYMBOL" .`.',
-        },
-        { speaker: 'human', text: 'Human: How do I move a file?' },
-        {
-            speaker: 'assistant',
-            text: 'Assistant: To move a file, you can use the following command: `mv <source> <destination>`.',
-        },
-        { speaker: 'human', text: 'Human: How do I see disk usage?' },
-        {
-            speaker: 'assistant',
-            text: 'Assistant: To check disk usage, you can use the following command: `df -h`.',
         },
     ]
 }
