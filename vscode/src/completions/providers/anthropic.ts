e1824456441cd44af92e27f70eefeea48a6695a2
Beatrix <beatrix@sourcegraph.com>
2023-10-02 15:32:06 -0700

    fix: preamble leak for anthropic

--- vscode/src/completions/providers/anthropic.ts
+++ vscode/src/completions/providers/anthropic.ts
@@ -85,8 +85,7 @@ export class AnthropicProvider extends Provider {
             },
             {
                 speaker: 'human',
-                text: `Below is the code from file path ${this.options.fileName}. Review the code outside the XML tags to detect the functionality, formats, style, patterns, and logics in use. Then, use what you detect and reuse methods/libraries to complete and enclose completed code only inside XML tags precisely without duplicating existing implementations.
-                Here is the code: ${infillPrefix}${OPENING_CODE_TAG}${CLOSING_CODE_TAG}${infillSuffix}`,
+                text: `Below is the code from file path ${this.options.fileName}. Review the code outside the XML tags to detect the functionality, formats, style, patterns, and logics in use. Then, use what you detect and reuse methods/libraries to complete and enclose completed code only inside XML tags precisely without duplicating existing implementations. Here is the code: ${infillPrefix}${OPENING_CODE_TAG}${CLOSING_CODE_TAG}${infillSuffix} \n<--End of code-->`,
             },
             {
                 speaker: 'assistant',
