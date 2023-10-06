2375d8a9e42c92237985ccb97904fbee8dd3c5c8
Danny Tuppeny <danny@tuppeny.com>
2023-10-06 14:59:51 +0100

    Add more tests

--- agent/src/vscode-shim.test.ts
+++ agent/src/vscode-shim.test.ts
@@ -5,12 +5,27 @@ import * as vscode from 'vscode'
 
 describe('vscode-shim', () => {
     describe('vscode.Uri', () => {
-        it('static joinPath() method is available', () => {
-            // Ensure the static methods like joinPath are available when using the shim.
+        it('static file() is available', () => {
+            assert.equal(vscode.Uri.file('a.txt').toString(), 'file:///a.txt')
+        })
+
+        it('static from() is available', () => {
+            assert.equal(vscode.Uri.from({ scheme: 'file://', path: '/a.txt' }).toString(), 'file:///a.txt')
+        })
+
+        it('static joinPath() is available', () => {
             assert.equal(
                 vscode.Uri.joinPath(vscode.Uri.parse('http://example.org'), 'one', 'two').toString(),
-                vscode.Uri.parse('http://example.org/one/two').toString()
+                'http://example.org/one/two'
             )
+        })
+
+        it('static parse() is available', () => {
+            assert.equal(vscode.Uri.parse('http://example.org').toString(), 'http://example.org')
+        })
+
+        it('fsPath is available', () => {
+            assert.equal(vscode.Uri.file('/a.txt').fsPath, 'file:///a.txt')
         })
 
         it('instanceof can be used', () => {
