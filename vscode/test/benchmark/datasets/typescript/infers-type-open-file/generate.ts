f568b58cbc900f19016306a25861b1df465aa80e
Taras Yemets <yemets.taras@gmail.com>
2023-10-06 12:31:23 +0300

    wip: add typescript dataset

---
+++ vscode/test/benchmark/datasets/typescript/infers-type-open-file/generate.ts
@@ -0,0 +1,12 @@
+import { Equipment, Sport } from './types'
+
+export function getEquipment(sport: Sport): Equipment {
+    switch (sport) {
+        case 'football':
+            return 'ball'
+        case 'hockey':
+            ◆
+        default:
+            throw new Error(`Unknown operation: ${sport}`)
+    }
+}
