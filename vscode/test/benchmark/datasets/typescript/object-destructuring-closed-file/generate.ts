ea338909fce72ce0d8f51e472170ed693a8e764b
Taras Yemets <yemets.taras@gmail.com>
2023-10-09 12:49:38 +0300

    fix closed files configs

--- vscode/test/benchmark/datasets/typescript/object-destructuring-closed-file/generate.ts
+++ vscode/test/benchmark/datasets/typescript/object-destructuring-closed-file/generate.ts
@@ -1,6 +1,5 @@
 import { Person } from './types'
 
-// TODO: Figure out why it throws "generate.ts(1,24): error TS2307: Cannot find module './types' or its corresponding type declarations." when completion is correct
 export function getSummary(person: Person): string {
     const █
     return `User ${name}, ${age} years old, was created on ${new Date(createdAt).toLocaleDateString()}.`
