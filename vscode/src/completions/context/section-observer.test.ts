5e1451097f489934532818555c58ced7f23e4190
Philipp Spiess <hello@philippspiess.com>
2023-10-10 18:19:39 +0200

    Fix linter issue

--- vscode/src/completions/context/section-observer.test.ts
+++ vscode/src/completions/context/section-observer.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expect, it, Mock, vi, vitest } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, Mock, vitest } from 'vitest'
 import { URI } from 'vscode-uri'
 
 import { range } from '../../testutils/textDocument'
