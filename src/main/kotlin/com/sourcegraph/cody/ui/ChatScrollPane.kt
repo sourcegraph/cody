3d31e86de8b17318cca57f1e1643062157959098
Jakub Błach <jakubblach@gmail.com>
2023-10-12 14:40:56 +0200

    Prevent auto-scroll in chat while reading previous messages (#10)
    Fixes #56494

--- src/main/kotlin/com/sourcegraph/cody/ui/ChatScrollPane.kt
+++ src/main/kotlin/com/sourcegraph/cody/ui/ChatScrollPane.kt
@@ -1,42 +1,38 @@
 package com.sourcegraph.cody.ui
 
 import com.intellij.ui.components.JBScrollPane
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
-import java.util.concurrent.atomic.AtomicBoolean
+import com.intellij.ui.components.JBViewport
+import java.awt.Point
+import java.awt.Rectangle
 import javax.swing.BorderFactory
 import javax.swing.JPanel
-import javax.swing.JScrollBar
+import kotlin.math.max
 
-class ChatScrollPane(messagesPanel: JPanel?) :
+class ChatScrollPane(private val messagesPanel: JPanel) :
     JBScrollPane(messagesPanel, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_NEVER) {
-  private val wasMouseWheelScrolled = AtomicBoolean(false)
-  private val wasScrollBarDragged = AtomicBoolean(false)
+
+  private var touchingBottom = false
 
   init {
-    setBorder(BorderFactory.createEmptyBorder())
-    // this hack allows us to guess if an adjustment event isn't caused by the user mouse wheel
-    // scrolling and drag scrolling;
-    // we need to do that, as AdjustmentEvent doesn't account for the mouse wheel/drag scroll,
-    // and we don't want to rob the user of those
-    addMouseWheelListener { wasMouseWheelScrolled.set(true) }
-    // Scroll all the way down after each message
-    val chatPanelVerticalScrollBar = getVerticalScrollBar()
-    chatPanelVerticalScrollBar.addMouseListener(
-        object : MouseAdapter() {
-          override fun mousePressed(e: MouseEvent) = wasScrollBarDragged.set(true)
-
-          override fun mouseReleased(e: MouseEvent) = wasScrollBarDragged.set(false)
-        })
-    chatPanelVerticalScrollBar.addAdjustmentListener {
-      (it.source as? JScrollBar)?.model?.let { brm ->
-        if (!brm.valueIsAdjusting &&
-            !wasMouseWheelScrolled.getAndSet(false) &&
-            !wasScrollBarDragged.getAndSet(false) &&
-            brm.value + brm.extent != brm.maximum) {
-          brm.value = brm.maximum
-        }
-      }
+    border = BorderFactory.createEmptyBorder()
+    verticalScrollBar.addAdjustmentListener { change ->
+      val distance = change.value - verticalScrollBar.maximum + verticalScrollBar.visibleAmount
+      touchingBottom = distance == 0
     }
+    setViewport(
+        object : JBViewport() {
+
+          override fun scrollRectToVisible(bounds: Rectangle?) {}
+
+          override fun setViewPosition(point: Point) {
+            if (touchingBottom) {
+              val maxHeight = max(0, messagesPanel.height - height)
+              super.setViewPosition(Point(0, maxHeight))
+            } else {
+              super.setViewPosition(point)
+            }
+          }
+        })
+    setViewportView(messagesPanel)
   }
 }
