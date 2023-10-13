466ddfbef450f4e9e589bac729e049b09b6892ca
Philipp Spiess <hello@philippspiess.com>
2023-10-13 16:06:04 +0200

    Swap out custom levenshtein implmentation for an npm package

--- vscode/src/completions/text-processing/string-comparator.ts
+++ vscode/src/completions/text-processing/string-comparator.ts
@@ -1,3 +1,5 @@
+import levenshtein from 'js-levenshtein'
+
 /**
  * Compares string by editing distance algorithm, returns true
  * whenever string are almost the same, means that strings are
@@ -43,49 +45,5 @@ export const isAlmostTheSameString = (stringA: string, stringB: string, percenta
  * - Insert at index 9 (cost = 5)
  */
 export function LevenshteinCompare(A: string, B: string): number {
-    // Invariants and common cases checks
-    if (A === B) {
-        return 0
-    }
-
-    if (A.length === 0) {
-        return B.length
-    }
-
-    if (B.length === 0) {
-        return A.length
-    }
-
-    // Edit matrix by (B length + 1) on (A length + 1)
-    const matrix: number[][] = []
-
-    // Populate matrix with initial values
-    // prefill first column with symbol indexes
-    for (let m = 0; m <= B.length; m++) {
-        matrix[m] = []
-
-        if (m === 0) {
-            matrix[0] = A.split('').map((_, index) => index)
-        } else {
-            matrix[m][0] = m
-        }
-    }
-
-    for (let n = 1; n <= B.length; ++n) {
-        for (let m = 1; m <= A.length; ++m) {
-            if (B[n - 1] === A[m - 1]) {
-                // Symbols are equal the minimal number of operations
-                // stays the same as it was on previous step
-                matrix[n][m] = matrix[n - 1][m - 1]
-            } else {
-                const deletion = matrix[n - 1][m]
-                const insertion = matrix[n][m - 1]
-                const substitution = matrix[n - 1][m - 1]
-
-                matrix[n][m] = Math.min(deletion, insertion, substitution) + 1
-            }
-        }
-    }
-
-    return matrix[B.length][A.length]
+    return levenshtein(A, B)
 }
