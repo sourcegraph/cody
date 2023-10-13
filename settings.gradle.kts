c0c5530d84f974e5ed4d11057481323cc46da766
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-10-13 13:49:56 +0200

    Turn on CI caching

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,7 @@ rootProject.name = "Sourcegraph"
 include(":jetbrains-shared")
 
 project(":jetbrains-shared").projectDir = file("../jetbrains-shared")
+
+val isCiServer = System.getenv().containsKey("CI")
+
+buildCache { local { isEnabled = !isCiServer } }
