b1bf64c3894f65f8e3d440d31eb069f7d6d05a36
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-10-13 15:16:33 +0200

    Use x86 on macOS regardless of CPU architecture
    This introduces a performance overhead from Rosetta but it should at
    least unblock us for now.

--- src/main/java/com/sourcegraph/cody/agent/CodyAgent.kt
+++ src/main/java/com/sourcegraph/cody/agent/CodyAgent.kt
@@ -216,7 +216,9 @@ class CodyAgent(private val project: Project) : Disposable {
 
     private fun agentBinaryName(): String {
       val os = if (SystemInfoRt.isMac) "macos" else if (SystemInfoRt.isWindows) "win" else "linux"
-      val arch = if (CpuArch.isArm64()) "arm64" else "x64"
+      // Only use x86 for macOS because of this issue here https://github.com/vercel/pkg/issues/2004
+      // TLDR; we're not able to run macos-arm64 binaries when they're created on ubuntu-latest
+      val arch = if (CpuArch.isArm64() && !SystemInfoRt.isMac) "arm64" else "x64"
       return "agent-" + os + "-" + arch + binarySuffix()
     }
 
