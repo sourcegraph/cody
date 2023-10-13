f318e2aca372e7f9ba27ba178660686b70daba0f
Tom Ross <tom@umpox.com>
2023-10-13 09:59:31 +0100

    Benchmark: Add error messages for env (#1389)

--- vscode/test/benchmark/vscode/env.ts
+++ vscode/test/benchmark/vscode/env.ts
@@ -1,8 +1,8 @@
 import assert from 'assert'
 
-assert(process.env.BENCHMARK_EXTENSION_ID)
-assert(process.env.BENCHMARK_WORKSPACE)
-assert(process.env.BENCHMARK_CONFIG_FILE)
+assert(process.env.BENCHMARK_EXTENSION_ID, 'BENCHMARK_EXTENSION_ID is required')
+assert(process.env.BENCHMARK_WORKSPACE, 'BENCHMARK_WORKSPACE is required')
+assert(process.env.BENCHMARK_CONFIG_FILE, 'BENCHMARK_CONFIG_FILE is required')
 
 export const BENCHMARK_EXTENSION_ID = process.env.BENCHMARK_EXTENSION_ID
 export const BENCHMARK_WORKSPACE = process.env.BENCHMARK_WORKSPACE
