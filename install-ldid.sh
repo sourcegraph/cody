eda3ffbcbaa2fd2233f7ec22ae4e1b44fca8461e
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-10-16 18:58:16 +0200

    Experiment with signing binaries in CI

---
+++ install-ldid.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Script to install `ldid2` on Linux computers to codesign the macos-arm64 binary for the agent.
+
+set -eux
+
+# Check if ldid is installed
+if command -v ldid &>/dev/null; then
+  echo "ldid is already installed."
+  exit 0
+fi
+
+curl -Lo ldid.zip https://github.com/xerub/ldid/archive/refs/heads/master.zip
+unzip ldid.zip
+cd ldid-master
+./make.sh
+cp ldid2 /usr/local/bin/
