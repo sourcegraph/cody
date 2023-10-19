efe99b8ac346991c37dd6b07970f00d3432c4abe
Olafur Geirsson <olafurpg@gmail.com>
2023-10-19 15:50:06 +0200

    Agent: exist process when stdout/stdin close
    We are seeing some cases for JetBrains team where the Cody agent process
    is still running after IntelliJ exits. I'm not sure if the diff in this
    PR fixes the problem but I figured it was worth a shot.

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -6,5 +6,8 @@ const agent = new Agent()
 
 console.log = console.error
 
+process.stdout.on('close', () => process.exit(1))
+process.stdin.on('close', () => process.exit(1))
+
 process.stdin.pipe(agent.messageDecoder)
 agent.messageEncoder.pipe(process.stdout)
