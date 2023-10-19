81231dffead6821704be4b5450de01bbd42a284a
Olafur Geirsson <olafurpg@gmail.com>
2023-10-19 16:11:38 +0200

    Add comment explaining motivation behind behavior

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -6,6 +6,12 @@ const agent = new Agent()
 
 console.log = console.error
 
+// Force the agent process to exit when stdin/stdout close as an attempt to
+// prevent zombie agent processes. We experienced this problem when we
+// forcefully exit the IntelliJ process during local `./gradlew :runIde`
+// workflows. We manually confirmed that this logic makes the agent exit even
+// when we forcefully quit IntelliJ
+// https://github.com/sourcegraph/cody/pull/1439#discussion_r1365610354
 process.stdout.on('close', () => process.exit(1))
 process.stdin.on('close', () => process.exit(1))
 
