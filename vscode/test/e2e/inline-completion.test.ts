870d00c9b993bed29f5ac3d18b5bb33a40f3997b
Danny Tuppeny <danny@tuppeny.com>
2023-10-20 12:38:11 +0100

    Add test

--- vscode/test/e2e/inline-completion.test.ts
+++ vscode/test/e2e/inline-completion.test.ts
@@ -5,16 +5,16 @@ import { loggedEvents, resetLoggedEvents } from '../fixtures/mock-server'
 import { sidebarExplorer, sidebarSignin } from './common'
 import { test } from './helpers'
 
-const expectedOrderedEvents = [
-    'CodyVSCodeExtension:completion:suggested',
-    'CodyVSCodeExtension:completion:accepted',
-    'CodyVSCodeExtension:completion:suggested',
-]
-
 test.beforeEach(() => {
     resetLoggedEvents()
 })
-test('shows completion onboarding notice on first completion accept', async ({ page, sidebar }) => {
+test('shows chat sidebar completion onboarding notice on first completion accept', async ({ page, sidebar }) => {
+    const expectedOrderedEvents = [
+        'CodyVSCodeExtension:completion:suggested',
+        'CodyVSCodeExtension:completion:accepted',
+        'CodyVSCodeExtension:completion:suggested',
+    ]
+
     const indexFile = page.getByRole('treeitem', { name: 'index.html' }).locator('a')
     const editor = page.locator('[id="workbench\\.parts\\.editor"]')
     const notice = sidebar.locator('.onboarding-autocomplete')
@@ -49,6 +49,54 @@ test('shows completion onboarding notice on first completion accept', async ({ p
     await acceptInlineCompletion(page)
     await expect(otherAcceptedCompletion).toBeVisible()
     await expect(notice).not.toBeVisible()
+    await expect.poll(() => loggedEvents.sort()).toEqual(expectedOrderedEvents.sort())
+})
+
+test('inline completion onboarding notice on first completion accept', async ({ page, sidebar }) => {
+    const expectedOrderedEvents = [
+        'CodyVSCodeExtension:completion:suggested',
+        'CodyVSCodeExtension:completion:accepted',
+        'CodyVSCodeExtension:completion:suggested', // Extra trigger from pressing 'a' to test hiding.
+        'CodyVSCodeExtension:completion:suggested',
+    ]
+
+    const indexFile = page.getByRole('treeitem', { name: 'index.html' }).locator('a')
+    const editor = page.locator('[id="workbench\\.parts\\.editor"]')
+    // The text in the decoration is part of the CSS rule (it's in :after) so we can't locate it
+    // directly, we just have to assume this is the only decoration in the editor during this test.
+    // If that assumption ceases to be the case, this test will fail on the initial check that it's
+    // not visible.
+    const decoration = editor.locator('css=span[class*="TextEditorDecorationType"]')
+
+    const firstAcceptedCompletion = editor.getByText('myFirstCompletion')
+    const otherAcceptedCompletion = editor.getByText('myNotFirstCompletion')
+
+    // Sign into Cody.
+    await sidebarSignin(page, sidebar)
+
+    // Open the index.html file from explorer.
+    await sidebarExplorer(page).click()
+    await indexFile.dblclick()
+
+    // Trigger inline-completion and ensure no notice (yet).
+    await triggerInlineCompletionInBody(page)
+    await expect(decoration).not.toBeVisible()
+
+    // Accept the completion and expect the text to be added and
+    // the notice to be shown.
+    await acceptInlineCompletion(page)
+    await expect(firstAcceptedCompletion).toBeVisible()
+    await expect(decoration).toBeVisible()
+
+    // Modify the document to hide the decoration.
+    await page.keyboard.press('a')
+    await expect(decoration).not.toBeVisible()
+
+    // Trigger/accept another completion, but don't expect the notification.
+    await triggerInlineCompletionInBody(page)
+    await acceptInlineCompletion(page)
+    await expect(otherAcceptedCompletion).toBeVisible()
+    await expect(decoration).not.toBeVisible()
     await expect.poll(() => loggedEvents.sort()).toEqual(expectedOrderedEvents.sort())
 })
 
