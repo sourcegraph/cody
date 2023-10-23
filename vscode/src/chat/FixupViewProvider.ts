de16ebe5d8f0d46e11fb71c87df17c0585e6329a
Tom Ross <tom@umpox.com>
2023-10-23 16:14:01 +0100

    add catch for no text

--- vscode/src/chat/FixupViewProvider.ts
+++ vscode/src/chat/FixupViewProvider.ts
@@ -98,6 +98,11 @@ export class FixupProvider extends MessageProvider {
             return
         }
 
+        // Error state: The transcript finished but we didn't receive any text
+        if (!lastMessage.text && !isMessageInProgress) {
+            this.handleError('Error: Cody did not respond with any text')
+        }
+
         if (lastMessage.text) {
             void this.editor.controllers.fixups?.didReceiveFixupText(
                 this.task.id,
