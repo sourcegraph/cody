6726975ca7cab26bd4fc1729f8f952f8ece6f8ce
Chris Warwick <christopher.warwick@sourcegraph.com>
2023-10-23 15:28:54 -0400

    openai prompt changes

--- vscode/src/completions/providers/unstable-openai.ts
+++ vscode/src/completions/providers/unstable-openai.ts
@@ -1,9 +1,11 @@
+import * as vscode from 'vscode'
+
 import { tokensToChars } from '@sourcegraph/cody-shared/src/prompt/constants'
 import { CompletionResponse } from '@sourcegraph/cody-shared/src/sourcegraph-api/completions/types'
 
 import { canUsePartialCompletion } from '../can-use-partial-completion'
 import { CodeCompletionsClient, CodeCompletionsParams } from '../client'
-import { getHeadAndTail } from '../text-processing'
+import { CLOSING_CODE_TAG, getHeadAndTail, MULTILINE_STOP_SEQUENCE, OPENING_CODE_TAG } from '../text-processing'
 import { parseAndTruncateCompletion } from '../text-processing/parse-and-truncate-completion'
 import { InlineCompletionItemWithAnalytics } from '../text-processing/process-inline-completions'
 import { ContextSnippet } from '../types'
@@ -17,6 +19,9 @@ import {
     standardContextSizeHints,
 } from './provider'
 
+const MULTI_LINE_STOP_SEQUENCES = [CLOSING_CODE_TAG]
+const SINGLE_LINE_STOP_SEQUENCES = [CLOSING_CODE_TAG, MULTILINE_STOP_SEQUENCE]
+
 interface UnstableOpenAIOptions {
     maxContextTokens?: number
     client: Pick<CodeCompletionsClient, 'complete'>
@@ -24,12 +29,11 @@ interface UnstableOpenAIOptions {
 
 const PROVIDER_IDENTIFIER = 'unstable-openai'
 const MAX_RESPONSE_TOKENS = 256
-const OPENING_CODE_TAG = '```'
-const CLOSING_CODE_TAG = '```'
 
 export class UnstableOpenAIProvider extends Provider {
     private client: Pick<CodeCompletionsClient, 'complete'>
     private promptChars: number
+    private instructions: string = `You are a code completion AI designed to take the surrounding code and shared context into account in order to predict and suggest high-quality code to complete the code enclosed in ${OPENING_CODE_TAG} tags.  You only respond with code that works and fits seamlessly with surrounding code do not include anything else beyond the code.`
 
     constructor(options: ProviderOptions, { maxContextTokens, client }: Required<UnstableOpenAIOptions>) {
         super(options)
@@ -37,27 +41,57 @@ export class UnstableOpenAIProvider extends Provider {
         this.client = client
     }
 
-    private createPrompt(snippets: ContextSnippet[]): string {
-        const { head, tail } = getHeadAndTail(this.options.docContext.prefix)
-        const intro: string[] = ['Human: You are a senior engineer assistant working on a codebase.']
-        let prompt = ''
+    public emptyPromptLength(): number {
+        const promptNoSnippets = [this.instructions, this.createPromptPrefix()].join('\n\n')
+        return promptNoSnippets.length - 10 // extra 10 chars of buffer cuz who knows
+    }
 
-        for (let snippetsToInclude = 0; snippetsToInclude < snippets.length + 1; snippetsToInclude++) {
-            if (snippetsToInclude > 0) {
-                const snippet = snippets[snippetsToInclude - 1]
-                intro.push(`File: ${snippet.fileName}\n${OPENING_CODE_TAG}\n${snippet.content}\n${CLOSING_CODE_TAG}`)
-            }
-            const introString = intro.join('\n\n')
-            const nextPrompt = `${introString}\n\nComplete the following code:\n\n${head.trimmed}\n\nAssistant:\n${tail.trimmed}`
-
-            if (nextPrompt.length >= this.promptChars) {
-                return prompt
-            }
-
-            prompt = nextPrompt
+    private createPromptPrefix(): string {
+        const prefixLines = this.options.docContext.prefix.split('\n')
+        if (prefixLines.length === 0) {
+            throw new Error('no prefix lines')
         }
 
-        return prompt
+        const { head, tail } = getHeadAndTail(this.options.docContext.prefix)
+
+        // Infill block represents the code we want the model to complete
+        const infillBlock = tail.trimmed
+        // code before the cursor, without the code extracted for the infillBlock
+        const infillPrefix = head.raw?.startsWith(tail.trimmed) ? '' : `${head.raw}`
+        // code after the cursor
+        const infillSuffix = this.options.docContext.suffix
+        const relativeFilePath = vscode.workspace.asRelativePath(this.options.document.fileName)
+
+        return `Below is the code from file path ${relativeFilePath}. Review the code outside the XML tags to detect the functionality, formats, style, patterns, and logics in use. Then, use what you detect and reuse methods/libraries to complete and enclose completed code only inside XML tags precisely without duplicating existing implementations. Here is the code:\n\`\`\`\n${infillPrefix}${OPENING_CODE_TAG}${infillBlock}${CLOSING_CODE_TAG}${infillSuffix}\n\`\`\`
+
+${OPENING_CODE_TAG}${infillBlock}`
+    }
+
+    // Creates the resulting prompt and adds as many snippets from the reference
+    // list as possible.
+    protected createPrompt(snippets: ContextSnippet[]): string {
+        const prefix = this.createPromptPrefix()
+
+        const referenceSnippetMessages: string[] = []
+
+        let remainingChars = this.promptChars - this.emptyPromptLength()
+
+        for (const snippet of snippets) {
+            const snippetMessages: string[] = [
+                'symbol' in snippet && snippet.symbol !== ''
+                    ? `Additional documentation for \`${snippet.symbol}\`: ${OPENING_CODE_TAG}${snippet.content}${CLOSING_CODE_TAG}`
+                    : `Codebase context from file path '${snippet.fileName}': ${OPENING_CODE_TAG}${snippet.content}${CLOSING_CODE_TAG}`,
+            ]
+            const numSnippetChars = snippetMessages.join(`\n\n`).length + 1
+            if (numSnippetChars > remainingChars) {
+                break
+            }
+            referenceSnippetMessages.push(...snippetMessages)
+            remainingChars -= numSnippetChars
+        }
+
+        const messages = [this.instructions, ...referenceSnippetMessages, prefix]
+        return messages.join('\n\n')
     }
 
     public async generateCompletions(
@@ -67,17 +101,12 @@ export class UnstableOpenAIProvider extends Provider {
     ): Promise<InlineCompletionItemWithAnalytics[]> {
         const prompt = this.createPrompt(snippets)
 
-        const stopSequences = ['Human:', 'Assistant:']
-        if (!this.options.multiline) {
-            stopSequences.push('\n')
-        }
-
         const args: CodeCompletionsParams = {
             messages: [{ speaker: 'human', text: prompt }],
             maxTokensToSample: this.options.multiline ? MAX_RESPONSE_TOKENS : 50,
             temperature: 1,
             topP: 0.5,
-            stopSequences,
+            stopSequences: this.options.multiline ? MULTI_LINE_STOP_SEQUENCES : SINGLE_LINE_STOP_SEQUENCES,
             timeoutMs: this.options.multiline ? 15000 : 5000,
         }
 
@@ -131,6 +160,12 @@ export class UnstableOpenAIProvider extends Provider {
     }
 
     private postProcess(content: string): string {
+        if (content.startsWith('```')) {
+            let arr = content.split('\n')
+            arr.shift()
+            content = arr.join('\n')
+        }
+
         return content.trimEnd()
     }
 }
