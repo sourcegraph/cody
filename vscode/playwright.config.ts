9c969cb45eaf492480709d90a1c68b336346ea94
Danny Tuppeny <danny@tuppeny.com>
2023-10-27 11:24:44 +0100

    TEMP: Capture playwright videos on failures
    Since merging main, this bot fails on macOS (but not Ubuntu or Windows).. This is temporary to try to understand what's happening.

--- vscode/playwright.config.ts
+++ vscode/playwright.config.ts
@@ -5,4 +5,8 @@ export default defineConfig({
     // Give failing tests a second chance
     retries: 2,
     testDir: 'test/e2e',
+    use: {
+        screenshot: 'only-on-failure',
+        video: 'on-first-retry',
+    },
 })
