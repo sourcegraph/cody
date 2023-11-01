9699225894cffdec4adf87c7c5f606661d2ddab0
Taras Yemets <yemets.taras@gmail.com>
2023-11-01 15:57:38 +0200

    add comment

--- vscode/test/benchmark/datasets/typescript/fs-write-file/generate.ts
+++ vscode/test/benchmark/datasets/typescript/fs-write-file/generate.ts
@@ -1,5 +1,6 @@
 import fs from 'fs/promises'
 
+// Updates the logs file with the new logs.
 export function updateLogs(logs: string[], separator: string, path: string): Promise<void> {
     █
 }
--- vscode/test/benchmark/datasets/typescript/fs-write-file/solution.ts
+++ vscode/test/benchmark/datasets/typescript/fs-write-file/solution.ts
@@ -1,5 +1,6 @@
 import fs from 'fs/promises'
 
+// Updates the logs file with the new logs.
 export function updateLogs(logs: string[], separator: string, path: string): Promise<void> {
     return fs.appendFile(path, logs.join(separator))
 }
