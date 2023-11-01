148fc1acfdf869fc5c79f19ed834160a080fa40e
Beatrix <beatrix@sourcegraph.com>
2023-11-01 13:14:02 -0700

    use object destructuring instead of spread

--- vscode/src/chat/MessageProvider.ts
+++ vscode/src/chat/MessageProvider.ts
@@ -423,7 +423,7 @@ export abstract class MessageProvider extends MessageHandler implements vscode.D
             }
         }
 
-        const args = { ...contextSummary, ...{ request_id } }
+        const args = { ...contextSummary, request_id }
         telemetryService.log(`CodyVSCodeExtension:recipe:${recipe.id}:executed`, args, { hasV2Event: true })
         telemetryRecorder.recordEvent(`cody.recipe.${recipe.id}`, 'executed', { metadata: { ...args } })
     }
