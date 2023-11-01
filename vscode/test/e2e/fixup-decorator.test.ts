3393fb751be3303c548b7122d41961fd56020eba
Tom Ross <tom@umpox.com>
2023-11-01 14:17:07 +0000

    E2E: Skip flaky decoration test (#1578)
    ## Description
    
    This test is very flaky as we have a race condition:
    - The mock server returns the response instantly, but we have a
    typewriter effect which currently delays the response slightly.
    - This delay can sometimes mean the decorations are visible, and
    sometimes not.
    
    This PR proposes removing the typewriter effect on response complete:
    https://github.com/sourcegraph/cody/pull/1561 Further investigation will
    be done there as we have quite a few racey tests that have been
    uncovered further by this change.
    
    ## Test plan
    
    E2E tests pass
    
    <!-- Required. See
    https://docs.sourcegraph.com/dev/background-information/testing_principles.
    -->

--- vscode/test/e2e/fixup-decorator.test.ts
+++ vscode/test/e2e/fixup-decorator.test.ts
@@ -17,7 +17,9 @@ const expectedOrderedEvents = [
 test.beforeEach(() => {
     resetLoggedEvents()
 })
-test('decorations from un-applied Cody changes appear', async ({ page, sidebar }) => {
+
+// TODO: Fix flaky test due to typewriter delay: https://github.com/sourcegraph/cody/pull/1578
+test.skip('decorations from un-applied Cody changes appear', async ({ page, sidebar }) => {
     // Sign into Cody
     await sidebarSignin(page, sidebar)
 
