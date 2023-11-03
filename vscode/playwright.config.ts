f0f4f51d9f6adf05e96c1b1ea52602f89ee3b76d
Tom Ross <tom@umpox.com>
2023-11-03 15:51:26 +0000

    rm test code

--- vscode/playwright.config.ts
+++ vscode/playwright.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
     workers: 1,
     // Give failing tests a second chance
-    retries: 0,
+    retries: 2,
     testDir: 'test/e2e',
 })
