402f6ca1e89368737458b07f561796b0a9e340c3
Beyang Liu <beyang.liu@gmail.com>
2023-11-03 16:05:46 -0700

    fix e2e test

--- vscode/test/e2e/helpers.ts
+++ vscode/test/e2e/helpers.ts
@@ -62,7 +62,9 @@ export const test = base
             // Bring the cody sidebar to the foreground
             await page.click('[aria-label="Cody"]')
             // Ensure that we remove the hover from the activity icon
-            await page.getByRole('heading', { name: 'Cody: Chat' }).hover()
+            await page.getByRole('heading', { name: 'Cody' }).hover()
+            // Minimize the search panel for now
+            await page.getByRole('heading', { name: 'Search' }).click()
             // Wait for Cody to become activated
             // TODO(philipp-spiess): Figure out which playwright matcher we can use that works for
             // the signed-in and signed-out cases
