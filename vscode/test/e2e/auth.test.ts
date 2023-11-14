f3aab51d604c41278ab8ab2b0bc80078c908d882
Tim Lucas <t@toolmantim.com>
2023-11-14 22:06:24 +1100

    Clean up login page styles (#1708)
    * Moves the Enterprise sign in link up under the social login links
    * Makes the button logos monochrome
    * Updates the window sizing and button width behaviour to match standard
    VS Code behaviour
    
    | Before | After |
    | - | - |
    | <img width="1253" alt="Screenshot 2023-11-10 at 2 37 12 pm"
    src="https://github.com/sourcegraph/cody/assets/153/3591da87-5990-4f17-ac0e-f1ea5a45b878">
    | <img width="1253" alt="Screenshot 2023-11-10 at 2 36 59 pm"
    src="https://github.com/sourcegraph/cody/assets/153/aab2f046-b214-408c-a8a5-b0621e84ba96">
    |
    
    ## Test plan
    
    - Loaded extension
    - Previewed widths and clicked buttons

--- vscode/test/e2e/auth.test.ts
+++ vscode/test/e2e/auth.test.ts
@@ -12,7 +12,7 @@ test.beforeEach(() => {
 
 test('requires a valid auth token and allows logouts', async ({ page, sidebar }) => {
     await expect(page.getByText('Authentication failed.')).not.toBeVisible()
-    await sidebar.getByRole('button', { name: 'Sign In to Enterprise Instance' }).click()
+    await sidebar.getByRole('button', { name: 'Sign In to Your Enterprise Instance' }).click()
     await page.getByRole('option', { name: 'Sign in with URL and Access Token' }).click()
     await page.getByRole('combobox', { name: 'input' }).fill(SERVER_URL)
     await page.getByRole('combobox', { name: 'input' }).press('Enter')
@@ -21,7 +21,7 @@ test('requires a valid auth token and allows logouts', async ({ page, sidebar })
 
     await expect(page.getByRole('alert').getByText('Authentication failed.')).toBeVisible()
 
-    await sidebar.getByRole('button', { name: 'Sign In to Enterprise Instance' }).click()
+    await sidebar.getByRole('button', { name: 'Sign In to Your Enterprise Instance' }).click()
     await page.getByRole('option', { name: 'Sign in with URL and Access Token' }).click()
     await page.getByRole('combobox', { name: 'input' }).fill(SERVER_URL)
     await page.getByRole('combobox', { name: 'input' }).press('Enter')
@@ -39,7 +39,7 @@ test('requires a valid auth token and allows logouts', async ({ page, sidebar })
     // Sign out.
     await signOut(page)
 
-    await expect(sidebar.getByRole('button', { name: 'Sign In to Enterprise Instance' })).toBeVisible()
+    await expect(sidebar.getByRole('button', { name: 'Sign In to Your Enterprise Instance' })).toBeVisible()
     await expect(sidebar.getByText('Invalid credentials')).not.toBeVisible()
     await assertEvents(loggedEvents, expectedEvents)
 })
