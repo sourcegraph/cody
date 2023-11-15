a1472945a673b37be1fa25ecd8cf3e3f64b785fe
Tom Ross <tom@umpox.com>
2023-11-15 14:13:57 +0000

    Add generate code action

--- vscode/src/code-actions/edit.ts
+++ vscode/src/code-actions/edit.ts
@@ -6,21 +6,30 @@ export class EditCodeAction implements vscode.CodeActionProvider {
     public provideCodeActions(document: vscode.TextDocument): vscode.CodeAction[] {
         const selection = vscode.window.activeTextEditor?.selection
         if (!selection || selection.isEmpty) {
-            // Do nothing
-            return []
+            return [this.createCommandCodeAction(document, 'Ask Cody to Generate')]
         }
 
-        return [this.createCommandCodeAction(document, selection)]
+        return [this.createCommandCodeAction(document, 'Ask Cody to Edit', selection)]
     }
 
-    private createCommandCodeAction(document: vscode.TextDocument, selection: vscode.Selection): vscode.CodeAction {
-        const action = new vscode.CodeAction('Ask Cody to Edit', vscode.CodeActionKind.RefactorRewrite)
+    private createCommandCodeAction(
+        document: vscode.TextDocument,
+        displayText: string,
+        selection?: vscode.Selection
+    ): vscode.CodeAction {
+        const action = new vscode.CodeAction(displayText, vscode.CodeActionKind.RefactorRewrite)
         const source = 'code-action'
-        const range = new vscode.Range(selection.start, selection.end)
         action.command = {
             command: 'cody.command.edit-code',
-            arguments: [{ range, intent: 'edit', document }, source],
-            title: 'Ask Cody to Edit',
+            arguments: [
+                {
+                    range: selection ? new vscode.Range(selection.start, selection.end) : undefined,
+                    intent: 'edit',
+                    document,
+                },
+                source,
+            ],
+            title: displayText,
         }
         return action
     }
