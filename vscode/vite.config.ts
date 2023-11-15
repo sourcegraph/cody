c2799f99d89693aaec11def625096c86f5aa3c47
Tim Lucas <t@toolmantim.com>
2023-11-16 10:40:27 +1100

    Add hanging-process

--- vscode/vite.config.ts
+++ vscode/vite.config.ts
@@ -6,5 +6,18 @@ export default defineConfig({
     test: {
         include: ['src/**/*.test.ts?(x)'],
         setupFiles: ['src/testutils/vscode.ts'],
-    },
+        reporters: [
+            'default',
+            // fixme(toolmantim): Remove this when flakiness is gone
+            //
+            // Enable the hanging-process reporter to try to spot why sometimes unit tests are timing out on exit:
+            //   close timed out after 10000ms
+            //   Failed to terminate worker while running /home/runner/work/cody/cody/lib/shared/src/common/markdown/markdown.test.ts.
+            //   Tests closed successfully but something prevents Vite server from exiting
+            //   You can try to identify the cause by enabling "hanging-process" reporter. See https://vitest.dev/config/#reporters
+            //
+            // https://vitest.dev/guide/reporters.html#hanging-process-reporter
+            'hanging-process'
+        ]
+    }
 })
