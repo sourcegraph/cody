f42f6c627f7ebfc735ec753ad6f558bc9e0bba2d
Beatrix <beatrix@sourcegraph.com>
2023-11-17 13:11:18 -0800

    Update EnhancedContextToggler

--- vscode/webviews/Components/EnhancedContextToggler.tsx
+++ vscode/webviews/Components/EnhancedContextToggler.tsx
@@ -21,6 +21,8 @@ export const EnhancedContextToggler: React.FunctionComponent<{
         [setEnhanceContext]
     )
 
+    const codebaseName = contextStatus.codebase ?? 'a local codebase'
+
     return (
         <div className={styles.container}>
             {open && (
@@ -32,8 +34,11 @@ export const EnhancedContextToggler: React.FunctionComponent<{
                     >
                         Enhanced Context ✨
                     </VSCodeCheckbox>
-                    <p className={styles.enhancedContextHelpText}>
-                        Automatically include additional context from your codebase {contextStatus.codebase}
+                    <p className={styles.enhancedContextDescription}>
+                        Automatically include additional context from your codebase.
+                    </p>
+                    <p className={styles.codebaseConnection}>
+                        Connected to <span className={contextStatus.codebase && styles.codebase}>{codebaseName}</span>
                     </p>
                 </div>
             )}
