80795625d9be5abe3d5748fc0e3b7b9d3fd94f7b
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-11-21 19:17:24 +0100

    Agent: add option to emit CSV file for `evaluate-autocomplete` (#1847)
    Previously, `evaluate-autocomplete` only generated snapshots to process
    the results. This PR adds a new CSV writer so that you can
    process/analyze the results with CSV tooling like xsv.
    
    ## Test plan
    
    Ran large scale evaluation with the command
    ```
    ❯ pnpm agent evaluate-autocomplete --evaluation-config ~/dev/sourcegraph/bfg-evaluation/evaluation.json --test-count 400
    ```
    <!-- Required. See
    https://docs.sourcegraph.com/dev/background-information/testing_principles.
    -->

---
+++ agent/src/cli/evaluate-autocomplete/strategy-bfg.ts
@@ -0,0 +1,142 @@
+import { execSync } from 'child_process'
+import * as fspromises from 'fs/promises'
+import * as path from 'path'
+
+import { createObjectCsvWriter } from 'csv-writer'
+import { CsvWriter } from 'csv-writer/src/lib/csv-writer'
+import { rimraf } from 'rimraf'
+import * as vscode from 'vscode'
+
+import { getParseLanguage } from '../../../../vscode/src/tree-sitter/grammars'
+import { createParser } from '../../../../vscode/src/tree-sitter/parser'
+import { MessageHandler } from '../../jsonrpc-alias'
+import { getLanguageForFileName } from '../../language'
+
+import { AutocompleteDocument, autocompleteItemHeaders } from './AutocompleteDocument'
+import { EvaluateAutocompleteOptions, matchesGlobPatterns } from './evaluate-autocomplete'
+import { Queries } from './Queries'
+import { triggerAutocomplete } from './triggerAutocomplete'
+
+/**
+ * Runs autocomplete evaluation. The current logic is specifically optimized
+ * to evaluate BFG.  The best way to customize the logic is by changing the
+ * code. Eventually, we could make the logic configurable via command-line
+ * flags so that we can reuse this command for different kinds of evaluations.
+ */
+export async function evaluateBfgStrategy(
+    client: MessageHandler,
+    options: EvaluateAutocompleteOptions,
+    workspace: string
+): Promise<void> {
+    const queries = new Queries(options.queriesDirectory)
+    const grammarDirectory = path.normalize(options.treeSitterGrammars)
+    const files = execSync('git ls-files', { cwd: workspace }).toString().split('\n')
+    files.sort()
+    let remainingTests = options.testCount
+    let csvWriter: CsvWriter<any> | undefined
+    if (options.snapshotDirectory) {
+        await rimraf(options.snapshotDirectory)
+        await fspromises.mkdir(options.snapshotDirectory, { recursive: true })
+        if (options.csvPath) {
+            csvWriter = createObjectCsvWriter({
+                header: autocompleteItemHeaders,
+                path: options.csvPath,
+            })
+        }
+    }
+    for (const file of files) {
+        if (!matchesGlobPatterns(options.includeFilepath ?? [], options.excludeFilepath ?? [], file)) {
+            continue
+        }
+        const filePath = path.join(workspace, file)
+        const stat = await fspromises.stat(filePath)
+        if (!stat.isFile()) {
+            continue
+        }
+        const content = (await fspromises.readFile(filePath)).toString()
+        const languageid = getLanguageForFileName(file)
+        const language = getParseLanguage(languageid)
+        if (!language) {
+            continue
+        }
+        client.notify('textDocument/didOpen', { filePath, content })
+        const parser = await createParser({ language, grammarDirectory })
+        const tree = parser.parse(content)
+        const query = await queries.loadQuery(parser, language, 'context')
+        if (!query) {
+            continue
+        }
+
+        const document = new AutocompleteDocument(
+            {
+                languageid,
+                filepath: file,
+                strategy: options.fixture.strategy,
+                fixture: options.fixture.name,
+                workspace: path.basename(options.workspace),
+            },
+            content
+        )
+        for (const match of query.matches(tree.rootNode)) {
+            if (remainingTests <= 0) {
+                break
+            }
+            for (const capture of match.captures) {
+                if (remainingTests <= 0) {
+                    break
+                }
+                if (capture.name === 'range') {
+                    // Modify the content by replacing the argument list to the call expression
+                    // with an empty argument list. This evaluation is interesting because it
+                    // allows us to test how good Cody is at inferring the original argument
+                    // list.
+
+                    const isArgumentList =
+                        content.slice(capture.node.startIndex, capture.node.startIndex + 1) === '(' &&
+                        content.slice(capture.node.endIndex - 1, capture.node.endIndex) === ')'
+                    const range = isArgumentList
+                        ? new vscode.Range(
+                              new vscode.Position(
+                                  capture.node.startPosition.row,
+                                  capture.node.startPosition.column + 1
+                              ),
+                              new vscode.Position(capture.node.endPosition.row, capture.node.endPosition.column - 1)
+                          )
+                        : new vscode.Range(
+                              new vscode.Position(capture.node.startPosition.row, capture.node.startPosition.column),
+                              new vscode.Position(capture.node.endPosition.row, capture.node.endPosition.column)
+                          )
+
+                    const modifiedContent = [
+                        document.textDocument.getText(new vscode.Range(new vscode.Position(0, 0), range.start)),
+                        document.textDocument.getText(
+                            new vscode.Range(range.end, new vscode.Position(document.textDocument.lineCount, 0))
+                        ),
+                    ].join('')
+                    const removedContent = document.textDocument.getText(range)
+                    const position = new vscode.Position(
+                        capture.node.startPosition.row,
+                        capture.node.startPosition.column + 1
+                    )
+                    await triggerAutocomplete({
+                        range,
+                        client,
+                        document,
+                        emptyMatchContent: isArgumentList ? '()' : '',
+                        modifiedContent,
+                        removedContent,
+                        position,
+                    })
+                    remainingTests--
+                }
+            }
+        }
+
+        if (options.snapshotDirectory && document.items.length > 0) {
+            await document.writeSnapshot(options.snapshotDirectory)
+            if (csvWriter) {
+                await csvWriter.writeRecords(document.items)
+            }
+        }
+    }
+}
---
+++ agent/src/cli/evaluate-autocomplete/triggerAutocomplete.ts
@@ -0,0 +1,88 @@
+import { calcPatch } from 'fast-myers-diff'
+import * as vscode from 'vscode'
+
+import { AgentTextDocument } from '../../AgentTextDocument'
+import { MessageHandler } from '../../jsonrpc-alias'
+import { AutocompleteResult } from '../../protocol-alias'
+
+import { AutocompleteDocument } from './AutocompleteDocument'
+
+export interface AutocompleteParameters {
+    client: MessageHandler
+    document: AutocompleteDocument
+
+    range: vscode.Range
+    modifiedContent: string
+    removedContent: string
+    position: vscode.Position
+    emptyMatchContent: string
+}
+
+export async function triggerAutocomplete(parameters: AutocompleteParameters): Promise<void> {
+    const { range, client, document, modifiedContent, removedContent, position, emptyMatchContent } = parameters
+
+    client.notify('textDocument/didChange', { filePath: document.params.filepath, content: modifiedContent })
+    let result: AutocompleteResult
+    try {
+        result = await client.request('autocomplete/execute', {
+            filePath: document.params.filepath,
+            position,
+            // We don't use the "automatic" trigger to avoid certain code paths like
+            // synthetic latency when acceptance rate is low.
+            triggerKind: 'Invoke',
+        })
+    } catch (error) {
+        const resultError = error instanceof Error ? error.message : String(error)
+        document.pushItem({
+            range,
+            resultError,
+        })
+        return
+    }
+
+    const didNotSendNetworkRequest =
+        result.items.length === 0 && result.completionEvent?.networkRequestStartedAt === null
+    if (didNotSendNetworkRequest) {
+        return
+    }
+
+    const textDocument = new AgentTextDocument({ filePath: document.params.filepath, content: modifiedContent })
+    for (const item of result.items) {
+        const original = textDocument.getText(
+            new vscode.Range(
+                item.range.start.line,
+                item.range.start.character,
+                item.range.end.line,
+                item.range.end.character
+            )
+        )
+        const completion = item.insertText
+        const patches: string[] = []
+        let hasNonInsertPatch = false
+        for (const [sx, ex, text] of calcPatch(original, completion)) {
+            if (sx !== ex) {
+                hasNonInsertPatch = true
+                continue
+            }
+            patches.push(text)
+        }
+        if (hasNonInsertPatch) {
+            document.pushItem({ resultText: item.insertText, range, resultNonInsertPatch: true })
+        } else if (patches.length > 0) {
+            const text = patches.join('')
+            if (text === removedContent) {
+                document.pushItem({ range, resultExact: true })
+            } else {
+                document.pushItem({ range, resultText: text })
+            }
+        }
+    }
+    if (result.items.length === 0) {
+        const expectedEmptyMatch = removedContent === emptyMatchContent
+        document.pushItem({
+            range,
+            resultExact: expectedEmptyMatch,
+            resultEmpty: !expectedEmptyMatch,
+        })
+    }
+}
