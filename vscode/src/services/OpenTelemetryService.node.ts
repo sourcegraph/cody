69ca8eca9ab40b8d4d5bcf54517a534c27e1e743
Philipp Spiess <hello@philippspiess.com>
2023-11-24 13:18:14 +0100

    More fixes

--- vscode/src/services/OpenTelemetryService.node.ts
+++ vscode/src/services/OpenTelemetryService.node.ts
@@ -5,25 +5,39 @@ import { NodeSDK } from '@opentelemetry/sdk-node'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 
 import { Configuration } from '@sourcegraph/cody-shared/src/configuration'
+import { FeatureFlag, featureFlagProvider } from '@sourcegraph/cody-shared/src/experimentation/FeatureFlagProvider'
 
 import { extensionDetails } from './telemetry'
 
 export class OpenTelemetryService {
     private sdk: NodeSDK | undefined
+    private lastTraceUrl: string | undefined
+    // We use a single promise object that we chain on to, to avoid multiple reconfigure calls to
+    // be run in parallel
+    private reconfigurePromiseMutex: Promise<void> = Promise.resolve()
+
     constructor(protected config: Pick<Configuration, 'serverEndpoint'>) {
-        void this.reconfigure()
+        this.reconfigurePromiseMutex = this.reconfigurePromiseMutex.then(() => this.reconfigure())
     }
 
     public onConfigurationChange(newConfig: Pick<Configuration, 'serverEndpoint'>): void {
         this.config = newConfig
-        // TODO: Fix race condition when this changes
-        // void this.reconfigure()
+        this.reconfigurePromiseMutex = this.reconfigurePromiseMutex.then(() => this.reconfigure())
     }
 
     private async reconfigure(): Promise<void> {
-        await this.sdk?.shutdown()
+        if (!(await featureFlagProvider.evaluateFeatureFlag(FeatureFlag.CodyAutocompleteTracing))) {
+            return
+        }
 
         const traceUrl = new URL('/-/debug/otlp/v1/traces', this.config.serverEndpoint).toString()
+        if (this.lastTraceUrl === traceUrl) {
+            return
+        }
+        this.lastTraceUrl = traceUrl
+
+        await this.sdk?.shutdown()
+        this.sdk = undefined
 
         this.sdk = new NodeSDK({
             resource: new Resource({
