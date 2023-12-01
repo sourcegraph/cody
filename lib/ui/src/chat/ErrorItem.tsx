5e70aa39faec02d94e1131cb8fefa787bab9c462
Beatrix <beatrix@sourcegraph.com>
2023-12-01 15:22:30 -0800

    log chat rate limit error handling

--- lib/ui/src/chat/ErrorItem.tsx
+++ lib/ui/src/chat/ErrorItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import { ChatError, RateLimitError } from '@sourcegraph/cody-shared'
 
@@ -10,21 +10,35 @@ import styles from './ErrorItem.module.css'
  * An error message shown in the chat.
  */
 export const ErrorItem: React.FunctionComponent<{
-    error: string | ChatError
+    error: ChatError
     ChatButtonComponent?: React.FunctionComponent<ChatButtonProps>
     userInfo?: UserAccountInfo
     postMessage?: ApiPostMessage
 }> = React.memo(function ErrorItemContent({ error, ChatButtonComponent, userInfo, postMessage }) {
-    return typeof error !== 'string' && error.name === RateLimitError.errorName && postMessage ? (
-        <RateLimitErrorItem
-            error={error as RateLimitError}
-            ChatButtonComponent={ChatButtonComponent}
-            postMessage={postMessage}
-        />
-    ) : (
+    if (typeof error !== 'string' && error.name === RateLimitError.errorName && postMessage) {
+        return (
+            <RateLimitErrorItem
+                error={error as RateLimitError}
+                ChatButtonComponent={ChatButtonComponent}
+                postMessage={postMessage}
+                userInfo={userInfo}
+            />
+        )
+    }
+
+    return <RequestErrorItem error={error.message} />
+})
+
+/**
+ * Renders a generic error message for chat request failures.
+ */
+export const RequestErrorItem: React.FunctionComponent<{
+    error: string
+}> = React.memo(function ErrorItemContent({ error }) {
+    return (
         <div className="cody-chat-error">
             <span>Request failed: </span>
-            {typeof error === 'string' ? error : error.message}
+            {error}
         </div>
     )
 })
@@ -40,7 +54,37 @@ export const RateLimitErrorItem: React.FunctionComponent<{
 }> = React.memo(function RateLimitErrorItemContent({ error, ChatButtonComponent, userInfo, postMessage }) {
     // Only show Upgrades if both the error said an upgrade was available and we know the user
     // has not since upgraded.
+
     const canUpgrade = error.upgradeIsAvailable && userInfo?.isCodyProUser !== true
+    const isEnterpriseUser = userInfo?.isDotComUser !== true
+    const tier = isEnterpriseUser ? 'enterprise' : userInfo?.isCodyProUser ? 'pro' : 'free'
+
+    // Only log once on mount
+    React.useEffect(() => {
+        // Log as abuseUsageLimit if pro user run into rate limit
+        postMessage({
+            command: 'event',
+            eventName: userInfo?.isCodyProUser
+                ? 'CodyVSCodeExtension:abuseUsageLimitCTA:shown'
+                : 'CodyVSCodeExtension:upsellUsageLimitCTA:shown',
+            properties: { limit_type: 'chat_commands', tier },
+        })
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    const onButtonClick = useCallback(
+        (page: 'upgrade' | 'rate-limits'): void => {
+            postMessage({ command: 'show-page', page })
+            postMessage({
+                command: 'event',
+                eventName: 'CodyVSCodeExtension:upsellUsageLimitCTA:clicked',
+                properties: { limit_type: 'chat_commands', page, tier },
+            })
+        },
+        [postMessage, tier]
+    )
+
     return (
         <div className={styles.errorItem}>
             <h1>{canUpgrade ? 'Upgrade to Cody Pro' : 'Unable to Send Message'}</h1>
@@ -51,14 +95,14 @@ export const RateLimitErrorItem: React.FunctionComponent<{
                         <ChatButtonComponent
                             label="Upgrade"
                             action=""
-                            onClick={() => postMessage({ command: 'show-page', page: 'upgrade' })}
+                            onClick={() => onButtonClick('upgrade')}
                             appearance="primary"
                         />
                     )}
                     <ChatButtonComponent
                         label="Learn More"
                         action=""
-                        onClick={() => postMessage({ command: 'show-page', page: 'rate-limits' })}
+                        onClick={() => onButtonClick('rate-limits')}
                         appearance="secondary"
                     />
                 </div>
