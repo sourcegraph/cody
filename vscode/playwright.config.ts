0aef7c5c2c25943f27b41d7c0264c82f0d0f6309
Danny Tuppeny <danny@tuppeny.com>
2023-12-08 17:47:52 +0000

    Revert "Debug stuff"
    This reverts commit 7927d06f7458322843fa37e6aa88487d6e11dd8c.

--- vscode/playwright.config.ts
+++ vscode/playwright.config.ts
@@ -5,15 +5,4 @@ export default defineConfig({
     // Give failing tests a second chance
     retries: 2,
     testDir: 'test/e2e',
-    timeout: 120000,
-    expect: {
-        timeout: 10000,
-    },
-    use: {
-        // screenshot: 'only-on-failure',
-        video: 'retain-on-failure',
-        actionTimeout: 200000,
-        navigationTimeout: 600000,
-        trace: 'retain-on-failure',
-    },
 })
