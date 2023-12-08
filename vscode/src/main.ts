7d87d73fdf8f4f11a2861a9030bb935064db80ad
Beyang Liu <beyang.liu@gmail.com>
2023-12-07 22:55:07 -0800

    symf: support cancelling index

--- vscode/src/main.ts
+++ vscode/src/main.ts
@@ -126,6 +126,9 @@ const register = async (
     await authProvider.init()
 
     const symfRunner = platform.createSymfRunner?.(context, initialConfig.serverEndpoint, initialConfig.accessToken)
+    if (symfRunner) {
+        disposables.push(symfRunner)
+    }
 
     graphqlClient.onConfigurationChange(initialConfig)
     void featureFlagProvider.syncAuthStatus()
@@ -200,8 +203,8 @@ const register = async (
 
     if (symfRunner) {
         const searchViewProvider = new SearchViewProvider(context.extensionUri, symfRunner)
-        searchViewProvider.initialize()
         disposables.push(searchViewProvider)
+        searchViewProvider.initialize()
         disposables.push(
             vscode.window.registerWebviewViewProvider('cody.search', searchViewProvider, {
                 webviewOptions: { retainContextWhenHidden: true },
