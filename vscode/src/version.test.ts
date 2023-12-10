4928460f4f499e47ed24676bb1cc9bdde775f17a
Tim Lucas <t@toolmantim.com>
2023-12-10 22:33:00 +1100

    Split vscode-dependent code

--- vscode/src/version.test.ts
+++ vscode/src/version.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it, vi } from 'vitest'
 
-import { majorMinorVersion, releaseNotesURL, releaseType, version } from './version'
+import { version } from './version'
 
 vi.mock('vscode', () => ({
     extensions: {
@@ -9,35 +9,7 @@ vi.mock('vscode', () => ({
 }))
 
 describe('version', () => {
-    it('returns the version from the extension manifest', () => {
+    it('returns the version from the runtime extension info', () => {
         expect(version).toEqual('1.2.3')
-    })
-})
-
-describe('majorMinorVersion', () => {
-    it('returns the first two components', () => {
-        expect(majorMinorVersion('0.2.1')).toEqual('0.2')
-        expect(majorMinorVersion('4.2.1')).toEqual('4.2')
-        expect(majorMinorVersion('4.3.1689391131')).toEqual('4.3')
-    })
-})
-
-describe('releaseType', () => {
-    it('returns stable if no dash', () => {
-        expect(releaseType('4.2.1')).toEqual('stable')
-    })
-    it('returns insiders if it is an odd minor version', () => {
-        expect(releaseType('4.3.1689391131')).toEqual('insiders')
-    })
-})
-
-describe('releaseNotesURL', () => {
-    it('returns GitHub release notes for stable builds', () => {
-        expect(releaseNotesURL('4.2.1')).toEqual('https://github.com/sourcegraph/cody/releases/tag/vscode-v4.2.1')
-    })
-    it('returns changelog for insiders builds', () => {
-        expect(releaseNotesURL('4.3.1689391131')).toEqual(
-            'https://github.com/sourcegraph/cody/blob/main/vscode/CHANGELOG.md'
-        )
     })
 })
--- vscode/src/version.ts
+++ vscode/src/version.ts
@@ -2,27 +2,9 @@ import * as vscode from 'vscode'
 
 import { version as packageVersion } from '../package.json'
 
-export type ReleaseType = 'stable' | 'insiders'
-
-// For insiders builds, packageVersion above will be wrong. This is because
-// `pnpm build` bakes the version in to the source _before_ the the pre-release
-// build modifies the package.json. So instead of packageVersion which may not
-// match the version in package.json, we prefer the value given by
-// vscode.extensions.getExtension.
+// The runtime version (available from the host extension) will represent
+// pre-release numbers properly, otherwise fall back to the stable/static one
+// inlined at build time
 export const version =
     (vscode.extensions.getExtension('sourcegraph.cody-ai')?.packageJSON as { version: string })?.version ??
     packageVersion
-
-export const majorVersion = (version: string): string => version.split('.')[0]
-
-export const minorVersion = (version: string): string => version.split('.')[1]
-
-export const majorMinorVersion = (version: string): string => [majorVersion(version), minorVersion(version)].join('.')
-
-export const releaseType = (version: string): ReleaseType =>
-    Number(minorVersion(version)) % 2 === 1 ? 'insiders' : 'stable'
-
-export const releaseNotesURL = (version: string): string =>
-    releaseType(version) === 'stable'
-        ? `https://github.com/sourcegraph/cody/releases/tag/vscode-v${version}`
-        : 'https://github.com/sourcegraph/cody/blob/main/vscode/CHANGELOG.md'
