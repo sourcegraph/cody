8f9a80745fd08aa806a4f4b5c337161456624e86
Tim Lucas <t@toolmantim.com>
2023-12-12 22:59:33 +1100

    Lint

--- vscode/webviews/App.tsx
+++ vscode/webviews/App.tsx
@@ -342,9 +342,13 @@ function addInstructions<T extends CodyPrompt>([key, command]: [string, T]): [st
 function getWelcomeMessageByOS(os: string): string {
     const welcomeMessageMarkdown = `Welcome to Cody! Start writing code and Cody will autocomplete lines and entire functions for you.
 
-To run [Cody Commands](command:cody.action.commands.menu) use the keyboard shortcut <span class="keyboard-shortcut"><span>${os === 'darwin' ? '⌥' : 'Alt'}</span><span>C</span></span>, the <span class="cody-icons">A</span> button, or right-click anywhere in your code.
+To run [Cody Commands](command:cody.action.commands.menu) use the keyboard shortcut <span class="keyboard-shortcut"><span>${
+        os === 'darwin' ? '⌥' : 'Alt'
+    }</span><span>C</span></span>, the <span class="cody-icons">A</span> button, or right-click anywhere in your code.
 
-You can start a new chat at any time with <span class="keyboard-shortcut"><span>${os === 'darwin' ? '⌥' : 'Alt'}</span><span>/</span></span> or using the <span class="cody-icons">H</span> button.
+You can start a new chat at any time with <span class="keyboard-shortcut"><span>${
+        os === 'darwin' ? '⌥' : 'Alt'
+    }</span><span>/</span></span> or using the <span class="cody-icons">H</span> button.
 
 For more tips and tricks, see the [Getting Started Guide](command:cody.welcome) and [docs](https://docs.sourcegraph.com/cody).
 `
