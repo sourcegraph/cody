bc1937829ee07144273b1f5e91de96131acc621c
Rik Nauta <riknauta@gmail.com>
2023-12-13 14:55:51 +0100

    feat: use completions API instead of chat

--- vscode/src/scm/CommitMessageProvider.ts
+++ vscode/src/scm/CommitMessageProvider.ts
@@ -2,10 +2,22 @@ import * as vscode from 'vscode';
 import type { CommitMessageProvider as ICommitMessageProvider, Repository, API as ScmAPI} from '../repository/builtinGitExtension';
 import { ConfigurationWithAccessToken } from '@sourcegraph/cody-shared/src/configuration'
 import { ChatClient } from '@sourcegraph/cody-shared/src/chat/chat'
+import { Message } from '@sourcegraph/cody-shared/src/sourcegraph-api'
+import { isRateLimitError } from '@sourcegraph/cody-shared/dist/sourcegraph-api/errors'
+
+import { CodeCompletionsClient } from '../completions/client'
+import dedent from 'dedent'
 export interface CommitMessageProviderOptions {
     chatClient: ChatClient,
+    codeCompletionsClient: CodeCompletionsClient
     gitApi: ScmAPI,
 }
+
+export interface CommitMessageGuide {
+    examples?: string[],
+    template?: string
+}
+
 export class CommitMessageProvider implements ICommitMessageProvider, vscode.Disposable {
     public icon = new vscode.ThemeIcon('cody-logo');
     public title = 'Generate Commit Message (Cody)'
@@ -29,49 +41,83 @@ export class CommitMessageProvider implements ICommitMessageProvider, vscode.Dis
     }
     public async provideCommitMessage(repository: Repository, changes: string[], cancellationToken?: any): Promise<string | undefined> {
         // TODO: load commit template from settings
-        // TODO: handle which changes are actually staged or not?
-        // TODO: Logging, metrics & telemetry. I'm too unfamiliar.
+        // TODO: Logging, metrics & telemetry.
+        // TODO: Handle loading of examples? Maybe a query for similar commits based on the changes?
         // TODO: Handle cancellation
+        if(changes.length == 0){
+            return Promise.reject()
+        }
+        try {
+            return await this.generateCommitMessage(changes)
+        }catch(error){
+            if(isRateLimitError(error)){
+                //TODO: we probably want a typed union of all commands & arg arrays somewhere?
+                await vscode.commands.executeCommand('cody.show-rate-limit-modal',
+                ...[error.userMessage, error.retryMessage, error.upgradeIsAvailable])
+            }
+            throw error
+        }
+    }
 
-        const response = await new Promise<string>((resolve, reject) => {
+    private async generateCommitMessage(changes: string[], guide: CommitMessageGuide = {}): Promise<string> {
+        // TODO: ideally this would me moved to a shared module so we can add way more smart context logic to it
+        // that can be used in similar places like similar to  a similar thing in `@cli/src/commands/generateCommitMessage.ts`
 
-            // TODO: This generates a terrible output but has the basics in place.
-            // There's a similar thing in `cli/src/commands` that uses the autocomplete API (since I want structured output.
-            // Or should I instead add a recipe in `lib/shared/recipes` and if so how would I execute that so I get some structured output?
-            let responseText = '';
-            this.options.chatClient.chat(
-                [
-                    {
-                        speaker: 'human',
-                        // TODO: The custom command in `cody.json` is what inspired this. But I can't quite find the code where this is translated into
-                        text: `Suggest an informative but succinct commit message by summarizing code changes from the shared list of changes.
-                        The commit message should follow the conventional commit format and provide meaningful context for future readers.
+        const template = guide.template?.trim() || 'A commit message consists of scoped title of max 72 characters, and a body with additional details about and reasons for the change. Commit messages are concise, technical, and specific to the change. They also mention any UI or user-facing changes.'
+        const examples = guide.examples?.map(msg => dedent`
+            <commit-message>
+            ${msg.trim()}
+            </commit-message>
+        `) || [
+            dedent`
+            <commit-message>
+            feat(app): add new feature
 
-                        Changes:
-                        \`\`\`
-                        ${changes.map(c => c).join('\n')}
-                        \`\`\`
-                        `
-                    },
-                ],
-                {
-                    onChange: (text: string) => {
-                        responseText = text
-                    },
-                    onComplete: () => {
-                        resolve(responseText)
-                    },
-                    onError: (error: Error, statusCode?: number) => reject(error),
-                },
-                {
-                    temperature: 0,
-                    fast: true,
-                }
+            - adds new UX elements
+            - updated tests
+            - improves the way teh feature loads data
+            </commit-message>
+        `]
 
-            )
+        const instructions: Message[] = [
+            {speaker: 'human', text: template},
+            {speaker: 'human', text: dedent`
+            Here are a few examples:
+            <examples>
+                ${examples.map(example => `<example>
+                    ${example}
+                </example>`).join('\n')}
+            </examples>`
+            },
+            {speaker: 'human', text: dedent`
+            Here is a diff:
+            <diffs>
+                ${changes.map(diff => `<diff>
+                    ${diff}
+                </diff>`).join('\n')}
+            </diffs>
+            `}, // IDEA: handle max / pick most important changes?
+            {
+                speaker: 'human',
+                text: 'Based on the the instructions and examples write a commit message for the diff.'
+            },
+            {
+                speaker: 'assistant',
+                text: '<commit-message>'
+            }
+        ]
+
+        const {completion, stopReason: _} = await this.options.codeCompletionsClient.complete({
+            maxTokensToSample: 1000,
+            messages: instructions,
+            temperature: 0,
+            stopSequences: ['</commit-message>'],
+            timeoutMs: 5000 // TODO: is there a standard defined somewhere? Is this including network latency or the "model" timeout?
         })
+        return completion.slice(0, completion.indexOf('/commit-message')).trim()
 
-        return response
+        // TODO: check if we completed succesfully. Bit difficult now because response.stopReason isn't typed
+
     }
 }
 
