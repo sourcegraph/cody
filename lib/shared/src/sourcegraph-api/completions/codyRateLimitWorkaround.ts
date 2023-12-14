08e963b1ab9a9dbbf5a2363a6f0ccad44fee93fa
Philipp Spiess <hello@philippspiess.com>
2023-12-14 16:04:22 +0100

    WIP work around rate limiting bug

---
+++ lib/shared/src/sourcegraph-api/completions/codyRateLimitWorkaround.ts
@@ -0,0 +1,36 @@
+import { RateLimitError } from '../errors'
+import { graphqlClient } from '../graphql'
+
+export function convertCodyGatewayErrorToRateLimitError(error: string): Promise<RateLimitError> {
+    return new Promise(resolve => {
+        const limit = /exceeded the rate limit of (\d+) requests/.exec(error)
+        const retryAfter = /Retry after (.*)\n/.exec(error)
+
+        graphqlClient
+            .getCurrentUserCodyProEnabled()
+            .then(user => {
+                console.log({ user })
+                if (!('codyProEnabled' in user)) {
+                    throw user
+                }
+                const rateLimitError = new RateLimitError(
+                    'chat messages and commands',
+                    error,
+                    !user.codyProEnabled,
+                    limit ? parseInt(limit[0], 10) : undefined,
+                    retryAfter ? retryAfter[0] : undefined
+                )
+                resolve(rateLimitError)
+            })
+            .catch(() => {
+                const rateLimitError = new RateLimitError(
+                    'chat messages and commands',
+                    error,
+                    true,
+                    limit ? parseInt(limit[0], 10) : undefined,
+                    retryAfter ? retryAfter[0] : undefined
+                )
+                resolve(rateLimitError)
+            })
+    })
+}
