5106ecb99ee5597ced14ec3232fb5f3022b4d1b4
Rik Nauta <riknauta@gmail.com>
2023-12-15 15:28:26 +0100

    wip: use a recipe

--- lib/shared/src/chat/recipes/recipe.ts
+++ lib/shared/src/chat/recipes/recipe.ts
@@ -15,6 +15,40 @@ export interface RecipeContext {
     userInputContextFiles?: ContextFile[]
 }
 
+// TODO:
+// If instead of a union like this we'd make a discriminative union out of all recipes
+// we could then make the ID a simple mapping over they keys for those.
+// This has as a benefit that if you select a recipe with a specific ID you get them
+// typed, so if you have any recipe specific public methods they would now be exposed
+// without further type-checking.
+
+// Here's how that would look:
+
+// interface ExampleRecipeA {
+//     id: 'recipe-a'
+//     a: boolean
+// }
+// interface ExampleRecipeB {
+//     id: 'recipe-b'
+//     b: boolean
+// }
+// type ExampleAllRecipes = ExampleRecipeA | ExampleRecipeB // ...etc
+
+// // Map each case in the original union to the corresponding case in MyUnionWithIDs
+// type ExampleAddId<T extends { id: string }> = T & Recipe
+
+// // Apply AddID to each case in the original union
+// type ExampleAllRecipesUnique = {
+//     [K in ExampleAllRecipes['id']]: ExampleAddId<Extract<ExampleAllRecipes, { type: K }>>
+// }
+// // Get all possible keys
+// type ExampleAllRecipeIds = keyof ExampleAllRecipesUnique
+// // Now we have typed access to additional properties
+// const unk: ExampleAllRecipes = { id: 'recipe-a', a: false } as any
+// if(unk.id === 'recipe-a'){ //knows ids
+//     unk.a //exists
+// }
+
 export type RecipeID =
     | 'chat-question'
     | 'code-question'
@@ -31,6 +65,7 @@ export type RecipeID =
     | 'custom-prompt'
     | 'next-questions'
     | 'pr-description'
+    | 'commit-message'
     | 'release-notes'
     | 'translate-to-language'
 
