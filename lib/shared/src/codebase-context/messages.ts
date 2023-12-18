7bdf8189b0b19636f279183c5cdd3f3c23dbf63f
Tom Ross <tom@umpox.com>
2023-12-18 15:10:46 +0000

    Add suggested changes and small fixes

--- lib/shared/src/codebase-context/messages.ts
+++ lib/shared/src/codebase-context/messages.ts
@@ -8,7 +8,7 @@ import { Message } from '../sourcegraph-api'
 // user: context file provided by the user explicitly via chat input
 // keyword: the context file returned from local keyword search
 // editor: context file retrieved from the current editor
-// unified: ???
+// unified: multi-repo context files from the Sourcegraph API
 export type ContextFileSource = 'embeddings' | 'user' | 'keyword' | 'editor' | 'filename' | 'unified'
 
 export type ContextFileType = 'file' | 'symbol'
