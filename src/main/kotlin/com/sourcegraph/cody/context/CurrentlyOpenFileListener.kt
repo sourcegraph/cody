d7dac57203eecc113078f9b6b5e76ef08473ed7e
Mikołaj Kondratek <mik.kondratek@gmail.com>
2023-12-20 09:49:54 +0100

    Update selected file label on the ide start up & fix slow operation on selectionChanged (#216)
    The first attempt has been reverted here:
    https://github.com/sourcegraph/jetbrains/pull/215.
    
    `runIde` was fine - the issue appeared in a newer version of intellij.
    now it should be fixed.
    
    now, now slow operation error should appear on start up
    
    ## Test plan 
    - runIde
    - play with files (open a file, have a file opened at start up)
    - test against 2023.3

--- src/main/kotlin/com/sourcegraph/cody/context/CurrentlyOpenFileListener.kt
+++ src/main/kotlin/com/sourcegraph/cody/context/CurrentlyOpenFileListener.kt
@@ -1,5 +1,6 @@
 package com.sourcegraph.cody.context
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.project.Project
@@ -10,12 +11,15 @@ class CurrentlyOpenFileListener(
     private val embeddingStatusView: EmbeddingStatusView
 ) : FileEditorManagerListener {
   override fun selectionChanged(event: FileEditorManagerEvent) {
-    val newFile = event.newFile
-    val openedFileName = newFile?.name ?: ""
-    var relativeFilePath: String? = null
-    if (newFile != null) {
-      relativeFilePath = ProjectFileUtils.getRelativePathToProjectRoot(project, newFile)
+    ApplicationManager.getApplication().runReadAction {
+      val newFile = event.newFile
+      val openedFileName = newFile?.name ?: ""
+      var relativeFilePath: String? = null
+
+      if (newFile != null) {
+        relativeFilePath = ProjectFileUtils.getRelativePathToProjectRoot(project, newFile)
+      }
+      embeddingStatusView.setOpenedFileName(openedFileName, relativeFilePath)
     }
-    embeddingStatusView.setOpenedFileName(openedFileName, relativeFilePath)
   }
 }
