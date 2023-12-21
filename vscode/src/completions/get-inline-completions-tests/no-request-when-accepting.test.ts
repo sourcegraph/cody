6fccf251af8129e123d66783b9f4237cbc779a55
Philipp Spiess <hello@philippspiess.com>
2023-12-21 13:29:02 +0100

    Clean up tests with a dedicated helper function

--- vscode/src/completions/get-inline-completions-tests/no-request-when-accepting.test.ts
+++ vscode/src/completions/get-inline-completions-tests/no-request-when-accepting.test.ts
@@ -1,10 +1,35 @@
 import dedent from 'dedent'
 import { describe, expect, it } from 'vitest'
 
+import { CompletionResponse } from '@sourcegraph/cody-shared/src/sourcegraph-api/completions/types'
+
 import { TriggerKind } from '../get-inline-completions'
 import { completion } from '../test-helpers'
 
 import { getInlineCompletions, params, V } from './helpers'
+
+// Simulate the VS Code behavior where accepting a completion will immediately start a new
+// completion request.
+async function getInlineCompletionAfterAccepting(
+    initialCode: string,
+    completion: CompletionResponse,
+    acceptedCode: string,
+    triggerKind: TriggerKind = TriggerKind.Automatic
+): ReturnType<typeof getInlineCompletions> {
+    const initialRequestParams = params(initialCode, [completion])
+    const item = await getInlineCompletions(initialRequestParams)
+
+    return getInlineCompletions(
+        params(acceptedCode, [], {
+            triggerKind,
+            lastAcceptedCompletionItem: {
+                requestParams: initialRequestParams,
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                analyticsItem: item!.items[0]!,
+            },
+        })
+    )
+}
 
 describe('[getInlineCompletions] no request when accepting', () => {
     // In VS Code, accepting a completion will immediately start a new completion request. If the
@@ -13,97 +38,56 @@ describe('[getInlineCompletions] no request when accepting', () => {
     //
     // Thus, this results in a request that almost always has zero results but still incurs network
     // and inference costs.
-    it('should not make a request after accepting a completion', async () => {
-        const initialRequestParams = params(
-            dedent`
-                function test() {
-                    console.l█
-                }
-            `,
-            [completion`├og = 123┤`]
-        )
-        const item = await getInlineCompletions(initialRequestParams)
+    it('should not make a request after accepting a completion', async () =>
         expect(
-            await getInlineCompletions(
-                params(
-                    dedent`
-                        function test() {
-                            console.log = 123█
-                        }
-                    `,
-                    [],
-                    {
-                        lastAcceptedCompletionItem: {
-                            requestParams: initialRequestParams,
-                            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                            analyticsItem: item!.items[0]!,
-                        },
+            await getInlineCompletionAfterAccepting(
+                dedent`
+                    function test() {
+                        console.l█
                     }
-                )
+                `,
+                completion`├og = 123┤`,
+                dedent`
+                    function test() {
+                        console.log = 123█
+                    }
+                `
             )
-        ).toEqual<V>(null)
-    })
+        ).toEqual<V>(null))
 
-    it('should make the request when manually invoked', async () => {
-        const initialRequestParams = params(
-            dedent`
-                function test() {
-                    console.l█
-                }
-            `,
-            [completion`├og = 123┤`]
-        )
-        const item = await getInlineCompletions(initialRequestParams)
+    it('should make the request when manually invoked', async () =>
         expect(
-            await getInlineCompletions(
-                params(
-                    dedent`
-                        function test() {
-                            console.log = 123█
-                        }
-                    `,
-                    [],
-                    {
-                        triggerKind: TriggerKind.Manual,
-                        lastAcceptedCompletionItem: {
-                            requestParams: initialRequestParams,
-                            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                            analyticsItem: item!.items[0]!,
-                        },
+            await getInlineCompletionAfterAccepting(
+                dedent`
+                    function test() {
+                        console.l█
                     }
-                )
+                `,
+                completion`├og = 123┤`,
+                dedent`
+                    function test() {
+                        console.log = 123█
+                    }
+                `,
+                TriggerKind.Manual
             )
-        ).not.toEqual<V>(null)
-    })
+        ).not.toEqual<V>(null))
 
-    it('should make the request when the accepted completion was multi-line', async () => {
-        const initialRequestParams = params(
-            dedent`
+    it('should make the request when the accepted completion was multi-line', async () =>
+        expect(
+            await getInlineCompletionAfterAccepting(
+                dedent`
                 function test() {
                     █
                 }
             `,
-            [completion`├console.log = 123┤`]
-        )
-        const item = await getInlineCompletions(initialRequestParams)
-        expect(
-            await getInlineCompletions(
-                params(
-                    dedent`
-                        function test() {
-                            console.log = 123█
-                        }
-                    `,
-                    [],
-                    {
-                        lastAcceptedCompletionItem: {
-                            requestParams: initialRequestParams,
-                            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                            analyticsItem: item!.items[0]!,
-                        },
-                    }
-                )
+                completion`├console.log = 123┤`,
+                dedent`
+                function test() {
+                    console.log = 123█
+                }
+            `,
+                TriggerKind.Manual
             )
-        ).not.toEqual<V>(null)
-    })
+        ).not.toEqual<V>(null))
 })
