8107b8467afb92b7b89856357ea5cfbea947fddb
Piotr Kukielka <pkukielka@virtuslab.com>
2024-01-02 18:04:54 +0100

    Fix double agent messages with rate limit errors

--- vscode/src/chat/chat-view/SimpleChatPanelProvider.ts
+++ vscode/src/chat/chat-view/SimpleChatPanelProvider.ts
@@ -723,6 +723,9 @@ export class SimpleChatPanelProvider implements vscode.Disposable, IChatPanelPro
             close: () => {
                 callbacks.close(lastContent)
             },
+            error: error => {
+                callbacks.error(lastContent, error)
+            },
         })
 
         this.cancelInProgressCompletion()
@@ -741,7 +744,6 @@ export class SimpleChatPanelProvider implements vscode.Disposable, IChatPanelPro
                     this.cancelInProgressCompletion()
                     typewriter.close()
                     typewriter.stop(error)
-                    callbacks.error(lastContent, error)
                 },
             },
             { model: this.chatModel.modelID }
