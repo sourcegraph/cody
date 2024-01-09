de27a59593396432d264e51a24e038180e57ed3d
Tom Ross <tom@umpox.com>
2024-01-09 08:57:51 +0000

    Edit: Refactor away from `MessageProvider` and `Recipe` (#2549)
    ## Description
    
    Edit is now:
    - Using it's own logic instead of sharing `MessageProvider`. 
    - No longer a recipe, all logic lives inside of `/edit` and `non-stop`.
    In a follow up PR I will consolidate the logic in `non-stop` into this
    directory too and clean that up.
    - Refactored to easily support changes without affecting other
    commands/chat/recipes.
    - Refactored to support easily adding prompts for new models such as
    GPT4
    
    closes https://github.com/sourcegraph/cody/issues/2545
    
    ## Test plan
    
    Followed edit test plans in
    https://docs.google.com/document/d/1all3z8dPs52amhNs3m4O1Lu7V6VsCaU7jXovprte-EE/edit#heading=h.o8dycc3mlio
    
    Followed telemetry test plan via
    https://github.com/sourcegraph/cody/issues/2545
    
    <!-- Required. See
    https://sourcegraph.com/docs/dev/background-information/testing_principles.
    -->

---
+++ vscode/src/edit/manager.ts
@@ -0,0 +1,118 @@
+import * as vscode from 'vscode'
+
+import { ChatClient } from '@sourcegraph/cody-shared/src/chat/chat'
+import { ChatEventSource } from '@sourcegraph/cody-shared/src/chat/transcript/messages'
+
+import { ContextProvider } from '../chat/ContextProvider'
+import { getEditor } from '../editor/active-editor'
+import { VSCodeEditor } from '../editor/vscode-editor'
+import { FixupController } from '../non-stop/FixupController'
+import { FixupTask } from '../non-stop/FixupTask'
+import { telemetryService } from '../services/telemetry'
+import { telemetryRecorder } from '../services/telemetry-v2'
+
+import { ExecuteEditArguments } from './execute'
+import { EditProvider } from './provider'
+import { EditIntent } from './types'
+
+export interface EditManagerOptions {
+    editor: VSCodeEditor
+    chat: ChatClient
+    contextProvider: ContextProvider
+}
+
+export class EditManager implements vscode.Disposable {
+    private controller: FixupController
+    private disposables: vscode.Disposable[] = []
+    private editProviders = new Map<FixupTask, EditProvider>()
+
+    constructor(public options: EditManagerOptions) {
+        this.controller = new FixupController()
+        this.disposables.push(
+            this.controller,
+            vscode.commands.registerCommand(
+                'cody.command.edit-code',
+                (
+                    args: {
+                        range?: vscode.Range
+                        instruction?: string
+                        intent?: EditIntent
+                        document?: vscode.TextDocument
+                        insertMode?: boolean
+                    },
+                    source?: ChatEventSource
+                ) => this.executeEdit(args, source)
+            )
+        )
+    }
+
+    public async executeEdit(args: ExecuteEditArguments = {}, source: ChatEventSource = 'editor'): Promise<void> {
+        const commandEventName = source === 'doc' ? 'doc' : 'edit'
+        telemetryService.log(
+            `CodyVSCodeExtension:command:${commandEventName}:executed`,
+            { source },
+            { hasV2Event: true }
+        )
+        telemetryRecorder.recordEvent(`cody.command.${commandEventName}`, 'executed', { privateMetadata: { source } })
+
+        const editor = getEditor()
+        if (editor.ignored) {
+            void vscode.window.showInformationMessage('Cannot edit Cody ignored file.')
+            return
+        }
+
+        const document = args.document || editor.active?.document
+        if (!document) {
+            void vscode.window.showErrorMessage('Please open a file before running a command.')
+            return
+        }
+
+        const range = args.range || editor.active?.selection
+        if (!range) {
+            return
+        }
+
+        const task = args.instruction?.trim()
+            ? await this.controller.createTask(
+                  document.uri,
+                  args.instruction,
+                  range,
+                  args.intent,
+                  args.insertMode,
+                  source
+              )
+            : await this.controller.promptUserForTask(args, source)
+        if (!task) {
+            return
+        }
+
+        const provider = this.getProviderForTask(task)
+        return provider.startEdit()
+    }
+
+    public getProviderForTask(task: FixupTask): EditProvider {
+        let provider = this.editProviders.get(task)
+
+        if (!provider) {
+            provider = new EditProvider({ task, controller: this.controller, ...this.options })
+            this.editProviders.set(task, provider)
+        }
+
+        return provider
+    }
+
+    public removeProviderForTask(task: FixupTask): void {
+        const provider = this.editProviders.get(task)
+
+        if (provider) {
+            this.editProviders.delete(task)
+        }
+    }
+
+    public dispose(): void {
+        for (const disposable of this.disposables) {
+            disposable.dispose()
+        }
+        this.disposables = []
+    }
+}
