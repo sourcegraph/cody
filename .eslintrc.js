f722db4bcebe898b2a171857970714f588ef42ca
Quinn Slack <quinn@slack.org>
2024-01-09 03:19:42 -0800

    use new simpler/faster eslint project config (#2630)
    This is the new and faster way of configuring eslint for TypeScript.
    Another benefit is that we no longer need to list all tsconfig.json
    files here.
    
    
    
    ## Test plan
    
    CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 // @ts-check
 
-/** @type {import('eslint').Linter.Config} */
 const config = {
   extends: ['@sourcegraph/eslint-config', 'plugin:storybook/recommended'],
   env: {
@@ -14,21 +13,10 @@ const config = {
     ecmaFeatures: {
       jsx: true,
     },
-    EXPERIMENTAL_useSourceOfProjectReferenceRedirect: true,
-    project: [
-      __dirname + '/e2e-inspector/tsconfig.json',
-      __dirname + '/e2e/tsconfig.json',
-      __dirname + '/agent/tsconfig.json',
-      __dirname + '/cli/tsconfig.json',
-      __dirname + '/lib/ui/tsconfig.json',
-      __dirname + '/lib/shared/tsconfig.json',
-      __dirname + '/slack/tsconfig.json',
-      __dirname + '/vscode/tsconfig.json',
-      __dirname + '/vscode/test/integration/tsconfig.json',
-      __dirname + '/vscode/scripts/tsconfig.json',
-      __dirname + '/web/tsconfig.json',
-      __dirname + '/tsconfig.json',
-    ],
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    EXPERIMENTAL_useProjectService: true,
+    project: true,
   },
   settings: {
     react: {
