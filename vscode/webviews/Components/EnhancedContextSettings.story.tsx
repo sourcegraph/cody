2e83895ccc9af1f40cb8fbf0dd578f8e6fcd80d7
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-01-11 17:58:45 +0900

    Remove close button from context settings.

--- vscode/webviews/Components/EnhancedContextSettings.story.tsx
+++ vscode/webviews/Components/EnhancedContextSettings.story.tsx
@@ -1,4 +1,4 @@
-import { useArgs } from '@storybook/preview-api'
+import { useArgs, useState } from '@storybook/preview-api'
 import { type Meta, type StoryObj } from '@storybook/react'
 
 import {
@@ -126,54 +126,57 @@ export const SingleTile: StoryObj<typeof EnhancedContextSettings | SingleTileArg
 }
 
 export const Smorgasbord: StoryObj<typeof EnhancedContextSettings> = {
-    render: () => (
-        <EnhancedContextContext.Provider
-            value={{
-                groups: [
-                    {
-                        name: '~/projects/foo',
-                        providers: [
-                            { kind: 'embeddings', type: 'local', state: 'unconsented' },
-                            { kind: 'graph', state: 'ready' },
-                            { kind: 'search', state: 'indexing' },
-                        ],
-                    },
-                    {
-                        name: 'gitlab.com/my/repo',
-                        providers: [
-                            {
-                                kind: 'embeddings',
-                                type: 'remote',
-                                remoteName: 'gitlab.com/my/repo',
-                                origin: 'sourcegraph.com',
-                                state: 'ready',
-                            },
-                        ],
-                    },
-                    {
-                        name: 'github.com/sourcegraph/bar',
-                        providers: [
-                            {
-                                kind: 'embeddings',
-                                type: 'remote',
-                                remoteName: 'github.com/sourcegraph/bar',
-                                origin: 'sourcegraph.sourcegraph.com',
-                                state: 'no-match',
-                            },
-                        ],
-                    },
-                ],
-            }}
-        >
-            <div
-                style={{
-                    position: 'absolute',
-                    bottom: 20,
-                    right: 20,
+    render: () => {
+        const [isOpen, setIsOpen] = useState<boolean>(true)
+        return (
+            <EnhancedContextContext.Provider
+                value={{
+                    groups: [
+                        {
+                            name: '~/projects/foo',
+                            providers: [
+                                { kind: 'embeddings', type: 'local', state: 'unconsented' },
+                                { kind: 'graph', state: 'ready' },
+                                { kind: 'search', state: 'indexing' },
+                            ],
+                        },
+                        {
+                            name: 'gitlab.com/my/repo',
+                            providers: [
+                                {
+                                    kind: 'embeddings',
+                                    type: 'remote',
+                                    remoteName: 'gitlab.com/my/repo',
+                                    origin: 'sourcegraph.com',
+                                    state: 'ready',
+                                },
+                            ],
+                        },
+                        {
+                            name: 'github.com/sourcegraph/bar',
+                            providers: [
+                                {
+                                    kind: 'embeddings',
+                                    type: 'remote',
+                                    remoteName: 'github.com/sourcegraph/bar',
+                                    origin: 'sourcegraph.sourcegraph.com',
+                                    state: 'no-match',
+                                },
+                            ],
+                        },
+                    ],
                 }}
             >
-                <EnhancedContextSettings isOpen={true} setOpen={() => {}} />
-            </div>
-        </EnhancedContextContext.Provider>
-    ),
+                <div
+                    style={{
+                        position: 'absolute',
+                        bottom: 20,
+                        right: 20,
+                    }}
+                >
+                    <EnhancedContextSettings isOpen={isOpen} setOpen={setIsOpen} />
+                </div>
+            </EnhancedContextContext.Provider>
+        )
+    },
 }
