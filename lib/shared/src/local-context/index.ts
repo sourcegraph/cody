b7421699cbc88027d9477db8bc68fc750f9294af
Quinn Slack <quinn@slack.org>
2024-01-11 01:48:03 -0800

    hydrate values passed via postMessage (#2678)
    There were some places in the code where either (1) we were manually
    re-hydrating `vscode-uri.URI`/`vscode.Uri` instances or (2) there were
    subtle bugs that occurred because we were not doing that (the most
    common of which is broken uniquifying because
    `unhydratedUriValue.toString()` == `[Object object]`).
    
    
    
    ## Test plan
    
    - CI
    - Run a NLS search and ensure that clicking on files works (for the
    `uriJSON` change)
    - In chat, `@`-mention some files, ensure the chat response comes
    through, and ensure that the EnhancedContext files are clickable

--- lib/shared/src/local-context/index.ts
+++ lib/shared/src/local-context/index.ts
@@ -1,3 +1,5 @@
+import { type URI } from 'vscode-uri'
+
 import { type ContextFile } from '../codebase-context/messages'
 import { type EmbeddingsSearchResult } from '../sourcegraph-api/graphql/client'
 
@@ -53,8 +55,7 @@ export interface IndexedKeywordContextFetcher {
  * File result that renders in the search panel webview
  */
 export interface SearchPanelFile {
-    uriString: string
-    uriJSON: unknown
+    uri: URI
     basename: string
     dirname: string
     wsname?: string
