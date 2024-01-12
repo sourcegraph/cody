4533b73edcb22b547113aa111b2c069ed05947f2
Beatrix <beatrix@sourcegraph.com>
2024-01-12 10:29:33 -0800

    revert Integration test for command

--- vscode/test/integration/commands.test.ts
+++ vscode/test/integration/commands.test.ts
@@ -54,8 +54,12 @@ suite('Commands', function () {
 
         // Check the chat transcript contains text from prompt
         const humanMessage = await getTranscript(0)
-        // The result should match the display text from the last command instead
-        // as the test command is now and edit command and does not update chat transcript
-        assert.match(humanMessage.displayText || '', /\/smell \[_@Main.java/)
+        assert.match(humanMessage.displayText || '', /\/test \[_@Main.java/)
+
+        // Has one or more context
+        const contextFileSize = humanMessage?.contextFiles?.length || 0
+        assert.ok(contextFileSize > 1)
+
+        await waitUntil(async () => assistantRegex.test((await getTranscript(1)).displayText || ''))
     })
 })
