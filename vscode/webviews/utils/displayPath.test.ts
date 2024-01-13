a9893631047401885e8ae10345c0d6b4cd841c16
Danny Tuppeny <danny@tuppeny.com>
2024-01-13 00:57:14 +0000

    Fix some issues on Windows with removed context filenames (#2715)
    This makes some fixes to https://github.com/sourcegraph/cody/pull/2653
    for Windows where the paths were appearing incorrectly (wrong slash
    direction, or not detected as part of the workspace so showing as
    absolute paths).
    
    This PR is targetting @sqs rm-filename branch since it's fixed to
    unmerged changes.
    
    
    ## Test plan
    
    - Run `pnpm test:e2e` on Windows and verify all is green

--- vscode/webviews/utils/displayPath.test.ts
+++ vscode/webviews/utils/displayPath.test.ts
@@ -10,18 +10,14 @@ describe('displayPathForWebviews', () => {
     test('no workspace folders', () => {
         updateWorkspaceFolderUris([])
         expect(displayPathForWebviews(URI.file('/foo/bar.ts'))).toBe('/foo/bar.ts')
-        expect(displayPathForWebviews(URI.parse('https://example.com/foo/bar.ts'))).toBe(
-            'https://example.com/foo/bar.ts'
-        )
+        expect(displayPathForWebviews(URI.parse('https://ex.com/foo/bar.ts'))).toBe('https://ex.com/foo/bar.ts')
     })
 
     test('1 workspace folder', () => {
         updateWorkspaceFolderUris(['file:///workspace'])
         expect(displayPathForWebviews(URI.file('/workspace/foo/bar.ts'))).toBe('foo/bar.ts')
         expect(displayPathForWebviews(URI.file('/other/foo/bar.ts'))).toBe('/other/foo/bar.ts')
-        expect(displayPathForWebviews(URI.parse('https://example.com/foo/bar.ts'))).toBe(
-            'https://example.com/foo/bar.ts'
-        )
+        expect(displayPathForWebviews(URI.parse('https://ex.com/foo/bar.ts'))).toBe('https://ex.com/foo/bar.ts')
     })
 
     test('2 workspace folders', () => {
@@ -29,21 +25,29 @@ describe('displayPathForWebviews', () => {
         expect(displayPathForWebviews(URI.file('/workspace1/foo/bar.ts'))).toBe('workspace1/foo/bar.ts')
         expect(displayPathForWebviews(URI.file('/workspace2/foo/bar.ts'))).toBe('workspace2/foo/bar.ts')
         expect(displayPathForWebviews(URI.file('/other/foo/bar.ts'))).toBe('/other/foo/bar.ts')
-        expect(displayPathForWebviews(URI.parse('https://example.com/foo/bar.ts'))).toBe(
-            'https://example.com/foo/bar.ts'
-        )
+        expect(displayPathForWebviews(URI.parse('https://ex.com/foo/bar.ts'))).toBe('https://ex.com/foo/bar.ts')
     })
 })
 
 describe('uriHasPrefix', () => {
     test('same url', () =>
-        expect(uriHasPrefix(URI.parse('https://example.com/a/b'), URI.parse('https://example.com/a/b'))).toBe(true))
+        expect(uriHasPrefix(URI.parse('https://ex.com/a/b'), URI.parse('https://ex.com/a/b'), false)).toBe(true))
 
-    test('path prefix', () => {
-        expect(uriHasPrefix(URI.parse('https://example.com/a/b'), URI.parse('https://example.com/a'))).toBe(true)
-        expect(uriHasPrefix(URI.parse('https://example.com/a/b'), URI.parse('other://example.com/a'))).toBe(false)
-        expect(uriHasPrefix(URI.parse('https://example.com/a/b'), URI.parse('https://example.com/a/'))).toBe(true)
-        expect(uriHasPrefix(URI.parse('https://example.com/a'), URI.parse('https://example.com/a/'))).toBe(true)
-        expect(uriHasPrefix(URI.parse('https://example.com/a-b'), URI.parse('https://example.com/a'))).toBe(false)
+    test('https path prefix', () => {
+        expect(uriHasPrefix(URI.parse('https://ex.com/a/b'), URI.parse('https://ex.com/a'), false)).toBe(true)
+        expect(uriHasPrefix(URI.parse('https://ex.com/a/b'), URI.parse('other://ex.com/a'), false)).toBe(false)
+        expect(uriHasPrefix(URI.parse('https://ex.com/a/b'), URI.parse('https://ex.com/a/'), false)).toBe(true)
+        expect(uriHasPrefix(URI.parse('https://ex.com/a'), URI.parse('https://ex.com/a/'), false)).toBe(true)
+        expect(uriHasPrefix(URI.parse('https://ex.com/a-b'), URI.parse('https://ex.com/a'), false)).toBe(false)
+    })
+
+    test('file path prefix', () => {
+        expect(uriHasPrefix(URI.parse('file:///a/b'), URI.parse('file:///a'), false)).toBe(true)
+        expect(uriHasPrefix(URI.parse('file:///a/b'), URI.parse('file:///A'), false)).toBe(false)
+        expect(uriHasPrefix(URI.parse('file:///a/b'), URI.parse('file:///b'), false)).toBe(false)
+        expect(uriHasPrefix(URI.parse('file:///c:/a/b'), URI.parse('file:///c:/a'), true)).toBe(true)
+        expect(uriHasPrefix(URI.parse('file:///c:/a/b'), URI.parse('file:///C:/a'), true)).toBe(true)
+        expect(uriHasPrefix(URI.parse('file:///c:/a/b'), URI.parse('file:///c:/A'), true)).toBe(false)
+        expect(uriHasPrefix(URI.parse('file:///c:/a/b'), URI.parse('file:///c:/b'), true)).toBe(false)
     })
 })
