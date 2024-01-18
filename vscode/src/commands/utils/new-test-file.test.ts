6de4b22e7a5b582cf01ab66ab69b110909b72367
Beatrix <68532117+abeatrix@users.noreply.github.com>
2024-01-18 07:09:11 -0800

    refactor: convert fs paths to uris in test file utils (#2765)

--- vscode/src/commands/utils/new-test-file.test.ts
+++ vscode/src/commands/utils/new-test-file.test.ts
@@ -1,106 +1,119 @@
-import { posix, sep } from 'path'
-
 import { describe, expect, it } from 'vitest'
+import { URI } from 'vscode-uri'
 
-import { convertFsPathToTestFile, createDefaultTestFileNameByLanguageExt } from './new-test-file'
+import { convertFileUriToTestFileUri, createDefaultTestFile } from './new-test-file'
 
-describe('createDefaultTestFileNameByLanguageExt', () => {
-    // All test should work with ext including . or not
-    it('should create a test file name with .test suffix for js and ts files', () => {
-        expect(createDefaultTestFileNameByLanguageExt('file', 'ts')).toBe('file.test.ts')
-        expect(createDefaultTestFileNameByLanguageExt('file', '.js')).toBe('file.test.js')
-    })
-
-    it('should create a test file name with _test suffix for py and go files', () => {
-        expect(createDefaultTestFileNameByLanguageExt('file', '.py')).toBe('file_test.py')
-        expect(createDefaultTestFileNameByLanguageExt('file', 'go')).toBe('file_test.go')
-    })
-
-    it('should create a test file name with _spec suffix for rb files', () => {
-        expect(createDefaultTestFileNameByLanguageExt('file', '.rb')).toBe('file_spec.rb')
-    })
-
-    it('should create a test file name with Test suffix for other files', () => {
-        expect(createDefaultTestFileNameByLanguageExt('file', '.cpp')).toBe('fileTest.cpp')
-        expect(createDefaultTestFileNameByLanguageExt('file', 'java')).toBe('fileTest.java')
+describe('createDefaultTestFile', () => {
+    it.each([
+        ['/path/to/file.java', '/path/to/fileTest.java'],
+        ['/path/to/file.js', '/path/to/file.test.js'],
+        ['/path/to/file.go', '/path/to/file_test.go'],
+        ['/path/to/test_file.py', '/path/to/test_file.py'],
+        ['/path/to/test-file.js', '/path/to/test-file.test.js'],
+        ['/path/to/node_modules/file.js', '/path/to/node_modules/file.test.js'],
+        ['/path/to/node_modules/file_test.ts', '/path/to/node_modules/file_test.ts'],
+        ['/path/to/fileTest.js', '/path/to/fileTest.js'],
+        ['test_example.py', 'test_example.py'],
+        ['example.cpp', 'exampleTest.cpp'],
+        ['example.test.js', 'example.test.js'],
+        ['Example.java', 'ExampleTest.java'],
+        ['ExampleTest.java', 'ExampleTest.java'],
+        ['example.rb', 'example_spec.rb'],
+        ['Example.cs', 'ExampleTest.cs'],
+        ['ExampleTest.php', 'ExampleTest.php'],
+        ['ExampleSpec.scala', 'ExampleSpec.scala'],
+        ['file.rb', 'file_spec.rb'],
+        ['contest.ts', 'contest.test.ts'],
+    ])('for file %j it returns %j', (file, test) => {
+        expect(createDefaultTestFile(URI.file(file)).toString()).toBe(URI.file(test).toString())
     })
 })
 
-describe('convertFsPathToTestFile', () => {
-    it('should return the current file path if it is already a test file', () => {
-        const testFilePath = '/path/to/testFile.test.ts'
-        expect(convertFsPathToTestFile(testFilePath)).toBe(testFilePath)
+describe('convertFileUriToTestFileUri', () => {
+    it('should return the current file uri if it is already a test file', () => {
+        expect(convertFileUriToTestFileUri(URI.file('/path/to/testFile.test.ts')).toString()).toBe(
+            URI.file('/path/to/testFile.test.ts').toString()
+        )
     })
 
-    it('should return the current file path if it is already a spec file', () => {
-        const testFilePath = '/path/to/testFile.spec.rb'
-        expect(convertFsPathToTestFile(testFilePath)).toBe(testFilePath)
+    it('should return the current file uri if it is already a spec file', () => {
+        expect(convertFileUriToTestFileUri(URI.file('/path/to/testFile.spec.rb')).toString()).toBe(
+            URI.file('/path/to/testFile.spec.rb').toString()
+        )
     })
 
-    it('should generate a test file path from a non-test file path', () => {
-        const filePath = '/path/to/file.ts'
-        const existingTestFilePath = '/path/to/testFile.ts'
-        const expectedFilePath = '/path/to/file.test.ts'
-        const expected = withPlatformSlashes(expectedFilePath)
-        expect(convertFsPathToTestFile(filePath, existingTestFilePath)).toBe(expected)
+    it('should generate a test file uri from a non-test file uri', () => {
+        expect(
+            convertFileUriToTestFileUri(URI.file('/path/to/file.ts'), URI.file('/path/to/testFile.ts')).toString()
+        ).toBe(URI.file('/path/to/file.test.ts').toString())
     })
 
-    it('should generate the default spec file path from a non-test file path for ruby', () => {
-        const filePath = '/path/to/file.rb'
-        const existingTestFilePath = '/path/to/testFile.ts'
-        const expectedFilePath = '/path/to/file_spec.rb'
-        const expected = withPlatformSlashes(expectedFilePath)
-        expect(convertFsPathToTestFile(filePath, existingTestFilePath)).toBe(expected)
+    it('should generate the default spec file uri from a non-test file uri for ruby', () => {
+        expect(
+            convertFileUriToTestFileUri(URI.file('/path/to/file.rb'), URI.file('/path/to/testFile.ts')).toString()
+        ).toBe(URI.file('/path/to/file_spec.rb').toString())
     })
 
-    it('should follow an existing test file path', () => {
-        const filePath = '/path/to/file.ts'
-        const existingTestFilePath = '/path/to/existingTestFile.test.ts'
-        const expectedFilePath = '/path/to/file.test.ts'
-        const expected = withPlatformSlashes(expectedFilePath)
-        expect(convertFsPathToTestFile(filePath, existingTestFilePath)).toBe(expected)
+    it('should follow an existing test file uri', () => {
+        expect(
+            convertFileUriToTestFileUri(
+                URI.file('/path/to/file.ts'),
+                URI.file('/path/to/existingTestFile.test.ts')
+            ).toString()
+        ).toBe(URI.file('/path/to/file.test.ts').toString())
     })
 
     it('should respect test file with different naming conventions', () => {
-        const filePath = '/path/to/file.ts'
-        const existingTestFilePath = '/path/to/testExistingFile.test.ts'
-        const expectedFilePath = '/path/to/file.test.ts'
-        const expected = withPlatformSlashes(expectedFilePath)
-        expect(convertFsPathToTestFile(filePath, existingTestFilePath)).toBe(expected)
+        expect(
+            convertFileUriToTestFileUri(
+                URI.file('/path/to/file.ts'),
+                URI.file('/path/to/testExistingFile.test.ts')
+            ).toString()
+        ).toBe(URI.file('/path/to/file.test.ts').toString())
     })
 
     it('should handle a non-alphanumeric character at the test character index', () => {
-        const filePath = '/path/to/file.ts'
-        const existingTestFilePath = '/path/to/test-ExistingFile.test.ts'
-        const expectedFilePath = '/path/to/file.test.ts'
-        const expected = withPlatformSlashes(expectedFilePath)
-        expect(convertFsPathToTestFile(filePath, existingTestFilePath)).toBe(expected)
+        expect(
+            convertFileUriToTestFileUri(
+                URI.file('/path/to/file.ts'),
+                URI.file('/path/to/test-ExistingFile.test.ts')
+            ).toString()
+        ).toBe(URI.file('/path/to/file.test.ts').toString())
     })
 
-    it('should generate a test file path for a non-test file path in python', () => {
-        const filePath = '/path/to/file.py'
-        const existingTestFilePath = '/path/to/testFile_test.py'
-        const expectedFilePath = '/path/to/file_test.py'
-        const expected = withPlatformSlashes(expectedFilePath)
-        expect(convertFsPathToTestFile(filePath, existingTestFilePath)).toBe(expected)
+    it('should generate a test file uri for a non-test file uri in python', () => {
+        expect(
+            convertFileUriToTestFileUri(URI.file('/path/to/file.py'), URI.file('/path/to/testFile_test.py')).toString()
+        ).toBe(URI.file('/path/to/file_test.py').toString())
     })
 
-    it('should generate a test file path for a non-test file path in python when no exisiting test path provided', () => {
-        const filePath = '/path/to/test-file.py'
-        const expectedFilePath = '/path/to/test-file_test.py'
-        const expected = withPlatformSlashes(expectedFilePath)
-        expect(convertFsPathToTestFile(filePath)).toBe(expected)
+    it('should generate a test file uri for a non-test file uri in python when no existing test path provided', () => {
+        expect(convertFileUriToTestFileUri(URI.file('/path/to/test-file.py')).toString()).toBe(
+            URI.file('/path/to/test-file_test.py').toString()
+        )
     })
 
-    it('should generate the default spec file path for ruby when no exisiting test files is found', () => {
-        const filePath = '/path/to/file.rb'
-        const existingTestFilePath = undefined
-        const expectedFilePath = '/path/to/file_spec.rb'
-        const expected = withPlatformSlashes(expectedFilePath)
-        expect(convertFsPathToTestFile(filePath, existingTestFilePath)).toBe(expected)
+    it('should generate the default spec file uri for ruby when no existing test files is found', () => {
+        expect(convertFileUriToTestFileUri(URI.file('/path/to/file.rb'), undefined).toString()).toBe(
+            URI.file('/path/to/file_spec.rb').toString()
+        )
+    })
+
+    it('should generate the corrent test file uri for window files', () => {
+        expect(
+            convertFileUriToTestFileUri(
+                URI.file('\\path\\to\\file.ts'),
+                URI.file('\\path\\to\\testFile.test.ts')
+            ).toString()
+        ).toBe(URI.file('\\path\\to\\file.test.ts').toString())
+    })
+
+    it('should follow an existing test file uri format to generate new test file uri on windows', () => {
+        expect(
+            convertFileUriToTestFileUri(
+                URI.file('\\server\\c$\\folder\\current-file.go'),
+                URI.file('\\path\\to\\file_test.go')
+            ).toString()
+        ).toBe(URI.file('\\server\\c$\\folder\\current-file_test.go').toString())
     })
 })
-
-function withPlatformSlashes(input: string) {
-    return input.replaceAll(posix.sep, sep)
-}
