fa5b596637bacea0b1b53cc19dbd21b9a6d28dcb
Beyang Liu <beyang.liu@gmail.com>
2024-01-18 21:47:50 -0800

    test: add squirrel.ts

---
+++ agent/src/__tests__/example-ts/src/squirrel.ts
@@ -0,0 +1,5 @@
+/**
+ * Squirrel is an interface that mocks something completely unrelated to squirrels.
+ * It is related to the implementation of precise code navigation in Sourcegraph.
+ */
+export interface Squirrel {}
