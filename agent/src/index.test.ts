3b9bc79505956a4cd25894007434f90d1e222809
Beatrix <beatrix@sourcegraph.com>
2024-01-19 00:21:08 -0800

    test: increase timeout for edit test

--- agent/src/index.test.ts
+++ agent/src/index.test.ts
@@ -670,7 +670,7 @@ describe('Agent', () => {
                     expect(lastMessage?.text?.toLocaleLowerCase().includes('nana')).toBeTruthy()
                 }
             },
-            { timeout: mayRecord ? 10_000 : undefined }
+            { timeout: mayRecord ? 30_000 : undefined }
         )
     })
 
