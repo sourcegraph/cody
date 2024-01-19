23d77a33401c10cf933259a1f6efd8e3fe477df6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2024-01-19 17:13:02 +0100

    Agent: fix critical bugs related to document synchronization (#2821)
    This PR fixes two critical bugs that prevented enhanced context from
    working as expected:
    
    - Changes to the user's selection were not respected. The agent only
    kept the first selection. This might be a regression from
    https://github.com/sourcegraph/cody/pull/2048
    - Symf results were ignored if they came from files that the user hadn't
    explicitly opened. Now, the agent reads files from disk on
    `vscode.workspace.openTextDocument`.
    
    
    Massive kudos to @pkukielka for spotting the bug related to selections
    and coming up with a reliable minimized reproduction in his PR #2803.
    
    This PR also fixes a few other smaller issues that hit on while working
    on the changes:
    
    - Wrap notification handlers around try/catch. This should fix stability
    issues where the agent process went into a bad state.
    - Document TODOs to all relevant APIs in vscode-shim.ts. I did this
    while trying to see what APIs we might be missing to fix the tests.
    - Sort symf results for more stability in prompt. This is necessary for
    test replay mode, but not necessary for production. I'm OK with adding a
    special case to only do this during testing.
    - Fire `onDidChangeTextEditorSelection` event when the selection
    changes. This influences context logic in some places.
    
    ## Test plan
    
    See updated tests. 
    <!-- Required. See
    https://sourcegraph.com/docs/dev/background-information/testing_principles.
    -->

--- vscode/src/chat/chat-view/SimpleChatPanelProvider.ts
+++ vscode/src/chat/chat-view/SimpleChatPanelProvider.ts
@@ -1310,7 +1310,21 @@ class ContextProvider implements IContextProvider {
             })
             return (await Promise.all(items)).flat()
         })
-        return (await Promise.all(r0)).flat()
+        const allResults = (await Promise.all(r0)).flat()
+
+        // Sort results for deterministic ordering for stable tests. Ideally, we
+        // could sort by some numerical score from symf based on how relevant
+        // the matches are for the query.
+        // Feel free to change this behavior to only sort while running tests.
+        allResults.sort((a, b) => {
+            const byUri = a.uri.fsPath.localeCompare(b.uri.fsPath)
+            if (byUri !== 0) {
+                return byUri
+            }
+            return a.text.localeCompare(b.text)
+        })
+
+        return allResults
     }
 
     private async searchEmbeddingsLocal(text: string): Promise<ContextItem[]> {
