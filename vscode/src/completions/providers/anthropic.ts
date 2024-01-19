872c0e86a038af48eb09dac56c5eaa00bf0e89b2
Philipp Spiess <hello@philippspiess.com>
2024-01-19 16:07:25 +0100

    Whoopsie

--- vscode/src/completions/providers/anthropic.ts
+++ vscode/src/completions/providers/anthropic.ts
@@ -15,7 +15,7 @@ import {
     type PrefixComponents,
 } from '../text-processing'
 import { type ContextSnippet } from '../types'
-import { forkSignal, generatorWithErrorObserver, generatorWithTimeout, messagesToText } from '../utils'
+import { forkSignal, generatorWithErrorObserver, generatorWithTimeout, messagesToText, zipGenerators } from '../utils'
 
 import { type FetchCompletionResult } from './fetch-and-process-completions'
 import { getCompletionParamsAndFetchImpl, getLineNumberDependentCompletionParams } from './get-completion-params'
@@ -207,6 +207,8 @@ class AnthropicProvider extends Provider {
                 providerOptions: this.options,
             })
         })
+
+        return zipGenerators(completionsGenerators)
     }
 
     private postProcess = (rawResponse: string): string => {
