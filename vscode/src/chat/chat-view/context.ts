5415e2b8b87764a66e2bc91f17cee8771d9f0bdc
Philipp Spiess <hello@philippspiess.com>
2024-01-22 12:50:13 +0100

    Extract fusion logic

--- vscode/src/chat/chat-view/context.ts
+++ vscode/src/chat/chat-view/context.ts
@@ -24,6 +24,7 @@ import type { CachedRemoteEmbeddingsClient } from '../CachedRemoteEmbeddingsClie
 import { viewRangeToRange } from './chat-helpers'
 import type { CodebaseStatusProvider } from './CodebaseStatusProvider'
 import type { ContextItem } from './SimpleChatModel'
+import { fuseContext } from '../../context/fuse-context'
 
 const isAgentTesting = process.env.CODY_SHIM_TESTING === 'true'
 
@@ -114,7 +115,7 @@ async function getEnhancedContextFused(
         return getVisibleEditorContext(editor)
     }
 
-    const retrievers: Promise<{ strategy: string; items: ContextItem[] }>[] = []
+    const retrievers: Promise<ContextItem[]>[] = []
     // Get embeddings context if useContext Config is not set to 'keyword' only
     if (useContextConfig !== 'keyword') {
         // Query local embeddings (dense)
@@ -129,7 +130,7 @@ async function getEnhancedContextFused(
 
     const searchContext = await Promise.all(retrievers)
 
-    const fusedContext = fuseContext(searchContext)
+    const fusedContext = fuseContext(searchContext, item => item.uri.toString())
 
     const priorityContext = await getPriorityContext(text, editor, fusedContext)
     return priorityContext.concat(fusedContext)
@@ -521,104 +522,14 @@ function extractQuestion(input: string): string | undefined {
     return undefined
 }
 
-async function retrieveContextGracefully<T>(
-    promise: Promise<T[]>,
-    strategy: string
-): Promise<{ strategy: string; items: T[] }> {
+async function retrieveContextGracefully<T>(promise: Promise<T[]>, strategy: string): Promise<T[]> {
     try {
         logDebug('SimpleChatPanelProvider', `getEnhancedContext > ${strategy} (start)`)
-        return { strategy, items: await promise }
+        return await promise
     } catch (error) {
         logError('SimpleChatPanelProvider', `getEnhancedContext > ${strategy}' (error)`, error)
-        return { strategy, items: [] }
+        return []
     } finally {
         logDebug('SimpleChatPanelProvider', `getEnhancedContext > ${strategy} (end)`)
     }
-}
-
-// k parameter for the reciprocal rank fusion scoring. 60 is the default value in many places
-//
-// c.f. https://learn.microsoft.com/en-us/azure/search/hybrid-search-ranking#how-rrf-ranking-works
-const RRF_K = 60
-function fuseContext(searchContext: { strategy: string; items: ContextItem[] }[]): ContextItem[] {
-    // For every strategy, create a map of snippets by document.
-    const resultsByDocument = new Map<string, { [identifier: string]: ContextItem[] }>()
-    for (const { strategy, items } of searchContext) {
-        for (const item of items) {
-            const documentId = item.uri.toString()
-
-            let document = resultsByDocument.get(documentId)
-            if (!document) {
-                document = {}
-                resultsByDocument.set(documentId, document)
-            }
-            if (!document[strategy]) {
-                document[strategy] = []
-            }
-            document[strategy].push(item)
-        }
-    }
-
-    // Rank the order of documents using reciprocal rank fusion.
-    //
-    // For this, we take the top rank of every document from each retrieved set and compute a
-    // combined rank. The idea is that a document that ranks highly across multiple retrievers
-    // should be ranked higher overall.
-    const fusedDocumentScores: Map<string, number> = new Map()
-    for (const { strategy, items } of searchContext) {
-        items.forEach((item, rank) => {
-            const documentId = item.uri.toString()
-
-            // Since every retriever can return many snippets for a given document, we need to
-            // only consider the best rank for each document.
-            // We can use the previous map by document to find the highest ranked snippet for a
-            // retriever
-            const isBestRankForRetriever = resultsByDocument.get(documentId)?.[strategy][0] === item
-            if (!isBestRankForRetriever) {
-                return
-            }
-
-            const reciprocalRank = 1 / (RRF_K + rank)
-
-            const score = fusedDocumentScores.get(documentId)
-            if (score === undefined) {
-                fusedDocumentScores.set(documentId, reciprocalRank)
-            } else {
-                fusedDocumentScores.set(documentId, score + reciprocalRank)
-            }
-        })
-    }
-
-    const fusedDocuments = [...fusedDocumentScores.entries()].sort((a, b) => b[1] - a[1]).map(e => e[0])
-
-    const fusedContext: ContextItem[] = []
-    // Now that we have a sorted list of documents (with the first document being the highest
-    // ranked one), we use top-k to combine snippets from each retriever into a result set.
-    //
-    // We start with the highest ranked document and include all retrieved snippets from this
-    // document into the result set, starting with the top retrieved snippet from each retriever
-    // and adding entries greedily.
-    for (const documentId of fusedDocuments) {
-        const resultByDocument = resultsByDocument.get(documentId)
-        if (!resultByDocument) {
-            continue
-        }
-
-        // We want to start iterating over every retrievers first rank, then every retrievers
-        // second rank etc. The termination criteria is thus defined to be the length of the
-        // largest snippet list of any retriever.
-        const maxMatches = Math.max(...Object.values(resultByDocument).map(r => r.length))
-
-        for (let i = 0; i < maxMatches; i++) {
-            for (const [_, snippets] of Object.entries(resultByDocument)) {
-                if (i >= snippets.length) {
-                    continue
-                }
-                const snippet = snippets[i]
-
-                fusedContext.push(snippet)
-            }
-        }
-    }
-    return fusedContext
 }
