070edc5b206dfbc1b8572ec7d897ab09cb30351d
Philipp Spiess <hello@philippspiess.com>
2024-01-24 11:29:09 +0100

    Fix tests

--- vscode/src/completions/logger.test.ts
+++ vscode/src/completions/logger.test.ts
@@ -14,6 +14,7 @@ import { documentAndPosition } from './test-helpers'
 const defaultArgs = {
     multiline: false,
     triggerKind: TriggerKind.Automatic,
+    testFile: false,
     providerIdentifier: 'bfl',
     providerModel: 'blazing-fast-llm',
     languageId: 'typescript',
