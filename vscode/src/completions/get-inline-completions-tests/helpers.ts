82d51a1fd782519945d62c72e55ac426487d7d7a
Valery Bugakov <skymk1@gmail.com>
2024-01-24 19:04:19 -0800

    Autocomplete: add test utility for completions streaming (#2880)
    - Adds a new autocomplete testing utility,
    `getInlineCompletionsWithInlinedChunks,` which aims to reduce the
    boilerplate code required to test completions generated from multiple
    chunks received during HTTP streaming.
    - On the separator symbol: I want to reduce the number of symbols we
    need to copy-paste manually to annotate code sections as prefix, suffix,
    and completion chunks; that's why I propose using only one symbol for
    annotation. There's no ambiguity regarding how this annotation can be
    interpreted, and it allows us to add new tests quickly (or change
    existing ones). It also makes it easier to understand what the final
    code would look like.
    - I tried different symbols from the emoji keyboard, like ⎮, ⌷, ⍁, etc.
    But I didn't find a symbol that would provide as good readability as the
    one we use right now (because it's bold and makes it easy to identify
    parts of the code). I'm happy to switch to whatever other symbol is
    proposed.
    - Migrate hot-streak and dynamic multiline completions integration tests
    to this utility.
    - Adds new dynamic multiline completions integration tests to cover more
    cases.

--- vscode/src/completions/get-inline-completions-tests/helpers.ts
+++ vscode/src/completions/get-inline-completions-tests/helpers.ts
@@ -1,3 +1,4 @@
+import dedent from 'dedent'
 import { isEqual } from 'lodash'
 import { expect } from 'vitest'
 
@@ -21,14 +22,16 @@ import {
     TriggerKind,
     getInlineCompletions as _getInlineCompletions,
     type InlineCompletionsParams,
+    type InlineCompletionsResult,
 } from '../get-inline-completions'
 import {
+    createProviderConfig,
     MULTI_LINE_STOP_SEQUENCES,
     SINGLE_LINE_STOP_SEQUENCES,
-    createProviderConfig,
 } from '../providers/anthropic'
+import type { ProviderOptions } from '../providers/provider'
 import { RequestManager } from '../request-manager'
-import { documentAndPosition } from '../test-helpers'
+import { documentAndPosition, sleep } from '../test-helpers'
 
 // The dedent package seems to replace `\t` with `\\t` so in order to insert a tab character, we
 // have to use interpolation. We abbreviate this to `T` because ${T} is exactly 4 characters,
@@ -44,6 +47,7 @@ type Params = Partial<Omit<InlineCompletionsParams, 'document' | 'position' | 'd
     completionResponseGenerator?: (
         params: CompletionParameters
     ) => CompletionResponseGenerator | Generator<CompletionResponse>
+    providerOptions?: Partial<ProviderOptions>
 }
 
 interface ParamsResult extends InlineCompletionsParams {
@@ -73,6 +77,7 @@ export function params(
         selectedCompletionInfo,
         takeSuggestWidgetSelectionIntoAccount,
         isDotComUser = false,
+        providerOptions,
         ...restParams
     } = params
 
@@ -96,14 +101,17 @@ export function params(
             }
 
             if (responses === 'never-resolve') {
-                await new Promise(() => {})
+                return new Promise(() => {})
             }
 
-            return responses?.[requestCounter++] || { completion: '', stopReason: 'unknown' }
+            return responses[requestCounter++] || { completion: '', stopReason: 'unknown' }
         },
     }
 
-    const providerConfig = createProviderConfig({ client })
+    const providerConfig = createProviderConfig({
+        client,
+        providerOptions,
+    })
 
     const { document, position } = documentAndPosition(code, languageId, URI_FIXTURE.toString())
 
@@ -152,29 +160,138 @@ export function params(
     }
 }
 
+interface ParamsWithInlinedCompletion extends Params {
+    delayBetweenChunks?: number
+}
+
+/**
+ * A test helper to create the parameters for {@link getInlineCompletions} with a completion
+ * that's inlined in the code. Examples:
+ *
+ * 1. Params with prefix and suffix only and no completion response.
+ *
+ * function myFunction() {
+ *   █
+ * }
+ *
+ * E.g. { prefix: "function myFunction() {\n  ", suffix: "\n}" }
+ *
+ * 2. Params with prefix, suffix and the full completion response received with no intermediate chunks.
+ *
+ * function myFunction() {
+ *   █const result = {
+ *     value: 1,
+ *     debug: true
+ *   }
+ *   return result█
+ * }
+ *
+ * 3. Params with prefix, suffix and three completion chunks.
+ *
+ * function myFunction() {
+ *   █const result = {
+ *     value: 1,█
+ *     debug: true
+ *   }█
+ *   return result█
+ * }
+ */
+export function paramsWithInlinedCompletion(
+    code: string,
+    { delayBetweenChunks, ...completionParams }: ParamsWithInlinedCompletion = {}
+): ParamsResult {
+    const chunks = dedent(code).split('█')
+
+    if (chunks.length < 2) {
+        throw new Error(
+            'Code example must include a block character (█) to denote the current cursor position.'
+        )
+    }
+
+    // For cases where no network request needed because a completion is cached already
+    if (chunks.length === 2) {
+        const [prefix, suffix] = chunks
+        return params([prefix, suffix].join('█'), [], completionParams)
+    }
+
+    // The full completion is received right away with no intermediate chunks
+    if (chunks.length === 3) {
+        const [prefix, completion, suffix] = chunks
+        return params([prefix, suffix].join('█'), [{ completion, stopReason: '' }], completionParams)
+    }
+
+    const [prefix, ...completionChunks] = chunks
+    const suffix = completionChunks.pop()!
+    const completion = completionChunks.join('')
+
+    // The completion is streamed and processed chunk by chunk
+    return params([prefix, suffix].join('█'), [{ completion, stopReason: '' }], {
+        async *completionResponseGenerator() {
+            let lastResponse = ''
+
+            for (const completionChunk of completionChunks) {
+                lastResponse += completionChunk
+                yield {
+                    completion: lastResponse,
+                    stopReason: STOP_REASON_STREAMING_CHUNK,
+                }
+
+                if (delayBetweenChunks) {
+                    await sleep(delayBetweenChunks)
+                }
+            }
+        },
+        ...completionParams,
+    })
+}
+
+/**
+ * A wrapper around `getInlineCompletions` helper with a few differences optimized for the
+ * most popular test cases with the aim to reduce the boilerplate code:
+ *
+ * 1. Uses `paramsWithInlinedCompletion` internally to create arguments for `getInlineCompletions`
+ * which allows the consumer to define prefix, suffix and completion chunks in one template literal.
+ * 2. Throws an error is the returned result is `null`. We can still use a lower level.
+ * 3. Returns `params` a part of the result too, allowing to use its values in tests.
+ */
+export async function getInlineCompletionsWithInlinedChunks(
+    code: string,
+    completionParams: ParamsWithInlinedCompletion = {}
+): Promise<Omit<ParamsResult & InlineCompletionsResult, 'logId'>> {
+    const params = paramsWithInlinedCompletion(code, completionParams)
+    const result = await getInlineCompletions(params)
+
+    if (!result) {
+        throw new Error('This test helpers should always return a result')
+    }
+
+    return { ...params, ...result }
+}
+
 /**
  * Wraps the `getInlineCompletions` function to omit `logId` so that test expected values can omit
  * it and be stable.
  */
 export async function getInlineCompletions(
-    ...args: Parameters<typeof _getInlineCompletions>
-): Promise<Omit<NonNullable<Awaited<ReturnType<typeof _getInlineCompletions>>>, 'logId'> | null> {
-    const result = await _getInlineCompletions(...args)
+    params: ParamsResult
+): Promise<Omit<InlineCompletionsResult, 'logId'> | null> {
+    const result = await _getInlineCompletions(params)
+
     if (result) {
         const { logId: _discard, ...rest } = result
+
         return {
             ...rest,
             items: result.items.map(({ stopReason: discard, ...item }) => item),
         }
     }
+
     return result
 }
 
 /** Test helper for when you just want to assert the completion strings. */
-export async function getInlineCompletionsInsertText(
-    ...args: Parameters<typeof _getInlineCompletions>
-): Promise<string[]> {
-    const result = await getInlineCompletions(...args)
+export async function getInlineCompletionsInsertText(params: ParamsResult): Promise<string[]> {
+    const result = await getInlineCompletions(params)
     return result?.items.map(c => c.insertText) ?? []
 }
 
