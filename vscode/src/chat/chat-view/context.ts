3a474ca702300ef883e577c0192751472729c2e8
Beatrix <beatrix@sourcegraph.com>
2024-01-26 11:35:33 -0800

    add security messages for cody ignore

--- vscode/src/chat/chat-view/context.ts
+++ vscode/src/chat/chat-view/context.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode'
 
 import {
-    isCodyIgnoredFile,
     isFileURI,
     MAX_BYTES_PER_FILE,
     NUM_CODE_RESULTS,
@@ -204,9 +203,6 @@ async function searchSymf(
     const r0 = (await symf.getResults(userText, [workspaceRoot])).flatMap(async results => {
         const items = (await results).flatMap(
             async (result: Result): Promise<ContextItem[] | ContextItem> => {
-                if (isCodyIgnoredFile(result.file)) {
-                    return []
-                }
                 const range = new vscode.Range(
                     result.range.startPoint.row,
                     result.range.startPoint.col,
@@ -274,15 +270,12 @@ async function searchEmbeddingsLocal(
             new vscode.Position(result.endLine, 0)
         )
 
-        // Filter out ignored files
-        if (!isCodyIgnoredFile(result.uri)) {
-            contextItems.push({
-                uri: result.uri,
-                range,
-                text: result.content,
-                source: 'embeddings',
-            })
-        }
+        contextItems.push({
+            uri: result.uri,
+            range,
+            text: result.content,
+            source: 'embeddings',
+        })
     }
     return contextItems
 }
@@ -296,7 +289,7 @@ const userAttentionRegexps: RegExp[] = [
 
 function getCurrentSelectionContext(editor: VSCodeEditor): ContextItem[] {
     const selection = editor.getActiveTextEditorSelection()
-    if (!selection?.selectedText || isCodyIgnoredFile(selection.fileUri)) {
+    if (!selection?.selectedText) {
         return []
     }
     let range: vscode.Range | undefined
@@ -325,7 +318,7 @@ function getVisibleEditorContext(editor: VSCodeEditor): ContextItem[] {
     if (!visible || !fileUri) {
         return []
     }
-    if (isCodyIgnoredFile(fileUri) || !visible.content.trim()) {
+    if (!visible.content.trim()) {
         return []
     }
     return [
@@ -433,7 +426,7 @@ async function getReadmeContext(): Promise<ContextItem[]> {
     // global pattern for readme file
     const readmeGlobalPattern = '{README,README.,readme.,Readm.}*'
     const readmeUri = (await vscode.workspace.findFiles(readmeGlobalPattern, undefined, 1)).at(0)
-    if (!readmeUri || isCodyIgnoredFile(readmeUri)) {
+    if (!readmeUri?.path) {
         return []
     }
     const readmeDoc = await vscode.workspace.openTextDocument(readmeUri)
