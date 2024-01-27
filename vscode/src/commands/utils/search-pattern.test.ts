1a2e11945538e0733a3c435a2bd15e8cd70849a7
Beatrix <beatrix@sourcegraph.com>
2024-01-26 17:53:41 -0800

    support windows path

--- vscode/src/commands/utils/search-pattern.test.ts
+++ vscode/src/commands/utils/search-pattern.test.ts
@@ -1,31 +1,34 @@
 import { describe, expect, it } from 'vitest'
 import { URI } from 'vscode-uri'
 import { getSearchPatternForTestFiles } from './search-pattern'
+import * as path from 'path'
 
-describe('getSearchPatternForTestFiles', () => {
+describe.only('getSearchPatternForTestFiles', () => {
     it('returns pattern searching current directory for test files with same extension', () => {
         const file = URI.file('/path/to/file.js')
         const pattern = getSearchPatternForTestFiles(file, true)
-        expect(pattern).toEqual('/path/to/*{test,spec}*.js')
+        expect(pattern).toEqual(path.normalize('/path/to/*{test,spec}*.js'))
     })
 
     it('returns pattern searching workspace for test files matching file name', () => {
         const file = URI.file('/path/to/file.ts')
         const pattern = getSearchPatternForTestFiles(file, false, true)
         expect(pattern).toEqual(
-            '**/*{test_file,file_test,test.file,file.test,fileTest,spec_file,file_spec,spec.file,file.spec,fileSpec}.ts'
+            path.normalize(
+                '**/*{test_file,file_test,test.file,file.test,fileTest,spec_file,file_spec,spec.file,file.spec,fileSpec}.ts'
+            )
         )
     })
 
     it('returns pattern searching workspace for test files with same extension', () => {
         const file = URI.file('/path/to/file.py')
         const pattern = getSearchPatternForTestFiles(file)
-        expect(pattern).toEqual('**/*{test,spec}*.py')
+        expect(pattern).toEqual(path.normalize('**/*{test,spec}*.py'))
     })
 
     it('handles files with no extension', () => {
         const file = URI.file('/path/to/file')
         const pattern = getSearchPatternForTestFiles(file)
-        expect(pattern).toEqual('**/*{test,spec}*')
+        expect(pattern).toEqual(path.normalize('**/*{test,spec}*'))
     })
 })
