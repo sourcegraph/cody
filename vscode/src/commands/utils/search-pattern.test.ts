c5d77745e8f01977f1e6bfb7e1055b9ed0c399db
Beatrix <beatrix@sourcegraph.com>
2024-01-26 18:29:33 -0800

    ooops

--- vscode/src/commands/utils/search-pattern.test.ts
+++ vscode/src/commands/utils/search-pattern.test.ts
@@ -1,20 +1,20 @@
 import { describe, expect, it } from 'vitest'
 import { URI } from 'vscode-uri'
 import { getSearchPatternForTestFiles } from './search-pattern'
-import * as path from 'path'
+import path from 'path/posix'
 
 describe.only('getSearchPatternForTestFiles', () => {
     it('returns pattern searching current directory for test files with same extension', () => {
         const file = URI.file('/path/to/file.js')
         const pattern = getSearchPatternForTestFiles(file, true)
-        expect(pattern).toEqual(path.normalize('/path/to/*{test,spec}*.js'))
+        expect(pattern).toEqual(osPath('path/to/*{test,spec}*.js'))
     })
 
     it('returns pattern searching workspace for test files matching file name', () => {
         const file = URI.file('/path/to/file.ts')
         const pattern = getSearchPatternForTestFiles(file, false, true)
         expect(pattern).toEqual(
-            path.normalize(
+            osPath(
                 '**/*{test_file,file_test,test.file,file.test,fileTest,spec_file,file_spec,spec.file,file.spec,fileSpec}.ts'
             )
         )
@@ -23,12 +23,17 @@ describe.only('getSearchPatternForTestFiles', () => {
     it('returns pattern searching workspace for test files with same extension', () => {
         const file = URI.file('/path/to/file.py')
         const pattern = getSearchPatternForTestFiles(file)
-        expect(pattern).toEqual(path.normalize('**/*{test,spec}*.py'))
+        expect(pattern).toEqual(osPath('**/*{test,spec}*.py'))
     })
 
     it('handles files with no extension', () => {
         const file = URI.file('/path/to/file')
         const pattern = getSearchPatternForTestFiles(file)
-        expect(pattern).toEqual(path.normalize('**/*{test,spec}*'))
+        expect(pattern).toEqual(osPath('**/*{test,spec}*'))
     })
 })
+
+// regex used for removing the leading separator from the path
+const systemSep = path.sep
+const regex = new RegExp(`^${systemSep}`)
+const osPath = (pattern: string): string => URI.file(pattern).path.replace(regex, '')
--- vscode/src/commands/utils/search-pattern.ts
+++ vscode/src/commands/utils/search-pattern.ts
@@ -17,21 +17,25 @@ export function getSearchPatternForTestFiles(
     // Files with the same name as the current file
     fileNameMatchesOnly?: boolean
 ): string {
+    const osSep = path.posix.sep
     const fileExtension = path.posix.parse(file.path).ext
     const basenameWithoutExt = path.posix.parse(file.path).name
-    // create a relative path using path package
-    const curDirRelativePath = path.posix.parse(file.path).dir
 
-    const root = currentDirectoryOnly ? curDirRelativePath : '**'
     const testPattern = `*{test,spec}*${fileExtension}`
     const nameMatchPattern = `*{test_${basenameWithoutExt},${basenameWithoutExt}_test,test.${basenameWithoutExt},${basenameWithoutExt}.test,${basenameWithoutExt}Test,spec_${basenameWithoutExt},${basenameWithoutExt}_spec,spec.${basenameWithoutExt},${basenameWithoutExt}.spec,${basenameWithoutExt}Spec}${fileExtension}`
 
-    const osSep = path.sep
-
-    // pattern to search for test files in the current directory
+    // Pattern to search for test files in the current directory
     if (currentDirectoryOnly) {
-        return root + osSep + testPattern
+        // Create a relative path using path package
+        // and use regex to remove the leading separator from the path
+        const dir = path.posix.parse(file.path).dir
+        const osSep = path.sep
+        const regex = new RegExp(`^${osSep}`)
+
+        return dir.replace(regex, '') + osSep + testPattern
     }
+
+    const root = '**'
 
     // pattern to search for test files with the same name as current file
     if (fileNameMatchesOnly) {
