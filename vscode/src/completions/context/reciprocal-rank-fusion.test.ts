4ebcaab7bed5fcf207e3c4799ed327ada1c1f805
Philipp Spiess <hello@philippspiess.com>
2024-01-30 12:50:24 +0100

    Add a better example

--- vscode/src/completions/context/reciprocal-rank-fusion.test.ts
+++ vscode/src/completions/context/reciprocal-rank-fusion.test.ts
@@ -9,9 +9,9 @@ describe('fuseResults', () => {
             new Set([{ id: 1 }, { id: 2 }]),
             new Set([{ id: 2 }, { id: 3 }]),
         ]
-        const rankingIdentity = (item: { id: number }) => [item.id.toString()]
+        const rankingIdentities = (item: { id: number }) => [item.id.toString()]
 
-        const results = fuseResults(retrievers, rankingIdentity)
+        const results = fuseResults(retrievers, rankingIdentities)
 
         expect(results).toEqual(new Set([{ id: 2 }, { id: 2 }, { id: 1 }, { id: 3 }]))
     })
@@ -22,9 +22,9 @@ describe('fuseResults', () => {
             new Set([{ id: 1 }, { id: 2 }, { id: 2 }]),
             new Set([{ id: 2 }, { id: 3 }])
         ]
-        const rankingIdentity = (item: { id: number }) => [item.id.toString()]
+        const rankingIdentities = (item: { id: number }) => [item.id.toString()]
 
-        const results = fuseResults(retrievers, rankingIdentity)
+        const results = fuseResults(retrievers, rankingIdentities)
 
         expect(results).toEqual(new Set([{ id: 2 }, { id: 2 }, { id: 2 }, { id: 1 }, { id: 3 }]))
     })
@@ -34,9 +34,9 @@ describe('fuseResults', () => {
             new Set([{ lines: [1, 2, 3, 4] }, { lines: [7, 8] }]),
             new Set([{ lines: [1, 2] }, { lines: [5, 6] }]),
         ]
-        const rankingIdentity = (item: { lines: number[] }) => item.lines.map(id => id.toString())
+        const rankingIdentities = (item: { lines: number[] }) => item.lines.map(id => id.toString())
 
-        const results = fuseResults(retrievers, rankingIdentity)
+        const results = fuseResults(retrievers, rankingIdentities)
 
         expect(results).toEqual(
             new Set([{ lines: [1, 2, 3, 4] }, { lines: [1, 2] }, { lines: [7, 8] }, { lines: [5, 6] }])
@@ -45,9 +45,9 @@ describe('fuseResults', () => {
 
     it('retains the right order of duplicated documents', () => {
         const retrievers = [new Set([3]), new Set([1, 2])]
-        const rankingIdentity = () => ['same']
+        const rankingIdentities = () => ['same']
 
-        const results = fuseResults(retrievers, rankingIdentity)
+        const results = fuseResults(retrievers, rankingIdentities)
 
         expect(results).toEqual(new Set([3, 1, 2]))
     })
--- vscode/src/completions/context/reciprocal-rank-fusion.ts
+++ vscode/src/completions/context/reciprocal-rank-fusion.ts
@@ -7,23 +7,43 @@ const RRF_K = 60
  * Implements a basic variant of reciprocal rank fusion to combine context items from various
  * retrievers into one result set.
  *
- * Since the definition of documents can vary across the consumers, a rankingIdentity function needs
+ * Since the definition of documents can vary across the consumers, a rankingIdentities function needs
  * to be implemented that returns a set of identifiers for a document. A set is used to support our
  * needs of varying context windows across Cody retrievers. In our case, these ranking identity
  * would yield one identifier for every line of code affected by a context window.
  *
  * When combining the top ranked documents, we will make sure that no item is added twice (as
  * denoted by their referential equality).
+ *
+ * For an example, consider two retrievers that return results of line ranges within a code file:
+ *
+ *   - Set 1: [foo.ts#20-25, foo.ts#1-5]
+ *   - Set 2: [foo.ts#3-7]
+ *
+ * The ranking identity function can now generate unique identifiers for every row that is being
+ * retrieved, so we end up having the following IDs in this example:
+ *
+ *   - foo.ts#20-25 => [foo.ts:20, foo.ts:21, foo.ts:22, foo.ts:23, foo.ts:24, foo.ts:25]
+ *   - foo.ts#1-5   => [foo.ts:1, foo.ts:2, foo.ts:3, foo.ts:4, foo.ts:5]
+ *   - foo.ts#3-7   => [foo.ts:3, foo.ts:3, foo.ts:4, foo.ts:5, foo.ts:6, foo.ts:7]
+ *
+ * Based on this expanded set of "documents", we now apply RRF and find overlaps at the following
+ * places:
+ *
+ *  - [foo.ts:3 foo.ts:4, foo.ts:5]
+ *
+ * RRF will now boost the rank for these three lins and when we reconcile the result set, we
+ * will start by picking all ranges that overlap with the first line.
  */
 export function fuseResults<T>(
     retrievedSets: Set<T>[],
-    rankingIdentity: (result: T) => string[]
+    rankingIdentities: (result: T) => string[]
 ): Set<T> {
     // For every retrieved result set, create a map of results by document.
     const resultsByDocument = new Map<string, Map<number, T[]>>()
     retrievedSets.forEach((results, retrieverIndex) => {
         for (const result of results) {
-            for (const id of rankingIdentity(result)) {
+            for (const id of rankingIdentities(result)) {
                 let document = resultsByDocument.get(id)
                 if (!document) {
                     document = new Map()
@@ -48,7 +68,7 @@ export function fuseResults<T>(
         let i = 0
         for (const result of results) {
             const rank = i++
-            for (const id of rankingIdentity(result)) {
+            for (const id of rankingIdentities(result)) {
                 // There is no guarantee that retrievers do not have more than one result in the
                 // same document (e.g. when the document definition is too broad). For these cases,
                 // we only consider the best ranked result per document for each retriever. We can
