08403943021ed2290e7d1417281596f83901fd29
Philipp Spiess <hello@philippspiess.com>
2024-01-30 12:57:21 +0100

    Fixes

--- vscode/src/completions/context/reciprocal-rank-fusion.test.ts
+++ vscode/src/completions/context/reciprocal-rank-fusion.test.ts
@@ -13,6 +13,10 @@ describe('fuseResults', () => {
 
         const results = fuseResults(retrievers, rankingIdentities)
 
+        // The first set has documents with IDs 1 and 2. The second set has documents with IDs 2 and 3.
+        //
+        // The document with ID 2 appears in both sets, making it more significant according to RRF.
+        // Therefore, it gains a higher combined rank and appears first in the fused result.
         expect(results).toEqual(new Set([{ id: 2 }, { id: 2 }, { id: 1 }, { id: 3 }]))
     })
 
@@ -29,17 +33,31 @@ describe('fuseResults', () => {
         expect(results).toEqual(new Set([{ id: 2 }, { id: 2 }, { id: 2 }, { id: 1 }, { id: 3 }]))
     })
 
+    // Note: Dedupe here refers to the same result being added two times, we don't drop results that
+    //       point to the same document ID. We `fuseResults` should always return the same number of
+    //       results as the sum of all result sets.
     it('handles the same result being part of multiple documents without getting duplicated', () => {
         const retrievers = [
-            new Set([{ lines: [1, 2, 3, 4] }, { lines: [7, 8] }]),
-            new Set([{ lines: [1, 2] }, { lines: [5, 6] }]),
+            new Set([
+                { name: 'set1doc1', lines: [1, 2, 3, 4] },
+                { name: 'set1doc2', lines: [7, 8] },
+            ]),
+            new Set([
+                { name: 'set2doc1', lines: [1, 2] },
+                { name: 'set2doc2', lines: [5, 6] },
+            ]),
         ]
         const rankingIdentities = (item: { lines: number[] }) => item.lines.map(id => id.toString())
 
         const results = fuseResults(retrievers, rankingIdentities)
 
         expect(results).toEqual(
-            new Set([{ lines: [1, 2, 3, 4] }, { lines: [1, 2] }, { lines: [7, 8] }, { lines: [5, 6] }])
+            new Set([
+                { name: 'set1doc1', lines: [1, 2, 3, 4] },
+                { name: 'set2doc1', lines: [1, 2] },
+                { name: 'set1doc2', lines: [7, 8] },
+                { name: 'set2doc2', lines: [5, 6] },
+            ])
         )
     })
 
