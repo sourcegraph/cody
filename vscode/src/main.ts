594a1d8272bc43b1163e934837f84e0f31486bf6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2024-01-30 21:10:21 +0100

    INC-267: add test case to reproduce regression (#2954)
    The PR https://github.com/sourcegraph/cody/pull/2943 removed an `await`
    to resolve an incident where users were unable to activate the Cody
    extension if they were signed out. This PR adds test cases to reproduce
    the original bug to prevent similar regressions from getting adding back
    into the codebase later.
    
    ## Test plan
    
    Green CI.
    <!-- Required. See
    https://sourcegraph.com/docs/dev/background-information/testing_principles.
    -->

--- vscode/src/main.ts
+++ vscode/src/main.ts
@@ -584,9 +584,7 @@ const register = async (
         return setupAutocompleteQueue
     }
 
-    const promises: Promise<void>[] = []
-
-    promises.push(setupAutocomplete().catch(() => {}))
+    const autocompleteSetup = setupAutocomplete().catch(() => {})
 
     if (initialConfig.experimentalGuardrails) {
         const guardrailsProvider = new GuardrailsProvider(guardrails, editor)
@@ -597,7 +595,11 @@ const register = async (
         )
     }
 
-    promises.push(showSetupNotification(initialConfig))
+    // INC-267 do NOT await on this promise. This promise triggers
+    // `vscode.window.showInformationMessage()`, which only resolves after the
+    // user has clicked on "Setup". Awaiting on this promise will make the Cody
+    // extension timeout during activation.
+    void showSetupNotification(initialConfig)
 
     // Register a serializer for reviving the chat panel on reload
     if (vscode.window.registerWebviewPanelSerializer) {
@@ -610,6 +612,8 @@ const register = async (
             },
         })
     }
+
+    await autocompleteSetup
 
     return {
         disposable: vscode.Disposable.from(...disposables),
