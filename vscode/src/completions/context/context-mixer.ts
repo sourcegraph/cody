21809f8c39576191a7427298b086c4d6a9fe2605
Arafatkatze <arafat.da.khan@gmail.com>
2024-01-30 17:24:06 -0800

    Adding basic cody ignore for inline completions

--- vscode/src/completions/context/context-mixer.ts
+++ vscode/src/completions/context/context-mixer.ts
@@ -91,6 +91,7 @@ export class ContextMixer implements vscode.Disposable {
                             },
                         })
                 )
+                // Filtering out snippets that belong to ignored files
                 const filteredSnippets = allSnippets.filter(snippet => !isCodyIgnoredFile(snippet.uri))
 
                 return {
