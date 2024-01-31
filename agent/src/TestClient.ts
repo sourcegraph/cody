0e9e929aa6ae760480d9ea6ee9a165d7151776ce
Olafur Geirsson <olafurpg@gmail.com>
2024-01-31 13:06:05 +0100

    Disable feature flags in agent tests

--- agent/src/TestClient.ts
+++ agent/src/TestClient.ts
@@ -63,6 +63,7 @@ export class TestClient extends MessageHandler {
             readonly accessToken?: string
             serverEndpoint?: string
             telemetryExporter?: 'testing' | 'graphql' // defaults to testing, which doesn't send telemetry
+            featureFlags?: 'enabled' // defaults to testing, which doesn't send telemetry
             logEventMode?: 'connected-instance-only' | 'all' | 'dotcom-only'
             onWindowRequest?: (params: ShowWindowMessageParams) => Promise<string>
         }
@@ -478,6 +479,8 @@ export class TestClient extends MessageHandler {
                 CODY_RECORDING_NAME: this.name,
                 SRC_ACCESS_TOKEN: this.accessToken,
                 CODY_TELEMETRY_EXPORTER: this.params.telemetryExporter ?? 'testing',
+                BENCHMARK_DISABLE_FEATURE_FLAGS:
+                    this.params.featureFlags === 'enabled' ? undefined : 'true',
                 CODY_LOG_EVENT_MODE: this.params.logEventMode,
                 ...process.env,
             },
