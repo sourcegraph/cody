f6801b65d6b369d7b6ca29bc54fcd8b7ebfae4f6
Beatrix <68532117+abeatrix@users.noreply.github.com>
2024-01-31 08:09:27 -0800

    Apply suggestions from code review
    Co-authored-by: Tom Ross <tom@umpox.com>

--- vscode/src/commands/execute/test-case.ts
+++ vscode/src/commands/execute/test-case.ts
@@ -17,7 +17,7 @@ export async function executeTestCaseCommand(
     args?: Partial<CodyCommandArgs>
 ): Promise<EditCommandResult | undefined> {
     const instruction =
-        'Review the shared code context to identify the testing framework and libraries in use. Then, create multiple new unit tests for the functions in <selected> following the same patterns, testing conventions and testing library as shown in shared context. Pay attention to the shared context to ensure your response code do not contain cases that have already been covered. Focus on generating new unit tests for uncovered cases. Response only with the full completed code with the new unit tests added at the end, without any comments, fragments or TODO. The new tests should validate expected functionality and cover edge cases for <selected>. Do not include any markdown formatting or triple backticks. The goal is to provide me with code that I can add to the end of existing test file. Enclose only the new tests WITHOUT ANY suit, import statements or packages in your response.'
+        'Review the shared code context to identify the testing framework and libraries in use. Then, create multiple new unit tests for the functions in <selected> following the same patterns, testing conventions and testing library as shown in shared context. Pay attention to the shared context to ensure your response code do not contain cases that have already been covered. Focus on generating new unit tests for uncovered cases. Response only with the full completed code with the new unit tests added at the end, without any comments, fragments or TODO. The new tests should validate expected functionality and cover edge cases for <selected>. Do not include any markdown formatting or triple backticks. The goal is to provide me with code that I can add to the end of existing test file. Enclose only the new tests WITHOUT ANY suite, import statements or packages in your response.'
 
     const editor = getEditor()?.active
     const document = editor?.document
@@ -34,9 +34,7 @@ export async function executeTestCaseCommand(
         logError('executeNewTestCommand', 'failed to fetch context', { verbose: error })
     }
 
-    const startLine = editor.selection.start.line + 1
-    const endLine = Math.max(startLine, editor.selection.end.line)
-    const range = new Range(startLine, 0, endLine, 0)
+    const range = new Range(editor.selection.end.line, 0, editor.selection.end.line, 0)
 
     return {
         type: 'edit',
@@ -45,7 +43,7 @@ export async function executeTestCaseCommand(
                 instruction,
                 document,
                 range,
-                intent: 'edit',
+                intent: 'add',
                 mode: 'insert',
                 userContextFiles: contextFiles,
             } satisfies ExecuteEditArguments,
--- vscode/src/commands/execute/test-file.ts
+++ vscode/src/commands/execute/test-file.ts
@@ -20,9 +20,9 @@ export async function executeTestCommand(
 ): Promise<EditCommandResult | undefined> {
     // The prompt for generating tests in a new test file
     const newTestFilePrompt = defaultCommands.test.prompt
-    // The prompt for adding new test suit to an existing test file
-    const newTestSuitPrompt =
-        'Review the shared code context to identify the testing framework and libraries in use. Then, create multiple new unit tests for the functions in <selected> following the same patterns, testing conventions and testing library as shown in shared context. Pay attention to the shared context to ensure your response code do not contain cases that have already been covered. Focus on generating new unit tests for uncovered cases. Response only with the full completed code with the new unit tests added at the end, without any comments, fragments or TODO. The new tests should validate expected functionality and cover edge cases for <selected>. Do not include any markdown formatting or triple backticks. The goal is to provide me with code that I can add to the end of existing test file. Enclose only the new test suit WITHOUT ANY import statements or packages in your response.'
+    // The prompt for adding new test suite to an existing test file
+    const newTestSuitePrompt =
+        'Review the shared code context to identify the testing framework and libraries in use. Then, create multiple new unit tests for the functions in <selected> following the same patterns, testing conventions and testing library as shown in shared context. Pay attention to the shared context to ensure your response code do not contain cases that have already been covered. Focus on generating new unit tests for uncovered cases. Response only with the full completed code with the new unit tests added at the end, without any comments, fragments or TODO. The new tests should validate expected functionality and cover edge cases for <selected>. Do not include any markdown formatting or triple backticks. The goal is to provide me with code that I can add to the end of existing test file. Enclose only the new test suite WITHOUT ANY import statements or packages in your response.'
 
     const editor = getEditor()?.active
     const document = editor?.document
