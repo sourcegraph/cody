a6f91e8d83c81bf691fabd85fb11897cc48d5fbf
Valery Bugakov <skymk1@gmail.com>
2024-02-01 01:17:40 -0800

    Autocomplete: update BFG snippets structure to match the format expected on the client (#2987)
    - I started testing BFG locally and bumped into a runtime error. The
    context mixer
    [logic](https://github.com/sourcegraph/cody/blob/7106a4fc2c8d088961f66de2336217a87805136f/vscode/src/completions/context/context-mixer.ts#L110)
    expects context snippets to have the `uri` field. This field [was
    added](https://github.com/sourcegraph/cody/pull/2749) relatively
    recently, but neither the BFG implementation nor the client retriever
    was updated accordingly.
    - This PR fixes the issue on the client side and adds additional types
    that would prevent the problem from happening. AFAIU, this solution is
    similar to the approach `LocalEmbeddingsController` uses, where local
    embeddings snippet types returned by Cody engine [are defined
    separately](https://github.com/sourcegraph/cody/blob/main/vscode/src/jsonrpc/embeddings-protocol.ts#L21)
    from [global
    types](https://github.com/sourcegraph/cody/blob/main/vscode/src/completions/types.ts#L32)
    used by the context mixer.

--- vscode/src/jsonrpc/bfg-protocol.ts
+++ vscode/src/jsonrpc/bfg-protocol.ts
@@ -2,14 +2,22 @@
  * This file declares the protocol for communicating between Cody and BFG (Blazingly Fast Graph), a Rust implementation
  * of the "Graph Context" feature flag.
  */
-import type { FileContextSnippet, SymbolContextSnippet } from '../completions/types'
-
 import type { Position, Range } from './agent-protocol'
+
+export interface BFGFileContextSnippet {
+    fileName: string
+    content: string
+}
+
+export interface BFGSymbolContextSnippet extends BFGFileContextSnippet {
+    symbol: string
+}
+
 export type Requests = {
     'bfg/initialize': [{ clientName: string }, { serverVersion: string }]
     'bfg/contextAtPosition': [
         { uri: string; content: string; position: Position; maxChars: number; contextRange?: Range },
-        { symbols?: SymbolContextSnippet[]; files?: FileContextSnippet[] },
+        { symbols?: BFGSymbolContextSnippet[]; files?: BFGFileContextSnippet[] },
     ]
     // biome-ignore lint/suspicious/noConfusingVoidType: this models a function returning void
     'bfg/gitRevision/didChange': [{ gitDirectoryUri: string }, void]
