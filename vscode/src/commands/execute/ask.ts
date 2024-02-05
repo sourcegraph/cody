48c109f106cc8359525276236cc5b28e95b0d604
Beatrix <beatrix@sourcegraph.com>
2024-02-05 15:57:28 -0800

    Add config feature gating for chat and commands

--- vscode/src/commands/execute/ask.ts
+++ vscode/src/commands/execute/ask.ts
@@ -1,7 +1,8 @@
 import * as vscode from 'vscode'
 import type { ChatSession } from '../../chat/chat-view/SimpleChatPanelProvider'
 import type { WebviewSubmitMessage } from '../../chat/protocol'
-import type { ChatEventSource } from '@sourcegraph/cody-shared'
+import { ConfigFeaturesSingleton, type ChatEventSource } from '@sourcegraph/cody-shared'
+import { isDefaultChatCommand } from '.'
 
 export interface ExecuteChatArguments extends WebviewSubmitMessage {
     source?: ChatEventSource
@@ -11,5 +12,14 @@ export interface ExecuteChatArguments extends WebviewSubmitMessage {
  * Wrapper around the `cody.action.chat` command that can be used anywhere but with better type-safety.
  */
 export const executeChat = async (args: ExecuteChatArguments): Promise<ChatSession | undefined> => {
+    const { chat, commands } = await ConfigFeaturesSingleton.getInstance().getConfigFeatures()
+    const isCommand = isDefaultChatCommand(args.source || '')
+    if ((!isCommand && !chat) || (isCommand && !commands)) {
+        void vscode.window.showErrorMessage(
+            'This feature has been disabled by your Sourcegraph site admin.'
+        )
+        return undefined
+    }
+
     return vscode.commands.executeCommand<ChatSession | undefined>('cody.action.chat', args)
 }
