0a79c3cd069c87e83c4435eb3b8c83be2f85156b
Beatrix <beatrix@sourcegraph.com>
2024-02-06 21:35:38 -0800

    update test

--- vscode/test/e2e/chat-edits.test.ts
+++ vscode/test/e2e/chat-edits.test.ts
@@ -95,7 +95,7 @@ test('editing follow-up messages in chat view', async ({ page, sidebar }) => {
     await expect(chatInput).not.toHaveValue('Four')
     await expect(chatFrame.getByRole('button', { name: 'Main.java' })).toBeVisible()
     await chatInput.press('Tab')
-    await expect(chatInput).toHaveValue('Explain @Main.java')
+    await expect(chatInput).toHaveValue('Explain @Main.java ')
 
     // Enter should submit the message and exit editing mode
     // The last message should be "Explain @Main.java"
@@ -103,7 +103,7 @@ test('editing follow-up messages in chat view', async ({ page, sidebar }) => {
     await chatInput.press('Enter')
     await expect(cancelEditButton).not.toBeVisible()
     await expect(chatInput).toBeEmpty()
-    await expect(chatFrame.getByText('Explain @Main.java ')).toBeVisible()
+    await expect(chatFrame.getByText('Explain @Main.java')).toBeVisible()
     await chatInput.press('Escape')
 
     // Meta+/ also creates a new chat session
