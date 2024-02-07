ef684a18f74cc106e1659f59d298aa72c379b1c1
Tom Ross <tom@umpox.com>
2024-02-07 11:31:10 +0000

    fix

--- vscode/src/main.ts
+++ vscode/src/main.ts
@@ -219,7 +219,7 @@ const register = async (
         guardrails
     )
 
-    const ghostHintDecorator = new GhostHintDecorator({ authProvider })
+    const ghostHintDecorator = new GhostHintDecorator(authProvider)
     const editorManager = new EditManager({
         chat: chatClient,
         editor,
