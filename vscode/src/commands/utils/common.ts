8928a10deea050c626722b95d3fdf6344484f72b
Tom Ross <tom@umpox.com>
2024-02-07 10:24:23 +0000

    remove test doc

--- vscode/src/commands/utils/common.ts
+++ vscode/src/commands/utils/common.ts
@@ -1,5 +1,8 @@
 const leadingForwardSlashRegex = /^\/+/
 
+/**
+ * Removes leading forward slashes from slash command string.
+ */
 export function fromSlashCommand(slashCommand: string): string {
     return slashCommand.replace(leadingForwardSlashRegex, '')
 }
