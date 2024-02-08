e413d9db7c33c8dda776c720866d618d695cbf82
Ólafur Páll Geirsson <olafurpg@gmail.com>
2024-02-08 21:30:22 +0100

    Delete agent binary file on exit (#542)
    Addresses https://github.com/sourcegraph/jetbrains/issues/439
    
    Previously, the JetBrains plugin created a new temporary file every time
    we started a new agent process and only deleted this file when the IDE
    shut down. This buggy behavior has existed since Septemeber and we
    haven't received user complaints about it, likely because the plugin was
    conservative about restarting the agent process. Recent changes
    introduced retry logic where we could start a new agent process in a
    loop, and never delete the underlying binary. Some users have reported
    that this bug caused the plugin to take up unreasonable amounts of disk
    space. I'm sorry for users who were impacted by this behavior.
    
    This PR addresses the problem with a new callback hook that deletes the
    agent binary after the agent process finishes running. This change means
    that we never have more than one agent binary sitting on the disk per
    IntelliJ project. This should prevent the catastrophic situation some
    users have reported.
    
    
    ## Test plan
    I manually tested this change by adding a println to when the file gets
    deleted, started IntelliJ, triggered the "Restart Cody Agent" action and
    confirmed that the file got deleted.
    
    <!-- All pull requests REQUIRE a test plan:
    https://sourcegraph.com/docs/dev/background-information/testing_principles
    
    Why does it matter?
    
    These test plans are there to demonstrate that are following industry
    standards which are important or critical for our customers.
    They might be read by customers or an auditor. There are meant be simple
    and easy to read. Simply explain what you did to ensure
    your changes are correct!
    
    Here are a non exhaustive list of test plan examples to help you:
    
    - Making changes on a given feature or component:
    - "Covered by existing tests" or "CI" for the shortest possible plan if
    there is zero ambiguity
      - "Added new tests"
    - "Manually tested" (if non trivial, share some output, logs, or
    screenshot)
    - Updating docs:
      - "previewed locally"
      - share a screenshot if you want to be thorough
    - Updating deps, that would typically fail immediately in CI if
    incorrect
      - "CI"
      - "locally tested"
    -->

--- src/main/kotlin/com/sourcegraph/cody/agent/CodyAgent.kt
+++ src/main/kotlin/com/sourcegraph/cody/agent/CodyAgent.kt
@@ -8,6 +8,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.SystemInfoRt
 import com.intellij.util.system.CpuArch
 import com.sourcegraph.cody.agent.protocol.*
+import com.sourcegraph.cody.vscode.CancellationToken
 import com.sourcegraph.config.ConfigUtil
 import java.io.*
 import java.net.Socket
@@ -128,6 +129,7 @@ private constructor(
       if (shouldConnectToDebugAgent()) {
         return connectToDebugAgent()
       }
+      val token = CancellationToken()
       val command: List<String> =
           if (System.getenv("CODY_DIR") != null) {
             val script = File(System.getenv("CODY_DIR"), "agent/dist/index.js")
@@ -138,7 +140,7 @@ private constructor(
               listOf("node", "--enable-source-maps", script.absolutePath)
             }
           } else {
-            val binary = agentBinary()
+            val binary = agentBinary(token)
             logger.info("starting Cody agent " + binary.absolutePath)
             listOf(binary.absolutePath)
           }
@@ -158,6 +160,7 @@ private constructor(
               .redirectErrorStream(false)
               .redirectError(ProcessBuilder.Redirect.PIPE)
               .start()
+      process.onExit().thenAccept { token.abort() }
 
       // Redirect agent stderr into idea.log by buffering line by line into `logger.warn()`
       // statements. Without this logic, the stderr output of the agent process is lost if
@@ -219,7 +222,7 @@ private constructor(
     }
 
     @Throws(CodyAgentException::class)
-    private fun agentBinary(): File {
+    private fun agentBinary(token: CancellationToken): File {
       val pluginPath =
           agentDirectory()
               ?: throw CodyAgentException("Sourcegraph Cody + Code Search plugin path not found")
@@ -228,8 +231,14 @@ private constructor(
         throw CodyAgentException(
             "Cody agent binary not found at path " + binarySource.toAbsolutePath())
       }
+      val binaryTarget = Files.createTempFile("cody-agent", binarySuffix())
       return try {
-        val binaryTarget = Files.createTempFile("cody-agent", binarySuffix())
+        token.onFinished {
+          // Important: delete the file from disk after the process exists
+          // Ideally, we should eventually replace this temporary file with a permanent location
+          // in the plugin directory.
+          Files.deleteIfExists(binaryTarget)
+        }
         logger.info("extracting Cody agent binary to " + binaryTarget.toAbsolutePath())
         Files.copy(binarySource, binaryTarget, StandardCopyOption.REPLACE_EXISTING)
         val binary = binaryTarget.toFile()
@@ -240,6 +249,7 @@ private constructor(
           throw CodyAgentException("failed to make executable " + binary.absolutePath)
         }
       } catch (e: IOException) {
+        Files.deleteIfExists(binaryTarget)
         throw CodyAgentException("failed to create agent binary", e)
       }
     }
