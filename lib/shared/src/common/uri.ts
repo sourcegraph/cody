623fe31b31b461ad1c348193b0e2fd21f4ce2b25
Danny Tuppeny <danny@tuppeny.com>
2024-02-09 15:48:06 +0000

    Make relative() case-insensitive for Windows paths + URIs
    + fix some other minor inconsistencies with slash directions such as relative always assuming input and output slash directions match (on Windows, we'd expect relative() on two file URIs to return a relative path, not URI).
    
    Removes a workaround in the ignore helper (See https://github.com/sourcegraph/cody/issues/2929), although it might be that there are still other places that are incorrectly case-sensitive on Windows (that aren't using these path helpers).

--- lib/shared/src/common/uri.ts
+++ lib/shared/src/common/uri.ts
@@ -1,6 +1,6 @@
 import type { URI } from 'vscode-uri'
 
-import { posixAndURIPaths } from './path'
+import { pathFunctionsForURI } from './path'
 
 /**
  * dirname, but operates on a {@link URI}.
@@ -9,7 +9,7 @@ import { posixAndURIPaths } from './path'
  * separators, which will break because URI paths are always separated with '/'.
  */
 export function uriDirname(uri: URI): URI {
-    return uri.with({ path: posixAndURIPaths.dirname(uri.path) })
+    return uri.with({ path: pathFunctionsForURI(uri).dirname(uri.path) })
 }
 
 /**
@@ -18,7 +18,7 @@ export function uriDirname(uri: URI): URI {
  * See {@link uriDirname} for why we use this instead of Node's `path` module.
  */
 export function uriBasename(uri: URI, suffix?: string): string {
-    return posixAndURIPaths.basename(uri.path, suffix)
+    return pathFunctionsForURI(uri).basename(uri.path, suffix)
 }
 
 /**
@@ -27,7 +27,7 @@ export function uriBasename(uri: URI, suffix?: string): string {
  * See {@link uriDirname} for why we use this instead of Node's `path` module.
  */
 export function uriExtname(uri: URI): string {
-    return posixAndURIPaths.extname(uri.path)
+    return pathFunctionsForURI(uri).extname(uri.path)
 }
 
 /**
