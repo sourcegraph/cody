bdd5d0ecb06eb11ee3685be52a5cca27c4cf9257
Beatrix <68532117+abeatrix@users.noreply.github.com>
2024-02-10 07:26:47 -0800

    Rename function

--- agent/src/__tests__/example-ts/src/is_ignored.ts
+++ agent/src/__tests__/example-ts/src/is_ignored.ts
@@ -1,4 +1,4 @@
-function notIgnoredByCody() {
+function isAlsoIgnoredByCody() {
     console.log('This file is ignored by Cody via .cody/ignore.')
     console.log('Used in testing for confirming the ignore rule is NOT case sensitive')
 }
