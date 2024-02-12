91677cf8805b6087139d861e7ff4dcfcbbe79348
Tom Ross <tom@umpox.com>
2024-02-12 14:19:39 +0000

    Update vscode/src/non-stop/codelenses/items.ts
    Co-authored-by: Beatrix <68532117+abeatrix@users.noreply.github.com>

--- vscode/src/non-stop/codelenses/items.ts
+++ vscode/src/non-stop/codelenses/items.ts
@@ -188,7 +188,7 @@ function getUndoLens(codeLensRange: vscode.Range, id: string): vscode.CodeLens {
 
 function getAcceptLens(codeLensRange: vscode.Range, id: string): vscode.CodeLens {
     const lens = new vscode.CodeLens(codeLensRange)
-    const shortcut = process.platform === 'win32' ? 'Alt+A' : '⌥A'
+    const shortcut = process.platform === 'darwin' ? '⌥A' : 'Alt+A'
     lens.command = {
         title: `Accept [${shortcut}]`,
         command: 'cody.fixup.codelens.accept',
