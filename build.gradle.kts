4456773eb07d3206719bb44da6ffe2e97dad3e28
Piotr Kukiełka <pkukielka@users.noreply.github.com>
2024-02-12 22:39:35 +0100

    Bump Cody commit (#607)
    ## Test plan
    
    Full manual QA from TESTING.MD

--- build.gradle.kts
+++ build.gradle.kts
@@ -199,7 +199,7 @@ tasks {
     return destinationDir
   }
 
-  val codyCommit = "70e927ce69c75aea5b2053574333e6525b717ba3"
+  val codyCommit = "f6473571f0031e77f4e06ecd06fd7fae9a5330b9"
   fun downloadCody(): File {
     val url = "https://github.com/sourcegraph/cody/archive/$codyCommit.zip"
     val destination = githubArchiveCache.resolve("$codyCommit.zip")
