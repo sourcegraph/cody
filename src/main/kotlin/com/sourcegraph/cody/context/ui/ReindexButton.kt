2445b0e3d9a945ec91dabef0901bb33386c8591a
Jakub Błach <jakubblach@gmail.com>
2024-02-12 16:38:47 +0100

    Fix symf reindexing error notification (#593)
    Fixes #381

--- src/main/kotlin/com/sourcegraph/cody/context/ui/ReindexButton.kt
+++ src/main/kotlin/com/sourcegraph/cody/context/ui/ReindexButton.kt
@@ -6,16 +6,19 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.Messages
 import com.sourcegraph.cody.agent.CodyAgentService
 import com.sourcegraph.cody.agent.CommandExecuteParams
 import com.sourcegraph.common.CodyBundle
-import com.sourcegraph.common.CodyBundle.fmt
 import java.util.concurrent.atomic.AtomicBoolean
 
 class ReindexButton(private val project: Project) :
     ContextToolbarButton(
         CodyBundle.getString("context-panel.button.reindex"), AllIcons.Actions.Refresh) {
+
+  private val isReindexingInProgress = AtomicBoolean(false)
+
+  override fun isEnabled() = !isReindexingInProgress.get()
+
   override fun actionPerformed(p0: AnActionEvent) {
     CodyAgentService.applyAgentOnBackgroundThread(project) { agent ->
       ProgressManager.getInstance()
@@ -28,11 +31,6 @@ class ReindexButton(private val project: Project) :
                     isReindexingInProgress.set(true)
                     val cmd = CommandExecuteParams("cody.search.index-update", emptyList())
                     agent.server.commandExecute(cmd).get()
-                  } catch (e: Exception) {
-                    val errMsg = e.message ?: e.toString()
-                    Messages.showErrorDialog(
-                        CodyBundle.getString("context-panel.error-message").fmt(errMsg),
-                        CodyBundle.getString("context-panel.error-title"))
                   } finally {
                     indicator.stop()
                     isReindexingInProgress.set(false)
@@ -41,8 +39,4 @@ class ReindexButton(private val project: Project) :
               })
     }
   }
-
-  override fun isEnabled(): Boolean = !isReindexingInProgress.get()
-
-  private val isReindexingInProgress = AtomicBoolean(false)
 }
