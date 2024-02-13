75e21163008920bdde7693c92090b90e53580958
Beatrix <68532117+abeatrix@users.noreply.github.com>
2024-02-13 13:06:01 -0800

    Auth: Add input validation for instance URL (#3156)
    This commit adds validation for sourcegraph tokens in the AuthMenus and
    AuthProvider files. The showInstanceURLInputBox function in AuthMenus
    now checks if the user is entering a token as a URL and displays an
    error message if so.
    Similarly, the formatURL function in AuthProvider now throws an error if
    the URI is a sourcegraph token, and return `null`.
    
    Additionally, the LocalStorageProvider file now ignores and clears the
    last used endpoint if the provided endpoint is a sourcegraph token.
    
    
    ![image](https://github.com/sourcegraph/cody/assets/68532117/c77005be-edd9-4018-9c42-4655baff2782)
    
    Update placeholder value for URL to always starts with `https://` to
    make it clear that the field is for URL input:
    
    
    ![image](https://github.com/sourcegraph/cody/assets/68532117/cea4f78a-d39e-4819-bc97-a59f6af4c369)
    
    
    
    ## Test plan
    
    <!-- Required. See
    https://sourcegraph.com/docs/dev/background-information/testing_principles.
    -->
    
    
    
    
    https://github.com/sourcegraph/cody/assets/68532117/9d44427a-3b8b-4c35-812d-aa5b22120d6d

--- vscode/src/chat/protocol.ts
+++ vscode/src/chat/protocol.ts
@@ -315,3 +315,18 @@ export function isLoggedIn(authStatus: AuthStatus): boolean {
 }
 
 export type AuthMethod = 'dotcom' | 'github' | 'gitlab' | 'google'
+
+// Provide backward compatibility for the old token regex
+// Details: https://docs.sourcegraph.com/dev/security/secret_formats
+const sourcegraphTokenRegex =
+    /(sgp_(?:[a-fA-F0-9]{16}|local)|sgp_)?[a-fA-F0-9]{40}|(sgd|slk|sgs)_[a-fA-F0-9]{64}/
+
+/**
+ * Checks if the given text matches the regex for a Sourcegraph access token.
+ *
+ * @param text - The text to check against the regex.
+ * @returns Whether the text matches the Sourcegraph token regex.
+ */
+export function isSourcegraphToken(text: string): boolean {
+    return sourcegraphTokenRegex.test(text)
+}
