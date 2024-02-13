7a9ac5b2714ce3f9fc3aab3e616f319423427230
Piotr Kukiełka <pkukielka@users.noreply.github.com>
2024-02-13 15:05:07 +0100

    Add additional help for context add remote window (#623)
    Fixes 
    [#597](https://github.com/sourcegraph/jetbrains/issues/597)
    [#557](https://github.com/sourcegraph/jetbrains/issues/557)
    
    ## Changes
    * Added autofocus on input field when dialog is open
    * Improved description
    
    ## Test plan
    
    Manual testing:
    
    
    ![image](https://github.com/sourcegraph/jetbrains/assets/1519649/234bf910-31d1-43ba-b19d-19fb2f3cc5f4)

--- src/main/kotlin/com/sourcegraph/cody/context/ui/AddRepositoryDialog.kt
+++ src/main/kotlin/com/sourcegraph/cody/context/ui/AddRepositoryDialog.kt
@@ -9,7 +9,10 @@ import com.intellij.ui.TextFieldWithAutoCompletionListProvider
 import com.intellij.util.Alarm
 import com.sourcegraph.cody.config.DialogValidationUtils
 import com.sourcegraph.cody.context.RemoteRepoUtils
+import com.sourcegraph.common.CodyBundle
 import com.sourcegraph.vcs.convertGitCloneURLToCodebaseNameOrError
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
 import java.net.URL
 import java.util.concurrent.TimeUnit
 import javax.swing.JComponent
@@ -24,28 +27,33 @@ class AddRepositoryDialog(private val project: Project, private val addAction: (
 
   init {
     init()
-    title = "Add Remote Repository"
+    title = CodyBundle.getString("context-panel.add-repo-dialog.title")
     setOKButtonText("Add")
     setValidationDelay(100)
   }
 
   override fun doValidateAll(): List<ValidationInfo> {
     fun validateNonEmpty() =
-        DialogValidationUtils.custom(repoUrlInputField, "Remote repository URL cannot be empty") {
-          repoUrlInputField.text.isNotBlank()
-        }
+        DialogValidationUtils.custom(
+            repoUrlInputField,
+            CodyBundle.getString("context-panel.add-repo-dialog.error-empty-url")) {
+              repoUrlInputField.text.isNotBlank()
+            }
 
     fun validateValidUrl() =
-        DialogValidationUtils.custom(repoUrlInputField, "Remote repository URL must be valid") {
-          val url =
-              if (repoUrlInputField.text.startsWith("http")) repoUrlInputField.text
-              else "http://" + repoUrlInputField.text
-          runCatching { URL(url) }.isSuccess
-        }
+        DialogValidationUtils.custom(
+            repoUrlInputField,
+            CodyBundle.getString("context-panel.add-repo-dialog.error-invalid-url")) {
+              val url =
+                  if (repoUrlInputField.text.startsWith("http")) repoUrlInputField.text
+                  else "http://" + repoUrlInputField.text
+              runCatching { URL(url) }.isSuccess
+            }
 
     fun validateRepoExists() =
         DialogValidationUtils.custom(
-            repoUrlInputField, "Remote repository not found on the server") {
+            repoUrlInputField,
+            CodyBundle.getString("context-panel.add-repo-dialog.error-no-repo")) {
               val codebaseName = convertGitCloneURLToCodebaseNameOrError(repoUrlInputField.text)
               val repo =
                   RemoteRepoUtils.getRepository(project, codebaseName)
@@ -67,9 +75,8 @@ class AddRepositoryDialog(private val project: Project, private val addAction: (
   }
 
   override fun createCenterPanel(): JComponent {
-    val panel = JPanel()
-    val label = JLabel("Repository URL: ")
-    panel.add(label)
+    val mainPanel = JPanel(GridBagLayout())
+    val rightSidePanel = JPanel(GridBagLayout())
 
     // TODO: we can provide repository suggestions using `provider.setItems` method
     val completionProvider: TextFieldWithAutoCompletionListProvider<String> =
@@ -80,8 +87,10 @@ class AddRepositoryDialog(private val project: Project, private val addAction: (
           }
         }
 
-    repoUrlInputField.setPreferredWidth(300)
+    myPreferredFocusedComponent = repoUrlInputField
+    repoUrlInputField.setPreferredWidth(350)
     repoUrlInputField.installProvider(completionProvider)
+    repoUrlInputField.requestFocusInWindow()
     repoUrlInputField.addDocumentListener(
         object : DocumentListener {
           override fun documentChanged(event: com.intellij.openapi.editor.event.DocumentEvent) {
@@ -89,8 +98,26 @@ class AddRepositoryDialog(private val project: Project, private val addAction: (
           }
         })
 
-    panel.add((repoUrlInputField))
+    fun constraints(gridx: Int, gridy: Int, ipadx: Int = 0, ipady: Int = 0): GridBagConstraints {
+      val c = GridBagConstraints()
+      c.gridx = gridx
+      c.gridy = gridy
+      c.ipadx = ipadx
+      c.ipady = ipady
+      c.anchor = GridBagConstraints.FIRST_LINE_START
+      return c
+    }
 
-    return panel
+    mainPanel.add(
+        JLabel(CodyBundle.getString("context-panel.add-repo-dialog.url-input-label")),
+        constraints(gridx = 0, gridy = 0, ipady = 10))
+    mainPanel.add(rightSidePanel, constraints(gridx = 1, gridy = 0, ipadx = 10))
+
+    rightSidePanel.add(repoUrlInputField, constraints(gridx = 0, gridy = 0))
+    rightSidePanel.add(
+        JLabel(CodyBundle.getString("context-panel.add-repo-dialog.url-input-help")),
+        constraints(gridx = 0, gridy = 1, ipadx = 10, ipady = 10))
+
+    return mainPanel
   }
 }
