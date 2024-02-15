92cbc71ed1ad497f93a52aad0e1065a2663a8aa3
Beatrix <68532117+abeatrix@users.noreply.github.com>
2024-02-15 07:25:57 -0800

    Test: fix failed e2e tests (#3180)
    Fix the e2e test that's failing on Windows.
    
    ## Test plan
    
    <!-- Required. See
    https://sourcegraph.com/docs/dev/background-information/testing_principles.
    -->
    
    Green bots 🙏 
    
    ### Before (currently on main)
    
    
    ![image](https://github.com/sourcegraph/cody/assets/68532117/e242ea1b-30fd-4397-a6fc-834109c00c5d)
    
    
    ### Current
    
    
    ![image](https://github.com/sourcegraph/cody/assets/68532117/91fdfc73-0cdf-4df1-bdca-7af9ae6e32ce)

--- vscode/webviews/App.tsx
+++ vscode/webviews/App.tsx
@@ -4,13 +4,13 @@ import './App.css'
 
 import {
     GuardrailsPost,
-    type ChatHistory,
     type ChatInputHistory,
     type ChatMessage,
     type ModelProvider,
     type Configuration,
     type ContextFile,
     type EnhancedContextContextT,
+    type TranscriptJSON,
 } from '@sourcegraph/cody-shared'
 import type { UserAccountInfo } from '@sourcegraph/cody-ui/src/Chat'
 import { EnhancedContextEnabled } from '@sourcegraph/cody-ui/src/chat/components/EnhancedContext'
@@ -47,7 +47,7 @@ export const App: React.FunctionComponent<{ vscodeAPI: VSCodeWrapper }> = ({ vsc
 
     const [formInput, setFormInput] = useState('')
     const [inputHistory, setInputHistory] = useState<ChatInputHistory[]>([])
-    const [userHistory, setUserHistory] = useState<ChatHistory | null>(null)
+    const [userHistory, setUserHistory] = useState<TranscriptJSON[]>([])
     const [chatIDHistory, setChatIDHistory] = useState<string[]>([])
 
     const [contextSelection, setContextSelection] = useState<ContextFile[] | null>(null)
@@ -130,7 +130,7 @@ export const App: React.FunctionComponent<{ vscodeAPI: VSCodeWrapper }> = ({ vsc
                         break
                     case 'history':
                         setInputHistory(message.localHistory?.input ?? [])
-                        setUserHistory(message.localHistory?.chat ?? null)
+                        setUserHistory(Object.values(message.localHistory?.chat ?? {}))
                         break
                     case 'enhanced-context':
                         setEnhancedContextStatus(message.enhancedContextStatus)
@@ -218,8 +218,7 @@ export const App: React.FunctionComponent<{ vscodeAPI: VSCodeWrapper }> = ({ vsc
                 <>
                     <Notices
                         probablyNewInstall={
-                            !!userHistory &&
-                            [...Object.values(userHistory)].every(chat => chat.interactions.length === 0)
+                            !userHistory.filter(chat => chat.interactions.length)?.length
                         }
                     />
                     {errorMessages && (
