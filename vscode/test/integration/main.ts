5a638a14ff9a8d7606497e323eda21c98b131ea0
Danny Tuppeny <danny@tuppeny.com>
2024-02-16 13:36:05 +0000

    Always uses latest stable VS Code version for integration tests (#3021)
    This was previously hard-coded and needed updating (and confusingly in
    two places, with one being bumped without the other in the past). This
    just tracks latest stable.
    
    If we want to also run tests on the oldest supported version (if this is
    defined), we could split this out into an env VAR and have them both
    (`['stable', 'x.y.z']`) in the GH Actions yaml file.
    
    ## Test plan
    
    Non-functional... ensure bots are still running and the logs show that
    they used the current latest version of VS Code (1.86).

--- vscode/test/integration/main.ts
+++ vscode/test/integration/main.ts
@@ -36,7 +36,12 @@ async function main(): Promise<void> {
         await MockServer.run(async () => {
             for (const testConfig of testConfigs) {
                 await runTests({
-                    version: '1.81.1',
+                    // The VS Code version to use for integration tests (there is also a version in ../e2e/install-deps.ts for e2e tests).
+                    //
+                    // We set this to stable so that tests are always running on the version of VS Code users are likely to be using. This may
+                    // result in tests breaking after a VS Code release but it's better for them to be investigated than potential bugs being
+                    // missed because we're running on an older version than users.
+                    version: 'stable',
                     extensionDevelopmentPath,
                     extensionTestsPath: path.normalize(
                         path.resolve(integrationTestsPath, testConfig.testsFolder, 'index')
