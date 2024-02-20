889105c40f9798a087f36d1f825a25668cbd0866
Tom Ross <tom@umpox.com>
2024-02-20 10:11:33 +0000

    add test

--- agent/src/index.test.ts
+++ agent/src/index.test.ts
@@ -639,6 +639,13 @@ describe('Agent', () => {
                         `Expected accept command, found none. Lenses ${JSON.stringify(lenses, null, 2)}`
                     )
                 }
+
+                // Check the command is corrected parsed by the agent
+                expect(acceptCommand.command.title).toBe(' Accept')
+                expect(acceptCommand.command.icons).toStrictEqual([
+                    { position: 0, value: '$(cody-logo)' },
+                ])
+
                 await documentClient.request('command/execute', acceptCommand.command)
                 expect(documentClient.codeLenses.get(uri.toString()) ?? []).toHaveLength(0)
                 const newContent = documentClient.workspace.getDocument(uri)?.content
