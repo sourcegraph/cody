60c96bf0f1861b4dfb823810b91b611eae291a95
Quinn Slack <quinn@slack.org>
2024-02-25 12:07:40 +0100

    remove unused userContextFiles kind (#3266)

--- vscode/src/chat/protocol.ts
+++ vscode/src/chat/protocol.ts
@@ -6,7 +6,6 @@ import type {
     CodyLLMSiteConfiguration,
     ConfigurationWithAccessToken,
     ContextFile,
-    ContextFileType,
     EnhancedContextContextT,
     ModelProvider,
     SearchPanelFile,
@@ -123,7 +122,6 @@ export type ExtensionMessage =
     | {
           type: 'userContextFiles'
           userContextFiles: ContextFile[] | null
-          kind?: ContextFileType
       }
     | { type: 'chatModels'; models: ModelProvider[] }
     | {
