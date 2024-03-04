974075957ea8603f41518000fad25baaa253dc6a
Quinn Slack <quinn@slack.org>
2024-03-04 00:06:59 -0800

    use `page.waitForTimeout` instead of custom `sleep`
    Playwright provides `page.waitForTimeout`, so we should use it instead of our own `sleep` for consistency.

--- vscode/test/e2e/command-custom.test.ts
+++ vscode/test/e2e/command-custom.test.ts
@@ -1,7 +1,6 @@
 import { expect } from '@playwright/test'
 import * as mockServer from '../fixtures/mock-server'
 
-import { sleep } from '../../src/completions/utils'
 import { sidebarExplorer, sidebarSignin } from './common'
 import {
     type DotcomUrlOverride,
@@ -280,7 +279,7 @@ test.extend<ExpectedEvents>({
 
     // Open the cody.json from User Settings
     // NOTE: This is expected to fail locally if you currently have User commands configured
-    await sleep(100)
+    await page.waitForTimeout(100)
     await customCommandSidebar.click()
     await page.locator('a').filter({ hasText: 'Open User Settings (JSON)' }).hover()
     await page.getByRole('button', { name: 'Open or Create Settings File' }).hover()
