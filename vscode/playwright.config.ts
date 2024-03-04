ca8f0510b25d282456982f8a2aa52e65bafa04fe
Quinn Slack <quinn@slack.org>
2024-03-03 21:44:07 -0800

    wip

--- vscode/playwright.config.ts
+++ vscode/playwright.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
     workers: 1,
     // Give failing tests a second chance
-    retries: 0, // TODO(sqs): back to 2
+    retries: 2,
     testDir: 'test/e2e',
-    expect: {
-        timeout: 2000,
-    },
 })
