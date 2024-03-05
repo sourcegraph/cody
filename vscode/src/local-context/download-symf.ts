52777cf42313d27932027dbcac0c3b0d12caa23f
Beyang Liu <beyang@sourcegraph.com>
2024-03-04 16:34:04 -0800

    ensure symf is only downloaded once (#3286)

--- vscode/src/local-context/download-symf.ts
+++ vscode/src/local-context/download-symf.ts
@@ -7,6 +7,7 @@ import axios from 'axios'
 import * as unzipper from 'unzipper'
 import * as vscode from 'vscode'
 
+import { Mutex } from 'async-mutex'
 import { logDebug } from '../log'
 import { getOSArch } from '../os'
 import { captureException } from '../services/sentry/sentry'
@@ -25,6 +26,21 @@ export async function getSymfPath(context: vscode.ExtensionContext): Promise<str
         return userSymfPath
     }
 
+    const symfContainingDir = path.join(context.globalStorageUri.fsPath, 'symf')
+    return await _getSymfPath(symfContainingDir)
+}
+
+const downloadLock = new Mutex()
+
+export async function _getSymfPath(
+    symfContainingDir: string,
+    actualDownloadSymf: (op: {
+        symfPath: string
+        symfFilename: string
+        symfUnzippedFilename: string
+        symfURL: string
+    }) => Promise<void> = downloadSymf
+): Promise<string | null> {
     const { platform, arch } = getOSArch()
     if (!platform || !arch) {
         // show vs code error message
@@ -33,57 +49,80 @@ export async function getSymfPath(context: vscode.ExtensionContext): Promise<str
         )
         return null
     }
+    // Releases (eg at https://github.com/sourcegraph/symf/releases) are named with the Zig platform
+    // identifier (linux-musl, windows-gnu, macos).
+    const zigPlatform =
+        platform === 'linux' ? 'linux-musl' : platform === 'windows' ? 'windows-gnu' : platform
 
-    const symfContainingDir = path.join(context.globalStorageUri.fsPath, 'symf')
     const symfFilename = `symf-${symfVersion}-${arch}-${platform}`
+    const symfUnzippedFilename = `symf-${arch}-${zigPlatform}` // the filename inside the zip
     const symfPath = path.join(symfContainingDir, symfFilename)
     if (await fileExists(symfPath)) {
         logDebug('symf', `using downloaded symf "${symfPath}"`)
         return symfPath
     }
 
-    // Releases (eg at https://github.com/sourcegraph/symf/releases) are named with the Zig platform
-    // identifier (linux-musl, windows-gnu, macos).
-    const zigPlatform =
-        platform === 'linux' ? 'linux-musl' : platform === 'windows' ? 'windows-gnu' : platform
-
     const symfURL = `https://github.com/sourcegraph/symf/releases/download/${symfVersion}/symf-${arch}-${zigPlatform}.zip`
-    logDebug('symf', `downloading symf from ${symfURL}`)
 
     // Download symf binary with vscode progress api
     try {
-        await vscode.window.withProgress(
-            {
-                location: vscode.ProgressLocation.Notification,
-                title: 'Downloading Cody search engine (symf)',
-                cancellable: false,
-            },
-            async progress => {
-                const symfTmpDir = `${symfPath}.tmp`
-                progress.report({ message: 'Downloading symf and extracting symf' })
+        await downloadLock.acquire()
+        // Re-check if it has been downloaded
+        if (await fileExists(symfPath)) {
+            logDebug('symf', 'symf already downloaded, reusing')
+            return symfPath
+        }
 
-                await fspromises.mkdir(symfTmpDir, { recursive: true })
-                const symfZipFile = path.join(symfTmpDir, `${symfFilename}.zip`)
-                await downloadFile(symfURL, symfZipFile)
-                await unzipSymf(symfZipFile, symfTmpDir)
-                logDebug('symf', `downloaded symf to ${symfTmpDir}`)
-
-                const tmpFile = path.join(symfTmpDir, `symf-${arch}-${zigPlatform}`)
-                await fspromises.chmod(tmpFile, 0o755)
-                await fspromises.rename(tmpFile, symfPath)
-                await fspromises.rm(symfTmpDir, { recursive: true })
-
-                logDebug('symf', `extracted symf to ${symfPath}`)
-            }
-        )
+        await actualDownloadSymf({ symfPath, symfURL, symfFilename, symfUnzippedFilename })
         void removeOldSymfBinaries(symfContainingDir, symfFilename)
     } catch (error) {
         captureException(error)
         void vscode.window.showErrorMessage(`Failed to download symf: ${error}`)
         return null
+    } finally {
+        downloadLock.release()
     }
 
     return symfPath
+}
+
+async function downloadSymf({
+    symfPath,
+    symfFilename,
+    symfUnzippedFilename,
+    symfURL,
+}: {
+    symfPath: string
+    symfFilename: string
+    symfUnzippedFilename: string
+    symfURL: string
+}): Promise<void> {
+    logDebug('symf', `downloading symf from ${symfURL}`)
+
+    await vscode.window.withProgress(
+        {
+            location: vscode.ProgressLocation.Notification,
+            title: 'Downloading Cody search engine (symf)',
+            cancellable: false,
+        },
+        async progress => {
+            const symfTmpDir = `${symfPath}.tmp`
+            progress.report({ message: 'Downloading symf and extracting symf' })
+
+            await fspromises.mkdir(symfTmpDir, { recursive: true })
+            const symfZipFile = path.join(symfTmpDir, `${symfFilename}.zip`)
+            await downloadFile(symfURL, symfZipFile)
+            await unzipSymf(symfZipFile, symfTmpDir)
+            logDebug('symf', `downloaded symf to ${symfTmpDir}`)
+
+            const tmpFile = path.join(symfTmpDir, symfUnzippedFilename)
+            await fspromises.chmod(tmpFile, 0o755)
+            await fspromises.rename(tmpFile, symfPath)
+            await fspromises.rm(symfTmpDir, { recursive: true })
+
+            logDebug('symf', `extracted symf to ${symfPath}`)
+        }
+    )
 }
 
 export async function fileExists(path: string): Promise<boolean> {
