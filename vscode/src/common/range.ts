1180901c61e3d3d94d6bd4d8d4dde85e4dde7c2c
Quinn Slack <quinn@slack.org>
2024-03-05 14:38:17 -0800

    handle incorrectly serialized vscode.Range values in toRangeData
    We should fix the source of any bugs like this, but it's also helpful to handle them gracefully.

--- vscode/src/common/range.ts
+++ vscode/src/common/range.ts
@@ -1,13 +1,16 @@
 import type { RangeData } from '@sourcegraph/cody-shared'
 import * as vscode from 'vscode'
 
-export function toVSCodeRange(range?: RangeData): vscode.Range | undefined {
+export function toVSCodeRange(
+    range?: RangeData | [{ line: number; character: number }, { line: number; character: number }]
+): vscode.Range | undefined {
     if (!range) {
         return undefined
     }
 
-    // HACK: If value was accidentally serialized as [start, end] (which `vscode.Range` instances do
-    // when JSON-serialized), handle it.
+    // HACK: Handle if the `vscode.Range` value was accidentally JSON-serialized as [start, end],
+    // which `vscode.Range` instances do when JSON-serialized because of their `toJSON()` method
+    // that misleading and not represented in the type system).
     if (Array.isArray(range)) {
         return new vscode.Range(range[0].line, range[0].character, range[1].line, range[1].character)
     }
