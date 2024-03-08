272773c810695409f42acf0766de839533266cdf
Philipp Spiess <hello@philippspiess.com>
2024-03-08 13:35:32 +0100

    Update vscode/webviews/chat/TextArea.tsx
    Co-authored-by: Tom Ross <tom@umpox.com>

--- vscode/webviews/chat/TextArea.tsx
+++ vscode/webviews/chat/TextArea.tsx
@@ -109,11 +109,10 @@ export const TextArea: React.FunctionComponent<ChatUITextAreaProps> = ({
                 const language = parseEditorData(clipboardData.getData('vscode-editor-data'))
 
                 if (language) {
-                    event.preventDefault()
-                    event.stopPropagation()
-
                     const text = clipboardData.getData('text/plain')
                     if (text.split('\n').length > 1) {
+                        event.preventDefault()
+                        event.stopPropagation()
                         const newText = '```\n' + text.replace(/\r\n/g, '\n') + '\n```\n'
 
                         const startPos = textarea.selectionStart
