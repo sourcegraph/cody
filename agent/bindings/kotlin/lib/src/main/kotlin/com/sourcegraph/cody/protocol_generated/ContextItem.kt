4ffb8aff49275cf54fe984f62fc213538277770a
Beyang Liu <beyang.liu@gmail.com>
2024-03-11 13:15:58 -0700

    regenerate kotlin bindings

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/ContextItem.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/ContextItem.kt
@@ -29,6 +29,7 @@ data class ContextItemFile(
   val revision: String? = null,
   val title: String? = null,
   val source: ContextItemSource? = null, // Oneof: embeddings, user, keyword, editor, filename, search, unified, selection, terminal
+  val metadata: ContextItemMetadata? = null,
   val type: TypeEnum? = null, // Oneof: file
   val isTooLarge: Boolean? = null,
 ) : ContextItem() {
@@ -46,6 +47,7 @@ data class ContextItemSymbol(
   val revision: String? = null,
   val title: String? = null,
   val source: ContextItemSource? = null, // Oneof: embeddings, user, keyword, editor, filename, search, unified, selection, terminal
+  val metadata: ContextItemMetadata? = null,
   val type: TypeEnum? = null, // Oneof: symbol
   val symbolName: String? = null,
   val kind: SymbolKind? = null, // Oneof: class, function, method
---
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/ContextItemMetadata.kt
@@ -0,0 +1,9 @@
+@file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
+package com.sourcegraph.cody.protocol_generated
+
+data class ContextItemMetadata(
+  val expandedQuery: String? = null,
+  val blugeScore: Int? = null,
+  val otherDisplayInfo: String? = null,
+)
+
