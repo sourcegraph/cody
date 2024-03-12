ab78b89c89b653aac26e2648455eb989546c7940
Valery Bugakov <skymk1@gmail.com>
2024-03-12 14:43:42 +0800

    Autocomplete: remove duplicate code

--- vscode/src/completions/doc-context-getters.ts
+++ vscode/src/completions/doc-context-getters.ts
@@ -3,10 +3,7 @@ import * as vscode from 'vscode'
 import { getLanguageConfig } from '../tree-sitter/language'
 import { type CompletionIntent, execQueryWrapper, positionToQueryPoints } from '../tree-sitter/query-sdk'
 
-import { asPoint } from '../tree-sitter/parse-tree-cache'
-import { parseString } from '../tree-sitter/parser'
 import type { DocumentContext } from './get-current-doc-context'
-import { lines } from './text-processing'
 
 export function getCurrentLinePrefixWithoutInjectedPrefix(docContext: DocumentContext): string {
     const { currentLinePrefix, injectedPrefix } = docContext
@@ -63,87 +60,4 @@ export function getCompletionIntent(params: GetCompletionIntentParams): Completi
     })
 
     return completionIntent?.name
-}
-
-interface GetLastNGraphContextFromDocumentParams {
-    n: number
-    document: vscode.TextDocument
-    position: vscode.Position
-    currentLinePrefix: string
-    /**
-     * Parse this source string to get the tree for the tree-sitter query
-     * instead of using `document.getText`
-     */
-    source?: string
-}
-
-export function getLastNGraphContextFromDocument(
-    params: GetLastNGraphContextFromDocumentParams
-): string[] {
-    const { document, currentLinePrefix, position, n } = params
-
-    const queryPoints = {
-        startPoint: asPoint({
-            line: Math.max(position.line - 100, 0),
-            character: 0,
-        }),
-        endPoint: asPoint({
-            line: position.line,
-            character: currentLinePrefix.length,
-        }),
-    }
-
-    const identifiers = execQueryWrapper({
-        document,
-        queryPoints,
-        queryWrapper: 'getGraphContextIdentifiers',
-    })
-        .map(identifier => identifier.node.text)
-        .filter(identifier => identifier.length > 2)
-
-    return Array.from(new Set(identifiers.reverse())).slice(0, n)
-}
-
-interface GetLastNGraphContextFromStringParams {
-    n: number
-    document: vscode.TextDocument
-    position: vscode.Position
-    currentLinePrefix: string
-    /**
-     * Parse this source string to get the tree for the tree-sitter query
-     * instead of using `document.getText()`
-     */
-    source: string
-}
-
-export function getLastNGraphContextFromString(params: GetLastNGraphContextFromStringParams): string[] {
-    const { document, source, n } = params
-
-    const queryPoints = {
-        startPoint: asPoint({
-            line: 0,
-            character: 0,
-        }),
-        endPoint: asPoint({
-            line: lines(source).length,
-            character: source.length,
-        }),
-    }
-
-    const tree = parseString(document.languageId, source)
-
-    if (!tree) {
-        return []
-    }
-
-    const identifiers = execQueryWrapper({
-        languageId: document.languageId,
-        queryPoints,
-        queryWrapper: 'getGraphContextIdentifiers',
-        tree,
-    })
-        .map(identifier => identifier.node.text)
-        .filter(identifier => identifier.length > 2)
-
-    return Array.from(new Set(identifiers.reverse())).slice(0, n)
 }
