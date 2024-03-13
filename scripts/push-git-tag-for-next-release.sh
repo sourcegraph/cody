fc71c43ab6520f84487c7daa10f4134d4d21b109
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-03-13 16:02:05 +0100

    Add additional checks to the release script (#1006)
    ## Test plan
    1. do a release

--- scripts/push-git-tag-for-next-release.sh
+++ scripts/push-git-tag-for-next-release.sh
@@ -3,6 +3,24 @@
 # No arguments needed, the version is automatically computed.
 set -eux
 
+# Check if the current branch is 'main'
+CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
+if [ "$CURRENT_BRANCH" != "main" ]; then
+  echo "Warning: You are not on the 'main' branch. You are on '$CURRENT_BRANCH'."
+  # shellcheck disable=SC2162
+  read -p "Are you sure you want to proceed? (y/n): " proceed
+  if [ "$proceed" != "y" ]; then
+    echo "Aborted."
+    exit 1
+  fi
+fi
+
+# Check if the working tree is clean
+if ! git diff-index --quiet HEAD --; then
+  echo "Error: Your working tree is not clean. Please commit or stash your changes."
+  exit 1
+fi
+
 # Check the number of arguments
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 [--major | --minor | --path]"
@@ -24,7 +42,7 @@ if [ "$NEXT_RELEASE_ARG" == "--major" ]; then
 fi
 
 # shellcheck disable=SC2162
-read -p "Confirm that you want to run the release v.$NEXT_VERSION (y/n): " choice
+read -p "Confirm that you want to run the release v$NEXT_VERSION (y/n): " choice
 if [ "$choice" == "y" ]; then
   echo "Running release..."
 else
