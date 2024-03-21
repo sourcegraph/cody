96433c415ad9040d0574f7abea76c9047d248ee7
Andrew Peebles <aqpeeb@gmail.com>
2024-03-21 10:58:24 -0700

    back to original proposal, to handle all cases

--- lib/shared/src/ollama/chat-client.ts
+++ lib/shared/src/ollama/chat-client.ts
@@ -73,7 +73,13 @@ export function ollamaChatClient(
             const textDecoder = new TextDecoder()
             const decoded = textDecoder.decode(value, { stream: true })
             // handle one or more ndjson records
-            const parsedLines = decoded.split('\n').map(m => JSON.parse(m)) as OllamaGenerateResponse[]
+            const matches = decoded.match(/.+/g)
+            let parsedLines: OllamaGenerateResponse[] = [];
+            if (matches !== null) {
+                parsedLines = matches.map(line => {
+                    return JSON.parse(line) as OllamaGenerateResponse
+                });
+            }
             for (const parsedLine of parsedLines) {
                 if (parsedLine.message) {
                     insertText += parsedLine.message.content
