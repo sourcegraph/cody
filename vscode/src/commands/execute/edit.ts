5356bb5af6919b86c783bf3a6ad6164951c990d9
Steve Yegge <steve.yegge@gmail.com>
2024-03-20 23:33:12 -0700

    V1 new client extension API, for inline edits + document code (#3445)
    Co-authored-by: Dominic Cooney <dominic.cooney@sourcegraph.com>
    Co-authored-by: Beatrix Woo <abeatrix@sourcegraph.com>

---
+++ vscode/src/commands/execute/edit.ts
@@ -0,0 +1,34 @@
+import { DefaultEditCommands } from '@sourcegraph/cody-shared/src/commands/types'
+import { type ExecuteEditArguments, executeEdit } from '../../edit/execute'
+import { getEditor } from '../../editor/active-editor'
+import type { EditCommandResult } from '../../main'
+
+import { wrapInActiveSpan } from '@sourcegraph/cody-shared/src/tracing'
+
+export async function executeEditCommand(
+    args: ExecuteEditArguments
+): Promise<EditCommandResult | undefined> {
+    return wrapInActiveSpan('command.test', async span => {
+        span.setAttribute('sampled', true)
+        const instruction = args.configuration?.instruction // get the instruction
+        const editor = getEditor()?.active // get the active editor
+        const document = editor?.document // get the document from the editor
+        if (!document || !instruction) {
+            return
+        }
+
+        // execute the edit with the provided configuration
+        return {
+            type: 'edit',
+            task: await executeEdit({
+                configuration: {
+                    ...args.configuration,
+                    instruction,
+                    document,
+                    mode: 'edit',
+                },
+                source: DefaultEditCommands.Test,
+            } satisfies ExecuteEditArguments),
+        }
+    })
+}
