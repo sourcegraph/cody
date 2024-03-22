217dc9039ddbb91550ec2fd61f9878d5879a9209
Beatrix <beatrix@sourcegraph.com>
2024-03-22 15:20:12 -0700

    Chat: display excluded @-files in UI

--- vscode/webviews/Components/FileLink.tsx
+++ vscode/webviews/Components/FileLink.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import type React from 'react'
 
 import { displayLineRange, displayPath, webviewOpenURIForContextItem } from '@sourcegraph/cody-shared'
@@ -12,7 +13,9 @@ export const FileLink: React.FunctionComponent<FileLinkProps> = ({
     repoName,
     title,
     revision,
+    isTooLarge,
 }) => {
+    const icon = getIconByFileSource(source)
     if (source === 'unified') {
         // This is a remote search result.
         const repoShortName = repoName?.slice(repoName.lastIndexOf('/') + 1)
@@ -32,13 +35,34 @@ export const FileLink: React.FunctionComponent<FileLinkProps> = ({
         )
     }
 
-    const pathToDisplay = `@${displayPath(uri)}`
+    const pathToDisplay = `${displayPath(uri)}`
     const pathWithRange = range ? `${pathToDisplay}:${displayLineRange(range)}` : pathToDisplay
     const tooltip = source ? `${pathWithRange} included via ${source}` : pathWithRange
     const { href, target } = webviewOpenURIForContextItem({ uri, range })
+    const warning = 'This file is excluded due to token limit reached'
     return (
-        <a className={styles.linkButton} title={tooltip} href={href} target={target}>
-            {pathWithRange}
-        </a>
+        <span className="styles.item">
+            {isTooLarge && <i className="codicon codicon-warning" />}
+            <i className={`codicon codicon-${icon}`} />
+            <a
+                className={classNames(styles.linkButton, isTooLarge && styles.excluded)}
+                title={isTooLarge ? warning : tooltip}
+                href={href}
+                target={target}
+            >
+                {pathWithRange}
+            </a>
+        </span>
     )
+}
+
+function getIconByFileSource(source?: string): string {
+    switch (source) {
+        case 'user':
+            return 'mention'
+        case 'uri':
+            return 'link'
+        default:
+            return 'sparkle'
+    }
 }
