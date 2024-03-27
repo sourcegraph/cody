411b276c09bbc132351e7ece6597e8cda0e57600
Beatrix <beatrix@sourcegraph.com>
2024-03-27 06:51:53 -0700

    E2E Test: Detect detached state instead of hidden

--- vscode/test/e2e/chat-atFile.test.ts
+++ vscode/test/e2e/chat-atFile.test.ts
@@ -302,10 +302,9 @@ test.extend<ExpectedEvents>({
     await page.getByRole('tab', { name: 'buzz.ts' }).click()
 
     // Wait for the tsserver to become ready: when loading status disappears
-    await expect(page.getByRole('button', { name: 'Editor Language Status: Loading' })).toBeVisible()
-    await page.waitForSelector('button[aria-label="Editor Language Status: Loading"]', {
-        state: 'hidden',
-    })
+    const langServerLoadingState = 'Editor Language Status: Loading'
+    await expect(page.getByRole('button', { name: langServerLoadingState })).toBeVisible()
+    await page.waitForSelector(`button[aria-label="${langServerLoadingState}"]`, { state: 'detached' })
 
     // Go back to the Cody chat tab
     await page.getByRole('tab', { name: 'New Chat' }).click()
@@ -322,7 +321,7 @@ test.extend<ExpectedEvents>({
 
     // Clicking on a file in the selector should autocomplete the file in chat input with added space
     await chatInput.clear()
-    await chatInput.pressSequentially('@#fizzb', { delay: 10 })
+    await chatInput.pressSequentially('@#fizzb', { delay: 200 })
     await expect(chatPanelFrame.getByRole('option', { name: 'fizzbuzz()' })).toBeVisible()
     await chatPanelFrame.getByRole('option', { name: 'fizzbuzz()' }).click()
     await expect(chatInput).toHaveText('@buzz.ts:1-15#fizzbuzz() ')
