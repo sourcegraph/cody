4753e435e7f232dd89044615fcd041b1fd41df7b
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-03-29 13:45:08 +0900

    Add missing token-count.ts

---
+++ lib/shared/src/prompt/token-count.ts
@@ -0,0 +1,11 @@
+import { Tiktoken } from 'tiktoken/lite'
+const cl100k_base = require('tiktoken/encoders/cl100k_base.json')
+
+// TODO: specify the model as a parameter; tokenization can vary by model.
+export function countTokens(s: string): number {
+    // TODO: Cache and reuse the encoding.
+    const encoding = new Tiktoken(cl100k_base.bpe_ranks, cl100k_base.special_tokens, cl100k_base.pat_str)
+    const tokens = encoding.encode('hello world')
+    encoding.free()
+    return tokens.length
+}
