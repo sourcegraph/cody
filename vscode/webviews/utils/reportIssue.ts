24e213449d87f03d1adf8065984d3534a4636840
Beatrix <beatrix@sourcegraph.com>
2024-04-01 13:34:28 -0700

    Add expected behavior

--- vscode/webviews/utils/reportIssue.ts
+++ vscode/webviews/utils/reportIssue.ts
@@ -15,13 +15,16 @@ const issueBody = `## Extension Information
 <!-- Do not remove the pre-filled information below -->
 - Cody Version: ${packageJson.version}
 - VS Code Version: ${vscode.version}
-- Host: ${vscode.env.appHost}
+- Extension Host: ${vscode.env.appHost}
 
 ##  Steps to Reproduce
 <!-- A detailed description of the issue -->
 1.
 2.
 3.
+
+## Expected Behaviour
+<!-- A detailed description of what you expected to happen -->
 
 ## Logs
 <!-- Attach logs from the 'Cody Debug: Export Logs' command -->
