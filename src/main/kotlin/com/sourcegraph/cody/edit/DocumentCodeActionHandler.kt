71e226944fa999b2ada48fadf5507e1bba14e0fc
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-04-03 23:19:08 +0200

    Fix Cannot add an action twice: Document Code (null) (closes #1233) (#1279)
    Fixes #1233.
    
    
    ## Test plan
    
    ### "Document Code" issue
    1. open a project
    2. enable inline edits 
    3. close project (but keep the IDE on)
    4. reopen the project
    
    expected:
    no error
    
    ### Bundles (https://github.com/sourcegraph/jetbrains/issues/1043)
    1. Have Cody plugin installed in your JetBrains IntelliJ IDEA editor
    2. User is already logged in to Cody with Pro account
    3. Use Cody for a while and click on Cody icon from status bar at bottom
    right corner
    4. Click on "Manage Accounts" and click on "+" button to add another
    account

--- src/main/kotlin/com/sourcegraph/cody/edit/DocumentCodeActionHandler.kt
+++ src/main/kotlin/com/sourcegraph/cody/edit/DocumentCodeActionHandler.kt
@@ -1,5 +1,6 @@
 package com.sourcegraph.cody.edit
 
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Caret
@@ -8,9 +9,19 @@ import com.intellij.openapi.editor.actionSystem.EditorAction
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler
 import com.intellij.openapi.project.DumbAware
 import com.sourcegraph.cody.autocomplete.action.CodyAction
+import com.sourcegraph.cody.config.CodyApplicationSettings
+import com.sourcegraph.config.ConfigUtil
 import com.sourcegraph.utils.CodyEditorUtil
 
-class DocumentCodeAction : EditorAction(DocumentCodeActionHandler()), CodyAction, DumbAware
+class DocumentCodeAction : EditorAction(DocumentCodeActionHandler()), CodyAction, DumbAware {
+  override fun update(e: AnActionEvent) {
+    super.update(e)
+
+    e.presentation.isVisible =
+        ConfigUtil.isFeatureFlagEnabled("cody.feature.inline-edits") ||
+            CodyApplicationSettings.instance.isInlineEditionEnabled
+  }
+}
 
 class DocumentCodeActionHandler : EditorActionHandler() {
   private val logger = Logger.getInstance(DocumentCodeActionHandler::class.java)
--- src/main/kotlin/com/sourcegraph/cody/initialization/PostStartupActivity.kt
+++ src/main/kotlin/com/sourcegraph/cody/initialization/PostStartupActivity.kt
@@ -1,15 +1,10 @@
 package com.sourcegraph.cody.initialization
 
-import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.Constraints
-import com.intellij.openapi.actionSystem.DefaultActionGroup
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import com.sourcegraph.cody.CodyFocusChangeListener
 import com.sourcegraph.cody.agent.CodyAgentService
 import com.sourcegraph.cody.auth.SelectOneOfTheAccountsAsActive
-import com.sourcegraph.cody.config.CodyApplicationSettings
 import com.sourcegraph.cody.config.SettingsMigration
 import com.sourcegraph.cody.config.ui.CheckUpdatesTask
 import com.sourcegraph.cody.statusbar.CodyStatusService
@@ -34,28 +29,5 @@ class PostStartupActivity : StartupActivity.DumbAware {
     CodyStatusService.resetApplication(project)
     CodyFocusChangeListener().runActivity(project)
     EndOfTrialNotificationScheduler.createAndStart(project)
-    initializeInlineEdits()
-  }
-
-  // TODO: This should go away (along with the feature flag) once Inline Edits are stable/released.
-  private fun initializeInlineEdits() {
-    ApplicationManager.getApplication().invokeLater {
-      if (ConfigUtil.isFeatureFlagEnabled("cody.feature.inline-edits") ||
-          CodyApplicationSettings.instance.isInlineEditionEnabled) {
-        val actionManager = ActionManager.getInstance()
-        (actionManager.getAction("CodyEditorActions") as? DefaultActionGroup)?.apply {
-          pushFrontAction(actionManager, "cody.documentCodeAction", this)
-          pushFrontAction(actionManager, "cody.editCodeAction", this)
-        }
-      }
-    }
-  }
-
-  private fun pushFrontAction(
-      actionManager: ActionManager,
-      actionId: String,
-      group: DefaultActionGroup
-  ) {
-    actionManager.getAction(actionId)?.let { group.add(it, Constraints.FIRST) }
   }
 }
