5a159d13d7456180b37bffea556c8eb182962c3e
Philipp Spiess <hello@philippspiess.com>
2024-04-09 19:06:20 +0200

    Autocomplete: Add Codegemma prompt to Ollama provider (#3754)

--- vscode/src/completions/providers/ollama-models.ts
+++ vscode/src/completions/providers/ollama-models.ts
@@ -129,6 +129,39 @@ class StarCoder2 extends DefaultOllamaModel {
     }
 }
 
+class CodeGemma extends DefaultOllamaModel {
+    getPrompt(ollamaPrompt: OllamaPromptContext): string {
+        const { context, prefix, suffix } = ollamaPrompt
+        // c.f. https://huggingface.co/blog/codegemma
+        // c.f. https://huggingface.co/google/codegemma-7b/blob/main/tokenizer.json
+        return `<|fim_prefix|>${context}${prefix}<|fim_suffix|>${suffix}<|fim_middle|>`
+    }
+
+    getRequestOptions(isMultiline: boolean): OllamaGenerateParameters {
+        const stop = [
+            '<|fim_prefix|>',
+            '<|fim_suffix|>',
+            '<|fim_middle|>',
+            '<|endoftext|>',
+            '<|file_separator|>',
+        ]
+
+        const params = {
+            stop: ['\n', ...stop],
+            temperature: 0.2,
+            top_k: -1,
+            top_p: -1,
+            num_predict: 256,
+        }
+
+        if (isMultiline) {
+            params.stop = ['\n\n', ...stop]
+        }
+
+        return params
+    }
+}
+
 export function getModelHelpers(model: string) {
     if (model.includes('codellama')) {
         return new CodeLlama()
@@ -140,6 +173,10 @@ export function getModelHelpers(model: string) {
 
     if (model.includes('starcoder2')) {
         return new StarCoder2()
+    }
+
+    if (model.includes('codegemma')) {
+        return new CodeGemma()
     }
 
     return new DefaultOllamaModel()
