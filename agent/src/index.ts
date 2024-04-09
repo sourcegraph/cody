e39a0757ac516c6cb350dee289a100ff916b73ad
Chris Warwick <christopher.warwick@sourcegraph.com>
2024-04-09 09:34:22 -0400

    add win-ca for windows certs

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -26,6 +26,10 @@ process.on('uncaughtException', e => {
 // This is a no op for non-mac environments
 require('mac-ca').addToGlobalAgent({ excludeBundled: false })
 
+// Installs windows root certs onto the global agent
+// This is a no op for non-windows environments
+require('win-ca').inject('+')
+
 const args = process.argv.slice(2)
 const { operands } = rootCommand.parseOptions(args)
 if (operands.length === 0) {
