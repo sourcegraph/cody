05b93385aaf0fddff15fb8ef836c5d6ddf9b25e6
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-04-12 01:42:42 +0900

    Re-generate Kotlin bindings

---
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/RemoteRepo_ListParams.kt
@@ -0,0 +1,9 @@
+@file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
+package com.sourcegraph.cody.protocol_generated
+
+data class RemoteRepo_ListParams(
+  val query: String? = null,
+  val first: Int? = null,
+  val afterId: String? = null,
+)
+
