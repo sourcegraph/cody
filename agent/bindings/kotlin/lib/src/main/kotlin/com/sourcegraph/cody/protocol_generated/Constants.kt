29654e64b87d3e802caa4c85010cb939a89ae70b
Rafal Gajdulewicz <rafal.gajdulewicz@sourcegraph.com>
2024-04-11 16:55:35 +0200

    Re-generate Kotlin bindings

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/Constants.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/Constants.kt
@@ -30,6 +30,8 @@ object Constants {
   const val ready = "ready"
   const val `not-a-git-repo` = "not-a-git-repo"
   const val `git-repo-has-no-remote` = "git-repo-has-no-remote"
+  const val sourcegraph = "sourcegraph"
+  const val openai = "openai"
   const val search = "search"
   const val local = "local"
   const val unindexed = "unindexed"
