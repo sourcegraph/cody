ad761b40e99df090d0d1584094945c6dcb6ec360
Beatrix <beatrix@sourcegraph.com>
2024-04-17 09:55:09 -0700

    create unique session ID for telemetry

--- vscode/src/chat/chat-view/SimpleChatPanelProvider.ts
+++ vscode/src/chat/chat-view/SimpleChatPanelProvider.ts
@@ -412,7 +412,7 @@ export class SimpleChatPanelProvider implements vscode.Disposable, ChatSession {
                 source,
                 command,
                 traceId: span.spanContext().traceId,
-                sessionID: this.chatModel.sessionID,
+                sessionID: `${vscode.env.sessionId}@${this.chatModel.sessionID}`,
             }
             telemetryService.log('CodyVSCodeExtension:chat-question:submitted', sharedProperties)
             telemetryRecorder.recordEvent('cody.chat-question', 'submitted', {
