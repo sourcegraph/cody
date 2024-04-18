0fac85f348b63dfb3ccec0c547b75cc7df08ffc8
Philipp Spiess <hello@philippspiess.com>
2024-04-18 11:07:55 +0200

    Convert editCommands/code to a proper PromptString (#3851)

--- vscode/src/jsonrpc/agent-protocol.ts
+++ vscode/src/jsonrpc/agent-protocol.ts
@@ -81,7 +81,7 @@ export type ClientRequests = {
     'commands/custom': [{ key: string }, CustomCommandResult]
 
     // Trigger commands that edit the code.
-    'editCommands/code': [{ params: { instruction: string } }, EditTask]
+    'editCommands/code': [{ instruction: string; model: string }, EditTask]
     'editCommands/test': [null, EditTask]
     'editCommands/document': [null, EditTask]
 
