6da09efb6d6f6d83b94920d7967288e425ce0897
Philipp Spiess <hello@philippspiess.com>
2024-04-22 15:51:12 +0200

    Add a debug log on why we're ignoring

--- vscode/src/completions/inline-completion-item-provider.ts
+++ vscode/src/completions/inline-completion-item-provider.ts
@@ -203,11 +203,13 @@ export class InlineCompletionItemProvider
     ): Promise<AutocompleteResult | null> {
         // Do not create item for files that are on the cody ignore list
         if (isCodyIgnoredFile(document.uri)) {
+            logIgnored(document.uri, 'cody-ignore')
             return null
         }
 
         return wrapInActiveSpan('autocomplete.provideInlineCompletionItems', async span => {
             if (!(await contextFiltersProvider.isUriAllowed(document.uri))) {
+                logIgnored(document.uri, 'context-filter')
                 return null
             }
 
@@ -798,4 +800,17 @@ function onlyCompletionWidgetSelectionChanged(
     }
 
     return prevSelectedCompletionInfo.text !== nextSelectedCompletionInfo.text
+}
+
+let lasIgnoredUriLogged: string | undefined = undefined
+function logIgnored(uri: vscode.Uri, reason: 'cody-ignore' | 'context-filter') {
+    const string = uri.toString()
+    if (lasIgnoredUriLogged === string) {
+        return
+    }
+    lasIgnoredUriLogged = string
+    logDebug(
+        'CodyCompletionProvider:ignored',
+        'Cody is disabled in file ' + uri.toString() + '(' + reason + ')'
+    )
 }
