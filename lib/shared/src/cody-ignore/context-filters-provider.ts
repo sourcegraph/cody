d7c6aee4fdb47af082f886e3d064177e514451f3
Philipp Spiess <hello@philippspiess.com>
2024-04-26 12:56:25 +0200

    Remove debug code

--- lib/shared/src/cody-ignore/context-filters-provider.ts
+++ lib/shared/src/cody-ignore/context-filters-provider.ts
@@ -1,219 +1,242 @@
-import { isEqual } from 'lodash'
-import { LRUCache } from 'lru-cache'
-import { RE2JS as RE2 } from 're2js'
-import type * as vscode from 'vscode'
-import { isFileURI } from '../common/uri'
-import { logDebug, logError } from '../logger'
-import { graphqlClient } from '../sourcegraph-api/graphql'
+import { isEqual } from "lodash"
+import { LRUCache } from "lru-cache"
+import { RE2JS as RE2 } from "re2js"
+import type * as vscode from "vscode"
+import { isFileURI } from "../common/uri"
+import { logDebug, logError } from "../logger"
+import { graphqlClient } from "../sourcegraph-api/graphql"
 import {
-    type CodyContextFilterItem,
-    type ContextFilters,
-    EXCLUDE_EVERYTHING_CONTEXT_FILTERS,
-    INCLUDE_EVERYTHING_CONTEXT_FILTERS,
-} from '../sourcegraph-api/graphql/client'
-import { wrapInActiveSpan } from '../tracing'
-import { createSubscriber } from '../utils'
+  type CodyContextFilterItem,
+  type ContextFilters,
+  EXCLUDE_EVERYTHING_CONTEXT_FILTERS,
+  INCLUDE_EVERYTHING_CONTEXT_FILTERS,
+} from "../sourcegraph-api/graphql/client"
+import { wrapInActiveSpan } from "../tracing"
+import { createSubscriber } from "../utils"
 
 export const REFETCH_INTERVAL = 60 * 60 * 1000 // 1 hour
 
 interface ParsedContextFilters {
-    include: null | ParsedContextFilterItem[]
-    exclude: null | ParsedContextFilterItem[]
+  include: null | ParsedContextFilterItem[]
+  exclude: null | ParsedContextFilterItem[]
 }
 
 interface ParsedContextFilterItem {
-    repoNamePattern: RE2
-    filePathPatterns?: RE2[]
+  repoNamePattern: RE2
+  filePathPatterns?: RE2[]
 }
 
 // Note: This can not be an empty string to make all non `false` values truthy.
 export type IsIgnored =
-    | false
-    | 'has-ignore-everything-filters'
-    | 'non-file-uri'
-    | 'no-repo-found'
-    | `repo:${string}`
+  | false
+  | "has-ignore-everything-filters"
+  | "non-file-uri"
+  | "no-repo-found"
+  | `repo:${string}`
 
-export type GetRepoNamesFromWorkspaceUri = (uri: vscode.Uri) => Promise<string[] | null>
+export type GetRepoNamesFromWorkspaceUri = (
+  uri: vscode.Uri,
+) => Promise<string[] | null>
 type RepoName = string
 type IsRepoNameIgnored = boolean
 
 // These schemes are always deemed safe. Remote context has https URIs, but
 // the remote applies Cody ignore rules.
-const allowedSchemes = new Set(['http', 'https'])
+const allowedSchemes = new Set(["http", "https"])
 
 export class ContextFiltersProvider implements vscode.Disposable {
-    /**
-     * `null` value means that we failed to fetch context filters.
-     * In that case, we should exclude all the URIs.
-     */
-    private lastContextFiltersResponse: ContextFilters | null = null
-    private parsedContextFilters: ParsedContextFilters | null = null
+  /**
+   * `null` value means that we failed to fetch context filters.
+   * In that case, we should exclude all the URIs.
+   */
+  private lastContextFiltersResponse: ContextFilters | null = null
+  private parsedContextFilters: ParsedContextFilters | null = null
 
-    private cache = new LRUCache<RepoName, IsRepoNameIgnored>({ max: 128 })
-    private getRepoNamesFromWorkspaceUri: GetRepoNamesFromWorkspaceUri | undefined = undefined
+  private cache = new LRUCache<RepoName, IsRepoNameIgnored>({ max: 128 })
+  private getRepoNamesFromWorkspaceUri:
+    | GetRepoNamesFromWorkspaceUri
+    | undefined = undefined
 
-    private fetchIntervalId: NodeJS.Timeout | undefined | number
+  private fetchIntervalId: NodeJS.Timeout | undefined | number
 
-    private readonly contextFiltersSubscriber = createSubscriber<ContextFilters>()
-    public readonly onContextFiltersChanged = this.contextFiltersSubscriber.subscribe
+  private readonly contextFiltersSubscriber = createSubscriber<ContextFilters>()
+  public readonly onContextFiltersChanged =
+    this.contextFiltersSubscriber.subscribe
 
-    async init(getRepoNamesFromWorkspaceUri: GetRepoNamesFromWorkspaceUri) {
-        this.getRepoNamesFromWorkspaceUri = getRepoNamesFromWorkspaceUri
-        this.dispose()
-        await this.fetchContextFilters()
-        this.startRefetchTimer()
+  async init(getRepoNamesFromWorkspaceUri: GetRepoNamesFromWorkspaceUri) {
+    this.getRepoNamesFromWorkspaceUri = getRepoNamesFromWorkspaceUri
+    this.dispose()
+    await this.fetchContextFilters()
+    this.startRefetchTimer()
+  }
+
+  private async fetchContextFilters(): Promise<void> {
+    try {
+      const response = await graphqlClient.contextFilters()
+      this.setContextFilters(response)
+    } catch (error) {
+      logError("ContextFiltersProvider", "fetchContextFilters", {
+        verbose: error,
+      })
+    }
+  }
+
+  private setContextFilters(contextFilters: ContextFilters): void {
+    if (isEqual(contextFilters, this.lastContextFiltersResponse)) {
+      return
     }
 
-    private async fetchContextFilters(): Promise<void> {
-        try {
-            const response = await graphqlClient.contextFilters()
-            this.setContextFilters(response)
-        } catch (error) {
-            logError('ContextFiltersProvider', 'fetchContextFilters', { verbose: error })
-        }
+    this.cache.clear()
+    this.parsedContextFilters = null
+    this.lastContextFiltersResponse = contextFilters
+    this.contextFiltersSubscriber.notify(contextFilters)
+
+    logDebug("ContextFiltersProvider", "setContextFilters", {
+      verbose: contextFilters,
+    })
+    this.parsedContextFilters = {
+      include: contextFilters.include?.map(parseContextFilterItem) || null,
+      exclude: contextFilters.exclude?.map(parseContextFilterItem) || null,
+    }
+  }
+
+  /**
+   * Overrides context filters for testing.
+   */
+  public setTestingContextFilters(contextFilters: ContextFilters | null): void {
+    if (process.env.VITEST !== "true") {
+      throw new Error(
+        "contextFiltersProvider.setTestingContextFilters should be only used in tests",
+      )
     }
 
-    private setContextFilters(contextFilters: ContextFilters): void {
-        if (isEqual(contextFilters, this.lastContextFiltersResponse)) {
-            return
-        }
+    if (contextFilters === null) {
+      // Reset context filters to the value from the Sourcegraph API.
+      this.init(this.getRepoNamesFromWorkspaceUri!)
+    } else {
+      this.setContextFilters(contextFilters)
+    }
+  }
 
-        this.cache.clear()
-        this.parsedContextFilters = null
-        this.lastContextFiltersResponse = contextFilters
-        this.contextFiltersSubscriber.notify(contextFilters)
+  private startRefetchTimer(): void {
+    this.fetchIntervalId = setTimeout(() => {
+      this.fetchContextFilters()
+      this.startRefetchTimer()
+    }, REFETCH_INTERVAL)
+  }
 
-        logDebug('ContextFiltersProvider', 'setContextFilters', { verbose: contextFilters })
-        this.parsedContextFilters = {
-            include: contextFilters.include?.map(parseContextFilterItem) || null,
-            exclude: contextFilters.exclude?.map(parseContextFilterItem) || null,
-        }
+  public isRepoNameIgnored(repoName: string): boolean {
+    const cached = this.cache.get(repoName)
+    if (cached !== undefined) {
+      return cached
     }
 
-    /**
-     * Overrides context filters for testing.
-     */
-    public setTestingContextFilters(contextFilters: ContextFilters | null): void {
-        if (process.env.VITEST !== 'true') {
-            throw new Error(
-                'contextFiltersProvider.setTestingContextFilters should be only used in tests'
-            )
-        }
+    // If we don't have any context filters, we exclude everything.
+    let isIgnored = this.parsedContextFilters === null
 
-        if (contextFilters === null) {
-            // Reset context filters to the value from the Sourcegraph API.
-            this.init(this.getRepoNamesFromWorkspaceUri!)
-        } else {
-            this.setContextFilters(contextFilters)
-        }
+    for (const parsedFilter of this.parsedContextFilters?.include || []) {
+      isIgnored = !matchesContextFilter(parsedFilter, repoName)
+      if (!isIgnored) {
+        break
+      }
     }
 
-    private startRefetchTimer(): void {
-        this.fetchIntervalId = setTimeout(() => {
-            this.fetchContextFilters()
-            this.startRefetchTimer()
-        }, REFETCH_INTERVAL)
+    for (const parsedFilter of this.parsedContextFilters?.exclude || []) {
+      if (matchesContextFilter(parsedFilter, repoName)) {
+        isIgnored = true
+        break
+      }
     }
 
-    public isRepoNameIgnored(repoName: string): boolean {
-        const cached = this.cache.get(repoName)
-        if (cached !== undefined) {
-            return cached
-        }
+    this.cache.set(repoName, isIgnored)
+    return isIgnored
+  }
 
-        // If we don't have any context filters, we exclude everything.
-        let isIgnored = this.parsedContextFilters === null
-
-        for (const parsedFilter of this.parsedContextFilters?.include || []) {
-            isIgnored = !matchesContextFilter(parsedFilter, repoName)
-            if (!isIgnored) {
-                break
-            }
-        }
-
-        for (const parsedFilter of this.parsedContextFilters?.exclude || []) {
-            if (matchesContextFilter(parsedFilter, repoName)) {
-                isIgnored = true
-                break
-            }
-        }
-
-        this.cache.set(repoName, isIgnored)
-        return isIgnored
+  public async isUriIgnored(uri: vscode.Uri): Promise<IsIgnored> {
+    if (allowedSchemes.has(uri.scheme) || this.hasAllowEverythingFilters()) {
+      return false
+    }
+    if (this.hasIgnoreEverythingFilters()) {
+      return "has-ignore-everything-filters"
     }
 
-    public async isUriIgnored(uri: vscode.Uri): Promise<IsIgnored> {
-        if (uri.toString().endsWith('Api.ts')) {
-            return 'repo:debug'
-        }
-
-        if (allowedSchemes.has(uri.scheme) || this.hasAllowEverythingFilters()) {
-            return false
-        }
-        if (this.hasIgnoreEverythingFilters()) {
-            return 'has-ignore-everything-filters'
-        }
-
-        // TODO: process non-file URIs https://github.com/sourcegraph/cody/issues/3893
-        if (!isFileURI(uri)) {
-            logDebug('ContextFiltersProvider', 'isUriIgnored', `non-file URI ${uri.scheme}`)
-            return 'non-file-uri'
-        }
-
-        const repoNames = await wrapInActiveSpan('repoNameResolver.getRepoNamesFromWorkspaceUri', () =>
-            this.getRepoNamesFromWorkspaceUri?.(uri)
-        )
-
-        if (!repoNames) {
-            return 'no-repo-found'
-        }
-
-        const ignoredRepo = repoNames.find(repoName => this.isRepoNameIgnored(repoName))
-        if (ignoredRepo) {
-            return `repo:${ignoredRepo}`
-        }
-
-        return false
+    // TODO: process non-file URIs https://github.com/sourcegraph/cody/issues/3893
+    if (!isFileURI(uri)) {
+      logDebug(
+        "ContextFiltersProvider",
+        "isUriIgnored",
+        `non-file URI ${uri.scheme}`,
+      )
+      return "non-file-uri"
     }
 
-    public dispose(): void {
-        this.cache.clear()
+    const repoNames = await wrapInActiveSpan(
+      "repoNameResolver.getRepoNamesFromWorkspaceUri",
+      () => this.getRepoNamesFromWorkspaceUri?.(uri),
+    )
 
-        if (this.fetchIntervalId) {
-            clearTimeout(this.fetchIntervalId)
-        }
+    if (!repoNames) {
+      return "no-repo-found"
     }
 
-    private hasAllowEverythingFilters() {
-        return this.lastContextFiltersResponse === INCLUDE_EVERYTHING_CONTEXT_FILTERS
+    const ignoredRepo = repoNames.find((repoName) =>
+      this.isRepoNameIgnored(repoName),
+    )
+    if (ignoredRepo) {
+      return `repo:${ignoredRepo}`
     }
 
-    private hasIgnoreEverythingFilters() {
-        return this.lastContextFiltersResponse === EXCLUDE_EVERYTHING_CONTEXT_FILTERS
-    }
+    return false
+  }
 
-    public toDebugObject() {
-        return {
-            lastContextFiltersResponse: JSON.parse(JSON.stringify(this.lastContextFiltersResponse)),
-        }
+  public dispose(): void {
+    this.cache.clear()
+
+    if (this.fetchIntervalId) {
+      clearTimeout(this.fetchIntervalId)
     }
+  }
+
+  private hasAllowEverythingFilters() {
+    return (
+      this.lastContextFiltersResponse === INCLUDE_EVERYTHING_CONTEXT_FILTERS
+    )
+  }
+
+  private hasIgnoreEverythingFilters() {
+    return (
+      this.lastContextFiltersResponse === EXCLUDE_EVERYTHING_CONTEXT_FILTERS
+    )
+  }
+
+  public toDebugObject() {
+    return {
+      lastContextFiltersResponse: JSON.parse(
+        JSON.stringify(this.lastContextFiltersResponse),
+      ),
+    }
+  }
 }
 
-function matchesContextFilter(parsedFilter: ParsedContextFilterItem, repoName: string): boolean {
-    // Calling `RE2.matches(input)` only looks for full matches, so we use
-    // `RE2.matcher(input).find(0)` to find matches anywhere in `input` (which is the standard way
-    // regexps work).
-    return Boolean(parsedFilter.repoNamePattern.matcher(repoName).find(0))
+function matchesContextFilter(
+  parsedFilter: ParsedContextFilterItem,
+  repoName: string,
+): boolean {
+  // Calling `RE2.matches(input)` only looks for full matches, so we use
+  // `RE2.matcher(input).find(0)` to find matches anywhere in `input` (which is the standard way
+  // regexps work).
+  return Boolean(parsedFilter.repoNamePattern.matcher(repoName).find(0))
 }
 
-function parseContextFilterItem(item: CodyContextFilterItem): ParsedContextFilterItem {
-    const repoNamePattern = RE2.compile(item.repoNamePattern)
-    const filePathPatterns = item.filePathPatterns
-        ? item.filePathPatterns.map(pattern => RE2.compile(pattern))
-        : undefined
+function parseContextFilterItem(
+  item: CodyContextFilterItem,
+): ParsedContextFilterItem {
+  const repoNamePattern = RE2.compile(item.repoNamePattern)
+  const filePathPatterns = item.filePathPatterns
+    ? item.filePathPatterns.map((pattern) => RE2.compile(pattern))
+    : undefined
 
-    return { repoNamePattern, filePathPatterns }
+  return { repoNamePattern, filePathPatterns }
 }
 
 /**
