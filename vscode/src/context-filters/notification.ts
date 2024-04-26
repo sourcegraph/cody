19a6679cb592ad313139e7e21e3e7517130aa7b2
Philipp Spiess <hello@philippspiess.com>
2024-04-26 20:16:55 +0200

    Remove leftover

--- vscode/src/context-filters/notification.ts
+++ vscode/src/context-filters/notification.ts
@@ -1,6 +1,4 @@
 import * as vscode from 'vscode'
-// import { repoNameResolver } from '../repository/repo-name-resolver'
-// import { localStorage } from '../services/LocalStorageProvider'
 
 export type CodyIgnoreType = 'cody-ignore' | 'context-filter'
 
@@ -15,25 +13,6 @@ export async function passiveNotification(uri: vscode.Uri, type: CodyIgnoreType)
     if (type === 'cody-ignore') {
         return
     }
-    // const repos = await repoNameResolver.getRepoNamesFromWorkspaceUri(uri)
-
-    // If we can't find a repo for the resolved file, we won't be showing a
-    // notification since we have no way to silence the message properly
-    // if (!repos || repos.length === 0) {
-    //     return
-    // }
-
-    // const isAnyRepoDismissed = repos.some(repo =>
-    //     localStorage.get('cody-ignore-notification-dismissed-' + repo)
-    // )
-
-    // if (isAnyRepoDismissed) {
-    //     return
-    // }
-
-    // await Promise.all(
-    //     repos.map(repo => localStorage.set('cody-ignore-notification-dismissed-' + repo, 'true'))
-    // )
 
     vscode.window.showInformationMessage('Cody ignores this file because of your admin policy.')
 }
