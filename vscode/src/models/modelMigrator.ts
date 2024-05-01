8dcdc038383dda22ce338a97ba3f6561d1d6dc6a
Beatrix <beatrix@sourcegraph.com>
2024-04-30 21:48:23 -0700

    verify new model availability before show notification

--- vscode/src/models/modelMigrator.ts
+++ vscode/src/models/modelMigrator.ts
@@ -1,19 +1,22 @@
+import { ModelProvider, getDotComDefaultModels } from '@sourcegraph/cody-shared'
 import * as vscode from 'vscode'
 import { isRunningInsideAgent } from '../jsonrpc/isRunningInsideAgent'
 import { localStorage } from '../services/LocalStorageProvider'
 
+const deprecatedModelSet = new Set(
+    getDotComDefaultModels()
+        .filter(m => m.deprecated)
+        .map(m => m.model)
+)
 export function migrateAndNotifyForOutdatedModels(model: string | null): string | null {
-    if (!model || isRunningInsideAgent()) {
+    if (!model || isRunningInsideAgent() || !deprecatedModelSet.has(model)) {
         return model
     }
 
-    // Claude 2 to Claude 3 migration
-    if (
-        model === 'anthropic/claude-instant-1.2' ||
-        model === 'anthropic/claude-2.0' ||
-        model === 'anthropic/claude-2.1'
-    ) {
-        const newModel = 'anthropic/claude-3-sonnet-20240229'
+    // Claude 2 to Claude 3 migration.
+    const newModel = 'anthropic/claude-3-sonnet-20240229'
+    // Verify that the new model is available before migrating.
+    if (ModelProvider.getProviderByModel(newModel)) {
         showNotificationIfNotShownYet(
             'We upgraded you to Claude 3! The Claude 2 family of models is no longer available but you have access to the better Claude 3 model instead.',
             'claude2-migration-notification-shown'
