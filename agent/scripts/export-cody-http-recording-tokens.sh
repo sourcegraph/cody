fb8997b945560adf454322be2dba7f8a79888bb1
Ólafur Páll Geirsson <olafurpg@gmail.com>
2024-05-01 12:21:24 +0200

    Inline Edit: add minimized test case for bug report (#4001)

--- agent/scripts/export-cody-http-recording-tokens.sh
+++ agent/scripts/export-cody-http-recording-tokens.sh
@@ -7,10 +7,14 @@
 #   pnpm update-agent-recordings
 #   pnpm update-symf-recordings
 #
+# Tips to update these secrets:
+#  - Use 1Password to find account passwords
+#  - Use no expiration dates when creating access tokens
+#  - You need to update the REDACTED_ access token in
+#    agent/src/testing-tokens.ts.  There's no automatic way to do this for now,
+#    you need to re-record with the new token, manually copy the updated REDACTED_
+#    token from the recording file and paste it into agent/src/testing-tokens.ts.
 
-# This is a token for a username+password account that was created by
-# @olafurpg. Ideally, we can change this account to a dedicated Cody testing
-# account in the future. This account has fairly large quotas.
 export SRC_ACCESS_TOKEN="$(gcloud secrets versions access latest --secret CODY_PRO_ACCESS_TOKEN --project cody-agent-tokens --quiet)"
 
 export SRC_ENTERPRISE_ACCESS_TOKEN="$(gcloud secrets versions access latest --secret CODY_ENTERPRISE_ACCESS_TOKEN --project cody-agent-tokens --quiet)"
