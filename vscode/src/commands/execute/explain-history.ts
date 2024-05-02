cfd515a34e05393a09b6ff1ca1ad1ab85ececc6c
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-05-02 14:43:18 +0200

    factor out explainHistoryCommand so we can add some telemetry
    This telemetry seems very barebones and copy pasta, but just following
    the pattern I see in the rest of the codebase.

--- vscode/src/commands/execute/explain-history.ts
+++ vscode/src/commands/execute/explain-history.ts
@@ -1,65 +1,83 @@
 import { type SpawnOptionsWithoutStdio, spawn } from 'node:child_process'
 import path from 'node:path'
+import type { Span } from '@opentelemetry/api'
 import {
     type ContextItem,
     ContextItemSource,
     PromptString,
     isFileURI,
     logDebug,
-    logError,
     ps,
+    telemetryRecorder,
+    wrapInActiveSpan,
 } from '@sourcegraph/cody-shared'
 import * as vscode from 'vscode'
 import { getEditor } from '../../editor/active-editor'
 import type { ChatCommandResult } from '../../main'
 import type { CodyCommandArgs } from '../types'
-import { executeChat } from './ask'
+import { type ExecuteChatArguments, executeChat } from './ask'
+
+async function explainHistoryCommand(
+    span: Span,
+    args?: Partial<CodyCommandArgs>
+): Promise<ExecuteChatArguments | undefined> {
+    // vscode git extension API doesn't offer a way to run git log with trace
+    // arguments. So we directly spawn git log to run against the active document.
+    const logArguments = getLogArguments(args)
+    if (!logArguments) {
+        return undefined
+    }
+
+    logDebug('explainHistoryCommand', 'computed log arguments', JSON.stringify(logArguments))
+
+    const output = await spawnGetStdout('git', ['log', ...logArguments.logArgs], {
+        cwd: logArguments.cwd,
+    })
+
+    const prompt = ps`Explain the history of the function \`${logArguments.symbolText}\`.`
+    const contextFiles: ContextItem[] = [
+        {
+            type: 'file',
+            content: output,
+            title: 'Terminal Output',
+            uri: vscode.Uri.file('terminal-output'),
+            source: ContextItemSource.Terminal,
+        },
+    ]
+
+    return {
+        text: prompt,
+        submitType: 'user-newchat',
+        addEnhancedContext: false,
+        contextFiles,
+        source: args?.source,
+    }
+}
 
 export async function executeExplainHistoryCommand(
     args: Partial<CodyCommandArgs> = {}
 ): Promise<ChatCommandResult | undefined> {
-    logDebug('executeExplainHistoryCommand', 'executing', args)
-
-    try {
-        // vscode git extension API doesn't offer a way to run git log with trace
-        // arguments. So we directly spawn git log to run against the active document.
-        const logArguments = getLogArguments(args)
-        if (!logArguments) {
-            return undefined
-        }
-
-        logDebug('executeExplainHistoryCommand', 'computed log arguments', JSON.stringify(logArguments))
-
-        const output = await spawnGetStdout('git', ['log', ...logArguments.logArgs], {
-            cwd: logArguments.cwd,
+    return wrapInActiveSpan('command.explain-history', async span => {
+        span.setAttribute('sampled', true)
+        logDebug('executeExplainHistoryCommand', 'executing', args)
+        telemetryRecorder.recordEvent('cody.command.explain-history', 'executed', {
+            interactionID: args?.requestID,
+            privateMetadata: {
+                requestID: args?.requestID,
+                source: args?.source,
+                traceId: span.spanContext().traceId,
+            },
         })
 
-        const prompt = ps`Explain the history of the function \`${logArguments.symbolText}\`.`
-        const contextFiles: ContextItem[] = [
-            {
-                type: 'file',
-                content: output,
-                title: 'Terminal Output',
-                uri: vscode.Uri.file('terminal-output'),
-                source: ContextItemSource.Terminal,
-            },
-        ]
-
+        const sessionArgs = await explainHistoryCommand(span, args)
+        if (!sessionArgs) {
+            return undefined
+        }
         return {
             type: 'chat',
-            session: await executeChat({
-                text: prompt,
-                submitType: 'user-newchat',
-                addEnhancedContext: false,
-                contextFiles,
-                source: args?.source,
-            }),
+            session: await executeChat(sessionArgs),
         }
-    } catch (error) {
-        logError('executeExplainHistoryCommand', 'error fetching history context', error)
-        vscode.window.showErrorMessage(`Error fetching history context: ${error}`)
-        return undefined
-    }
+    })
 }
 
 interface LogArguments {
@@ -68,7 +86,7 @@ interface LogArguments {
     cwd: string
 }
 
-function getLogArguments(args: Pick<CodyCommandArgs, 'range'>): LogArguments | undefined {
+function getLogArguments(args?: Pick<CodyCommandArgs, 'range'>): LogArguments | undefined {
     const activeEditor = getEditor().active
     const doc = activeEditor?.document
     if (!doc || !isFileURI(doc.uri)) {
