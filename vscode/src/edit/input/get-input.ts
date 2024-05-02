71ae8e615b4fcc2c7cd284b33d40248f406b8fb4
Philipp Spiess <hello@philippspiess.com>
2024-05-02 13:16:11 +0200

    Make sure it works for single line changes

--- vscode/src/edit/input/get-input.ts
+++ vscode/src/edit/input/get-input.ts
@@ -475,7 +475,6 @@ export const getInput = async (
             onDidAccept: () => {
                 const input = editInput.input
                 const instruction = PromptString.unsafe_fromUserQuery(input.value.trim() + '')
-                void PromptString.unsafe_fromUserQuery(input.value.trim() + ps(''))
 
                 // Selected item flow, update the input and store it for submission
                 const selectedItem = input.selectedItems[0]
