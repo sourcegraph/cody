8d2df1f4bccae423f842641acd4d40f6157c4952
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-05-02 15:42:06 +0900

    WIP labels on at-mentioned items.

--- lib/shared/src/codebase-context/messages.ts
+++ lib/shared/src/codebase-context/messages.ts
@@ -1,165 +1,175 @@
-import type { URI } from 'vscode-uri'
+import type { URI } from "vscode-uri";
 
-import type { RangeData } from '../common/range'
-import type { Message } from '../sourcegraph-api'
+import { IsIgnored } from "../cody-ignore/context-filters-provider";
+import type { RangeData } from "../common/range";
+import type { Message } from "../sourcegraph-api";
 
-export type ContextFileType = 'file' | 'symbol'
+export type ContextFileType = "file" | "symbol";
 
 /**
  * Fields that are common to any context item included in chat messages.
  */
 interface ContextItemCommon {
-    /**
-     * The URI of the document (such as a file) where this context resides.
-     */
-    uri: URI
+	/**
+	 * The URI of the document (such as a file) where this context resides.
+	 */
+	uri: URI;
 
-    /**
-     * If only a subset of a file is included as context, the range of that subset.
-     */
-    range?: RangeData
+	/**
+	 * If only a subset of a file is included as context, the range of that subset.
+	 */
+	range?: RangeData;
 
-    /**
-     * The content, either the entire document or the range subset.
-     */
-    content?: string
+	/**
+	 * The content, either the entire document or the range subset.
+	 */
+	content?: string;
 
-    repoName?: string
-    revision?: string
+	repoName?: string;
+	revision?: string;
 
-    /**
-     * For anything other than a file or symbol, the title to display (e.g., "Terminal Output").
-     */
-    title?: string
+	/**
+	 * For anything other than a file or symbol, the title to display (e.g., "Terminal Output").
+	 */
+	title?: string;
 
-    /**
-     * The source of this context item.
-     */
-    source?: ContextItemSource
+	/**
+	 * The source of this context item.
+	 */
+	source?: ContextItemSource;
 
-    /**
-     * The token count of the item's content.
-     */
-    size?: number
+	/**
+	 * The token count of the item's content.
+	 */
+	size?: number;
 
-    /**
-     * Whether the content of the item is too large to be included as context.
-     */
-    isTooLarge?: boolean
+	/**
+	 * Whether the item is excluded by Cody Ignore.
+	 */
+	isIgnored?: IsIgnored;
 
-    /**
-     * The ID of the {@link ContextMentionProvider} that supplied this context item (or `undefined`
-     * if from a built-in context source such as files and symbols).
-     */
-    provider?: string
+	/**
+	 * Whether the content of the item is too large to be included as context.
+	 */
+	isTooLarge?: boolean;
+
+	/**
+	 * The ID of the {@link ContextMentionProvider} that supplied this context item (or `undefined`
+	 * if from a built-in context source such as files and symbols).
+	 */
+	provider?: string;
 }
 
 /**
  * The source of this context.
  */
 export enum ContextItemSource {
-    /** From embeddings search */
-    Embeddings = 'embeddings',
+	/** From embeddings search */
+	Embeddings = "embeddings",
 
-    /** Explicitly @-mentioned by the user in chat */
-    User = 'user',
+	/** Explicitly @-mentioned by the user in chat */
+	User = "user",
 
-    /** From local keyword search */
-    Keyword = 'keyword',
+	/** From local keyword search */
+	Keyword = "keyword",
 
-    /** From the current editor state and open tabs/documents */
-    Editor = 'editor',
+	/** From the current editor state and open tabs/documents */
+	Editor = "editor",
 
-    Filename = 'filename',
+	Filename = "filename",
 
-    /** From symf search */
-    Search = 'search',
+	/** From symf search */
+	Search = "search",
 
-    /** Remote search */
-    Unified = 'unified',
+	/** Remote search */
+	Unified = "unified",
 
-    /** Selected code from the current editor */
-    Selection = 'selection',
+	/** Selected code from the current editor */
+	Selection = "selection",
 
-    /** Output from the terminal */
-    Terminal = 'terminal',
+	/** Output from the terminal */
+	Terminal = "terminal",
 
-    /** From URI */
-    Uri = 'uri',
+	/** From URI */
+	Uri = "uri",
 
-    /** From a package repository */
-    Package = 'package',
+	/** From a package repository */
+	Package = "package",
 }
 
 /**
  * An item (such as a file or symbol) that is included as context in a chat message.
  */
-export type ContextItem = ContextItemFile | ContextItemSymbol | ContextItemPackage
+export type ContextItem =
+	| ContextItemFile
+	| ContextItemSymbol
+	| ContextItemPackage;
 
 /**
  * A file (or a subset of a file given by a range) that is included as context in a chat message.
  */
 export interface ContextItemPackage extends ContextItemCommon {
-    type: 'package'
+	type: "package";
 
-    /**
-     * the repository id for this package.
-     */
-    repoID: string
+	/**
+	 * the repository id for this package.
+	 */
+	repoID: string;
 
-    /**
-     * the title for this package.
-     */
-    title: string
-    /**
-     * the ecosystem for this package.
-     */
-    ecosystem: string
-    /**
-     * the name for this package.
-     */
-    name: string
+	/**
+	 * the title for this package.
+	 */
+	title: string;
+	/**
+	 * the ecosystem for this package.
+	 */
+	ecosystem: string;
+	/**
+	 * the name for this package.
+	 */
+	name: string;
 }
 
 /**
  * A file (or a subset of a file given by a range) that is included as context in a chat message.
  */
 export interface ContextItemFile extends ContextItemCommon {
-    type: 'file'
+	type: "file";
 }
 
 /**
  * A symbol (which is a range within a file) that is included as context in a chat message.
  */
 export interface ContextItemSymbol extends ContextItemCommon {
-    type: 'symbol'
+	type: "symbol";
 
-    /** The name of the symbol, used for presentation only (not semantically meaningful). */
-    symbolName: string
+	/** The name of the symbol, used for presentation only (not semantically meaningful). */
+	symbolName: string;
 
-    /** The kind of symbol, used for presentation only (not semantically meaningful). */
-    kind: SymbolKind
+	/** The kind of symbol, used for presentation only (not semantically meaningful). */
+	kind: SymbolKind;
 }
 
 /** The valid kinds of a symbol. */
-export type SymbolKind = 'class' | 'function' | 'method'
+export type SymbolKind = "class" | "function" | "method";
 
 /** {@link ContextItem} with the `content` field set to the content. */
-export type ContextItemWithContent = ContextItem & Required<Pick<ContextItem, 'content'>>
+export type ContextItemWithContent = ContextItem &
+	Required<Pick<ContextItem, "content">>;
 
 /**
  * A system chat message that adds a context item to the conversation.
  */
 export interface ContextMessage extends Required<Message> {
-    /**
-     * Context messages are always "from" the human. (In the future, this could be from "system" for
-     * LLMs that support that kind of message, but that `speaker` value is not currently supported
-     * by the `Message` type.)
-     */
-    speaker: 'human'
+	/**
+	 * Context messages are always "from" the human. (In the future, this could be from "system" for
+	 * LLMs that support that kind of message, but that `speaker` value is not currently supported
+	 * by the `Message` type.)
+	 */
+	speaker: "human";
 
-    /**
-     * The context item that this message introduces into the conversation.
-     */
-    file: ContextItem
+	/**
+	 * The context item that this message introduces into the conversation.
+	 */
+	file: ContextItem;
 }
--- vscode/webviews/promptEditor/plugins/atMentions/OptionsList.tsx
+++ vscode/webviews/promptEditor/plugins/atMentions/OptionsList.tsx
@@ -1,178 +1,209 @@
-import type { MenuRenderFn } from '@lexical/react/LexicalTypeaheadMenuPlugin'
+import type { MenuRenderFn } from "@lexical/react/LexicalTypeaheadMenuPlugin";
 import {
-    type MentionQuery,
-    type RangeData,
-    displayLineRange,
-    displayPath,
-    displayPathBasename,
-    displayPathDirname,
-    parseMentionQuery,
-} from '@sourcegraph/cody-shared'
-import classNames from 'classnames'
-import { type FunctionComponent, useEffect, useRef } from 'react'
+	type MentionQuery,
+	type RangeData,
+	displayLineRange,
+	displayPath,
+	displayPathBasename,
+	displayPathDirname,
+	parseMentionQuery,
+} from "@sourcegraph/cody-shared";
+import classNames from "classnames";
+import { type FunctionComponent, useEffect, useRef } from "react";
 import {
-    FILE_HELP_LABEL,
-    FILE_RANGE_TOOLTIP_LABEL,
-    GENERAL_HELP_LABEL,
-    LARGE_FILE_WARNING_LABEL,
-    NO_FILE_MATCHES_LABEL,
-    NO_PACKAGE_MATCHES_LABEL,
-    NO_SYMBOL_MATCHES_HELP_LABEL,
-    NO_SYMBOL_MATCHES_LABEL,
-    PACKAGE_HELP_LABEL,
-    SYMBOL_HELP_LABEL,
-} from '../../../../src/chat/context/constants'
-import styles from './OptionsList.module.css'
-import { type MentionTypeaheadOption, RANGE_MATCHES_REGEXP } from './atMentions'
+	FILE_HELP_LABEL,
+	FILE_RANGE_TOOLTIP_LABEL,
+	GENERAL_HELP_LABEL,
+	IGNORED_FILE_WARNING_LABEL,
+	LARGE_FILE_WARNING_LABEL,
+	NO_FILE_MATCHES_LABEL,
+	NO_PACKAGE_MATCHES_LABEL,
+	NO_SYMBOL_MATCHES_HELP_LABEL,
+	NO_SYMBOL_MATCHES_LABEL,
+	PACKAGE_HELP_LABEL,
+	SYMBOL_HELP_LABEL,
+} from "../../../../src/chat/context/constants";
+import styles from "./OptionsList.module.css";
+import {
+	type MentionTypeaheadOption,
+	RANGE_MATCHES_REGEXP,
+} from "./atMentions";
 
 export const OptionsList: FunctionComponent<
-    { query: string; options: MentionTypeaheadOption[] } & Pick<
-        Parameters<MenuRenderFn<MentionTypeaheadOption>>[1],
-        'selectedIndex' | 'setHighlightedIndex' | 'selectOptionAndCleanUp'
-    >
-> = ({ query, options, selectedIndex, setHighlightedIndex, selectOptionAndCleanUp }) => {
-    const ref = useRef<HTMLUListElement>(null)
-    // biome-ignore lint/correctness/useExhaustiveDependencies: Intent is to run whenever `options` changes.
-    useEffect(() => {
-        // Scroll to top when options change because the prior `selectedIndex` is invalidated.
-        ref?.current?.scrollTo(0, 0)
-        setHighlightedIndex(0)
-    }, [options])
+	{ query: string; options: MentionTypeaheadOption[] } & Pick<
+		Parameters<MenuRenderFn<MentionTypeaheadOption>>[1],
+		"selectedIndex" | "setHighlightedIndex" | "selectOptionAndCleanUp"
+	>
+> = ({
+	query,
+	options,
+	selectedIndex,
+	setHighlightedIndex,
+	selectOptionAndCleanUp,
+}) => {
+	const ref = useRef<HTMLUListElement>(null);
+	// biome-ignore lint/correctness/useExhaustiveDependencies: Intent is to run whenever `options` changes.
+	useEffect(() => {
+		// Scroll to top when options change because the prior `selectedIndex` is invalidated.
+		ref?.current?.scrollTo(0, 0);
+		setHighlightedIndex(0);
+	}, [options]);
 
-    const mentionQuery = parseMentionQuery(query, [])
+	const mentionQuery = parseMentionQuery(query, []);
 
-    return (
-        <div className={styles.container}>
-            <h3 className={classNames(styles.item, styles.helpItem)}>
-                <span>{getHelpText(mentionQuery, options)}</span>
-                <br />
-            </h3>
-            {options.length > 0 && (
-                <ul ref={ref} className={styles.list}>
-                    {options.map((option, i) => (
-                        <Item
-                            query={query}
-                            isSelected={selectedIndex === i}
-                            onClick={() => {
-                                setHighlightedIndex(i)
-                                selectOptionAndCleanUp(option)
-                            }}
-                            onMouseEnter={() => {
-                                setHighlightedIndex(i)
-                            }}
-                            key={option.key}
-                            option={option}
-                            className={styles.item}
-                        />
-                    ))}
-                </ul>
-            )}
-        </div>
-    )
-}
+	return (
+		<div className={styles.container}>
+			<h3 className={classNames(styles.item, styles.helpItem)}>
+				<span>{getHelpText(mentionQuery, options)}</span>
+				<br />
+			</h3>
+			{options.length > 0 && (
+				<ul ref={ref} className={styles.list}>
+					{options.map((option, i) => (
+						<Item
+							query={query}
+							isSelected={selectedIndex === i}
+							onClick={() => {
+								setHighlightedIndex(i);
+								selectOptionAndCleanUp(option);
+							}}
+							onMouseEnter={() => {
+								setHighlightedIndex(i);
+							}}
+							key={option.key}
+							option={option}
+							className={styles.item}
+						/>
+					))}
+				</ul>
+			)}
+		</div>
+	);
+};
 
-function getHelpText(mentionQuery: MentionQuery, options: MentionTypeaheadOption[]): string {
-    switch (mentionQuery.provider) {
-        case 'default':
-            return GENERAL_HELP_LABEL
-        case 'package':
-            return options.length > 0 || mentionQuery.text.length < 3
-                ? PACKAGE_HELP_LABEL
-                : NO_PACKAGE_MATCHES_LABEL
-        case 'symbol':
-            return options.length > 0 || !mentionQuery.text.length
-                ? SYMBOL_HELP_LABEL
-                : NO_SYMBOL_MATCHES_LABEL +
-                      (mentionQuery.text.length < 3 ? NO_SYMBOL_MATCHES_HELP_LABEL : '')
-        default:
-            return options.length > 0
-                ? isValidLineRangeQuery(mentionQuery.text)
-                    ? FILE_RANGE_TOOLTIP_LABEL
-                    : FILE_HELP_LABEL
-                : NO_FILE_MATCHES_LABEL
-    }
+function getHelpText(
+	mentionQuery: MentionQuery,
+	options: MentionTypeaheadOption[],
+): string {
+	switch (mentionQuery.provider) {
+		case "default":
+			return GENERAL_HELP_LABEL;
+		case "package":
+			return options.length > 0 || mentionQuery.text.length < 3
+				? PACKAGE_HELP_LABEL
+				: NO_PACKAGE_MATCHES_LABEL;
+		case "symbol":
+			return options.length > 0 || !mentionQuery.text.length
+				? SYMBOL_HELP_LABEL
+				: NO_SYMBOL_MATCHES_LABEL +
+						(mentionQuery.text.length < 3 ? NO_SYMBOL_MATCHES_HELP_LABEL : "");
+		default:
+			return options.length > 0
+				? isValidLineRangeQuery(mentionQuery.text)
+					? FILE_RANGE_TOOLTIP_LABEL
+					: FILE_HELP_LABEL
+				: NO_FILE_MATCHES_LABEL;
+	}
 }
 
 const Item: FunctionComponent<{
-    query: string
-    isSelected: boolean
-    onClick: () => void
-    onMouseEnter: () => void
-    option: MentionTypeaheadOption
-    className?: string
+	query: string;
+	isSelected: boolean;
+	onClick: () => void;
+	onMouseEnter: () => void;
+	option: MentionTypeaheadOption;
+	className?: string;
 }> = ({ query, isSelected, onClick, onMouseEnter, option, className }) => {
-    const item = option.item
-    const isFileType = item.type === 'file'
-    const isPackageType = item.type === 'package'
-    const icon =
-        isFileType || isPackageType ? null : item.kind === 'class' ? 'symbol-structure' : 'symbol-method'
-    const title =
-        item.title ?? (isFileType || isPackageType ? displayPathBasename(item.uri) : item.symbolName)
+	const item = option.item;
+	const isFileType = item.type === "file";
+	const isPackageType = item.type === "package";
+	const icon =
+		isFileType || isPackageType
+			? null
+			: item.kind === "class"
+			  ? "symbol-structure"
+			  : "symbol-method";
+	const title =
+		item.title ??
+		(isFileType || isPackageType
+			? displayPathBasename(item.uri)
+			: item.symbolName);
 
-    const range = getLineRangeInMention(query, item.range)
-    const dir = decodeURIComponent(displayPathDirname(item.uri))
-    const description = isPackageType
-        ? ''
-        : isFileType
-          ? `${range ? `Lines ${range} · ` : ''}${dir === '.' ? '' : dir}`
-          : `${displayPath(item.uri)}:${getLineRangeInMention(query, item.range)}`
+	const range = getLineRangeInMention(query, item.range);
+	const dir = decodeURIComponent(displayPathDirname(item.uri));
+	const description = isPackageType
+		? ""
+		: isFileType
+		  ? `${range ? `Lines ${range} · ` : ""}${dir === "." ? "" : dir}`
+		  : `${displayPath(item.uri)}:${getLineRangeInMention(query, item.range)}`;
 
-    const isLargeFile = isFileType && item.isTooLarge
-    const warning =
-        isLargeFile && !item.range && !isValidLineRangeQuery(query) ? LARGE_FILE_WARNING_LABEL : ''
+	const isIgnored = isFileType && item.isIgnored;
+	const isLargeFile = isFileType && item.isTooLarge;
+	let warning: string;
+	if (isIgnored) {
+		warning = IGNORED_FILE_WARNING_LABEL;
+	} else if (isLargeFile && !item.range && !isValidLineRangeQuery(query)) {
+		warning = LARGE_FILE_WARNING_LABEL;
+	} else {
+		warning = "";
+	}
 
-    return (
-        // biome-ignore lint/a11y/useKeyWithClickEvents:
-        <li
-            key={option.key}
-            tabIndex={-1}
-            className={classNames(
-                className,
-                styles.optionItem,
-                isSelected && styles.selected,
-                warning && styles.disabled
-            )}
-            ref={option.setRefElement}
-            // biome-ignore lint/a11y/noNoninteractiveElementToInteractiveRole: This element is interactive, in a dropdown list.
-            role="option"
-            aria-selected={isSelected}
-            onMouseEnter={onMouseEnter}
-            onClick={onClick}
-        >
-            <div className={styles.optionItemRow}>
-                {item.type === 'symbol' && icon && (
-                    <i className={`codicon codicon-${icon}`} title={item.kind} />
-                )}
-                <span
-                    className={classNames(
-                        styles.optionItemTitle,
-                        warning && styles.optionItemTitleWithWarning
-                    )}
-                >
-                    {title}
-                </span>
-                {description && <span className={styles.optionItemDescription}>{description}</span>}
-            </div>
-            {warning && <span className={styles.optionItemWarning}>{warning}</span>}
-        </li>
-    )
-}
+	return (
+		// biome-ignore lint/a11y/useKeyWithClickEvents:
+		<li
+			key={option.key}
+			tabIndex={-1}
+			className={classNames(
+				className,
+				styles.optionItem,
+				isSelected && styles.selected,
+				warning && styles.disabled,
+			)}
+			ref={option.setRefElement}
+			// biome-ignore lint/a11y/noNoninteractiveElementToInteractiveRole: This element is interactive, in a dropdown list.
+			role="option"
+			aria-selected={isSelected}
+			onMouseEnter={onMouseEnter}
+			onClick={onClick}
+		>
+			<div className={styles.optionItemRow}>
+				{item.type === "symbol" && icon && (
+					<i className={`codicon codicon-${icon}`} title={item.kind} />
+				)}
+				<span
+					className={classNames(
+						styles.optionItemTitle,
+						warning && styles.optionItemTitleWithWarning,
+					)}
+				>
+					{title}
+				</span>
+				{description && (
+					<span className={styles.optionItemDescription}>{description}</span>
+				)}
+			</div>
+			{warning && <span className={styles.optionItemWarning}>{warning}</span>}
+		</li>
+	);
+};
 
 const isValidLineRangeQuery = (query: string): boolean =>
-    query.endsWith(':') || RANGE_MATCHES_REGEXP.test(query)
+	query.endsWith(":") || RANGE_MATCHES_REGEXP.test(query);
 
 /**
  * Gets the display line range from the query string.
  */
 function getLineRangeInMention(query: string, range?: RangeData): string {
-    // Parses out the start and end line numbers from the query if it contains a line range match.
-    const queryRange = query.match(RANGE_MATCHES_REGEXP)
-    if (query && queryRange?.[1]) {
-        const [_, start, end] = queryRange
-        const startLine = Number.parseInt(start)
-        const endLine = end ? Number.parseInt(end) : Number.POSITIVE_INFINITY
-        return `${startLine}-${endLine !== Number.POSITIVE_INFINITY ? endLine : '#'}`
-    }
-    // Passed in range string if no line number match.
-    return range ? displayLineRange(range).toString() : ''
+	// Parses out the start and end line numbers from the query if it contains a line range match.
+	const queryRange = query.match(RANGE_MATCHES_REGEXP);
+	if (query && queryRange?.[1]) {
+		const [_, start, end] = queryRange;
+		const startLine = Number.parseInt(start);
+		const endLine = end ? Number.parseInt(end) : Number.POSITIVE_INFINITY;
+		return `${startLine}-${
+			endLine !== Number.POSITIVE_INFINITY ? endLine : "#"
+		}`;
+	}
+	// Passed in range string if no line number match.
+	return range ? displayLineRange(range).toString() : "";
 }
