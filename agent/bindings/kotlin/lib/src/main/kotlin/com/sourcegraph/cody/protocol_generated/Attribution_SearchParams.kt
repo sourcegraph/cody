631cc13bdeffca49f66188fa9fa229198f360070
Olafur Geirsson <olafurpg@gmail.com>
2024-05-02 10:20:52 +0200

    Extend timeout for `beforeAll` hook in agent test
    It was failing in Ubuntu 18

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/Attribution_SearchParams.kt
+++
@@ -1,8 +0,0 @@
-@file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
-package com.sourcegraph.cody.protocol_generated
-
-data class Attribution_SearchParams(
-  val id: String,
-  val snippet: String,
-)
-
--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/TextDocumentEditParams.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
-package com.sourcegraph.cody.protocol_generated
-
-data class TextDocumentEditParams(
-  val uri: String,
-  val edits: List<TextEdit>,
-  val options: OptionsParams? = null,
-)
-
