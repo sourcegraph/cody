be138512431c3ffc1df2623c967773f8a1de0cb9
Philipp Spiess <hello@philippspiess.com>
2024-05-03 15:51:02 +0200

    Only attach listener in debug or test mode

--- vscode/src/main.ts
+++ vscode/src/main.ts
@@ -149,9 +149,7 @@ const register = async (
     const isExtensionModeDevOrTest =
         context.extensionMode === vscode.ExtensionMode.Development ||
         context.extensionMode === vscode.ExtensionMode.Test
-    if (isExtensionModeDevOrTest) {
-        await vscode.commands.executeCommand('setContext', 'cody.devOrTest', true)
-    }
+
     await configureEventsInfra(initialConfig, isExtensionModeDevOrTest, authProvider)
 
     const editor = new VSCodeEditor()
@@ -393,27 +391,35 @@ const register = async (
         )
     )
 
+    // Internal-only test commands
+    if (!isExtensionModeDevOrTest) {
+        await vscode.commands.executeCommand('setContext', 'cody.devOrTest', true)
+        disposables.push(
+            vscode.commands.registerCommand('cody.test.set-context-filters', async () => {
+                // Prompt the user for the policy
+                const raw = await vscode.window.showInputBox({ title: 'Context Filters Overwrite' })
+                if (!raw) {
+                    return
+                }
+                try {
+                    const policy = JSON.parse(raw)
+                    contextFiltersProvider.setTestingContextFilters(policy)
+                } catch (error) {
+                    vscode.window.showErrorMessage(
+                        'Failed to parse context filters policy. Please check your JSON syntax.'
+                    )
+                }
+            })
+        )
+    }
+
     disposables.push(
         // Tests
         // Access token - this is only used in configuration tests
         vscode.commands.registerCommand('cody.test.token', async (endpoint, token) =>
             authProvider.auth({ endpoint, token })
         ),
-        vscode.commands.registerCommand('cody.test.set-context-filters', async () => {
-            // Prompt the user for the policy
-            const raw = await vscode.window.showInputBox({ title: 'Context Filters Overwrite' })
-            if (!raw) {
-                return
-            }
-            try {
-                const policy = JSON.parse(raw)
-                contextFiltersProvider.setTestingContextFilters(policy)
-            } catch (error) {
-                vscode.window.showErrorMessage(
-                    'Failed to parse context filters policy. Please check your JSON syntax.'
-                )
-            }
-        }),
+
         // Auth
         vscode.commands.registerCommand('cody.auth.signin', () => authProvider.signinMenu()),
         vscode.commands.registerCommand('cody.auth.signout', () => authProvider.signoutMenu()),
