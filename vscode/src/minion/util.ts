4252f574438297fb52f37d15332954cd40fe92ee
Beyang Liu <beyang@sourcegraph.com>
2024-05-06 10:08:56 -0700

    anthropic utils

--- vscode/src/minion/util.ts
+++ vscode/src/minion/util.ts
@@ -11,15 +11,41 @@ export function extractXML(text: string, tag: string): string | null {
     return text.slice(startIndex + startTag.length, endIndex).trim()
 }
 
-export function extractXMLFromAnthropicResponse(message: Message, tag: string): string {
+function anthropicMessageToText(message: Message): string {
     if (message.content.length === 0 || message.content.length > 1) {
         throw new Error(
             `expected exactly one text block in claude response, got ${message.content.length})`
         )
     }
-    const extracted = extractXML(message.content[0].text, tag)
+    return message.content[0].text
+}
+
+export function extractXMLFromAnthropicResponse(message: Message, tag: string): string {
+    const text = anthropicMessageToText(message)
+    const extracted = extractXML(text, tag)
     if (extracted === null) {
-        throw new Error(`could not find tag ${tag} in claude response:\n${message.content[0].text}`)
+        throw new Error(`could not find tag ${tag} in claude response:\n${text}`)
     }
     return extracted
+}
+
+export function extractXMLArray(text: string, tag: string): string[] {
+    const startTag = `<${tag}>`
+    const endTag = `</${tag}>`
+    let start = 0
+    const extracted = []
+    while (start < text.length) {
+        const startIndex = text.indexOf(startTag, start)
+        const endIndex = text.indexOf(endTag, startIndex + 1)
+        if (startIndex === -1 || endIndex === -1) {
+            break
+        }
+        extracted.push(text.slice(startIndex + startTag.length, endIndex).trim())
+        start = endIndex + endTag.length
+    }
+    return extracted
+}
+
+export function extractXMLArrayFromAnthropicResponse(message: Message, tag: string): string[] {
+    return extractXMLArray(anthropicMessageToText(message), tag)
 }
