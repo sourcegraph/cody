0796b65f5c50374c28a2897d4eaf9ad4e9b08d41
Myroslav <odisseus@users.noreply.github.com>
2024-05-06 23:48:45 +0200

    Removed the reference to SEPARATOR_ABOVE_COLOR (#1453)
    ## Test plan
    
    Build the plugin and load it in IntelliJ 2022.3 or newer. Check the log
    for occurrences of `NoSuchFieldError`.

--- src/main/kotlin/com/sourcegraph/cody/ignore/CommandPanelIgnoreBanner.kt
+++ src/main/kotlin/com/sourcegraph/cody/ignore/CommandPanelIgnoreBanner.kt
@@ -27,9 +27,7 @@ class CommandPanelIgnoreBanner() : NonOpaquePanel() {
 
     // These colors cribbed from EditorComposite, createTopBottomSideBorder
     val scheme = EditorColorsManager.getInstance().globalScheme
-    val borderColor =
-        scheme.getColor(EditorColors.SEPARATOR_ABOVE_COLOR)
-            ?: scheme.getColor(EditorColors.TEARLINE_COLOR)
+    val borderColor = scheme.getColor(EditorColors.TEARLINE_COLOR)
     border = SideBorder(borderColor, SideBorder.TOP or SideBorder.BOTTOM)
   }
 
