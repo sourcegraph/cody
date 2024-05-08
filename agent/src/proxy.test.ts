8efb93372c474c8c88ea6ff5f66d12c00b95420a
Olafur Geirsson <olafurpg@gmail.com>
2024-05-08 22:25:55 +0200

    Add agent integration test to verify proxy works
    To reproduce:
    
    - Install SquidMan (as documented here https://github.com/sourcegraph/cody/pull/3695)
    - Run `CODY_RECORDING_MODE=passthrough pnpm test
      agent/src/proxy.test.ts`
    - Confirm the test passes
    - Confirm the SquidMan logs captured the traffic
    
    ```
    ❯ tail -f /Users/olafurpg/Library/Logs/squid/squid-access.log
    1715199742.480   1476 127.0.0.1 TCP_TUNNEL_ABORTED/200 6792 CONNECT sourcegraph.com:443 - HIER_DIRECT/172.64.155.69 -
    1715199742.510   2198 127.0.0.1 TCP_TUNNEL/200 6540 CONNECT sourcegraph.com:443 - HIER_DIRECT/172.64.155.69 -
    1715199742.510   2198 127.0.0.1 TCP_TUNNEL/200 11862 CONNECT sourcegraph.com:443 - HIER_DIRECT/172.64.155.69 -
    1715199742.510   2851 127.0.0.1 TCP_TUNNEL/200 15178 CONNECT sourcegraph.com:443 - HIER_DIRECT/172.64.155.69 -
    1715199742.510   2852 127.0.0.1 TCP_TUNNEL/200 8209 CONNECT sourcegraph.com:443 - HIER_DIRECT/172.64.155.69 -
    ```

---
+++ agent/src/proxy.test.ts
@@ -0,0 +1,49 @@
+import path from 'node:path'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { TESTING_CREDENTIALS } from '../../vscode/src/testutils/testing-credentials'
+import { TestClient } from './TestClient'
+import { TestWorkspace } from './TestWorkspace'
+
+describe('HTTP Proxy', () => {
+    const workspace = new TestWorkspace(path.join(__dirname, '__tests__', 'example-ts'))
+    const client = TestClient.create({
+        workspaceRootUri: workspace.rootUri,
+        name: 'http-proxy',
+        credentials: TESTING_CREDENTIALS.dotcom,
+        extraConfiguration: {
+            'cody.experimental.symfContext': false,
+        },
+        extraEnvironmentVariables: {
+            http_proxy: 'http://127.0.0.1:8080',
+            https_proxy: 'http://127.0.0.1:8080',
+        },
+    })
+
+    beforeAll(async () => {
+        await workspace.beforeAll()
+        await client.beforeAll()
+    })
+
+    it('autocomplete/execute (non-empty result)', async () => {
+        const sumUri = workspace.file('src', 'sum.ts')
+        await client.openFile(sumUri)
+        const completions = await client.request('autocomplete/execute', {
+            uri: sumUri.toString(),
+            position: { line: 1, character: 3 },
+            triggerKind: 'Invoke',
+        })
+        const texts = completions.items.map(item => item.insertText)
+        expect(completions.items.length).toBeGreaterThan(0)
+        expect(texts).toMatchInlineSnapshot(
+            `
+              [
+                "   return a + b;",
+              ]
+            `
+        )
+    }, 10_000)
+    afterAll(async () => {
+        await workspace.afterAll()
+        await client.afterAll()
+    })
+})
