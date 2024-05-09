841a1ff2d278c6670501cb8c1a45db3fac6eeeac
Tom Ross <tom@umpox.com>
2024-05-09 10:57:11 +0100

    Edit: Fix incorrect formatting with certain formatters

--- vscode/src/non-stop/FixupController.ts
+++ vscode/src/non-stop/FixupController.ts
@@ -7,6 +7,7 @@ import {
     type PromptString,
     displayPathBasename,
     getEditorInsertSpaces,
+    getEditorTabSize,
     telemetryRecorder,
 } from '@sourcegraph/cody-shared'
 
@@ -875,9 +876,9 @@ export class FixupController
                 'vscode.executeFormatDocumentProvider',
                 document.uri,
                 {
-                    tabSize: document.uri,
+                    tabSize: getEditorTabSize(document.uri, vscode.workspace, vscode.window),
                     insertSpaces: getEditorInsertSpaces(document.uri, vscode.workspace, vscode.window),
-                }
+                } satisfies vscode.FormattingOptions
             )) || []
 
         const formattingChangesInRange = formattingChanges.filter(change =>
