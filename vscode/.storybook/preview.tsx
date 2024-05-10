787cf32b56a6378edf18a31553cbc1c8533c74d0
Tim Lucas <t@toolmantim.com>
2024-05-10 17:49:43 +1000

    All the things

--- vscode/.storybook/preview.tsx
+++ vscode/.storybook/preview.tsx
@@ -1,5 +1,8 @@
 import type { Preview } from '@storybook/react'
 import '../webviews/components/shadcn/shadcn.css'
+// biome-ignore lint/correctness/noUnusedImports: needed because UMD import
+import React from 'react'
+import { HeadProvider, Link } from 'react-head'
 
 const preview: Preview = {
     parameters: {
@@ -37,7 +40,36 @@ const preview: Preview = {
                 ],
             },
         },
+        theme: {
+            description: 'VS Code theme',
+            defaultValue: 'dark-plus',
+            toolbar: {
+                title: 'VS Code Theme',
+                icon: 'photo',
+                items: [
+                    { value: 'dark-plus', title: 'Dark+ Theme' },
+                    { value: 'dark-modern', title: 'Dark Modern Theme' },
+                    { value: 'dark-high-contrast', title: 'Dark High Contrast Theme' },
+                    { value: 'light-plus', title: 'Light+ Theme' },
+                    { value: 'light-modern', title: 'Light Modern Theme' },
+                    { value: 'light-high-contrast', title: 'Light High Contrast Theme' },
+                    { value: 'red', title: 'Red Theme' },
+                ],
+                dynamicTitle: true,
+            },
+        },
     },
+    decorators: [
+        (Story, context) => {
+            const theme = context.globals.theme
+            return (
+                <HeadProvider>
+                    <Link rel="stylesheet" href={`/vscode-themes/${theme}.css`} />
+                    <Story />
+                </HeadProvider>
+            )
+        },
+    ],
 }
 
 export default preview
