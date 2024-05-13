066672d9767d4087230372db0b8019ad1233d831
Philipp Spiess <hello@philippspiess.com>
2024-05-13 13:49:05 +0200

    Remove workspace3

--- vscode/test/fixtures/workspace3/README.md
+++
@@ -1,2 +0,0 @@
-Another workspace, this one has no .cody/ignore rules and is used for testing
-Enterprise Cody Ignore.
--- vscode/test/fixtures/workspace3/src/error.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-const logNumber = (number: number) => {
-    console.log('What a great number!', number)
-}
-
-const hasError = () => {
-    const numbers = [1, 2, '3', 4, 5]
-    numbers.forEach(logNumber)
-}
