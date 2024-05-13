cb759fba496298a2b7a1be0cece8898a5a55f874
Rik Nauta <rik.nauta@sourcegraph.com>
2024-05-14 00:10:08 +0200

    Update e2e test

--- vscode/test/e2e/command-commit.test.ts
+++ vscode/test/e2e/command-commit.test.ts
@@ -13,11 +13,8 @@ test.beforeEach(() => {
 
 testGitWorkspace.extend<ExpectedEvents>({
     // list of events we expect this test to log, add to this list as needed
-    expectedEvents: [
-        'CodyVSCodeExtension:sidebar:commit:clicked',
-        'CodyVSCodeExtension:command:commit:executed',
-    ],
-    expectedV2Events: ['cody.sidebar.commit:clicked', 'cody.command.generate-commit:executed'],
+    expectedEvents: ['CodyVSCodeExtension:sidebar:commit:clicked'],
+    expectedV2Events: ['cody.sidebar.commit:clicked'],
 })('use terminal output as context', async ({ page, sidebar }) => {
     await sidebarSignin(page, sidebar)
 
@@ -27,38 +24,49 @@ testGitWorkspace.extend<ExpectedEvents>({
     await expect(page.getByRole('heading', { name: 'Source Control' })).toBeVisible()
     await expect(page.getByText('index.js')).toBeVisible()
 
+    // Disable do-not-disturb
+    await page.getByRole('button', { name: 'Do Not Disturb' }).click()
+
+    // We start by only staging the index.js file
+    await page.getByLabel('index.js • Modified').locator('div').filter({ hasText: 'index.js' }).click()
+    await page.getByRole('button', { name: 'Stage Changes' }).click()
+
     // Click on the command in the sidebar to verify it opens the source control panel.
     await page.getByRole('tab', { name: 'Cody', exact: true }).locator('a').click()
     await page.getByText('Generate Commit Message (Experimental)', { exact: true }).click()
 
-    // await page.getByRole('heading', { name: 'Source Control' }).hover()
-
     const scmInputBox = page.getByLabel('Source Control Input')
-
-    // Verify notification is shown if items are ignored
     await expect(scmInputBox.filter({ hasText: 'hello from the assistant' }).first()).toBeVisible()
-    try {
-        await expect(
-            page
-                .locator('.notification-list-item-message')
-                .filter({ hasText: 'Cody was forced to skip' })
-        ).toBeVisible()
-    } catch (e) {
-        //for some reason notifications aren't shown during tests. Skip for now
-    }
 
-    // Remove the ignored file from the staged changes.
-    await expect(page.getByText('Staged Changes')).toBeVisible()
-    await page.locator('a').filter({ hasText: 'ignored.js' }).hover()
-    await page.getByRole('button', { name: 'Unstage Changes' }).click()
+    //we reset the text in the commit box
+    scmInputBox
+        .getByLabel(
+            'The editor is not accessible at this time. To enable screen reader optimized mode, use Shift+Option+F1'
+        )
+        .fill('')
 
-    // Stage a non-ignored file.
-    await page.locator('a').filter({ hasText: 'index.js' }).hover()
-    await page.getByRole('button', { name: 'Stage Changes' }).click()
+    // Ensure that no notification is shown
+    await expect(page.getByLabel('Cody was forced to skip').first()).not.toBeVisible({ timeout: 1000 })
 
-    // Execute the command again from the Cody icon located inside the Source Control view.
+    // Ensure that no additional files have been staged
+    await expect(
+        page
+            .getByLabel('Staged Changes', { exact: true })
+            .locator('div')
+            .filter({ hasText: '1' })
+            .first()
+    ).toBeVisible()
+
+    // await page.getByRole('heading', { name: 'Source Control' }).hover()
+    await page.getByText('Staged Changes').click()
+    await page.getByLabel('Unstage All Changes').click()
+    await page.getByText('Changes4').click()
+    await page.getByLabel('Stage All Changes').click()
+
     await page.getByRole('button', { name: 'Generate Commit Message (Experimental)' }).click()
 
-    // Verify a commit message is generated without error for the staged file.
     await expect(scmInputBox.filter({ hasText: 'hello from the assistant' }).first()).toBeVisible()
+
+    // Verify notification is shown if items are ignored
+    await expect(page.getByLabel('Cody was forced to skip 1 file').first()).toBeVisible()
 })
