6979182723e862e0fb1ecee74559615343e460da
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-05-15 13:45:26 +0200

    Fix tests

--- agent/src/index.test.ts
+++ agent/src/index.test.ts
@@ -951,8 +951,9 @@ describe('Agent', () => {
                 .allUris()
                 .filter(uri => vscode.Uri.parse(uri).scheme === 'untitled')
             expect(untitledDocuments).toHaveLength(2)
-            const untitledDocument = untitledDocuments.find(d => d.endsWith('trickyLogic.test.ts'))!!
-            const testDocument = client.workspace.getDocument(vscode.Uri.parse(untitledDocument))
+            const untitledDocument = untitledDocuments.find(d => d.endsWith('trickyLogic.test.ts'))
+            expect(untitledDocument).toBeDefined()
+            const testDocument = client.workspace.getDocument(vscode.Uri.parse(untitledDocument ?? ''))
             expect(trimEndOfLine(testDocument?.getText())).toMatchInlineSnapshot(
                 `
               "import { expect } from 'vitest'
