ef79339d505d7bbe5afa8cf80e550760531afa48
Philipp Spiess <hello@philippspiess.com>
2024-05-16 14:42:32 +0200

    Simplify upstream latency collector and measure gateway latency

--- vscode/src/services/UpstreamHealthProvider.ts
+++ vscode/src/services/UpstreamHealthProvider.ts
@@ -3,7 +3,10 @@ import {
     type ConfigurationWithAccessToken,
     addCustomUserAgent,
     addTraceparent,
+    dotcomTokenToGatewayToken,
+    isDotCom,
     logDebug,
+    wrapInActiveSpan,
 } from '@sourcegraph/cody-shared'
 import { fetch } from '@sourcegraph/cody-shared'
 import type * as vscode from 'vscode'
@@ -11,7 +14,6 @@ import type * as vscode from 'vscode'
 // We choose an interval that gives us a reasonable aggregate without causing
 // too many requests
 const PING_INTERVAL_MS = 10 * 60 * 1000 // 10 minutes
-const TWO_HOURS_MS = 2 * 60 * 60 * 1000
 
 /**
  * A provider that regularly pings the connected Sourcegraph instance to
@@ -20,8 +22,9 @@ const TWO_HOURS_MS = 2 * 60 * 60 * 1000
  * You can query it to get aggregates of the most recent pings.
  */
 export class UpstreamHealthProvider implements vscode.Disposable {
-    // Array sorted by duration for easy median calculation
-    private recentDurationsSorted: { timestamp: number; duration: number }[] = []
+    private lastUpstreamLatency?: number
+    private lastGatewayLatency?: number
+    private fastPathAccessToken?: string
 
     private config: Pick<
         ConfigurationWithAccessToken,
@@ -29,21 +32,34 @@ export class UpstreamHealthProvider implements vscode.Disposable {
     > | null = null
     private nextTimeoutId: NodeJS.Timeout | null = null
 
-    public getMedianDuration(): number | undefined {
+    public getUpstreamLatency(): number | undefined {
         if (!this.config) {
             return undefined
         }
-        if (this.recentDurationsSorted.length === 0) {
+        return this.lastUpstreamLatency
+    }
+
+    public getGatewayLatency(): number | undefined {
+        if (!this.config) {
             return undefined
         }
-        return this.recentDurationsSorted[Math.floor(this.recentDurationsSorted.length / 2)].duration
+        return this.lastGatewayLatency
     }
 
     public onConfigurationChange(
         newConfig: Pick<ConfigurationWithAccessToken, 'serverEndpoint' | 'customHeaders' | 'accessToken'>
     ): this {
         this.config = newConfig
-        this.recentDurationsSorted = []
+        this.lastUpstreamLatency = undefined
+        this.lastGatewayLatency = undefined
+
+        this.fastPathAccessToken =
+            newConfig.accessToken &&
+            // Require the upstream to be dotcom
+            isDotCom(newConfig.serverEndpoint)
+                ? dotcomTokenToGatewayToken(newConfig.accessToken)
+                : undefined
+
         this.measure()
         return this
     }
@@ -53,7 +69,6 @@ export class UpstreamHealthProvider implements vscode.Disposable {
             clearTimeout(this.nextTimeoutId)
         }
 
-        const start = Date.now()
         try {
             if (!this.config) {
                 throw new Error('UpstreamHealthProvider not initialized')
@@ -67,35 +82,56 @@ export class UpstreamHealthProvider implements vscode.Disposable {
             addTraceparent(headers)
             addCustomUserAgent(headers)
             const url = new URL('/healthz', this.config.serverEndpoint)
+            const upstreamResult = await wrapInActiveSpan('upstream-latency.upstream', span => {
+                span.setAttribute('sampled', true)
+                return measureLatencyToUri(headers, url.toString())
+            })
 
-            // We use a GET request even though we do not want to consume the
-            // body to avoid internal networks interfering with the request.
-            //
-            // To make sure the content is garbage collected, we'll ensure that
-            // the body is consumed. We don't use undici yet but that might
-            // change in the future:
-            //
-            // https://undici.nodejs.org/#/?id=garbage-collection
-            const response = await fetch(url.toString(), { method: 'GET', headers })
-            void response.arrayBuffer() // consume the body
-
-            const duration = Date.now() - start
-            this.pushDuration(duration)
-
-            logDebug(
-                'UpstreamHealth',
-                `Ping took ${Math.round(duration)}ms (Median: ${Math.round(
-                    this.getMedianDuration() ?? 0
-                )}ms)`,
-                {
-                    verbose: {
-                        duration,
-                        url,
-                        status: response.status,
-                        headers: headersToObject(response.headers),
-                    },
+            // We don't want to congest the network so we run the test in serial
+            if (this.fastPathAccessToken) {
+                const headers = new Headers()
+                headers.set('Content-Type', 'application/json; charset=utf-8')
+                headers.set('Authorization', `Bearer ${this.fastPathAccessToken}`)
+                addTraceparent(headers)
+                addCustomUserAgent(headers)
+                const uri = 'https://cody-gateway.sourcegraph.com/healthz'
+                const gatewayResult = await wrapInActiveSpan('upstream-latency.gateway', span => {
+                    span.setAttribute('sampled', true)
+                    return measureLatencyToUri(headers, uri)
+                })
+                if (!('error' in gatewayResult)) {
+                    this.lastGatewayLatency = gatewayResult.latency
                 }
-            )
+            }
+
+            if ('error' in upstreamResult) {
+                this.lastUpstreamLatency = undefined
+
+                logDebug('UpstreamHealth', 'Failed to ping upstream host', {
+                    verbose: {
+                        error: upstreamResult.error,
+                    },
+                })
+            } else {
+                this.lastUpstreamLatency = upstreamResult.latency
+
+                logDebug(
+                    'UpstreamHealth',
+                    `Ping took ${Math.round(upstreamResult.latency)}ms ${
+                        this.lastGatewayLatency
+                            ? `(Gateway: ${Math.round(this.lastGatewayLatency)}ms)`
+                            : ''
+                    }`,
+                    {
+                        verbose: {
+                            Latency: upstreamResult.latency,
+                            url,
+                            status: upstreamResult.response.status,
+                            headers: headersToObject(upstreamResult.response.headers),
+                        },
+                    }
+                )
+            }
         } catch (error) {
             // We don't care about errors here, we just want to measure the latency
         } finally {
@@ -112,22 +148,6 @@ export class UpstreamHealthProvider implements vscode.Disposable {
             clearTimeout(this.nextTimeoutId)
         }
     }
-
-    private pushDuration(duration: number): void {
-        const entry = { timestamp: Date.now(), duration }
-
-        // Delete items that are older than 2 hours
-        this.recentDurationsSorted = this.recentDurationsSorted.filter(
-            item => Date.now() - item.timestamp < TWO_HOURS_MS
-        )
-
-        const position = this.recentDurationsSorted.findIndex(item => item.duration > duration)
-        if (position === -1) {
-            this.recentDurationsSorted.push(entry)
-        } else {
-            this.recentDurationsSorted.splice(position, 0, entry)
-        }
-    }
 }
 
 export const upstreamHealthProvider = new UpstreamHealthProvider()
@@ -138,4 +158,26 @@ function headersToObject(headers: BrowserOrNodeResponse['headers']) {
         result[key] = value
     }
     return result
+}
+
+async function measureLatencyToUri(
+    headers: Headers,
+    uri: string
+): Promise<{ latency: number; response: BrowserOrNodeResponse } | { error: Error }> {
+    try {
+        const start = performance.now()
+        // We use a GET request even though we do not want to consume the
+        // body to avoid internal networks interfering with the request.
+        //
+        // To make sure the content is garbage collected, we'll ensure that
+        // the body is consumed. We don't use undici yet but that might
+        // change in the future:
+        //
+        // https://undici.nodejs.org/#/?id=garbage-collection
+        const response = await fetch(uri, { method: 'GET', headers })
+        void response.arrayBuffer() // consume the body
+        return { latency: performance.now() - start, response }
+    } catch (error: any) {
+        return { error }
+    }
 }
