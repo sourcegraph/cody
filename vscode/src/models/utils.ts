3c8cbb7bca074bd13f33f5372534230fa2927f7d
Philipp Spiess <hello@philippspiess.com>
2024-05-18 00:15:45 +0200

    Increase the output window tokens for known-good enterprise models

--- vscode/src/models/utils.ts
+++ vscode/src/models/utils.ts
@@ -2,6 +2,7 @@ import {
     ANSWER_TOKENS,
     type AuthStatus,
     CHAT_INPUT_TOKEN_BUDGET,
+    CHAT_OUTPUT_TOKEN_BUDGET,
     ModelProvider,
     ModelUsage,
     getDotComDefaultModels,
@@ -43,13 +44,33 @@ export function syncModelProviders(authStatus: AuthStatus): void {
                 authStatus.configOverwrites.chatModel,
                 // TODO: Add configOverwrites.editModel for separate edit support
                 [ModelUsage.Chat, ModelUsage.Edit],
-                // TODO: Currently all enterprise models have a max output limit of 1000.
-                // We need to support configuring the maximum output limit at an instance level.
-                // This will allow us to increase this limit whilst still supporting models with a lower output limit.
-                // See: https://github.com/sourcegraph/cody/issues/3648#issuecomment-2056954101
-                { input: tokenLimit ?? CHAT_INPUT_TOKEN_BUDGET, output: ANSWER_TOKENS }
+
+                {
+                    input: tokenLimit ?? CHAT_INPUT_TOKEN_BUDGET,
+                    output: getEnterpriseOutputLimit(authStatus.configOverwrites?.chatModel),
+                }
             ),
         ])
+    }
+}
+
+// TODO: Currently all enterprise models have a max output limit of 1000.
+// We need to support configuring the maximum output limit at an instance level.
+// This will allow us to increase this limit whilst still supporting models with a lower output limit.
+//
+// To avoid Enterprise instances being stuck with low token counts, we will
+// detect our recommended Cody Gateway models and use a higher limit.
+//
+// See: https://github.com/sourcegrcaph/cody/issues/3648#issuecomment-2056954101
+function getEnterpriseOutputLimit(model?: string) {
+    switch (model) {
+        case 'anthropic/claude-3-sonnet-20240229':
+        case 'anthropic/claude-3-opus-20240229':
+        case 'openai/gpt-4o':
+        case 'openai/gpt-4-turbo':
+            return CHAT_OUTPUT_TOKEN_BUDGET
+        default:
+            return ANSWER_TOKENS
     }
 }
 
