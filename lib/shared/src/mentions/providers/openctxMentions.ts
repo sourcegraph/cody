8d11910f465044c4f2187c1e472f43682ea90513
Quinn Slack <quinn@slack.org>
2024-05-18 01:18:41 -0700

    bump @openctx/vscode-lib to 0.0.3

--- lib/shared/src/mentions/providers/openctxMentions.ts
+++ lib/shared/src/mentions/providers/openctxMentions.ts
@@ -8,27 +8,26 @@ export const OPENCTX_CONTEXT_MENTION_PROVIDER: ContextMentionProvider<'openctx'>
     id: 'openctx',
     title: 'OpenCtx',
     async queryContextItems(query) {
-        const client = await getOpenCtxClient()
+        const client = getOpenCtxClient()
         if (!client) {
             return []
         }
-        const results = await client.items({ query: query })
-        const items =
+        const results = await client.mentions({ query: query })
+        const mentions =
             results
-                ?.map((result, i) =>
-                    result.ai?.content
-                        ? ({
-                              type: 'file',
-                              title: result.title,
-                              uri: URI.parse(result.url ?? `openctx-item:${i}`),
-                              provider: 'openctx',
-                              content: result.ai?.content,
-                          } satisfies ContextItemWithContent & ContextItemFromProvider<'openctx'>)
-                        : null
+                ?.map(
+                    (result, i) =>
+                        ({
+                            type: 'file',
+                            title: result.title,
+                            uri: URI.parse(result.uri ?? `openctx-item:${i}`),
+                            provider: 'openctx',
+                            content: '',
+                        }) satisfies ContextItemWithContent & ContextItemFromProvider<'openctx'>
                 )
                 .filter(isDefined) ?? []
-        HACK_LAST_RESULTS = items
-        return items
+        HACK_LAST_RESULTS = mentions
+        return mentions
     },
     resolveContextItem(item) {
         return Promise.resolve(
