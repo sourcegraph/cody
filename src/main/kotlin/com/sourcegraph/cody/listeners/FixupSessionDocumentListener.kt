d078637e088514627964d35552f783f51e47c5aa
Piotr Kukiełka <pkukielka@users.noreply.github.com>
2024-05-23 21:16:12 +0200

    Fix listener logic, bump cody commit (#1616)
    ## Test plan
    
    1. Run Document Code action in the IDE
    2. Cancel it before accept lens appear

--- src/main/kotlin/com/sourcegraph/cody/listeners/FixupSessionDocumentListener.kt
+++ src/main/kotlin/com/sourcegraph/cody/listeners/FixupSessionDocumentListener.kt
@@ -2,7 +2,6 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.event.BulkAwareDocumentListener
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.sourcegraph.cody.edit.sessions.FixupSession
-import java.util.concurrent.atomic.AtomicBoolean
 
 // This is part of a workaround for https://github.com/sourcegraph/cody-issues/issues/315
 // The correct solution will involve a protocol change so the Agent can know when the
@@ -10,16 +9,8 @@ import java.util.concurrent.atomic.AtomicBoolean
 class FixupSessionDocumentListener(private val session: FixupSession) : BulkAwareDocumentListener {
   private val logger = Logger.getInstance(FixupSessionDocumentListener::class.java)
 
-  private val isAcceptLensGroupShown = AtomicBoolean(false)
-
   override fun documentChangedNonBulk(event: DocumentEvent) {
-    if (isAcceptLensGroupShown.get()) {
-      logger.info("Auto-accepting current Fixup session: ${session.taskId}")
-      session.accept()
-    }
-  }
-
-  fun setAcceptLensGroupShown(shown: Boolean) {
-    isAcceptLensGroupShown.set(shown)
+    logger.info("Auto-accepting current Fixup session: ${session.taskId}")
+    session.accept()
   }
 }
