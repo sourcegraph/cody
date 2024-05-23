sourcegraph/cody

commit db6d4eb84bd32b6141bb93e732d14c40a5775613

Edit (Test): Fix incorrect selection context (#3759)

vscode/src/edit/provider.ts
@@ -87,20 +87,29 @@ export class EditProvider {
                 },
             })
 
-            // Listen to test file name suggestion from responses
-            // Allows Cody to let us know which test file we should add the new content to
             if (this.config.task.intent === 'test') {
-                let filepath = ''
-                multiplexer.sub(PROMPT_TOPICS.FILENAME, {
-                    onResponse: async (content: string) => {
-                        filepath += content
-                        void this.handleFileCreationResponse(filepath, true)
-                        return Promise.resolve()
-                    },
-                    onTurnComplete: async () => {
-                        return Promise.resolve()
-                    },
-                })
+                if (this.config.task.destinationFile) {
+                    // We have already provided a destination file,
+                    // Treat this as the test file to insert to
+                    await this.config.controller.didReceiveNewFileRequest(
+                        this.config.task.id,
+                        this.config.task.destinationFile
+                    )
+                } else {
+                    // Listen to test file name suggestion from responses
+                    // Allows Cody to let us know which test file we should add the new content to
+                    let filepath = ''
+                    multiplexer.sub(PROMPT_TOPICS.FILENAME, {
+                        onResponse: async (content: string) => {
+                            filepath += content
+                            void this.handleFileCreationResponse(filepath, true)
+                            return Promise.resolve()
+                        },
+                        onTurnComplete: async () => {
+                            return Promise.resolve()
+                        },
+                    })
+                }
             }
 
             const abortController = new AbortController()

vscode/src/non-stop/FixupController.ts
@@ -1157,12 +1157,6 @@ export class FixupController
 
         task.state = state
 
-        // Creates new file if destinationFile is provided at task creation
-        if (task.state === CodyTaskState.pending && task.destinationFile) {
-            void this.didReceiveNewFileRequest(task.id, task.destinationFile)
-            return
-        }
-
         if (oldState !== CodyTaskState.working && task.state === CodyTaskState.working) {
             task.spinCount++
         }

commit 2841f571d4d7fd3afe2851a55cc53762e739bf07

Test Command: Fix issue with inserting test file name with unit test code
I suspect the issue was introduced in [#3759](https://sourcegraph.com/github.com/sourcegraph/cody/-/commit/db6d4eb84bd32b6141bb93e732d14c40a5775613?visible=3) where we updated to only subscribe to the multiplexer for file creation if we couldn't locate the test file internally. This becomes an issue when we ask for the LLM to always return the test file name in the [PROMPT_TOPICS.FILENAME](https://sourcegraph.com/github.com/sourcegraph/cody/-/blob/vscode/src/edit/prompt/models/generic.ts?L88) in our prompt.

- Fixes an issue where the test file name was incorrectly inserted when using the unit test command
- Ensures the test file name suggestion from Cody is properly handled and the file is created if necessary
- Keeps the multiplexer subscription for the test file name suggestion even if a destination file is set, to ensure any text returned by Cody wrapped in the PROMPT_TOPICS.FILENAME tags are properly handled

vscode/src/edit/provider.ts
@@ -96,21 +96,23 @@ export class EditProvider {
                         this.config.task.id,
                         this.config.task.destinationFile
                     )
-                } else {
-                    // Listen to test file name suggestion from responses
-                    // Allows Cody to let us know which test file we should add the new content to
-                    let filepath = ''
-                    multiplexer.sub(PROMPT_TOPICS.FILENAME.toString(), {
-                        onResponse: async (content: string) => {
-                            filepath += content
-                            void this.handleFileCreationResponse(filepath, true)
-                            return Promise.resolve()
-                        },
-                        onTurnComplete: async () => {
-                            return Promise.resolve()
-                        },
-                    })
                 }
+
+                // Listen to test file name suggestion from responses and create the file if we don't have one.
+                // This allows Cody to let us know which test file we should add the new content to.
+                // NOTE: Keep this multiplexer even if a destination file is set to catch the PROMPT_TOPICS.
+                let filepath = ''
+                multiplexer.sub(PROMPT_TOPICS.FILENAME.toString(), {
+                    onResponse: async (content: string) => {
+                        filepath += content
+                        // handleFileCreationResponse will verify if task.destinationFile is set before creating a new file.
+                        void this.handleFileCreationResponse(filepath, true)
+                        return Promise.resolve()
+                    },
+                    onTurnComplete: async () => {
+                        return Promise.resolve()
+                    },
+                })
             }
 
             this.abortController = new AbortController()