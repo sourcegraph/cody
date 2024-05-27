0ee4bcbd026f17f2a1c977fd76aa67146971f818
Quinn Slack <quinn@slack.org>
2024-05-27 21:59:00 +0800

    fix bug preventing vertically scrolling of long messages in the input
    Fixes https://linear.app/sourcegraph/issue/CODY-1881/cody-for-vs-code-long-user-messages-cant-be-scrolled-using-a-mouse

--- vscode/webviews/chat/cells/messageCell/human/HumanMessageCell.story.tsx
+++ vscode/webviews/chat/cells/messageCell/human/HumanMessageCell.story.tsx
@@ -1,9 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { HumanMessageCell } from './HumanMessageCell'
-
+import { PromptString } from '@sourcegraph/cody-shared'
 import { VSCodeCell } from '../../../../storybook/VSCodeStoryDecorator'
 import { FIXTURE_TRANSCRIPT, FIXTURE_USER_ACCOUNT_INFO } from '../../../fixtures'
+import { HumanMessageCell } from './HumanMessageCell'
 
 const meta: Meta<typeof HumanMessageCell> = {
     title: 'ui/HumanMessageCell',
@@ -45,5 +45,26 @@ export const FollowupWithText: StoryObj<typeof meta> = {
     args: {
         message: FIXTURE_TRANSCRIPT.explainCode2[0],
         isFirstMessage: false,
+    },
+}
+
+export const Scrolling: StoryObj<typeof meta> = {
+    args: {
+        message: {
+            speaker: 'human',
+            text: PromptString.unsafe_fromUserQuery(
+                new Array(100)
+                    .fill(0)
+                    .map(
+                        (_, index) =>
+                            `Line ${index} ${
+                                index % 5 === 0
+                                    ? 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+                                    : ''
+                            }`
+                    )
+                    .join('\n')
+            ),
+        },
     },
 }
