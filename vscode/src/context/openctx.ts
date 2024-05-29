3ac5a40cda77fcb25f278c2e4a6973e6f4ff261f
Naman Kumar <naman@outlook.in>
2024-05-30 01:10:49 +0800

    feature flag

--- vscode/src/context/openctx.ts
+++ vscode/src/context/openctx.ts
@@ -1,4 +1,4 @@
-import { isDotCom, setOpenCtxClient } from '@sourcegraph/cody-shared'
+import { type ConfigurationWithAccessToken, setOpenCtxClient } from '@sourcegraph/cody-shared'
 import type * as vscode from 'vscode'
 import { logDebug, outputChannel } from '../log'
 import RemoteFileProvider from './openctx/remoteFileSearch'
@@ -6,30 +6,33 @@ import RemoteRepositorySearch from './openctx/remoteRepositorySearch'
 
 export function exposeOpenCtxClient(
     secrets: vscode.SecretStorage,
-    config: { serverEndpoint: string }
+    config: ConfigurationWithAccessToken
 ): void {
     logDebug('openctx', 'OpenCtx is enabled in Cody')
     import('@openctx/vscode-lib')
         .then(({ createController }) => {
+            const providers = [
+                {
+                    providerUri: RemoteRepositorySearch.providerUri,
+                    settings: true,
+                    provider: RemoteRepositorySearch,
+                },
+            ]
+
+            if (config.experimentalNoodle) {
+                providers.push({
+                    providerUri: RemoteFileProvider.providerUri,
+                    settings: true,
+                    provider: RemoteFileProvider,
+                })
+            }
+
             setOpenCtxClient(
                 createController({
                     outputChannel,
                     secrets,
                     features: {},
-                    providers: isDotCom(config.serverEndpoint)
-                        ? []
-                        : [
-                              {
-                                  providerUri: RemoteRepositorySearch.providerUri,
-                                  settings: true,
-                                  provider: RemoteRepositorySearch,
-                              },
-                              {
-                                  providerUri: RemoteFileProvider.providerUri,
-                                  settings: true,
-                                  provider: RemoteFileProvider,
-                              },
-                          ],
+                    providers,
                 }).controller.client
             )
         })
