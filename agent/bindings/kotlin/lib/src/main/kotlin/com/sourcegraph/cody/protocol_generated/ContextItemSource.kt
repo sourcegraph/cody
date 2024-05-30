7f954504405f133c1bcd46e9ce25032d7d2b107f
Quinn Slack <quinn@slack.org>
2024-05-31 03:01:41 +0800

    remove old context providers (#4390)

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/ContextItemSource.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/ContextItemSource.kt
@@ -1,5 +1,5 @@
 @file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
 package com.sourcegraph.cody.protocol_generated
 
-typealias ContextItemSource = String // One of: embeddings, user, editor, search, unified, selection, terminal, uri, package, history, github
+typealias ContextItemSource = String // One of: embeddings, user, editor, search, unified, selection, terminal, uri, history
 
