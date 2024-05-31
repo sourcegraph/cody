a8b3c01768c2e141d56676cdc2c9db43367bd4a8
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-05-31 10:00:24 +0200

    remove unneccessary windows check in ContextItemMentionNode test
    The true and false branch are the same value.
    
    Test Plan: CI

--- vscode/webviews/promptEditor/nodes/ContextItemMentionNode.test.ts
+++ vscode/webviews/promptEditor/nodes/ContextItemMentionNode.test.ts
@@ -15,7 +15,7 @@ describe('contextItemMentionNodeDisplayText', () => {
                 uri: 'file:///a.go',
                 range: { start: { line: 1, character: 0 }, end: { line: 4, character: 0 } },
             })
-        ).toBe(`${isWindows() ? 'a.go' : 'a.go'}:2-4`))
+        ).toBe('a.go:2-4'))
 
     test('file range', () =>
         expect(
@@ -24,7 +24,7 @@ describe('contextItemMentionNodeDisplayText', () => {
                 uri: 'file:///a.go',
                 range: { start: { line: 1, character: 2 }, end: { line: 4, character: 4 } },
             })
-        ).toBe(`${isWindows() ? 'a.go' : 'a.go'}:2-5`))
+        ).toBe('a.go:2-5'))
 
     test('symbol', () =>
         expect(
