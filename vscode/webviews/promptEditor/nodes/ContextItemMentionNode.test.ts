01bc8503f09092c178ca8806d5202c9686edd812
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-05-31 10:28:33 +0200

    remove unneccessary windows check in ContextItemMentionNode test (#4399)
    Co-authored-by: Jean-Hadrien Chabran <jh@chabran.fr>

--- vscode/webviews/promptEditor/nodes/ContextItemMentionNode.test.ts
+++ vscode/webviews/promptEditor/nodes/ContextItemMentionNode.test.ts
@@ -1,4 +1,3 @@
-import { isWindows } from '@sourcegraph/cody-shared'
 import { describe, expect, test } from 'vitest'
 import { contextItemMentionNodeDisplayText } from './ContextItemMentionNode'
 
@@ -15,7 +14,7 @@ describe('contextItemMentionNodeDisplayText', () => {
                 uri: 'file:///a.go',
                 range: { start: { line: 1, character: 0 }, end: { line: 4, character: 0 } },
             })
-        ).toBe(`${isWindows() ? 'a.go' : 'a.go'}:2-4`))
+        ).toBe('a.go:2-4'))
 
     test('file range', () =>
         expect(
@@ -24,7 +23,7 @@ describe('contextItemMentionNodeDisplayText', () => {
                 uri: 'file:///a.go',
                 range: { start: { line: 1, character: 2 }, end: { line: 4, character: 4 } },
             })
-        ).toBe(`${isWindows() ? 'a.go' : 'a.go'}:2-5`))
+        ).toBe('a.go:2-5'))
 
     test('symbol', () =>
         expect(
