f98512f0a3d296b75e27b9b4e980e0bc21232b7f
jamesmcnamara <james.mcnamara@sourcegraph.com>
2024-06-03 12:12:08 -0700

    re-run generate test on windows to test flakiness

--- agent/src/generate-unit-test.test.ts
+++ agent/src/generate-unit-test.test.ts
@@ -1,5 +1,5 @@
 import path from 'node:path'
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 import * as vscode from 'vscode'
 import type { ReplaceTextEdit } from '../../vscode/src/jsonrpc/agent-protocol'
 import { TESTING_CREDENTIALS } from '../../vscode/src/testutils/testing-credentials'
@@ -8,43 +8,49 @@ import { TestWorkspace } from './TestWorkspace'
 import { trimEndOfLine } from './trimEndOfLine'
 
 describe('Generate Unit Test', () => {
-    const workspace = new TestWorkspace(path.join(__dirname, '__tests__', 'generate-unit-test'))
-    const client = TestClient.create({
-        workspaceRootUri: workspace.rootUri,
-        name: 'generate-unit-test',
-        credentials: TESTING_CREDENTIALS.dotcom,
-    })
+    let workspace: TestWorkspace
+    let client: TestClient
 
-    beforeAll(async () => {
+    beforeEach(async () => {
+        workspace = new TestWorkspace(path.join(__dirname, '__tests__', 'generate-unit-test'))
+        client = TestClient.create({
+            workspaceRootUri: workspace.rootUri,
+            name: 'generate-unit-test',
+            credentials: TESTING_CREDENTIALS.dotcom,
+        })
         await workspace.beforeAll()
         await client.beforeAll()
     })
 
-    afterAll(async () => {
+    afterEach(async () => {
         await workspace.afterAll()
         await client.afterAll()
     })
 
-    it('editCommands/test', async () => {
-        const uri = workspace.file('src', 'trickyLogic.ts')
-        await client.openFile(uri)
-        const id = await client.request('editCommands/test', null)
-        await client.taskHasReachedAppliedPhase(id)
-        const untitledDocuments = client.workspace
-            .allUris()
-            .filter(uri => vscode.Uri.parse(uri).scheme === 'untitled')
-        expect(untitledDocuments).toHaveLength(2)
-        const untitledDocument = untitledDocuments.find(d => d.endsWith('trickyLogic.test.ts'))
-        expect(untitledDocument).toBeDefined()
-        const testDocument = client.workspace.getDocument(vscode.Uri.parse(untitledDocument ?? ''))
-        expect(trimEndOfLine(testDocument?.getText())).toMatchSnapshot()
-        expect(client.textDocumentEditParams).toHaveLength(1)
-        for (const editParam of client.textDocumentEditParams) {
-            for (const edit of editParam.edits) {
-                const range = (edit as ReplaceTextEdit).range
-                expect(range.start.line).toBeGreaterThanOrEqual(0)
-                expect(range.end.line).toBeGreaterThanOrEqual(0)
+    it(
+        'editCommands/test',
+        { timeout: 30_000, repeats: process.platform === 'win32' ? 10 : 0 },
+        async () => {
+            const uri = workspace.file('src', 'trickyLogic.ts')
+            await client.openFile(uri)
+            const id = await client.request('editCommands/test', null)
+            await client.taskHasReachedAppliedPhase(id)
+            const untitledDocuments = client.workspace
+                .allUris()
+                .filter(uri => vscode.Uri.parse(uri).scheme === 'untitled')
+            expect(untitledDocuments).toHaveLength(2)
+            const untitledDocument = untitledDocuments.find(d => d.endsWith('trickyLogic.test.ts'))
+            expect(untitledDocument).toBeDefined()
+            const testDocument = client.workspace.getDocument(vscode.Uri.parse(untitledDocument ?? ''))
+            expect(trimEndOfLine(testDocument?.getText())).toMatchSnapshot()
+            expect(client.textDocumentEditParams).toHaveLength(1)
+            for (const editParam of client.textDocumentEditParams) {
+                for (const edit of editParam.edits) {
+                    const range = (edit as ReplaceTextEdit).range
+                    expect(range.start.line).toBeGreaterThanOrEqual(0)
+                    expect(range.end.line).toBeGreaterThanOrEqual(0)
+                }
             }
         }
-    }, 30_000)
+    )
 })
