629a2bd2bec7a6bd8b040ad481d580bbf82e6b5f
Chris Smith <chrsmith@sourcegraph.com>
2024-06-03 14:45:29 -0700

    Re-generate Kotlin bindings

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/Model.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/protocol_generated/Model.kt
@@ -1,13 +1,11 @@
 @file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
 package com.sourcegraph.cody.protocol_generated
 
-data class ModelProvider(
+data class Model(
   val default: Boolean,
   val codyProOnly: Boolean,
   val provider: String,
   val title: String,
   val deprecated: Boolean,
-  val primaryProviders: List<ModelProvider>,
-  val localProviders: List<ModelProvider>,
 )
 
