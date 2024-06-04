60bfb5c4bb695fff556d9b1304700b5e5a32e240
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-06-04 10:50:19 +0200

    unset range if full range

--- vscode/src/commands/context/file-path.ts
+++ vscode/src/commands/context/file-path.ts
@@ -20,12 +20,25 @@ export async function getContextFileFromUri(file: URI, range?: vscode.Range): Pr
                 return []
             }
 
+            const doc = await vscode.workspace.openTextDocument(file)
+
             // empty range can happen when user initiates action from right
             // click. Treat as wanting full document.
             range = range?.isEmpty ? undefined : range
 
-            const doc = await vscode.workspace.openTextDocument(file)
-            const content = doc?.getText(range).trim()
+            // if the range is the full file, remove our range specifier so it
+            // renders nicely in the UI
+            const fullRange = new vscode.Range(
+                0,
+                0,
+                doc.lineCount,
+                doc.lineAt(doc.lineCount - 1).text.length
+            )
+            if (range?.contains(fullRange)) {
+                range = undefined
+            }
+
+            const content = doc.getText(range).trim()
             if (!content) {
                 throw new Error('No file content')
             }
