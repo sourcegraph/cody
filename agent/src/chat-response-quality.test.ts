1cc1e472ee5331e6ec7edd90f14ac3c67ea44592
Julie Tibshirani <julie.tibshirani@sourcegraph.com>
2024-06-04 15:26:26 -0700

    Improve Chat response quality test

--- agent/src/chat-response-quality.test.ts
+++ agent/src/chat-response-quality.test.ts
@@ -15,11 +15,6 @@ import { TestClient } from './TestClient'
 import { TestWorkspace } from './TestWorkspace'
 
 const workspace = new TestWorkspace(path.join(__dirname, '__tests__', 'chat-response-quality'))
-
-const characterCheck = /anthropic|openai|gpt|claude/i
-const hedgingCheck =
-    /afraid|apologize|sorry|unfortunately|enough information|full contents|As an AI|without access/i
-
 describe('Chat response quality', () => {
     const client = TestClient.create({
         workspaceRootUri: workspace.rootUri,
@@ -48,88 +43,133 @@ describe('Chat response quality', () => {
         await client.request('testing/reset', null)
     })
 
-    describe('Questions about origin', () => {
-        it('Who are you?', async () => {
-            const lastMessage = await client.sendSingleMessageToNewChat('Who are you?')
-            checkLastMessage(lastMessage)
-            checkNoFiles(lastMessage)
-            expect(lastMessage?.text?.toLocaleLowerCase() ?? '').includes('cody')
-        }, 10_000)
+    const modelStrings = [
+        'anthropic/claude-3-sonnet-20240229',
+        'anthropic/claude-3-haiku-20240307',
+        'openai/gpt-3.5-turbo',
+    ]
+    for (const modelString of modelStrings) {
+        describe(modelString, () => {
+            it('Who are you?', async () => {
+                const lastMessage = await sendMessage(client, modelString, 'Who are you?')
+                checkAccess(lastMessage)
+            }, 10_000)
 
-        it.skip('Who created you??', async () => {
-            const lastMessage = await client.sendSingleMessageToNewChat('Who created you??')
-            checkLastMessage(lastMessage)
-            checkNoFiles(lastMessage)
-            expect(lastMessage?.text?.toLocaleLowerCase() ?? '').includes('sourcegraph')
-        }, 10_000)
+            // Skip because this currently fails.
+            // * anthropic/claude-3-sonnet: "Unfortunately, I don't have access to any actual code files from this codebase."
+            // * anthropic/claude-3-haiku: "I'm afraid I don't have direct access to any code in this case"
+            it.skip('What code do you have access to?', async () => {
+                const lastMessage = await sendMessage(
+                    client,
+                    modelString,
+                    'What code do you have access to?',
+                    { addEnhancedContext: false, contextFiles: [readmeItem] }
+                )
+                checkAccess(lastMessage)
+            }, 10_000)
 
-        it.skip('Who created you?? with irrelevant context', async () => {
-            const lastMessage = await client.sendSingleMessageToNewChat('Who created you??', {
-                addEnhancedContext: false,
-                contextFiles: contextFiles,
-            })
-            checkLastMessage(lastMessage)
-            checkNoFiles(lastMessage)
-            expect(lastMessage?.text?.toLocaleLowerCase() ?? '').includes('sourcegraph')
-        }, 10_000)
-    })
+            it('What does this repo do??', async () => {
+                const lastMessage = await sendMessage(client, modelString, 'What does this repo do??', {
+                    addEnhancedContext: false,
+                    contextFiles: [limitItem],
+                })
+                checkAccess(lastMessage)
+            }, 10_000)
 
-    describe('Questions about knowledge base', () => {
-        it.skip('What code do you have access to?', async () => {
-            const lastMessage = await client.sendSingleMessageToNewChat(
-                'What code do you have access to?'
-            )
-            checkLastMessage(lastMessage)
-        }, 10_000)
+            // Skip because this currently fails.
+            // * anthropic/claude-3-haiku: "Unfortunately, I don't have access to any code that you provided"
+            // * openai/gpt-3.5-turbo: "I am sorry, but the code snippets you provided are incomplete and out of context"
+            it.skip('describe my code', async () => {
+                const lastMessage = await sendMessage(client, modelString, 'describe my code', {
+                    addEnhancedContext: false,
+                    contextFiles: [readmeItem, evalItem, externalServicesItem, limitItem],
+                })
+                checkAccess(lastMessage)
+            }, 10_000)
 
-        it.skip('What does this repo do?', async () => {
-            const lastMessage = await client.sendSingleMessageToNewChat('What does this repo do?')
-            checkLastMessage(lastMessage)
-        }, 10_000)
-    })
+            it('Is my codebase clean?', async () => {
+                const lastMessage = await sendMessage(client, modelString, 'is my code base clean?', {
+                    addEnhancedContext: false,
+                    contextFiles: [readmeItem, limitItem],
+                })
+                checkAccess(lastMessage)
+            }, 10_000)
 
-    describe('Poor context provided', () => {
-        it.skip('No context', async () => {
-            const lastMessage = await client.sendSingleMessageToNewChat(
-                "What's the license of this repo?",
-                { addEnhancedContext: false, contextFiles: [] }
-            )
-            checkLastMessage(lastMessage)
-            checkNoFiles(lastMessage)
-        }, 10_000)
+            // Skip because this currently fails.
+            // * anthropic/claude-3-sonnet: "I don't have access ... As an AI assistant without a physical form ..."
+            // * anthropic/claude-3-haiku: "As an AI assistant without direct access to your development environment"
+            it.skip('Are you capable of upgrading my pytorch version', async () => {
+                const lastMessage = await sendMessage(
+                    client,
+                    modelString,
+                    'Are you capable of upgrading my pytorch version to 1.0.0, there is a guide in the pytorch site',
+                    { addEnhancedContext: false, contextFiles: [readmeItem, limitItem] }
+                )
+                checkAccess(lastMessage)
+            }, 10_000)
 
-        it('Relevant file missing', async () => {
-            const lastMessage = await client.sendSingleMessageToNewChat(
-                "Where's the Zoekt indexing logic?",
-                { addEnhancedContext: false, contextFiles: contextFiles }
-            )
-            checkLastMessage(lastMessage)
-            checkFilesExist(lastMessage, contextFiles)
-        }, 10_000)
+            // Skip because this currently fails.
+            // * openai/gpt-3.5-turbo: "I'm sorry, but I am unable to browse through specific files"
+            it.skip('Can you look through the files?', async () => {
+                const lastMessage = await sendMessage(
+                    client,
+                    modelString,
+                    'Can you look through the files and identify the conflicting packages that may be causing this?',
+                    { addEnhancedContext: false, contextFiles: [readmeItem, limitItem] }
+                )
+                checkAccess(lastMessage)
+            }, 10_000)
 
-        it('Relevant section missing from file', async () => {
-            const lastMessage = await client.sendSingleMessageToNewChat(
-                'What how does NewDisplayTruncator work in @limit.go?',
-                { addEnhancedContext: false, contextFiles: contextFiles }
-            )
-            checkLastMessage(lastMessage)
-            checkFilesExist(lastMessage, contextFiles)
-            checkSymbolsExist(lastMessage, contextFiles)
-        }, 10_000)
-    })
+            // Skip because this currently fails.
+            // * anthropic/claude-3-haiku: "Some key reasons why this project may use the MIT license..."
+            it.skip('Why does this project use the MIT license?', async () => {
+                const lastMessage = await sendMessage(
+                    client,
+                    modelString,
+                    'Why does this project use the MIT license?',
+                    { addEnhancedContext: false, contextFiles: [readmeItem, limitItem] }
+                )
+                checkAccess(lastMessage)
 
-    describe('Good context provided', () => {
-        it('Relevant file section provided', async () => {
-            const lastMessage = await client.sendSingleMessageToNewChat(
-                'Where does Zoekt trim the list of files?',
-                { addEnhancedContext: false, contextFiles: contextFiles }
-            )
+                // Check it doesn't hallucinate
+                expect(lastMessage?.text).not.includes('uses the MIT license because')
+                expect(lastMessage?.text).not.includes(
+                    'reasons why this project may use the MIT license'
+                )
+                expect(lastMessage?.text).not.includes(
+                    'reasons why this project might use the MIT license'
+                )
+            }, 10_000)
 
-            checkLastMessage(lastMessage)
-            checkFilesExist(lastMessage, contextFiles)
-            checkSymbolsExist(lastMessage, contextFiles)
-        }, 10_000)
-    })
+            // Skip because this currently fails.
+            // * openai/gpt-3.5-turbo: "I can't browse external repositories or specific codebases"
+            it.skip('See zoekt repo find location of tensor function', async () => {
+                let contextFiles = [readmeItem, limitItem, evalItem]
+                const lastMessage = await sendMessage(
+                    client,
+                    modelString,
+                    'See zoekt repo find location of tensor function',
+                    { addEnhancedContext: false, contextFiles: contextFiles}
+                )
+                checkAccess(lastMessage)
+                checkFilesExist(lastMessage, [], contextFiles)
+            }, 10_000)
+
+            it('Explain the logic in src/agent.go', async () => {
+                let contextFiles = [readmeItem, limitItem]
+                const lastMessage = await sendMessage(
+                    client,
+                    modelString,
+                    'Explain the logic in src/agent.go, particularly how agents interact with ranking',
+                    { addEnhancedContext: false, contextFiles: contextFiles}
+                )
+                // Don't check access, because this file does not exist in the context.
+                // Check it doesn't hallucinate
+                expect(lastMessage?.text).not.includes("Sure, let's")
+                checkFilesExist(lastMessage, ["agent.go"], contextFiles)
+            }, 10_000)
+        })
+    }
 
     afterAll(async () => {
         await workspace.afterAll()
@@ -137,74 +177,95 @@ describe('Chat response quality', () => {
         // Long timeout because to allow Polly.js to persist HTTP recordings
     }, 30_000)
 })
+async function sendMessage(
+    client: TestClient,
+    modelString: string,
+    text: string,
+    params?: { addEnhancedContext?: boolean; contextFiles?: ContextItem[] }
+) {
+    const id = await client.request('chat/new', null)
+    await client.setChatModel(id, modelString)
+    return await client.sendMessage(id, text, params)
+}
 
-function checkLastMessage(lastMessage: SerializedChatMessage | undefined) {
+const accessCheck =
+    /I don't (?:actually )?have (?:direct )?access|your actual codebase|can't browse external repositories|not able to access external information|unable to browse through specific files|As an AI/i
+
+function checkAccess(lastMessage: SerializedChatMessage | undefined) {
     expect(lastMessage?.speaker).toBe('assistant')
     expect(lastMessage?.text).not.toBeUndefined()
-    expect(lastMessage?.text ?? '').not.toMatch(characterCheck)
-    expect(lastMessage?.text ?? '').not.toMatch(hedgingCheck)
+    expect(lastMessage?.text ?? '').not.toMatch(accessCheck)
 }
 
-function checkNoFiles(lastMessage: SerializedChatMessage | undefined) {
-    return checkFilesExist(lastMessage, [])
-}
-
-function checkFilesExist(lastMessage: SerializedChatMessage | undefined, contextFiles: ContextItem[]) {
-    const filenameRegex = /\b`(\w+\.\w+)`\b/g
+function checkFilesExist(lastMessage: SerializedChatMessage | undefined, questionFiles: string[], contextFiles: ContextItem[]) {
+    const filenameRegex = /\b(\w+\.\w+)\b/g
     const files = lastMessage?.text?.match(filenameRegex) ?? []
     const contextFilePaths = new Set(contextFiles.map(file => file.uri.path))
     for (const file of files) {
-        expect(contextFilePaths.has(file), `file ${file} does not exist in context`).toBe(true)
-    }
-}
-
-function checkSymbolsExist(lastMessage: SerializedChatMessage | undefined, contextFiles: ContextItem[]) {
-    const symbolRegexp = /\b\w*[a-z][A-Z_]\w*\b/
-    const symbols = lastMessage?.text?.match(symbolRegexp) ?? []
-    for (const symbol of symbols) {
-        let found = false
-        for (const contextFile of contextFiles) {
-            if (contextFile.content?.includes(symbol)) {
+        let found = questionFiles.includes(file)
+        for (const contextFile of contextFilePaths) {
+            if (contextFile.endsWith(file)) {
                 found = true
                 break
             }
         }
-        expect(found, `symbol ${symbol} does not exist in context`).toBe(true)
+        if (!found) {
+            expect.fail(`file ${file} does not exist in context`)
+        }
     }
 }
 
-const contextFiles: ContextItem[] = [
-    {
-        uri: workspace.file('README.md'),
-        type: 'file',
-        content:
-            '  "Zoekt, en gij zult spinazie eten" - Jan Eertink\n' +
-            '\n' +
-            '    ("seek, and ye shall eat spinach" - My primary school teacher)\n' +
-            '\n' +
-            'This is a fast text search engine, intended for use with source\n' +
-            'code. (Pronunciation: roughly as you would pronounce "zooked" in English)\n' +
-            '\n' +
-            '**Note:** This is a [Sourcegraph](https://github.com/sourcegraph/zoekt) fork\n' +
-            'of [github.com/google/zoekt](https://github.com/google/zoekt). It is now the\n' +
-            'main maintained source of Zoekt.',
-    },
-    {
-        uri: workspace.file('limit.go'),
-        type: 'file',
-        content:
-            'package zoekt\n' +
-            '\n' +
-            'import "log"\n' +
-            '\n' +
-            '// SortAndTruncateFiles is a convenience around SortFiles and\n' +
-            '// DisplayTruncator. Given an aggregated files it will sort and then truncate\n' +
-            '// based on the search options.\n' +
-            'func SortAndTruncateFiles(files []FileMatch, opts *SearchOptions) []FileMatch {\n' +
-            '\tSortFiles(files)\n' +
-            '\ttruncator, _ := NewDisplayTruncator(opts)\n' +
-            '\tfiles, _ = truncator(files)\n' +
-            '\treturn files\n' +
-            '}',
-    },
-]
+const readmeItem: ContextItem = {
+    uri: workspace.file('README.md'),
+    type: 'file',
+    content:
+        '  "Zoekt, en gij zult spinazie eten" - Jan Eertink\n' +
+        '\n' +
+        '    ("seek, and ye shall eat spinach" - My primary school teacher)\n' +
+        '\n' +
+        'This is a fast text search engine, intended for use with source\n' +
+        'code. (Pronunciation: roughly as you would pronounce "zooked" in English)\n',
+}
+
+const limitItem: ContextItem = {
+    uri: workspace.file('limit.go'),
+    type: 'file',
+    content:
+        'package zoekt\n' +
+        '\n' +
+        'import "log"\n' +
+        '\n' +
+        '// SortAndTruncateFiles is a convenience around SortFiles and\n' +
+        '// DisplayTruncator. Given an aggregated files it will sort and then truncate\n' +
+        '// based on the search options.\n' +
+        'func SortAndTruncateFiles(files []FileMatch, opts *SearchOptions) []FileMatch {\n' +
+        '\tSortFiles(files)\n' +
+        '\ttruncator, _ := NewDisplayTruncator(opts)\n' +
+        '\tfiles, _ = truncator(files)\n' +
+        '\treturn files\n' +
+        '}',
+}
+
+const evalItem: ContextItem = {
+    uri: workspace.file('eval.go'),
+    type: 'file',
+    content:
+        '\t\tfor _, q := range qs {\n' +
+        '\t\t\tif _, ok := q.(*bruteForceMatchTree); ok {\n' +
+        '\t\t\t\treturn q, isEq, false, nil\n' +
+        '\t\t\t}\n' +
+        '\t\t}\n' +
+        '\t\tif len(qs) == 0 {\n' +
+        '\t\t\treturn &noMatchTree{Why: "const"}, isEq, false, nil\n' +
+        '\t\t}\n' +
+        '\t\treturn &orMatchTree{qs}, isEq, false, nil\n' +
+        '\tcase syntax.OpStar:\n' +
+        '\t\tif r.Sub[0].Op == syntax.OpAnyCharNotNL {\n' +
+        '}',
+}
+
+const externalServicesItem: ContextItem = {
+    uri: workspace.file('vscode/src/external-services.ts'),
+    type: 'file',
+    content: '\n```typescript\n        },\n    }\n}\n```',
+}
