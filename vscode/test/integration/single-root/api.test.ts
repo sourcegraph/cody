db2d2dcb84721f5b9ce509b076901ad8e8dd095a
Philipp Spiess <hello@philippspiess.com>
2024-06-04 16:01:56 +0200

    Remove history test from broken integratoin setup

--- vscode/test/integration/single-root/api.test.ts
+++ vscode/test/integration/single-root/api.test.ts
@@ -2,40 +2,10 @@ import * as assert from 'node:assert'
 
 import * as vscode from 'vscode'
 
-import { VSCodeDocumentHistory } from '../../../src/completions/context/retrievers/jaccard-similarity/history'
-
-import { testFileUri } from '../helpers'
-
 suite('API tests', () => {
     test('Cody registers some commands', async () => {
         const commands = await vscode.commands.getCommands(true)
         const codyCommands = commands.filter(command => command.includes('cody.'))
         assert.ok(codyCommands.length)
-    })
-
-    test('History', () => {
-        const h = new VSCodeDocumentHistory(() => null)
-        h.addItem({
-            document: {
-                uri: testFileUri('foo.ts'),
-                languageId: 'ts',
-            },
-        })
-        h.addItem({
-            document: {
-                uri: testFileUri('bar.ts'),
-                languageId: 'ts',
-            },
-        })
-        h.addItem({
-            document: {
-                uri: testFileUri('foo.ts'),
-                languageId: 'ts',
-            },
-        })
-        assert.deepStrictEqual(
-            h.lastN(20, 'typescript').map(h => h.document.uri.toString()),
-            [testFileUri('foo.ts').toString(), testFileUri('bar.ts').toString()]
-        )
     })
 })
