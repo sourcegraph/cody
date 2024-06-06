1a58e63365dce1f0f04fada7779e613b38d5d634
Quinn Slack <quinn@slack.org>
2024-06-06 02:02:49 -0700

    run biome on vite(st) files (#4473)

--- agent/vitest.config.ts
+++ agent/vitest.config.ts
@@ -1,5 +1,5 @@
-import { statSync } from 'fs'
-import { resolve } from 'path'
+import { statSync } from 'node:fs'
+import { resolve } from 'node:path'
 
 import { defineProjectWithDefaults } from '../.config/viteShared'
 
