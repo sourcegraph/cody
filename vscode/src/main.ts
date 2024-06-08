0d0feafc2721472f5f60ce72329cbe461f759ed5
Chris Smith <chrsmith@sourcegraph.com>
2024-06-08 16:12:47 -0700

    Add opt-in path to fetch models from the backend

--- vscode/src/main.ts
+++ vscode/src/main.ts
@@ -58,7 +58,7 @@ import { isRunningInsideAgent } from './jsonrpc/isRunningInsideAgent'
 import { logDebug, logError } from './log'
 import { MinionOrchestrator } from './minion/MinionOrchestrator'
 import { PoorMansBash } from './minion/environment'
-import { getChatModelsFromConfiguration, syncModels } from './models/sync'
+import { registerModelsFromVSCodeConfiguration, syncModels } from './models/sync'
 import { CodyProExpirationNotifications } from './notifications/cody-pro-expiration'
 import { showSetupNotification } from './notifications/setup-notification'
 import { initVSCodeGitApi } from './repository/git-extension-api'
@@ -112,7 +112,7 @@ export async function start(
     )
     disposables.push(disposable)
 
-    // Re-initialize when configuration
+    // Re-initialize when configuration changes.
     disposables.push(
         vscode.workspace.onDidChangeConfiguration(async event => {
             if (event.affectsConfiguration('cody')) {
@@ -122,7 +122,7 @@ export async function start(
                 if (config.chatPreInstruction.length > 0) {
                     PromptMixin.addCustom(newPromptMixin(config.chatPreInstruction))
                 }
-                getChatModelsFromConfiguration()
+                registerModelsFromVSCodeConfiguration()
             }
         })
     )
@@ -344,7 +344,9 @@ const register = async (
 
     // Adds a change listener to the auth provider that syncs the auth status
     authProvider.addChangeListener(async (authStatus: AuthStatus) => {
-        syncModels(authStatus)
+        // Reset the available models based on the auth change.
+        await syncModels(authStatus)
+
         // Chat Manager uses Simple Context Provider
         await chatManager.syncAuthStatus(authStatus)
         editorManager.syncAuthStatus(authStatus)
@@ -369,9 +371,9 @@ const register = async (
         sourceControl.syncAuthStatus(authStatus)
     })
 
-    // Sync initial auth status
+    // Sync the initial auth status.
     const initAuthStatus = authProvider.getAuthStatus()
-    syncModels(initAuthStatus)
+    await syncModels(initAuthStatus)
     await chatManager.syncAuthStatus(initAuthStatus)
     editorManager.syncAuthStatus(initAuthStatus)
     ModelsService.onConfigChange(initialConfig.experimentalOllamaChat)
