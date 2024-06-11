2da7a06368b3ca95b025c174e070defad85c4277
jamesmcnamara <james.mcnamara@sourcegraph.com>
2024-06-11 10:44:56 -0700

    updated promptstring test to use displayPath

--- vscode/src/prompt-builder/index.test.ts
+++ vscode/src/prompt-builder/index.test.ts
@@ -4,9 +4,9 @@ import type { ContextItem, Message } from '@sourcegraph/cody-shared'
 import {
     type ChatMessage,
     ContextItemSource,
-    PromptString,
     contextFiltersProvider,
     ps,
+    displayPath,
 } from '@sourcegraph/cody-shared'
 import { URI } from 'vscode-uri'
 import { PromptBuilder } from './index'
@@ -375,7 +375,7 @@ describe('PromptBuilder', () => {
             expect(builder.contextItems.length).toBe(1)
             expect(promptContent).toMatchInlineSnapshot(`
               "preamble
-              Codebase context from file ${PromptString.fromDisplayPath(file.uri)}:
+              Codebase context from file ${displayPath(file.uri)}:
               \`\`\`go
               foo\`\`\`
               Ok.
