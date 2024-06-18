b4cb8f6445a47c2777074bed2139a7d9d8394a29
Piotr Kukiełka <pkukielka@users.noreply.github.com>
2024-06-18 13:57:28 +0200

    Add ide version and proper Cody extension version to the telemetry (#4585)

--- vscode/src/jsonrpc/agent-protocol.ts
+++ vscode/src/jsonrpc/agent-protocol.ts
@@ -428,7 +428,8 @@ export interface AutocompleteItem {
 
 export interface ClientInfo {
     name: string
-    version: string
+    version: string // extension version
+    ideVersion?: string | undefined | null
     workspaceRootUri: string
 
     /** @deprecated Use `workspaceRootUri` instead. */
