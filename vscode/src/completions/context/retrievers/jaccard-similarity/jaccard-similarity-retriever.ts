91d48216720dc4334ba5a33db81ffdc9ea97baca
jamesmcnamara <james.mcnamara@sourcegraph.com>
2024-06-18 20:00:52 -0700

    WIP adds cached-retriever

--- vscode/src/completions/context/retrievers/jaccard-similarity/jaccard-similarity-retriever.ts
+++ vscode/src/completions/context/retrievers/jaccard-similarity/jaccard-similarity-retriever.ts
@@ -9,6 +9,7 @@ import { FeatureFlag } from '@sourcegraph/cody-shared'
 import { completionProviderConfig } from '../../../completion-provider-config'
 import { lastNLines } from '../../../text-processing'
 import { shouldBeUsedAsContext } from '../../utils'
+import { type CachedRerieverOptions, CachedRetriever } from '../cached-retriever'
 import { type JaccardMatch, bestJaccardMatches } from './bestJaccardMatch'
 
 /**
@@ -26,27 +27,36 @@ const SNIPPET_WINDOW_SIZE = 50
  */
 const MAX_MATCHES_PER_FILE = 20
 
+export interface JaccardSimilarityOptions extends CachedRerieverOptions {
+    snippetWindowSize?: number
+    maxMatchesPerFile?: number
+}
+
 /**
  * The Jaccard Similarity Retriever is a sparse, local-only, retrieval strategy that uses local
  * editor content (open tabs and file history) to find relevant code snippets based on the current
  * editor prefix.
  */
-export class JaccardSimilarityRetriever implements ContextRetriever {
-    constructor(
-        private snippetWindowSize: number = SNIPPET_WINDOW_SIZE,
-        private maxMatchesPerFile: number = MAX_MATCHES_PER_FILE
-    ) {}
+export class JaccardSimilarityRetriever extends CachedRetriever implements ContextRetriever {
+    private snippetWindowSize: number
+    private maxMatchesPerFile: number
+
+    constructor(options: JaccardSimilarityOptions = {}) {
+        super({ precomputeOnCursorMove: { debounceMs: 100 }, ...options })
+        this.snippetWindowSize = options.snippetWindowSize ?? SNIPPET_WINDOW_SIZE
+        this.maxMatchesPerFile = options.maxMatchesPerFile ?? MAX_MATCHES_PER_FILE
+    }
 
     public identifier = 'jaccard-similarity'
     private history = new VSCodeDocumentHistory()
 
-    public async retrieve({
+    public async doRetrieval({
         document,
         docContext,
         abortSignal,
     }: ContextRetrieverOptions): Promise<JaccardMatchWithFilename[]> {
         const targetText = lastNLines(docContext.prefix, this.snippetWindowSize)
-        const files = await getRelevantFiles(document, this.history)
+        const files = await this.getRelevantFiles(document, this.history)
 
         const contextRange = getContextRange(document, docContext)
         const contextLineRange = { start: contextRange.start.line, end: contextRange.end.line }
@@ -92,6 +102,141 @@ export class JaccardSimilarityRetriever implements ContextRetriever {
         return true
     }
 
+    /**
+     * Generates a cache key from each of the context retriever options we use.
+     *
+     */
+    public toCacheKey({
+        document,
+        docContext: {
+            prefix,
+            suffix,
+            position: { line, character },
+        },
+    }: ContextRetrieverOptions): string {
+        return `${document.uri}:${prefix}:${suffix}:${line}:${character}`
+    }
+
+    /**
+     * Loads all relevant files for for a given text editor. Relevant files are defined as:
+     *
+     * - All currently open tabs matching the same language
+     * - The last 10 files that were edited matching the same language
+     *
+     * For every file, we will load up to 10.000 lines to avoid OOMing when working with very large
+     * files.
+     */
+    async getRelevantFiles(
+        currentDocument: vscode.TextDocument,
+        history: DocumentHistory
+    ): Promise<FileContents[]> {
+        const files: FileContents[] = []
+
+        const curLang = currentDocument.languageId
+
+        function addDocument(document: vscode.TextDocument): void {
+            // Only add files and VSCode user settings.
+            if (!['file', 'vscode-userdata'].includes(document.uri.scheme)) {
+                return
+            }
+
+            if (
+                !shouldBeUsedAsContext(
+                    completionProviderConfig.getPrefetchedFlag(
+                        FeatureFlag.CodyAutocompleteContextExtendLanguagePool
+                    ),
+                    curLang,
+                    document.languageId
+                )
+            ) {
+                return
+            }
+
+            // TODO(philipp-spiess): Find out if we have a better approach to truncate very large files.
+            const endLine = Math.min(document.lineCount, 10_000)
+            const range = new vscode.Range(0, 0, endLine, 0)
+
+            files.push({
+                uri: document.uri,
+                contents: document.getText(range),
+            })
+        }
+
+        const visibleUris = this.window.visibleTextEditors.flatMap(e =>
+            e.document.uri.scheme === 'file' ? [e.document.uri] : []
+        )
+
+        // Use tabs API to get current docs instead of `vscode.workspace.textDocuments`.
+        // See related discussion: https://github.com/microsoft/vscode/issues/15178
+        // See more info about the API: https://code.visualstudio.com/api/references/vscode-api#Tab
+        const allUris: vscode.Uri[] = this.window.tabGroups.all
+            .flatMap(({ tabs }) => tabs.map(tab => (tab.input as any)?.uri))
+            .filter(Boolean)
+
+        // To define an upper-bound for the number of files to take into consideration, we consider all
+        // active editor tabs and the 5 tabs (7 when there are no split views) that are open around it
+        // (so we include 2 or 3 tabs to the left to the right).
+        //
+        // TODO(philipp-spiess): Consider files that are in the same directory or called similarly to be
+        // more relevant.
+        const uris: Map<string, vscode.Uri> = new Map()
+        const surroundingTabs = visibleUris.length <= 1 ? 3 : 2
+        for (const visibleUri of visibleUris) {
+            uris.set(visibleUri.toString(), visibleUri)
+            const index = allUris.findIndex(uri => uri.toString() === visibleUri.toString())
+
+            if (index === -1) {
+                continue
+            }
+
+            const start = Math.max(index - surroundingTabs, 0)
+            const end = Math.min(index + surroundingTabs, allUris.length - 1)
+
+            for (let j = start; j <= end; j++) {
+                uris.set(allUris[j].toString(), allUris[j])
+            }
+        }
+
+        const docs = (
+            await Promise.all(
+                [...uris.values()].map(async uri => {
+                    if (!uri) {
+                        return []
+                    }
+
+                    try {
+                        return [await this.workspace.openTextDocument(uri)]
+                    } catch (error) {
+                        console.error(error)
+                        return []
+                    }
+                })
+            )
+        ).flat()
+
+        for (const document of docs) {
+            if (document.fileName.endsWith('.git')) {
+                // The VS Code API returns fils with the .git suffix for every open file
+                continue
+            }
+            addDocument(document)
+        }
+
+        await Promise.all(
+            history
+                .lastN(10, curLang, [currentDocument.uri, ...files.map(f => f.uri)])
+                .map(async item => {
+                    try {
+                        const document = await this.workspace.openTextDocument(item.document.uri)
+                        addDocument(document)
+                    } catch (error) {
+                        console.error(error)
+                    }
+                })
+        )
+        return files
+    }
+
     public dispose(): void {
         this.history.dispose()
     }
@@ -104,124 +249,6 @@ interface JaccardMatchWithFilename extends JaccardMatch {
 interface FileContents {
     uri: vscode.Uri
     contents: string
-}
-
-/**
- * Loads all relevant files for for a given text editor. Relevant files are defined as:
- *
- * - All currently open tabs matching the same language
- * - The last 10 files that were edited matching the same language
- *
- * For every file, we will load up to 10.000 lines to avoid OOMing when working with very large
- * files.
- */
-async function getRelevantFiles(
-    currentDocument: vscode.TextDocument,
-    history: DocumentHistory
-): Promise<FileContents[]> {
-    const files: FileContents[] = []
-
-    const curLang = currentDocument.languageId
-
-    function addDocument(document: vscode.TextDocument): void {
-        // Only add files and VSCode user settings.
-        if (!['file', 'vscode-userdata'].includes(document.uri.scheme)) {
-            return
-        }
-
-        if (
-            !shouldBeUsedAsContext(
-                completionProviderConfig.getPrefetchedFlag(
-                    FeatureFlag.CodyAutocompleteContextExtendLanguagePool
-                ),
-                curLang,
-                document.languageId
-            )
-        ) {
-            return
-        }
-
-        // TODO(philipp-spiess): Find out if we have a better approach to truncate very large files.
-        const endLine = Math.min(document.lineCount, 10_000)
-        const range = new vscode.Range(0, 0, endLine, 0)
-
-        files.push({
-            uri: document.uri,
-            contents: document.getText(range),
-        })
-    }
-
-    const visibleUris = vscode.window.visibleTextEditors.flatMap(e =>
-        e.document.uri.scheme === 'file' ? [e.document.uri] : []
-    )
-
-    // Use tabs API to get current docs instead of `vscode.workspace.textDocuments`.
-    // See related discussion: https://github.com/microsoft/vscode/issues/15178
-    // See more info about the API: https://code.visualstudio.com/api/references/vscode-api#Tab
-    const allUris: vscode.Uri[] = vscode.window.tabGroups.all
-        .flatMap(({ tabs }) => tabs.map(tab => (tab.input as any)?.uri))
-        .filter(Boolean)
-
-    // To define an upper-bound for the number of files to take into consideration, we consider all
-    // active editor tabs and the 5 tabs (7 when there are no split views) that are open around it
-    // (so we include 2 or 3 tabs to the left to the right).
-    //
-    // TODO(philipp-spiess): Consider files that are in the same directory or called similarly to be
-    // more relevant.
-    const uris: Map<string, vscode.Uri> = new Map()
-    const surroundingTabs = visibleUris.length <= 1 ? 3 : 2
-    for (const visibleUri of visibleUris) {
-        uris.set(visibleUri.toString(), visibleUri)
-        const index = allUris.findIndex(uri => uri.toString() === visibleUri.toString())
-
-        if (index === -1) {
-            continue
-        }
-
-        const start = Math.max(index - surroundingTabs, 0)
-        const end = Math.min(index + surroundingTabs, allUris.length - 1)
-
-        for (let j = start; j <= end; j++) {
-            uris.set(allUris[j].toString(), allUris[j])
-        }
-    }
-
-    const docs = (
-        await Promise.all(
-            [...uris.values()].map(async uri => {
-                if (!uri) {
-                    return []
-                }
-
-                try {
-                    return [await vscode.workspace.openTextDocument(uri)]
-                } catch (error) {
-                    console.error(error)
-                    return []
-                }
-            })
-        )
-    ).flat()
-
-    for (const document of docs) {
-        if (document.fileName.endsWith('.git')) {
-            // The VS Code API returns fils with the .git suffix for every open file
-            continue
-        }
-        addDocument(document)
-    }
-
-    await Promise.all(
-        history.lastN(10, curLang, [currentDocument.uri, ...files.map(f => f.uri)]).map(async item => {
-            try {
-                const document = await vscode.workspace.openTextDocument(item.document.uri)
-                addDocument(document)
-            } catch (error) {
-                console.error(error)
-            }
-        })
-    )
-    return files
 }
 
 /**
