750104bd6d05144be96f7e43497227daa6eab8b2
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-06-24 20:44:48 +0200

    copy-paste linear-provider from openctx
    copy pasted then removed the reading of access tokens/settings since
    that won't be used in cody. Also removed a lot of the type annotations.

---
+++ vscode/src/context/openctx/linear-issues.ts
@@ -0,0 +1,196 @@
+import type { Provider } from '@openctx/client'
+import dedent from 'dedent'
+import { XMLBuilder } from 'fast-xml-parser'
+
+const xmlBuilder = new XMLBuilder({ format: true })
+
+interface Issue {
+    identifier: string
+    title: string
+    url: string
+    description: string
+    comments?: {
+        nodes: Comment[]
+    }
+}
+
+interface Comment {
+    body: string
+}
+
+const NUMBER_OF_ISSUES_TO_FETCH = 10
+
+const LinearIssuesProvider: Provider & { providerUri: string } = {
+    providerUri: 'internal-linear-issues',
+
+    meta() {
+        return { name: 'Linear Issues', mentions: {} }
+    },
+
+    async mentions({ query }) {
+        let issues: Issue[] = []
+
+        if (query) {
+            const variables = { query, first: NUMBER_OF_ISSUES_TO_FETCH }
+            const response = await linearApiRequest(issueSearchQuery, variables)
+            issues = response.data.issueSearch.nodes as Issue[]
+        } else {
+            const variables = { first: NUMBER_OF_ISSUES_TO_FETCH / 2 }
+            const response = await linearApiRequest(viewerIssuesQuery, variables)
+
+            const createdIssues = response.data.viewer.createdIssues.nodes as Issue[]
+            const assignedIssues = response.data.viewer.assignedIssues.nodes as Issue[]
+            issues = dedupeWith([...assignedIssues, ...createdIssues], 'url')
+        }
+
+        const mentions = (issues ?? []).map(issue => ({
+            title: `${issue.identifier} ${issue.title}`,
+            uri: issue.url,
+            description: issue.description,
+        }))
+
+        return mentions
+    },
+
+    async items(params) {
+        if (!params.mention) {
+            return []
+        }
+
+        const issueId = parseIssueIDFromURL(params.mention.uri)
+        if (!issueId) {
+            return []
+        }
+
+        const variables = { id: issueId }
+        const data = await linearApiRequest(issueWithCommentsQuery, variables)
+        const issue = data.data.issue as Issue
+        const comments = issue.comments?.nodes as Comment[]
+
+        const issueInfo = xmlBuilder.build({
+            title: issue.title,
+            description: issue.description || '',
+            comments: comments.map(comment => comment.body).join('\n'),
+            url: issue.url,
+        })
+        const content = dedent`
+            Here is the Linear issue. Use it to check if it helps.
+            Ignore it if it is not relevant.
+
+            ${issueInfo}
+        `
+
+        return [
+            {
+                title: issue.title,
+                url: issue.url,
+                ai: {
+                    content,
+                },
+            },
+        ]
+    },
+}
+
+export default LinearIssuesProvider
+
+async function linearApiRequest(query: string, variables: object): Promise<{ data: any }> {
+    const accessToken = 'TODO'
+    const response = await fetch('https://api.linear.app/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ query, variables }),
+    })
+
+    if (!response.ok) {
+        throw new Error(`Linear API request failed: ${response.statusText}`)
+    }
+
+    const json = (await response.json()) as { data: object }
+
+    if (!json.data) {
+        throw new Error('Linear API request failed: no data')
+    }
+
+    return json
+}
+
+function parseIssueIDFromURL(urlStr: string): string | undefined {
+    const url = new URL(urlStr)
+    if (!url.hostname.endsWith('linear.app')) {
+        return undefined
+    }
+    const match = url.pathname.match(/\/issue\/([a-zA-Z0-9_-]+)/)
+    return match ? match[1] : undefined
+}
+
+const dedupeWith = <T>(items: T[], key: keyof T | ((item: T) => string)): T[] => {
+    const seen = new Set()
+    const isKeyFunction = typeof key === 'function'
+
+    return items.reduce((result, item) => {
+        const itemKey = isKeyFunction ? key(item) : item[key]
+
+        if (!seen.has(itemKey)) {
+            seen.add(itemKey)
+            result.push(item)
+        }
+
+        return result
+    }, [] as T[])
+}
+
+const issueFragment = `
+  fragment IssueFragment on Issue {
+      identifier
+      title
+      url
+      description
+  }
+`
+const viewerIssuesQuery = `
+  query ViewerIssues($first: Int!) {
+    viewer {
+      createdIssues(first: $first, orderBy: updatedAt) {
+        nodes {
+          ...IssueFragment
+        }
+      }
+      assignedIssues(first: $first, orderBy: updatedAt) {
+        nodes {
+          ...IssueFragment
+        }
+      }
+    }
+  }
+
+  ${issueFragment}
+`
+const issueSearchQuery = `
+    query IssueSearch($query: String!, $first: Int!) {
+        issueSearch(query: $query, first: $first, orderBy: updatedAt) {
+            nodes {
+              ...IssueFragment
+            }
+        }
+    }
+
+    ${issueFragment}
+`
+const issueWithCommentsQuery = `
+  query IssueWithComment($id: String!) {
+    issue(id: $id) {
+      ...IssueFragment
+      comments {
+        nodes {
+          body
+        }
+      }
+    }
+  }
+
+  ${issueFragment}
+`
--- vscode/webviews/mentions/mentionMenu/MentionMenuItem.tsx
+++ vscode/webviews/mentions/mentionMenu/MentionMenuItem.tsx
@@ -25,6 +25,7 @@ import {
     IGNORED_FILE_WARNING_LABEL,
     LARGE_FILE_WARNING_LABEL,
 } from '../../../src/chat/context/constants'
+import LinearIssuesProvider from '../../../src/context/openctx/linear-issues'
 import RemoteFileProvider from '../../../src/context/openctx/remoteFileSearch'
 import RemoteRepositorySearch from '../../../src/context/openctx/remoteRepositorySearch'
 import WebProvider from '../../../src/context/openctx/web'
@@ -141,6 +142,7 @@ export const iconForProvider: Record<
     'https://openctx.org/npm/@openctx/provider-hello-world': SmileIcon,
     'https://openctx.org/npm/@openctx/provider-devdocs': LibraryBigIcon,
     'https://openctx.org/npm/@openctx/provider-sourcegraph-search': SourcegraphLogo,
+    [LinearIssuesProvider.providerUri]: LinearLogo,
     [RemoteRepositorySearch.providerUri]: FolderGitIcon,
     [RemoteFileProvider.providerUri]: FolderGitIcon,
     [WebProvider.providerUri]: LinkIcon,
