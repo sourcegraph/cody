d6eab1f05fc0fad631caacf0f8130359d9126513
Piotr Kukiełka <pkukielka@users.noreply.github.com>
2024-06-28 12:49:45 +0200

    Fix llm mode selector for pro users (#1847)
    Fixes https://github.com/sourcegraph/jetbrains/issues/1843.
    
    Rel to https://github.com/sourcegraph/jetbrains/issues/1839.
    
    ## Test plan
    1. Log in to a pro account
    2. Select a pro llm, e.g. gpt 4o
    3. Close the project
    4. Reopen the project

--- src/main/kotlin/com/sourcegraph/cody/chat/ui/LlmDropdown.kt
+++ src/main/kotlin/com/sourcegraph/cody/chat/ui/LlmDropdown.kt
@@ -9,6 +9,7 @@ import com.sourcegraph.cody.agent.CodyAgentService
 import com.sourcegraph.cody.agent.protocol.ChatModelsParams
 import com.sourcegraph.cody.agent.protocol.ChatModelsResponse
 import com.sourcegraph.cody.agent.protocol.ModelUsage
+import com.sourcegraph.cody.config.AccountTier
 import com.sourcegraph.cody.config.CodyAuthenticationManager
 import com.sourcegraph.cody.edit.EditCommandPrompt
 import com.sourcegraph.cody.history.HistoryService
@@ -25,8 +26,6 @@ class LlmDropdown(
     val parentDialog: EditCommandPrompt?,
     val chatModelProviderFromState: ChatModelsResponse.ChatModelProvider?,
 ) : ComboBox<ChatModelsResponse.ChatModelProvider>(MutableCollectionComboBoxModel()) {
-
-  var isCurrentUserFree = true
 
   init {
     renderer = LlmComboBoxRenderer(this)
@@ -65,6 +64,8 @@ class LlmDropdown(
 
     isEnabled = chatModelProviderFromState == null
     isVisible = !isEnterpriseAccount
+    setMaximumRowCount(15)
+
     revalidate()
   }
 
@@ -77,7 +78,7 @@ class LlmDropdown(
     if (project.isDisposed) return
     val modelProvider = anObject as? ChatModelsResponse.ChatModelProvider
     if (modelProvider != null) {
-      if (modelProvider.codyProOnly && isCurrentUserFree) {
+      if (modelProvider.codyProOnly && isCurrentUserFree()) {
         BrowserOpener.openInBrowser(project, "https://sourcegraph.com/cody/subscription")
         return
       }
@@ -87,6 +88,12 @@ class LlmDropdown(
       super.setSelectedItem(anObject)
       onSetSelectedItem(modelProvider)
     }
+  }
+
+  fun isCurrentUserFree(): Boolean {
+    return CodyAuthenticationManager.getInstance(project)
+        .getActiveAccountTier()
+        .getNow(AccountTier.DOTCOM_FREE) === AccountTier.DOTCOM_FREE
   }
 
   @RequiresEdt
