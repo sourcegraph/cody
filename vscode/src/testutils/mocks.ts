a4c65a8401a79bd5a9847dc467c25399ef3326de
Valery Bugakov <skymk1@gmail.com>
2024-07-02 19:41:00 +0800

    Autocomplete: limit the number of hot-streak lines (#4737)
    Restrict the number of lines we await during hot-streak completion generation to prevent overwhelming inference providers. Based on empirical observations, we'll initially set this limit to five. We can adjust this value later according to the A/B test results.

--- vscode/src/testutils/mocks.ts
+++ vscode/src/testutils/mocks.ts
@@ -756,12 +756,12 @@ export const vsCodeMocks = {
         fs: workspaceFs,
         getConfiguration() {
             return {
-                get(key: string) {
+                get(key: string, defaultValue: unknown = undefined) {
                     switch (key) {
                         case 'cody.debug.filter':
                             return '.*'
                         default:
-                            return undefined
+                            return defaultValue
                     }
                 },
                 update(): void {},
