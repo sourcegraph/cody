b2047fae262d4fbe269084bdc1ae6ddfe1fa77f5
Peter Guy <peter.guy@sourcegraph.com>
2024-07-02 11:59:40 -0700

    fix imports

--- lib/shared/src/sourcegraph-api/graphql/client.ts
+++ lib/shared/src/sourcegraph-api/graphql/client.ts
@@ -1,4 +1,4 @@
-import * as path from 'path'
+import * as path from 'node:path'
 import type { Response as NodeResponse } from 'node-fetch'
 import { URI } from 'vscode-uri'
 import { fetch } from '../../fetch'
