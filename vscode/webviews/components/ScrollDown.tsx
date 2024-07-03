95c9acefe1ec7acd78214d9b7d53ff7d0c170357
Vova Kulikov <vova@sourcegraph.com>
2024-07-03 17:42:30 -0300

    Fix ScrollDown button for cody web case

--- vscode/webviews/components/ScrollDown.tsx
+++ vscode/webviews/components/ScrollDown.tsx
@@ -1,46 +1,88 @@
 import { ArrowDownIcon } from 'lucide-react'
-import { type FunctionComponent, useCallback, useEffect, useState } from 'react'
+import { type FC, useCallback, useEffect, useMemo, useState } from 'react'
 import { Button } from './shadcn/ui/button'
 
 const MARGIN = 200 /* px */
+
+interface Scroller {
+    root: HTMLElement | Window
+    getObserveElement: () => Element
+    getScrollTop: () => number
+    getScrollHeight: () => number
+    getClientHeight: () => number
+}
+
+// Chat UI could be run in different modes, in VSCode the whole chat
+// is rendered within scrollable iframe and it scroll down button should
+// listen and work with window updates,In Cody Web (and possible in other
+// clients) chat might be rendered in arbitrary scollable element.
+// This Scroller API helps to observe complexity of both cases and provides
+// unified API to work with different root elements.
+function createScrollerAPI(element: HTMLElement | null | undefined): Scroller {
+    if (element) {
+        return {
+            root: element,
+            getObserveElement: () => element.children[0],
+            getScrollTop: () => element.scrollTop,
+            getScrollHeight: () => element.scrollHeight,
+            getClientHeight: () => element.getBoundingClientRect().height,
+        }
+    }
+
+    return {
+        root: window,
+        getObserveElement: () => window.document.body,
+        getScrollTop: () => window.scrollY,
+        getScrollHeight: () => window.document.body.scrollHeight,
+        getClientHeight: () => window.innerHeight,
+    }
+}
+
+interface ScrollDownProps {
+    scrollableParent?: HTMLElement | null
+    onClick?: () => void
+}
 
 /**
  * A component that displays a down arrow at the bottom of the viewport to inform the user that
  * there is more content if they scroll down.
  */
-export const ScrollDown: FunctionComponent<{ onClick?: () => void }> = ({ onClick: parentOnClick }) => {
+export const ScrollDown: FC<ScrollDownProps> = props => {
+    const { scrollableParent, onClick: parentOnClick } = props
     const [canScrollDown, setCanScrollDown] = useState(false)
+
+    const scrollerAPI = useMemo(() => createScrollerAPI(scrollableParent), [scrollableParent])
 
     useEffect(() => {
         function handleScroll() {
-            const scrollPosition = window.scrollY
-            const scrollHeight = window.document.body.scrollHeight
-            const clientHeight = window.innerHeight
+            const scrollPosition = scrollerAPI.getScrollTop()
+            const scrollHeight = scrollerAPI.getScrollHeight()
+            const clientHeight = scrollerAPI.getClientHeight()
             setCanScrollDown(scrollPosition + clientHeight < scrollHeight - MARGIN)
         }
         handleScroll()
-        window.addEventListener('scroll', handleScroll)
-        window.addEventListener('resize', handleScroll)
+        scrollerAPI.root.addEventListener('scroll', handleScroll)
+        scrollerAPI.root.addEventListener('resize', handleScroll)
 
         const resizeObserver = new ResizeObserver(() => {
             handleScroll()
         })
-        resizeObserver.observe(window.document.body)
+        resizeObserver.observe(scrollerAPI.getObserveElement())
 
         return () => {
-            window.removeEventListener('scroll', handleScroll)
-            window.removeEventListener('resize', handleScroll)
+            scrollerAPI.root.removeEventListener('scroll', handleScroll)
+            scrollerAPI.root.removeEventListener('resize', handleScroll)
             resizeObserver.disconnect()
         }
-    }, [])
+    }, [scrollerAPI])
 
     const onClick = useCallback(() => {
         setCanScrollDown(false) // immediately hide to avoid jitter
-        window.scrollTo({
-            top: window.document.body.scrollHeight,
+        scrollerAPI.root.scrollTo({
+            top: scrollerAPI.getScrollHeight(),
         })
         parentOnClick?.()
-    }, [parentOnClick])
+    }, [parentOnClick, scrollerAPI])
 
     return canScrollDown ? (
         <div className="tw-sticky tw-bottom-0 tw-w-full tw-text-center tw-py-4">
