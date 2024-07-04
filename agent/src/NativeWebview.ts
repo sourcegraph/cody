3956a0cc484aee874a66308170579ed1566505c5
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-07-04 11:36:20 +0900

    Have filled out as much of the webview API as agent shimmed create panel allows.

--- agent/src/NativeWebview.ts
+++ agent/src/NativeWebview.ts
@@ -1,9 +1,11 @@
 import uuid from 'uuid'
 import * as vscode from 'vscode'
+import type { Agent } from './agent'
+import * as vscode_shim from './vscode-shim'
+
+export type NativeWebviewCapabilities = { cspSource: string; webviewBundleServingPrefix: string }
 
 type NativeWebviewHandle = string
-
-// TODO: Add support for WebviewView
 
 /**
  * A delegate for adapting the VSCode Webview, WebviewPanel and WebviewView API
@@ -16,6 +18,12 @@ interface WebviewProtocolDelegate {
     readonly cspSource: string
 
     // WebviewPanel
+    createWebviewPanel(
+        handle: NativeWebviewHandle,
+        viewType: string,
+        title: string,
+        showOptions: { preserveFocus: boolean; viewColumn: vscode.ViewColumn }
+    ): void
     dispose(handle: NativeWebviewHandle): void
     reveal(handle: NativeWebviewHandle, viewColumn?: vscode.ViewColumn, preserveFocus?: boolean): void
     setTitle(handle: NativeWebviewHandle, title: string): void
@@ -28,6 +36,92 @@ interface WebviewProtocolDelegate {
     setHtml(handle: NativeWebviewHandle, value: string): void
     postMessage(handle: NativeWebviewHandle, message: any): Promise<boolean>
 }
+
+export function registerNativeWebviewHandlers(
+    agent: Agent,
+    capabilities: NativeWebviewCapabilities
+): void {
+    const delegate: WebviewProtocolDelegate = {
+        webviewBundleLocalPrefix: 'TODO', // TODO get the extension path
+        webviewBundleServingPrefix: capabilities.webviewBundleServingPrefix,
+        cspSource: capabilities.cspSource,
+        createWebviewPanel: (handle, viewType, title, showOptions) => {
+            agent.notify('webview/createWebviewPanel', {
+                handle,
+                viewType,
+                title,
+                showOptions,
+            })
+        },
+        dispose: handle => {
+            agent.notify('webview/dispose', {
+                handle,
+            })
+        },
+        reveal: (handle, viewColumn, preserveFocus) => {
+            agent.notify('webview/reveal', {
+                handle,
+                viewColumn: viewColumn ?? vscode.ViewColumn.Active,
+                preserveFocus: preserveFocus ?? false,
+            })
+        },
+        setTitle: (handle, title) => {
+            agent.notify('webview/setTitle', {
+                handle,
+                title,
+            })
+        },
+        setIconPath: (handle, iconPath) => {
+            agent.notify('webview/setIconPath', {
+                handle,
+                iconPathUri: iconPath?.toString(),
+            })
+        },
+        setHtml: (handle, html) => {
+            agent.notify('webview/setHtml', {
+                handle,
+                html,
+            })
+        },
+        postMessage: (handle, message) => {
+            agent.notify('webview/postMessageStringEncoded', {
+                id: handle,
+                stringEncodedMessage: message,
+            })
+            return Promise.resolve(true)
+        },
+    }
+    vscode_shim.setCreateWebviewPanel((viewType, title, showOptions, options) => {
+        const panel = new NativeWebviewPanel(
+            delegate,
+            viewType,
+            title,
+            {
+                retainContextWhenHidden: options?.retainContextWhenHidden ?? false,
+                enableFindWidget: options?.enableFindWidget ?? false,
+            },
+            {
+                enableScripts: options?.enableScripts ?? false,
+                enableCommandUris: options?.enableCommandUris ?? false,
+                localResourceRoots: [vscode.Uri.file(delegate.webviewBundleLocalPrefix)],
+            }
+        )
+        if (typeof showOptions === 'number') {
+            showOptions = {
+                viewColumn: showOptions,
+            }
+        }
+        delegate.createWebviewPanel(panel.handle, viewType, title, {
+            preserveFocus: showOptions?.preserveFocus ?? false,
+            viewColumn: showOptions?.viewColumn ?? vscode.ViewColumn.Active,
+        })
+        // TODO: What reads webPanels, and do we need to push into there?
+        // this.webPanels.add(panel)
+        return panel
+    })
+}
+
+// TODO: Add support for WebviewView
 
 class NativeWebview implements vscode.Webview {
     private readonly didReceiveMessageEmitter = new vscode.EventEmitter<vscode.Event<any>>()
@@ -86,12 +180,11 @@ class NativeWebview implements vscode.Webview {
  * models, etc. because those details are all delegated to the actual web
  * contents hosted by the client.
  */
-export class NativeWebviewPanel implements vscode.WebviewPanel {
+class NativeWebviewPanel implements vscode.WebviewPanel {
     // The identifier used to refer to the panel *and* Webview on the client
     // side. This identifier is allocated by the Agent because
     // createWebviewPanel is synchronous.
-    private handle: NativeWebviewHandle = `native-webview-panel-${uuid.v4()}`
-    private _title: string
+    public readonly handle: NativeWebviewHandle = `native-webview-panel-${uuid.v4()}`
     private _iconPath: vscode.Uri | { light: vscode.Uri; dark: vscode.Uri } | undefined
     public readonly webview: vscode.Webview
     // TODO: Implement active, visible and this event.
@@ -102,14 +195,14 @@ export class NativeWebviewPanel implements vscode.WebviewPanel {
     private readonly disposeEmitter: vscode.EventEmitter<void> = new vscode.EventEmitter()
     public readonly onDidDispose: vscode.Event<void> = this.disposeEmitter.event
 
+    // TODO: Consider passing an initial value of 'active' based on preserveFocus
     constructor(
         private readonly delegate: WebviewProtocolDelegate,
         public readonly viewType: string,
-        title: string,
+        private _title: string,
         public readonly options: vscode.WebviewPanelOptions,
         webviewOptions: vscode.WebviewOptions
     ) {
-        this._title = title
         this.webview = new NativeWebview(this.delegate, this.handle, webviewOptions)
     }
 
