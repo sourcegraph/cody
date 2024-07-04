affb68ecd582799317abd7106c3470c30c0c4c78
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-07-04 11:36:20 +0900

    Have filled out as much of the webview API as agent shimmed create panel allows.

--- agent/src/agent.ts
+++ agent/src/agent.ts
@@ -58,6 +58,7 @@ import { AgentGlobalState } from './AgentGlobalState'
 import { AgentProviders } from './AgentProviders'
 import { AgentWebviewPanel, AgentWebviewPanels } from './AgentWebviewPanel'
 import { AgentWorkspaceDocuments } from './AgentWorkspaceDocuments'
+import { registerNativeWebviewHandlers } from './NativeWebview'
 import type { PollyRequestError } from './cli/command-jsonrpc-stdio'
 import { codyPaths } from './codyPaths'
 import {
@@ -411,7 +412,12 @@ export class Agent extends MessageHandler implements ExtensionClient {
                     params.extensionActivate,
                     this
                 )
-                this.registerWebviewHandlers()
+                const webviewCapabilities = clientInfo.capabilities?.webview
+                if (webviewCapabilities instanceof Object && webviewCapabilities.type === 'native') {
+                    registerNativeWebviewHandlers(this, webviewCapabilities)
+                } else {
+                    this.registerWebviewHandlers()
+                }
 
                 this.authenticationPromise = clientInfo.extensionConfiguration
                     ? this.handleConfigChanges(clientInfo.extensionConfiguration, {
