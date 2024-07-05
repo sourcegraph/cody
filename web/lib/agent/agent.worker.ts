3f26ee7ffcdadfa6fc852480770532505c0d51db
Vova Kulikov <vova@sourcegraph.com>
2024-07-05 13:29:22 -0300

    Fix after rebasing main

--- web/lib/agent/agent.worker.ts
+++ web/lib/agent/agent.worker.ts
@@ -9,37 +9,6 @@ import {
 import { createActivation } from 'cody-ai/src/extension.web'
 import { IndexDBStorage } from './index-db-storage'
 
-declare const WorkerGlobalScope: never
-const isRunningInWebWorker =
-    typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope
-
-if (isRunningInWebWorker) {
-    // NOTE: If we need to add more hacks, or if this is janky, we should consider just setting
-    // `globalThis.window = globalThis` (see
-    // https://github.com/sourcegraph/cody/pull/4047#discussion_r1593823318).
-
-    ;(self as any).document = {
-        // HACK: @microsoft/fetch-event-source tries to call document.removeEventListener, which is
-        // not available in a worker.
-        removeEventListener: () => {},
-
-        // HACK: web-tree-sitter tries to read window.document.currentScript, which fails if this is
-        // running in a Web Worker.
-        currentScript: null,
-
-        // HACK: Vite HMR client tries to call querySelectorAll, which is not
-        // available in a web worker, without this cody demo fails in dev mode.
-        querySelectorAll: () => [],
-    }
-    ;(self as any).window = {
-        // HACK: @microsoft/fetch-event-source tries to call window.clearTimeout, which fails if this is
-        // running in a Web Worker.
-        clearTimeout: (...args: Parameters<typeof clearTimeout>) => clearTimeout(...args),
-
-        document: self.document,
-    }
-}
-
 const conn = createMessageConnection(new BrowserMessageReader(self), new BrowserMessageWriter(self))
 
 const agent = new Agent({
