12d7bda8dd329db8999c75127e2ed4c089f41d15
Olafur Geirsson <olafurpg@gmail.com>
2024-07-05 10:47:02 +0200

    Minor improvements to `cody --help`
    Fixes CODY-2777
    
    - Display `cody` in --help message
    - Show `auth` and `chat` subcommands at the top
    - Display correct version number
    - Move `cody cody-bench` to `cody internal bench`

--- agent/src/cli/command-root.ts
+++ agent/src/cli/command-root.ts
@@ -6,14 +6,15 @@ import { chatCommand } from './command-chat'
 import { jsonrpcCommand } from './command-jsonrpc-stdio'
 import { serverCommand } from './command-jsonrpc-websocket'
 
+import { version } from '../../package.json'
+
 export const rootCommand = new Command()
-    .name('cody-agent')
-    .version('0.1.0')
+    .name('cody')
+    .version(version)
     .description(
-        'Cody Agent supports running the Cody VS Code extension in headless mode and interact with it via JSON-RPC. ' +
-            'The Agent is used by editor clients like JetBrains and Neovim.'
+        'The Cody cli supports running Cody in headless mode and interacting with it via JSON-RPC. Run `cody chat -m "Hello" to get started.'
     )
-    .addCommand(new Command('api').addCommand(serverCommand).addCommand(jsonrpcCommand))
-    .addCommand(benchCommand)
-    .addCommand(chatCommand())
     .addCommand(authCommand())
+    .addCommand(chatCommand())
+    .addCommand(new Command('api').addCommand(serverCommand).addCommand(jsonrpcCommand))
+    .addCommand(new Command('internal').addCommand(benchCommand))
