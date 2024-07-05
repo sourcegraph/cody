6b5032a653bf5b3916827ad8ff0186a1d6b6988b
Beatrix <68532117+abeatrix@users.noreply.github.com>
2024-07-05 12:52:12 -0700

    Chat: update welcome message to be IDE based (#4797)

--- vscode/webviews/Chat.tsx
+++ vscode/webviews/Chat.tsx
@@ -2,7 +2,7 @@ import { clsx } from 'clsx'
 import type React from 'react'
 import { useCallback, useEffect, useMemo } from 'react'
 
-import type { AuthStatus, ChatMessage, Guardrails } from '@sourcegraph/cody-shared'
+import type { AuthStatus, ChatMessage, CodyIDE, Guardrails } from '@sourcegraph/cody-shared'
 import { Transcript, focusLastHumanMessageEditor } from './chat/Transcript'
 import type { VSCodeWrapper } from './utils/VSCodeApi'
 
@@ -180,7 +180,7 @@ export const Chat: React.FunctionComponent<React.PropsWithChildren<ChatboxProps>
                 postMessage={postMessage}
                 guardrails={guardrails}
             />
-            {transcript.length === 0 && showWelcomeMessage && <WelcomeMessage />}
+            {transcript.length === 0 && showWelcomeMessage && <WelcomeMessage IDE={userInfo.ide} />}
             <ScrollDown scrollableParent={scrollableParent} onClick={focusLastHumanMessageEditor} />
         </div>
     )
@@ -190,6 +190,7 @@ export interface UserAccountInfo {
     isDotComUser: boolean
     isCodyProUser: boolean
     user: Pick<AuthStatus, 'username' | 'displayName' | 'avatarURL'>
+    ide: CodyIDE
 }
 
 export type ApiPostMessage = (message: any) => void
