73204117f7a5c5a13505bc87609dd90acbd74969
Beyang Liu <beyang@sourcegraph.com>
2024-07-10 13:14:50 -0700

    reenable some tests

--- vscode/test/e2e/chat-context.test.ts
+++ vscode/test/e2e/chat-context.test.ts
@@ -8,7 +8,7 @@ import {
 } from './common'
 import { test } from './helpers'
 
-test.skip('chat followup context', async ({ page, sidebar }) => {
+test('chat followup context', async ({ page, sidebar }) => {
     await sidebarSignin(page, sidebar)
 
     // Open chat.
