9877bc4432efe98ae0bc1160628bef66eb8c1a66
Tom Ross <tom@umpox.com>
2024-07-15 19:40:16 +0100

    recordings

--- agent/src/autocomplete.test.ts
+++ agent/src/autocomplete.test.ts
@@ -59,9 +59,9 @@ describe('Autocomplete', () => {
             `
           [
             "    for (let i = 0; i < nums.length; i++) {
-                  for (let j = 0; j < nums.length - 1; j++) {
-                      if (nums[j] > nums[j + 1]) {
-                          [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+                  for (let j = i + 1; j < nums.length; j++) {
+                      if (nums[i] > nums[j]) {
+                          [nums[i], nums[j]] = [nums[j], nums[i]]
                       }
                   }
               }",
