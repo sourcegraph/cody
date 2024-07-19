33745a905f434cb1cc61d030611402213986c52a
Beatrix <68532117+abeatrix@users.noreply.github.com>
2024-07-19 14:43:29 -0700

    Chat UI: add custom commands list (#4948)
    After discussing with kevin, we believe removing the custom commands
    from the command list would cause discovery issuse. This PR update the
    Commands tab list Custom Commands if there is any.
    
    - Adds a `CustomCommandsList` component to the `CommandsTab` to display
    any custom commands the user has defined
    - The `CustomCommandsList` is only rendered if there are any custom
    commands to display
    
    ## Test plan
    
    <!-- Required. See
    https://docs-legacy.sourcegraph.com/dev/background-information/testing_principles.
    -->
    
    Check if your custom commands are showing up in the commands tab:
    
    
    ![image](https://github.com/user-attachments/assets/c6b34e82-4a82-4459-8893-74a3c8594d9a)

--- vscode/webviews/tabs/CommandsTab.tsx
+++ vscode/webviews/tabs/CommandsTab.tsx
@@ -1,4 +1,5 @@
 import type { CodyCommand, CodyIDE } from '@sourcegraph/cody-shared'
+import { CustomCommandsList } from '../chat/components/CustomCommandsList'
 import { DefaultCommandsList } from '../chat/components/DefaultCommandsList'
 
 interface CommandsTabProps {
@@ -9,5 +10,6 @@ interface CommandsTabProps {
 export const CommandsTab: React.FC<CommandsTabProps> = ({ commands, IDE }) => (
     <div className="tw-flex tw-flex-col tw-gap-4 tw-px-8">
         <DefaultCommandsList IDE={IDE} />
+        {commands.length && <CustomCommandsList commands={commands} IDE={IDE} />}
     </div>
 )
