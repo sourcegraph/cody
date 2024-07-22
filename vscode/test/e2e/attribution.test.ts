1488552e4598ea5d247bc8a79ff5cac5b3cbf41c
Beatrix <beatrix@sourcegraph.com>
2024-07-22 09:15:09 -0700

    update e2e tests

--- vscode/test/e2e/attribution.test.ts
+++ vscode/test/e2e/attribution.test.ts
@@ -2,7 +2,7 @@ import { expect } from '@playwright/test'
 
 import * as mockServer from '../fixtures/mock-server'
 
-import { createEmptyChatPanel, sidebarSignin } from './common'
+import { getChatInputs, getChatSidebarPanel, sidebarSignin } from './common'
 import { type DotcomUrlOverride, test as baseTest } from './helpers'
 
 const test = baseTest.extend<DotcomUrlOverride>({ dotcomUrl: mockServer.SERVER_URL })
@@ -10,7 +10,8 @@ const test = baseTest.extend<DotcomUrlOverride>({ dotcomUrl: mockServer.SERVER_U
 test('attribution search enabled in chat', async ({ page, sidebar }) => {
     await fetch(`${mockServer.SERVER_URL}/.test/attribution/enable`, { method: 'POST' })
     await sidebarSignin(page, sidebar)
-    const [chatFrame, chatInput] = await createEmptyChatPanel(page)
+    const chatFrame = getChatSidebarPanel(page)
+    const chatInput = getChatInputs(chatFrame)
     await chatInput.fill('show me a code snippet')
     await chatInput.press('Enter')
     await expect(chatFrame.getByTestId('attribution-indicator')).toBeVisible()
@@ -19,7 +20,8 @@ test('attribution search enabled in chat', async ({ page, sidebar }) => {
 test('attribution search disabled in chat', async ({ page, sidebar }) => {
     await fetch(`${mockServer.SERVER_URL}/.test/attribution/disable`, { method: 'POST' })
     await sidebarSignin(page, sidebar)
-    const [chatFrame, chatInput] = await createEmptyChatPanel(page)
+    const chatFrame = getChatSidebarPanel(page)
+    const chatInput = getChatInputs(chatFrame)
     await chatInput.fill('show me a code snippet')
     await chatInput.press('Enter')
     await expect(chatFrame.getByTestId('attribution-indicator')).toBeHidden()
