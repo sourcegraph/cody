760f7b63e9b95efe1a8d418c42a0fb52358af6c2
Tim Lucas <t@toolmantim.com>
2024-07-23 16:58:38 +1000

    Notice styles

--- vscode/webviews/Notices/index.tsx
+++ vscode/webviews/Notices/index.tsx
@@ -1,8 +1,6 @@
 import type { CodyIDE } from '@sourcegraph/cody-shared'
 import { VersionUpdatedNotice } from './VersionUpdatedNotice'
 
-import styles from './index.module.css'
-
 interface NoticesProps {
     probablyNewInstall: boolean | undefined
     IDE?: CodyIDE
@@ -13,7 +11,7 @@ export const Notices: React.FunctionComponent<NoticesProps> = ({ probablyNewInst
     probablyNewInstall !== undefined &&
     IDE !== undefined &&
     version !== undefined && (
-        <div className={styles.notices}>
+        <div className="tw-w-full tw-z-1 tw-flex tw-flex-col tw-gap-8 tw-pt-8 tw-px-[15px] empty:tw-hidden">
             <VersionUpdatedNotice probablyNewInstall={probablyNewInstall} IDE={IDE} version={version} />
         </div>
     )
