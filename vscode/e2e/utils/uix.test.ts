4eaf7d317d0dc9631435eaa0b2cb254aee2553f9
Rik Nauta <rik.nauta@sourcegraph.com>
2024-07-26 13:35:26 +0200

    migrate update-notice test

--- vscode/e2e/utils/uix.test.ts
+++ vscode/e2e/utils/uix.test.ts
@@ -1,11 +1,12 @@
 import { expect } from '@playwright/test'
 import { fixture as test, uix } from './vscody'
 
-test.describe('UIX', () => {
+test.skip('Sidebar selectors', () => {
+    //TODO: We don't use sidebars like this anymore so it's unclear if this helper still will bring value
     test.use({
         templateWorkspaceDir: 'test/fixtures/workspace',
     })
-    test('VSCode Sidebar', async ({ page, vscodeUI, executeCommand, workspaceDir }) => {
+    test('It works', async ({ page, vscodeUI, executeCommand, workspaceDir }) => {
         await uix.vscode.startSession({ page, vscodeUI, executeCommand, workspaceDir })
         const sidebar = uix.vscode.Sidebar.get({ page })
 
@@ -18,6 +19,34 @@ test.describe('UIX', () => {
         expect(await sidebar.activeView).toBe(uix.vscode.Sidebar.CODY_VIEW_ID)
     })
 })
+
+test.describe('Webview Selector', () => {
+    test.use({
+        templateWorkspaceDir: 'test/fixtures/workspace',
+    })
+
+    test('It can handle multiple webviews', async ({ page, vscodeUI, executeCommand, workspaceDir }) => {
+        await uix.cody.preAuthenticate({ workspaceDir })
+        await uix.vscode.startSession({ page, vscodeUI, executeCommand, workspaceDir })
+        await uix.cody.waitForStartup({ page })
+
+        await executeCommand('workbench.view.extension.cody')
+        const [sidebarChat] = await uix.cody.WebView.all({ page }, { atLeast: 1 })
+
+        await executeCommand('cody.chat.newEditorPanel')
+        const allWebviews = await uix.cody.WebView.all({ page }, { atLeast: 2 })
+
+        const [editorChat] = await uix.cody.WebView.all(
+            { page },
+            { atLeast: 1, ignoring: [sidebarChat] }
+        )
+
+        expect(allWebviews).toHaveLength(2)
+        expect(allWebviews).toContainEqual(sidebarChat)
+        expect(allWebviews).toContainEqual(editorChat)
+    })
+})
+
 test.describe('Workspace', () => {
     test.use({
         templateWorkspaceDir: 'test/fixtures/workspace',
