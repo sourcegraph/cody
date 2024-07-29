b0a0f582817b152d344d15164556c9cc98aae08e
Tom Ross <tom@umpox.com>
2024-07-29 16:58:37 +0200

    reliability improvemnts

--- vscode/src/edit/prompt/smart-apply.ts
+++ vscode/src/edit/prompt/smart-apply.ts
@@ -21,6 +21,15 @@ const SMART_APPLY_TOPICS = {
     ORIGINAL: ps`ORIGINAL`,
 } as const
 
+const RESPONSE_PREFIX = ps`<${SMART_APPLY_TOPICS.ORIGINAL}>`
+const SHARED_PARAMETERS = {
+    stopSequences: [`</${SMART_APPLY_TOPICS.ORIGINAL}>`],
+    assistantText: RESPONSE_PREFIX,
+    assistantPrefix: RESPONSE_PREFIX,
+}
+
+const SMART_APPLY_PREFIX = `<${SMART_APPLY_TOPICS.ORIGINAL}>`
+
 export const SMART_APPLY_PROMPT = {
     system: psDedent`
         - You are an AI programming assistant who is an expert in determiing the best way to apply a change to a codebase.
@@ -28,6 +37,8 @@ export const SMART_APPLY_PROMPT = {
         - You will be provided with the contents of the current active file, enclosed in <${SMART_APPLY_TOPICS.FILE_CONTENTS}></${SMART_APPLY_TOPICS.FILE_CONTENTS}> XML tags.
         - You will be provided with an incoming change to a file, enclosed in <${SMART_APPLY_TOPICS.INCOMING}></${SMART_APPLY_TOPICS.INCOMING}> XML tags.
         - You should respond with the original code that should be updated, enclosed in <${SMART_APPLY_TOPICS.ORIGINAL}></${SMART_APPLY_TOPICS.ORIGINAL}> XML tags.
+        - If this code should not replace any existing code, you should provide an empty <${SMART_APPLY_TOPICS.ORIGINAL}></${SMART_APPLY_TOPICS.ORIGINAL}> XML tag, at the position where the new code should be inserted.\
+        - We will paste the incoming change to replace the original code you produce, so ensure that the original code contains all code that should be replaced.
         - Do not provide any additional commentary about the changes you made. Only respond with the generated code.`,
     instruction: psDedent`
         We are in the file: {filePath}
@@ -46,7 +57,7 @@ export const getPrompt = async (
     replacement: string,
     document: vscode.TextDocument,
     model: EditModel
-): Promise<Message[]> => {
+): Promise<{ messages: Message[]; prefix: string }> => {
     const documentRange = new vscode.Range(0, 0, document.lineCount - 1, 0)
     const documentText = PromptString.fromDocumentText(document, documentRange)
     const tokenCount = TokenCounter.countPromptString(documentText)
@@ -67,9 +78,11 @@ export const getPrompt = async (
         .replaceAll('{filePath}', PromptString.fromDisplayPath(document.uri))
 
     const transcript: ChatMessage[] = [{ speaker: 'human', text: instruction }]
+    transcript.push({ speaker: 'assistant', text: SHARED_PARAMETERS.assistantText })
+
     promptBuilder.tryAddMessages(transcript.reverse())
 
-    return promptBuilder.build()
+    return { prefix: SMART_APPLY_PREFIX, messages: promptBuilder.build() }
 }
 
 export async function promptModelForOriginalCode(
@@ -92,7 +105,7 @@ export async function promptModelForOriginalCode(
     })
 
     const abortController = new AbortController()
-    const messages = await getPrompt(replacement, document, model)
+    const { prefix, messages } = await getPrompt(replacement, document, model)
     const stream = client.chat(
         messages,
         {
@@ -107,9 +120,9 @@ export async function promptModelForOriginalCode(
     for await (const message of stream) {
         switch (message.type) {
             case 'change': {
-                // if (textConsumed === 0 && responsePrefix) {
-                //     void multiplexer.publish(responsePrefix)
-                // }
+                if (textConsumed === 0 && prefix) {
+                    void multiplexer.publish(prefix)
+                }
                 const text = message.text.slice(textConsumed)
                 textConsumed += text.length
                 void multiplexer.publish(text)
