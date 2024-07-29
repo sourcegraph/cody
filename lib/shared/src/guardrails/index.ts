4e10354145037e55b8610f263f1b33f99ada0852
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-07-29 09:04:10 +0200

    guardrails: remove unused function summariseAttribution
    This was code from the original PoC of guardrails which is no longer
    used.
    
    Test Plan: CI

--- lib/shared/src/guardrails/index.ts
+++ lib/shared/src/guardrails/index.ts
@@ -1,6 +1,3 @@
-import { pluralize } from '../common'
-import { isError } from '../utils'
-
 export interface Attribution {
     limitHit: boolean
     repositories: RepositoryAttribution[]
@@ -62,27 +59,4 @@ class AttributionSearchSync {
             this.reject = reject
         })
     }
-}
-
-export function summariseAttribution(attribution: Attribution | Error): string {
-    if (isError(attribution)) {
-        return `guardrails attribution search failed: ${attribution.message}`
-    }
-
-    const repos = attribution.repositories
-    const count = repos.length
-    if (count === 0) {
-        return 'no matching repositories found'
-    }
-
-    const summary = repos.slice(0, count < 5 ? count : 5).map(repo => repo.name)
-    if (count > 5) {
-        summary.push('...')
-    }
-
-    return `found ${count}${attribution.limitHit ? '+' : ''} matching ${pluralize(
-        'repository',
-        count,
-        'repositories'
-    )} ${summary.join(', ')}`
 }
--- lib/shared/src/index.ts
+++ lib/shared/src/index.ts
@@ -148,7 +148,7 @@ export {
     FeatureFlagProvider,
     featureFlagProvider,
 } from './experimentation/FeatureFlagProvider'
-export { GuardrailsPost, summariseAttribution } from './guardrails'
+export { GuardrailsPost } from './guardrails'
 export type { Attribution, Guardrails } from './guardrails'
 export { SourcegraphGuardrailsClient, type GuardrailsClientConfig } from './guardrails/client'
 export {
