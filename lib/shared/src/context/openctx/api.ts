9421b34b3ca3b1bf5f456052bf67d154b391d8cf
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-07-31 15:48:05 +0200

    openctx: call disposable when replacing openctx controller (#5071)
    We call the setOpenCtx everytime the config or authed account changes.
    Previously we correctly would replace the OpenCtx client when that
    happened, but we did not call dispose on the old client or the
    observable disposables created by vscode-lib.
    
    Test Plan: at mentioned remote repos and got s2 repos. Switched to
    dotcom and got dotcom repos instead. So switching still works.

--- lib/shared/src/context/openctx/api.ts
+++ lib/shared/src/context/openctx/api.ts
@@ -1,19 +1,27 @@
 import type { Client } from '@openctx/client'
 import type * as vscode from 'vscode'
 
-type OpenCtxClient = Pick<Client<vscode.Range>, 'meta' | 'mentions' | 'items'>
+type OpenCtxClient = Pick<Client<vscode.Range>, 'meta' | 'mentions' | 'items' | 'dispose'>
 
-class OpenCtx {
-    constructor(public client: OpenCtxClient | undefined) {}
+interface OpenCtx {
+    client?: OpenCtxClient
+    disposable?: vscode.Disposable
 }
 
-export const openCtx = new OpenCtx(undefined)
+export const openCtx: OpenCtx = {}
 
 /**
- * Set the handle to the OpenCtx client.
+ * Set the handle to the OpenCtx. If there is an existing handle it will be
+ * disposed and replaced.
  */
-export function setOpenCtxClient(client: OpenCtxClient): void {
+export function setOpenCtx({ client, disposable }: OpenCtx): void {
+    const { client: oldClient, disposable: oldDisposable } = openCtx
+
     openCtx.client = client
+    openCtx.disposable = disposable
+
+    oldClient?.dispose()
+    oldDisposable?.dispose()
 }
 
 export const REMOTE_REPOSITORY_PROVIDER_URI = 'internal-remote-repository-search'
