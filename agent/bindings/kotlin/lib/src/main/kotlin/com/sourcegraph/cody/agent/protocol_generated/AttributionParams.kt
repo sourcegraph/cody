21a5b3a01113c28ebce9f1d32fce892f2b768fe2
Ólafur Páll Geirsson <olafurpg@gmail.com>
2024-08-02 08:41:56 +0200

    Agent: stop generating Kotlin bindings for the webview protocol (#5088)
    Previously, the Kotlin bindings generated classes for
    `webview/postMessage`, `weview/receiveMessage` and other webview-related
    JSON-RPC endpoints. Now that our IDE clients have moved to using
    webview-powered
    chat, there's no need to generate bindings for these classes because the
    IDE clients only need to proxy the raw JSON payloads instead of
    interaacting with the low-level WebView protocol.
    
    Removing these types from the bindings means Cody developers no longer
    need to run `pnpm generate-agent-kotlin-bindings` when they update the
    webview protocol.
    
    ## Test plan
    Green CI.
    <!-- Required. See
    https://docs-legacy.sourcegraph.com/dev/background-information/testing_principles.
    -->
    
    ## Changelog
    
    <!-- OPTIONAL; info at
    https://www.notion.so/sourcegraph/Writing-a-changelog-entry-dd997f411d524caabf0d8d38a24a878c
    -->

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/AttributionParams.kt
+++
@@ -1,8 +0,0 @@
-@file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
-package com.sourcegraph.cody.agent.protocol_generated;
-
-data class AttributionParams(
-  val repositoryNames: List<String>,
-  val limitHit: Boolean,
-)
-
