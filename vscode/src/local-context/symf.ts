a0061f5964f559153cf9329dc5c4a84c5f0c3a5f
Beyang Liu <beyang@sourcegraph.com>
2024-08-07 16:48:44 -0700

    log symf invocations

--- vscode/src/local-context/symf.ts
+++ vscode/src/local-context/symf.ts
@@ -303,27 +303,25 @@ export class SymfRunner implements IndexedKeywordContextFetcher, vscode.Disposab
     ): Promise<string> {
         const { indexDir } = this.getIndexDir(scopeDir)
         const symfPath = await this.mustSymfPath()
+        const symfArgs = [
+            '--index-root',
+            indexDir.fsPath,
+            'query',
+            '--scopes',
+            scopeDir.fsPath,
+            '--fmt',
+            'json',
+            '--boosted-keywords',
+            `"${userQuery}"`,
+            `${keywordQuery}`,
+        ]
+        logDebug('SymfRunner', 'running symf', symfPath, symfArgs.join(' '))
         try {
-            const { stdout } = await execFile(
-                symfPath,
-                [
-                    '--index-root',
-                    indexDir.fsPath,
-                    'query',
-                    '--scopes',
-                    scopeDir.fsPath,
-                    '--fmt',
-                    'json',
-                    '--boosted-keywords',
-                    `"${userQuery}"`,
-                    `${keywordQuery}`,
-                ],
-                {
-                    env: { HOME: process.env.HOME },
-                    maxBuffer: 1024 * 1024 * 1024,
-                    timeout: 1000 * 30, // timeout in 30 seconds
-                }
-            )
+            const { stdout } = await execFile(symfPath, symfArgs, {
+                env: { HOME: process.env.HOME },
+                maxBuffer: 1024 * 1024 * 1024,
+                timeout: 1000 * 30, // timeout in 30 seconds
+            })
             return stdout
         } catch (error) {
             throw toSymfError(error)
