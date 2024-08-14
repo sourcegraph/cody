a0b5cd58d16ab85618adfcf3645ffdfad1e85bbb
Tom Ross <tom@umpox.com>
2024-08-14 15:11:34 +0100

    e2e test

--- vscode/test/e2e/code-actions.test.ts
+++ vscode/test/e2e/code-actions.test.ts
@@ -109,7 +109,7 @@ test.extend<ExpectedV2Events>({
     await erredText.hover()
     await quickFix(page, 'Fix')
 
-    const acceptBtn = page.getByRole('button', { name: 'Accept' })
+    const acceptBtn = page.getByRole('button', { name: 'Accept', exact: true })
     await expect(acceptBtn).toBeVisible()
 
     // Accept the fix
