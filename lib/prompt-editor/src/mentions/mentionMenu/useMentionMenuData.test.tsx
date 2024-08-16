sourcegraph/cody

commit 137259e3fd9beae41671c3d42d98998917174037

don't show file 2x in mention menu (#5057)
In the chat @-mention menu, often the current file was shown twice: once
at the top because it's part of initial context, and once below because
it's an open tab. This caused a glitch where it would be highlighted in
2 places in the mention menu and would prevent navigating down with the
arrow key. Now it's only shown once.


![image](https://github.com/user-attachments/assets/8a276c5c-5228-441f-8f74-f0c23d16f3e8)


Fix
https://linear.app/sourcegraph/issue/CODY-2749/mention-menu-glitches-when-you-try-to-navigate-with-the-updown-arrows

## Test plan

Open mention menu and confirm only shown once.

## Changelog

- Fixes a bug where the same file was sometimes shown twice in the
@-mention menu, causing a glitch in arrow-key navigation.

lib/prompt-editor/src/mentions/mentionMenu/useMentionMenuData.ts
@@ -70,6 +70,15 @@ export function useMentionMenuData(
                       ),
             items: results
                 ?.slice(0, limit)
+                .filter(
+                    // If an item is in the initial context, don't show it twice.
+                    item =>
+                        !clientState.initialContext.some(
+                            initialItem =>
+                                initialItem.uri.toString() === item.uri.toString() &&
+                                initialItem.type === item.type
+                        )
+                )
                 .map(item => prepareContextItemForMentionMenu(item, remainingTokenBudget)),
             initialContextItems: clientState.initialContext.filter(item =>
                 queryLower

lib/prompt-editor/src/mentions/mentionMenu/useMentionMenuData.test.tsx
@@ -1,0 +1,65 @@
+import {
+    type ContextItem,
+    ContextItemSource,
+    type ContextMentionProviderMetadata,
+} from '@sourcegraph/cody-shared'
+import { renderHook } from '@testing-library/react'
+import { describe, expect, test, vi } from 'vitest'
+import { URI } from 'vscode-uri'
+import { useClientState } from '../../clientState'
+import {
+    useChatContextItems,
+    useChatContextMentionProviders,
+} from '../../plugins/atMentions/chatContextClient'
+import { useMentionMenuData } from './useMentionMenuData'
+
+vi.mock('../../plugins/atMentions/chatContextClient')
+vi.mock('../../clientState')
+
+describe('useMentionMenuData', () => {
+    test('items do not include values from initialContextItems', async () => {
+        const mockContextItems: ContextItem[] = [
+            {
+                uri: URI.parse('file1.ts'),
+                type: 'file',
+                isTooLarge: undefined,
+                source: ContextItemSource.User,
+            },
+            {
+                uri: URI.parse('file2.ts'),
+                type: 'file',
+                isTooLarge: undefined,
+                source: ContextItemSource.User,
+            },
+            {
+                uri: URI.parse('file3.ts'),
+                type: 'file',
+                isTooLarge: undefined,
+                source: ContextItemSource.User,
+            },
+        ]
+        const mockProviders: ContextMentionProviderMetadata[] = [
+            { title: 'My Provider', id: 'my-provider', queryLabel: '', emptyLabel: '' },
+        ]
+
+        vi.mocked(useChatContextItems).mockReturnValue([
+            mockContextItems[0],
+            mockContextItems[1],
+            mockContextItems[2],
+        ])
+        vi.mocked(useChatContextMentionProviders).mockReturnValue({
+            providers: mockProviders,
+            reload: () => {},
+        })
+        vi.mocked(useClientState).mockReturnValue({
+            initialContext: [mockContextItems[0]],
+        })
+
+        const { result } = renderHook(() =>
+            useMentionMenuData({ query: '', parentItem: null }, { remainingTokenBudget: 100, limit: 10 })
+        )
+        expect(result.current.providers).toEqual(mockProviders)
+        expect(result.current.initialContextItems).toEqual([mockContextItems[0]])
+        expect(result.current.items).toEqual<ContextItem[]>([mockContextItems[1], mockContextItems[2]])
+    })
+})

commit 3c415d00902dc9f5c6547c568f1bbad035a1bbcf

fix a bug where the current file would not show up when searching for @-file mentions
This was unintentionally caused by https://github.com/sourcegraph/cody/pull/5057. The intent was to not show 2 mention items `Current File: foo.ts` and `foo.ts`, but it meant that if you typed `@foo` then neither would show up.

Also simplify how initial context is passed to MentionMenu. Just include it in `items` so we don't spread responsibility for displaying and filtering it across multiple components.

lib/prompt-editor/src/mentions/mentionMenu/MentionMenu.tsx
@@ -120,18 +120,6 @@ export const MentionMenu: FunctionComponent<
         [data.providers, params.query, setEditorQuery, updateMentionMenuParams, mentionQuery]
     )
 
-    const onInitialContextItemSelect = useCallback(
-        (value: string): void => {
-            const item = data.initialContextItems?.find(item => commandRowValue(item) === value)
-            if (!item) {
-                throw new Error(`No item found with value ${value}`)
-            }
-
-            selectOptionAndCleanUp(createMentionMenuOption(item))
-        },
-        [data.initialContextItems, selectOptionAndCleanUp]
-    )
-
     const onCommandSelect = useCallback(
         (commandSelected: string): void => {
             const item = data.items?.find(item => commandRowValue(item) === commandSelected)
@@ -198,18 +186,14 @@ export const MentionMenu: FunctionComponent<
     // `value` in state, but when the options change, our state `value` may refer to a row that no
     // longer exists in the list. In that case, we want the first row to be selected.
     const firstProviderRow = data.providers.at(0)
-    const firstInitialContextItemRow = data.initialContextItems?.at(0)
     const firstItemRow = data.items?.at(0)
-    const firstRow = params.parentItem
-        ? firstItemRow
-        : firstProviderRow ?? firstInitialContextItemRow ?? firstItemRow
+    const firstRow = params.parentItem ? firstItemRow : firstProviderRow ?? firstItemRow
 
     const valueRow = useMemo(
         () =>
             data.providers.find(provider => commandRowValue(provider) === value) ??
-            data.initialContextItems?.find(item => commandRowValue(item) === value) ??
             data.items?.find(item => commandRowValue(item) === value),
-        [data.providers, data.items, data.initialContextItems, value]
+        [data.providers, data.items, value]
     )
     const effectiveValueRow = valueRow ?? firstRow
 
@@ -253,27 +237,6 @@ export const MentionMenu: FunctionComponent<
                 {providers.length > 0 && (
                     <CommandGroup className={COMMAND_GROUP_CLASS_NAME}>{providers}</CommandGroup>
                 )}
-
-                {!params.parentItem &&
-                    data.initialContextItems &&
-                    data.initialContextItems.length > 0 && (
-                        <CommandGroup className={COMMAND_GROUP_CLASS_NAME}>
-                            {data.initialContextItems.map(item => (
-                                <CommandItem
-                                    key={commandRowValue(item)}
-                                    value={commandRowValue(item)}
-                                    onSelect={onInitialContextItemSelect}
-                                    className={clsx(
-                                        styles.item,
-                                        styles.contextItem,
-                                        COMMAND_ROW_CLASS_NAME
-                                    )}
-                                >
-                                    <MentionMenuContextItemContent query={mentionQuery} item={item} />
-                                </CommandItem>
-                            ))}
-                        </CommandGroup>
-                    )}
 
                 {(heading || (data.items && data.items.length > 0)) && (
                     <CommandGroup heading={heading} className={COMMAND_GROUP_CLASS_NAME}>

lib/prompt-editor/src/mentions/mentionMenu/useMentionMenuData.ts
@@ -1,10 +1,13 @@
 import type { ContextItem, ContextMentionProviderMetadata } from '@sourcegraph/cody-shared'
-import { REMOTE_FILE_PROVIDER_URI, REMOTE_REPOSITORY_PROVIDER_URI } from '@sourcegraph/cody-shared'
+import {
+    ContextItemSource,
+    REMOTE_FILE_PROVIDER_URI,
+    REMOTE_REPOSITORY_PROVIDER_URI,
+} from '@sourcegraph/cody-shared'
 import { debounce } from 'lodash'
 import { useCallback, useContext, useMemo, useState } from 'react'
 import { useClientState } from '../../clientState'
 import { ChatMentionContext, useChatContextItems } from '../../plugins/atMentions/useChatContextItems'
-import { prepareContextItemForMentionMenu } from '../../plugins/atMentions/util'
 import { useAsyncGenerator } from '../../useAsyncGenerator'
 import { useExtensionAPI } from '../../useExtensionAPI'
 
@@ -61,8 +64,7 @@ export function useMentionMenuParams(): {
 
 export interface MentionMenuData {
     providers: ContextMentionProviderMetadata[]
-    initialContextItems?: (ContextItem & { icon?: string })[]
-    items: ContextItem[] | undefined
+    items: (ContextItem & { icon?: string })[] | undefined
 
     /**
      * If an error is present, the client should display the error *and* still display the other
@@ -91,49 +93,69 @@ export function useMentionMenuData(
     )
     const clientState = useClientState()
 
+    const isInProvider = !!params.parentItem
+
+    // Initial context items aren't filtered when we receive them, so we need to filter them here.
+    const filteredInitialContextItems = isInProvider
+        ? []
+        : clientState.initialContext.filter(item =>
+              queryLower
+                  ? item.title?.toLowerCase().includes(queryLower) ||
+                    item.uri.toString().toLowerCase().includes(queryLower) ||
+                    item.description?.toString().toLowerCase().includes(queryLower)
+                  : true
+          )
+
     return useMemo(
-        () => ({
-            providers:
-                params.parentItem || queryLower === null || !providers
-                    ? []
-                    : providers.filter(
-                          provider =>
-                              provider.id.toLowerCase().includes(queryLower) ||
-                              provider.title?.toLowerCase().includes(queryLower) ||
-                              provider.id.toLowerCase().replaceAll(' ', '').includes(queryLower) ||
-                              provider.title?.toLowerCase().replaceAll(' ', '').includes(queryLower)
-                      ),
-            items: contextItems
-                ?.slice(0, limit)
-                .filter(
-                    // If an item is in the initial context, don't show it twice.
-                    item =>
-                        !clientState.initialContext.some(
-                            initialItem =>
-                                initialItem.uri.toString() === item.uri.toString() &&
-                                initialItem.type === item.type
+        () =>
+            ({
+                providers:
+                    isInProvider || queryLower === null || !providers
+                        ? []
+                        : providers.filter(
+                              provider =>
+                                  provider.id.toLowerCase().includes(queryLower) ||
+                                  provider.title?.toLowerCase().includes(queryLower) ||
+                                  provider.id.toLowerCase().replaceAll(' ', '').includes(queryLower) ||
+                                  provider.title?.toLowerCase().replaceAll(' ', '').includes(queryLower)
+                          ),
+                items: [
+                    ...filteredInitialContextItems,
+                    ...(contextItems
+                        ?.filter(
+                            // If an item is shown as initial context, don't show it twice.
+                            item =>
+                                !filteredInitialContextItems.some(
+                                    initialItem =>
+                                        initialItem.uri.toString() === item.uri.toString() &&
+                                        initialItem.type === item.type
+                                )
                         )
-                )
-                .map(item => prepareContextItemForMentionMenu(item, remainingTokenBudget)),
-            initialContextItems: clientState.initialContext.filter(item =>
-                queryLower
-                    ? item.title?.toLowerCase().includes(queryLower) ||
-                      item.uri.toString().toLowerCase().includes(queryLower) ||
-                      item.description?.toString().toLowerCase().includes(queryLower)
-                    : true
-            ),
-            error: (contextItemsError ?? providersError)?.message,
-        }),
+                        .slice(0, limit)
+                        .map(item => prepareUserContextItem(item, remainingTokenBudget)) ?? []),
+                ],
+                error: (contextItemsError ?? providersError)?.message,
+            }) satisfies MentionMenuData,
         [
-            params.parentItem,
+            isInProvider,
             providers,
             providersError,
             queryLower,
             contextItems,
             contextItemsError,
+            filteredInitialContextItems,
             limit,
             remainingTokenBudget,
-            clientState,
         ]
     )
+}
+
+function prepareUserContextItem(item: ContextItem, remainingTokenBudget: number): ContextItem {
+    return {
+        ...item,
+        isTooLarge: item.size !== undefined ? item.size > remainingTokenBudget : item.isTooLarge,
+
+        // All @-mentions should have a source of `User`.
+        source: ContextItemSource.User,
+    }
 }

lib/prompt-editor/src/plugins/atMentions/util.ts
@@ -1,4 +1,4 @@
-import { type ContextItem, ContextItemSource } from '@sourcegraph/cody-shared'
+import type { ContextItem } from '@sourcegraph/cody-shared'
 
 export function contextItemID(item: ContextItem): string {
     return JSON.stringify([
@@ -11,18 +11,4 @@ export function contextItemID(item: ContextItem): string {
             ? `${item.range.start.line}:${item.range.start.character}-${item.range.end.line}:${item.range.end.character}`
             : '',
     ])
-}
-
-export function prepareContextItemForMentionMenu(
-    item: ContextItem,
-    remainingTokenBudget: number
-): ContextItem {
-    return {
-        ...item,
-
-        isTooLarge: item.size !== undefined ? item.size > remainingTokenBudget : item.isTooLarge,
-
-        // All @-mentions should have a source of `User`.
-        source: ContextItemSource.User,
-    }
 }

vscode/webviews/promptEditor/MentionMenu.story.tsx
@@ -56,13 +56,11 @@ function toParams(query: string, parentItem?: ContextMentionProviderMetadata): M
 
 function toData(
     items: ContextItem[] | undefined,
-    providers: ContextMentionProviderMetadata[] = [],
-    initialContextItems: MentionMenuData['initialContextItems'] = []
+    providers: ContextMentionProviderMetadata[] = []
 ): MentionMenuData {
     return {
         items,
         providers,
-        initialContextItems,
     }
 }
 
@@ -71,6 +69,26 @@ export const Default: StoryObj<typeof MentionMenu> = {
         params: toParams(''),
         data: toData(
             [
+                {
+                    type: 'tree',
+                    isWorkspaceRoot: true,
+                    name: 'my-repo',
+                    description: 'my-repo',
+                    title: 'Current Repository',
+                    source: ContextItemSource.Initial,
+                    content: null,
+                    uri: URI.file('a/b'),
+                    icon: 'folder',
+                },
+                {
+                    uri: URI.file('a/b/initial.go'),
+                    type: 'file',
+                    description: 'initial.go:8-13',
+                    title: 'Current Selection',
+                    source: ContextItemSource.Initial,
+                    range: { start: { line: 7, character: 5 }, end: { line: 12, character: 9 } },
+                    icon: 'list-selection',
+                },
                 {
                     uri: URI.file('a/b/x.go'),
                     type: 'file',
@@ -100,29 +118,7 @@ export const Default: StoryObj<typeof MentionMenu> = {
                     uri: URI.file(`/${'sub-dir/'.repeat(50)}/}/src/LoginDialog.tsx`),
                 },
             ],
-            [FILE_CONTEXT_MENTION_PROVIDER, SYMBOL_CONTEXT_MENTION_PROVIDER],
-            [
-                {
-                    type: 'tree',
-                    isWorkspaceRoot: true,
-                    name: 'my-repo',
-                    description: 'my-repo',
-                    title: 'Current Repository',
-                    source: ContextItemSource.Initial,
-                    content: null,
-                    uri: URI.file('a/b'),
-                    icon: 'folder',
-                },
-                {
-                    uri: URI.file('a/b/initial.go'),
-                    type: 'file',
-                    description: 'initial.go:8-13',
-                    title: 'Current Selection',
-                    source: ContextItemSource.Initial,
-                    range: { start: { line: 7, character: 5 }, end: { line: 12, character: 9 } },
-                    icon: 'list-selection',
-                },
-            ]
+            [FILE_CONTEXT_MENTION_PROVIDER, SYMBOL_CONTEXT_MENTION_PROVIDER]
         ),
     },
 }

lib/prompt-editor/src/mentions/mentionMenu/useMentionMenuData.test.tsx
@@ -18,52 +18,79 @@ vi.mock('../../useExtensionAPI')
 vi.mock('../../clientState')
 
 describe('useMentionMenuData', () => {
-    test('items do not include values from initialContextItems', async () => {
-        const mockContextItems: ContextItem[] = [
-            {
+    describe('initial context deduping', () => {
+        test('items does not duplicate items from initialContextItems', async () => {
+            const file1: ContextItem = {
                 uri: URI.parse('file1.ts'),
                 type: 'file',
                 isTooLarge: undefined,
                 source: ContextItemSource.User,
-            },
-            {
-                uri: URI.parse('file2.ts'),
-                type: 'file',
-                isTooLarge: undefined,
-                source: ContextItemSource.User,
-            },
-            {
-                uri: URI.parse('file3.ts'),
-                type: 'file',
-                isTooLarge: undefined,
-                source: ContextItemSource.User,
-            },
-        ]
-        const mockProviders: ContextMentionProviderMetadata[] = [
-            { title: 'My Provider', id: 'my-provider', queryLabel: '', emptyLabel: '' },
-        ]
+            }
+            const mockContextItems: ContextItem[] = [
+                file1,
+                {
+                    uri: URI.parse('file2.ts'),
+                    type: 'file',
+                    isTooLarge: undefined,
+                    source: ContextItemSource.User,
+                },
+                {
+                    uri: URI.parse('file3.ts'),
+                    type: 'file',
+                    isTooLarge: undefined,
+                    source: ContextItemSource.User,
+                },
+            ]
+            const mockProviders: ContextMentionProviderMetadata[] = [
+                { title: 'My Provider', id: 'my-provider', queryLabel: '', emptyLabel: '' },
+            ]
 
-        vi.mocked(useChatContextItems).mockReturnValue({
-            done: false,
-            error: null,
-            value: [mockContextItems[0], mockContextItems[1], mockContextItems[2]],
-        })
-        vi.mocked(useExtensionAPI).mockReturnValue({
-            ...MOCK_API,
-            mentionProviders: () => asyncGeneratorWithValues(mockProviders),
-        })
-        vi.mocked(useClientState).mockReturnValue({
-            initialContext: [mockContextItems[0]],
-        })
+            vi.mocked(useChatContextItems).mockReturnValue({
+                done: false,
+                error: null,
+                value: [file1, mockContextItems[1], mockContextItems[2]],
+            })
+            vi.mocked(useExtensionAPI).mockReturnValue({
+                ...MOCK_API,
+                mentionProviders: () => asyncGeneratorWithValues(mockProviders),
+            })
+            const file1FromInitialContext: ContextItem = {
+                ...mockContextItems[0],
+                source: ContextItemSource.Initial,
+            }
+            vi.mocked(useClientState).mockReturnValue({
+                initialContext: [file1FromInitialContext],
+            })
 
-        const { result } = renderHook(() =>
-            useMentionMenuData({ query: '', parentItem: null }, { remainingTokenBudget: 100, limit: 10 })
-        )
-        await waitForAsyncGeneratorInTest()
-        expect(result.current).toEqual<typeof result.current>({
-            providers: mockProviders,
-            initialContextItems: [mockContextItems[0]],
-            items: [mockContextItems[1], mockContextItems[2]],
+            const { result } = renderHook(() =>
+                useMentionMenuData(
+                    { query: '', parentItem: null },
+                    { remainingTokenBudget: 100, limit: 10 }
+                )
+            )
+            await waitForAsyncGeneratorInTest()
+            expect(result.current).toEqual<typeof result.current>({
+                providers: mockProviders,
+                items: [file1FromInitialContext, mockContextItems[1], mockContextItems[2]],
+            })
+
+            // When there's a query that matches the initial context, it should be included.
+            vi.mocked(useChatContextItems).mockReturnValue({
+                done: false,
+                error: null,
+                value: [file1],
+            })
+            const { result: result2 } = renderHook(() =>
+                useMentionMenuData(
+                    { query: 'file1', parentItem: null },
+                    { remainingTokenBudget: 100, limit: 10 }
+                )
+            )
+            await waitForAsyncGeneratorInTest()
+            expect(result2.current).toEqual<typeof result2.current>({
+                providers: [],
+                items: [file1FromInitialContext],
+            })
         })
     })
 
@@ -91,9 +118,8 @@ describe('useMentionMenuData', () => {
         await waitForAsyncGeneratorInTest()
         await waitForAsyncGeneratorInTest() // reduce flakiness
         expect(result.current).toEqual<typeof result.current>({
-            items: undefined,
+            items: [],
             providers: mockProviders,
-            initialContextItems: [],
             error: 'my error',
         })
     })