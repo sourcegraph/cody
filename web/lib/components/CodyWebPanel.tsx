11e3c6504f07f834c2c50e090208575c2b2c8520
Beatrix <beatrix@sourcegraph.com>
2024-08-20 09:26:45 -0700

    update cody web webview config

--- web/lib/components/CodyWebPanel.tsx
+++ web/lib/components/CodyWebPanel.tsx
@@ -96,6 +96,8 @@ export const CodyWebPanel: FC<CodyWebPanelProps> = props => {
                     setChatModels(message.models)
                     break
                 case 'config':
+                    message.config.webviewType = 'sidebar'
+                    message.config.multipleWebviewsEnabled = false
                     setConfig(message)
                     break
                 case 'clientAction':
