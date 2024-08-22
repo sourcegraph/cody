9787986f4ac177c44c21dd94ad64cdeba5855952
Hitesh Sagtani <sagtanih@gmail.com>
2024-08-22 11:16:23 +0530

    remove unused models (#5286)
    ## Context
    1. Cleaned unused fireworks models, earlier introduced for experiments
    2. No functional change
    
    
    ## Test plan
    CI
    
    <!-- Required. See
    https://docs-legacy.sourcegraph.com/dev/background-information/testing_principles.
    -->
    
    ## Changelog
    
    <!-- OPTIONAL; info at
    https://www.notion.so/sourcegraph/Writing-a-changelog-entry-dd997f411d524caabf0d8d38a24a878c
    -->

--- vscode/src/completions/providers/fireworks.ts
+++ vscode/src/completions/providers/fireworks.ts
@@ -60,21 +60,10 @@ const EOT_STARCODER = '<|endoftext|>'
 const EOT_LLAMA_CODE = ' <EOT>'
 const EOT_DEEPSEEK_CODE = '<|eos_token|>'
 
-// Fireworks hosted fine tuned model on py, tsx, jsx and starcoder-hybrid on other langs.
-export const FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID_WITH_200MS_DELAY =
-    'fim-fine-tuned-model-hybrid-200ms-delay'
-export const FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID = 'fim-fine-tuned-model-hybrid'
-export const FIREWORKS_FIM_LANG_SPECIFIC_MODEL_MIXTRAL = 'fim-lang-specific-model-mixtral'
-
 export const FIREWORKS_DEEPSEEK_7B_LANG_STACK_FINETUNED =
     'fim-lang-specific-model-deepseek-stack-trained'
 export const FIREWORKS_DEEPSEEK_7B_LANG_LOG_FINETUNED = 'fim-lang-specific-model-deepseek-logs-trained'
 
-// Huggingface link (https://huggingface.co/deepseek-ai/deepseek-coder-1.3b-base)
-export const DEEPSEEK_CODER_1P3_B = 'deepseek-coder-1p3b'
-// Huggingface link (https://huggingface.co/deepseek-ai/deepseek-coder-6.7b-base)
-export const DEEPSEEK_CODER_7B = 'deepseek-coder-7b'
-// Huggingface link (https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Lite-Base)
 export const DEEPSEEK_CODER_V2_LITE_BASE = 'deepseek-coder-v2-lite-base'
 
 // Context window experiments with DeepSeek Model
@@ -82,9 +71,6 @@ export const DEEPSEEK_CODER_V2_LITE_BASE_WINDOW_4096 = 'deepseek-coder-v2-lite-b
 export const DEEPSEEK_CODER_V2_LITE_BASE_WINDOW_8192 = 'deepseek-coder-v2-lite-base-context-8192'
 export const DEEPSEEK_CODER_V2_LITE_BASE_WINDOW_16384 = 'deepseek-coder-v2-lite-base-context-16383'
 export const DEEPSEEK_CODER_V2_LITE_BASE_WINDOW_32768 = 'deepseek-coder-v2-lite-base-context-32768'
-
-// Huggingface link (https://huggingface.co/Qwen/CodeQwen1.5-7B)
-export const CODE_QWEN_7B = 'code-qwen-7b'
 
 // Model identifiers can be found in https://docs.fireworks.ai/explore/ and in our internal
 // conversations
@@ -97,18 +83,9 @@ const MODEL_MAP = {
     // Fireworks model identifiers
     'llama-code-13b': 'fireworks/accounts/fireworks/models/llama-v2-13b-code',
 
-    // Fine-tuned model hybrid identifier
-    [FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID]:
-        'fireworks/accounts/sourcegraph/models/finetuned-fim-lang-all-model-mixtral-8x7b',
-    [FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID_WITH_200MS_DELAY]:
-        'fireworks/accounts/sourcegraph/models/finetuned-fim-lang-all-model-mixtral-8x7b',
-    [FIREWORKS_FIM_LANG_SPECIFIC_MODEL_MIXTRAL]: FIREWORKS_FIM_LANG_SPECIFIC_MODEL_MIXTRAL,
     [FIREWORKS_DEEPSEEK_7B_LANG_LOG_FINETUNED]: FIREWORKS_DEEPSEEK_7B_LANG_LOG_FINETUNED,
     [FIREWORKS_DEEPSEEK_7B_LANG_STACK_FINETUNED]: FIREWORKS_DEEPSEEK_7B_LANG_STACK_FINETUNED,
-    [DEEPSEEK_CODER_1P3_B]: 'fireworks/accounts/sourcegraph/models/custom-deepseek-1p3b-base-hf-version',
-    [DEEPSEEK_CODER_7B]: 'fireworks/accounts/sourcegraph/models/deepseek-coder-7b-base',
     [DEEPSEEK_CODER_V2_LITE_BASE]: 'accounts/sourcegraph/models/deepseek-coder-v2-lite-base',
-    [CODE_QWEN_7B]: 'accounts/sourcegraph/models/code-qwen-1p5-7b',
     [DEEPSEEK_CODER_V2_LITE_BASE_WINDOW_4096]: 'accounts/sourcegraph/models/deepseek-coder-v2-lite-base',
     [DEEPSEEK_CODER_V2_LITE_BASE_WINDOW_8192]: 'accounts/sourcegraph/models/deepseek-coder-v2-lite-base',
     [DEEPSEEK_CODER_V2_LITE_BASE_WINDOW_16384]:
@@ -136,14 +113,9 @@ function getMaxContextTokens(model: FireworksModel): number {
             // Llama 2 on Fireworks supports up to 4k tokens. We're constraining it here to better
             // compare the results
             return 2048
-        case FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID:
-        case FIREWORKS_FIM_LANG_SPECIFIC_MODEL_MIXTRAL:
         case FIREWORKS_DEEPSEEK_7B_LANG_STACK_FINETUNED:
         case FIREWORKS_DEEPSEEK_7B_LANG_LOG_FINETUNED:
-        case DEEPSEEK_CODER_1P3_B:
-        case DEEPSEEK_CODER_7B:
-        case DEEPSEEK_CODER_V2_LITE_BASE:
-        case CODE_QWEN_7B: {
+        case DEEPSEEK_CODER_V2_LITE_BASE: {
             return 2048
         }
         case DEEPSEEK_CODER_V2_LITE_BASE_WINDOW_4096:
@@ -177,9 +149,6 @@ class FireworksProvider extends Provider {
     private isLocalInstance: boolean
     private fireworksConfig?: Configuration['autocompleteExperimentalFireworksOptions']
     private promptExtractor: FIMModelSpecificPromptExtractor
-    // Todo: This variable is used to introduce an additional delay to collect the data on impact of latency on user experience.
-    // Todo: Delete this variable once the data is collected.
-    private shouldAddArtificialDelayForExperiment = false
     private anonymousUserID: string | undefined
 
     constructor(
@@ -196,10 +165,7 @@ class FireworksProvider extends Provider {
     ) {
         super(options)
         this.timeouts = timeouts
-        if (model === FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID_WITH_200MS_DELAY) {
-            this.shouldAddArtificialDelayForExperiment = true
-        }
-        this.model = this.adjustModelIdentifier(model, options.document.languageId)
+        this.model = model
         this.promptExtractor = this.getFIMPromptExtractorForModel()
         this.promptChars = tokensToChars(maxContextTokens - MAX_RESPONSE_TOKENS)
         this.client = client
@@ -237,12 +203,6 @@ class FireworksProvider extends Provider {
         if (isLlamaCode(this.model)) {
             return new fimPromptUtils.CodeLlamaPromptExtractor()
         }
-        if (isFinetunedV1ModelFamily(this.model)) {
-            return new fimPromptUtils.FinetunedModelV1PromptExtractor()
-        }
-        if (isCodeQwenFamily(this.model)) {
-            return new fimPromptUtils.CodeQwenModelPromptExtractor()
-        }
         if (isDeepSeekModelFamily(this.model)) {
             return new fimPromptUtils.DeepSeekPromptExtractor()
         }
@@ -251,34 +211,6 @@ class FireworksProvider extends Provider {
             this.model
         )
         return new fimPromptUtils.DefaultModelPromptExtractor()
-    }
-
-    private adjustModelIdentifier(model: FireworksModel, languageId: string): FireworksModel {
-        switch (model) {
-            case FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID:
-            case FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID_WITH_200MS_DELAY: {
-                if (['typescriptreact', 'javascriptreact', 'python'].includes(languageId)) {
-                    return FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID
-                }
-                return 'starcoder-hybrid'
-            }
-            case FIREWORKS_FIM_LANG_SPECIFIC_MODEL_MIXTRAL: {
-                if (
-                    [
-                        'typescriptreact',
-                        'javascriptreact',
-                        'typescript',
-                        'javascript',
-                        'python',
-                    ].includes(languageId)
-                ) {
-                    return FIREWORKS_FIM_LANG_SPECIFIC_MODEL_MIXTRAL
-                }
-                return 'starcoder-hybrid'
-            }
-            default:
-                return model
-        }
     }
 
     private createPrompt(snippets: AutocompleteContextSnippet[]): PromptString {
@@ -344,11 +276,7 @@ class FireworksProvider extends Provider {
     }
 
     private getIntroString(intro: PromptString[], languageConfig: LanguageConfig | null): PromptString {
-        if (
-            isFinetunedV1ModelFamily(this.model) ||
-            isDeepSeekModelFamily(this.model) ||
-            isCodeQwenFamily(this.model)
-        ) {
+        if (isDeepSeekModelFamily(this.model)) {
             // These model families take code from the context files without comments.
             return ps`${PromptString.join(intro, ps`\n\n`)}\n`
         }
@@ -386,16 +314,6 @@ class FireworksProvider extends Provider {
             model,
         } satisfies CodeCompletionsParams
 
-        if (isFinetunedV1ModelFamily(requestParams.model) || isCodeQwenFamily(requestParams.model)) {
-            requestParams.stopSequences = [
-                ...(requestParams.stopSequences || []),
-                '<fim_prefix>',
-                '<fim_suffix>',
-                '<fim_middle>',
-                '<|endoftext|>',
-                '<file_sep>',
-            ]
-        }
         if (isDeepSeekModelFamily(requestParams.model)) {
             requestParams.stopSequences = [
                 ...(requestParams.stopSequences || []),
@@ -445,11 +363,7 @@ class FireworksProvider extends Provider {
     }
 
     private postProcess = (content: string): string => {
-        if (
-            isStarCoderFamily(this.model) ||
-            isCodeQwenFamily(this.model) ||
-            isFinetunedV1ModelFamily(this.model)
-        ) {
+        if (isStarCoderFamily(this.model)) {
             return content.replace(EOT_STARCODER, '')
         }
         if (isLlamaCode(this.model)) {
@@ -476,7 +390,6 @@ class FireworksProvider extends Provider {
                 isLocalInstance: this.isLocalInstance,
                 fireworksConfig: this.fireworksConfig,
                 logger: this.client.logger,
-                shouldAddArtificialDelayForExperiment: this.shouldAddArtificialDelayForExperiment,
                 providerOptions: this.options,
                 fastPathAccessToken: this.fastPathAccessToken,
                 customHeaders: this.getCustomHeaders(),
@@ -544,24 +457,10 @@ function isLlamaCode(model: string): boolean {
     return model.startsWith('llama-code')
 }
 
-function isFinetunedV1ModelFamily(model: string): boolean {
-    return [
-        FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID,
-        FIREWORKS_FIM_LANG_SPECIFIC_MODEL_MIXTRAL,
-        FIREWORKS_FIM_FINE_TUNED_MODEL_HYBRID_WITH_200MS_DELAY,
-    ].includes(model)
-}
-
 function isDeepSeekModelFamily(model: string): boolean {
     return [
-        DEEPSEEK_CODER_1P3_B,
-        DEEPSEEK_CODER_7B,
         DEEPSEEK_CODER_V2_LITE_BASE,
         FIREWORKS_DEEPSEEK_7B_LANG_STACK_FINETUNED,
         FIREWORKS_DEEPSEEK_7B_LANG_LOG_FINETUNED,
     ].includes(model)
-}
-
-function isCodeQwenFamily(model: string): boolean {
-    return [CODE_QWEN_7B].includes(model)
 }
