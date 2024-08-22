dfc1e162f7405b429d67b1cba70474a496514e0b
Beatrix <beatrix@sourcegraph.com>
2024-08-21 19:32:24 -0700

    fix script env var names

--- agent/scripts/generate-agent-bindings.sh
+++ agent/scripts/generate-agent-bindings.sh
@@ -2,13 +2,11 @@
 set -eux
 INDEXER_DIR=${SCIP_TYPESCRIPT_DIR:-../scip-typescript-cody-bindings}
 
-# Use LANG if it's not empty, or fall back to "kotlin". Example:
-# LANG=csharp OUT_DIR=agent/bindings/csharp NAMESPACE=Cody.Core.Agent.Protocol pnpm generate-agent-kotlin-bindings
-LANG=${LANG:-"kotlin"}
-OUT=${OUT:-"agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated"}
+# Use OUT_LANG if it's not empty, or fall back to "kotlin". Example:
+# OUT_LANG=csharp OUT_DIR=agent/bindings/csharp NAMESPACE=Cody.Core.Agent.Protocol pnpm generate-agent-kotlin-bindings
+OUT_LANG=${OUT_LANG:-"kotlin"}
+OUT_DIR=${OUT_DIR:-"agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated"}
 NAMESPACE=${NAMESPACE:-"com.sourcegraph.cody.agent.protocol_generated"}
-
-echo "Generating $LANG bindings in $OUT for namespace $NAMESPACE"
 
 if [ ! -d "$INDEXER_DIR" ]; then
   git clone https://github.com/sourcegraph/scip-typescript.git "$INDEXER_DIR"
@@ -25,4 +23,4 @@ pnpm install --prefer-offline
 pnpm build
 # TODO: invoke @sourcegraph/scip-typescript npm package instead
 pnpm exec ts-node "$INDEXER_DIR"/src/main.ts index --emit-signatures --emit-external-symbols
-pnpm exec ts-node agent/src/cli/scip-codegen/command.ts --output "$OUT" --language "$LANG" --kotlin-package "$NAMESPACE"
+pnpm exec ts-node agent/src/cli/scip-codegen/command.ts --output "$OUT_DIR" --language "$OUT_LANG" --kotlin-package "$NAMESPACE"
