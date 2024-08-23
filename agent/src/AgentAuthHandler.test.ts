bea1b34b708eb4fe207037e6935305b98c4574f1
Beatrix <beatrix@sourcegraph.com>
2024-08-23 12:58:55 -0700

    Add unit test

---
+++ agent/src/AgentAuthHandler.test.ts
@@ -0,0 +1,58 @@
+import express from 'express'
+import open from 'open'
+import { describe, expect, it, vi } from 'vitest'
+import { AgentAuthHandler } from './AgentAuthHandler'
+
+vi.mock('open')
+vi.mock('express', () => {
+    const mockExpress = vi.fn().mockReturnValue({
+        get: vi.fn(),
+        listen: vi.fn().mockImplementation((port, callback) => {
+            callback()
+        }),
+        on: vi.fn(),
+    })
+    return { default: mockExpress }
+})
+
+describe('AgentAuthHandler', () => {
+    describe('redirectToEndpointLoginPage', () => {
+        /**
+         * Tests the redirectToEndpointLoginPage function of the AgentAuthHandler class.
+         * This function constructs a callback URI with a Cody auth referral code and server port,
+         * then opens the URI in the user's default web browser.
+         *
+         * The test cases cover:
+         * - When a valid endpointUri is set, the correct callback URI is constructed and opened.
+         * - When endpointUri is null, nothing happens (open is not called).
+         */
+        it.each([
+            [
+                'valid endpointUri for JetBrains',
+                'JetBrains',
+                'https://sourcegraph.test',
+                'https://sourcegraph.test/user/settings/tokens/new/callback?requestFrom=CODY_JETBRAINS-43452',
+            ],
+            [
+                'valid endpointUri for VisualStudio',
+                'VisualStudio',
+                'https://sourcegraph.com',
+                'https://sourcegraph.com/user/settings/tokens/new/callback?requestFrom=VISUAL_STUDIO-43452',
+            ],
+            ['invalid IDE', 'Sublime', 'https://sourcegraph.com', undefined],
+            ['invalid endpointUri', 'Web', '', undefined],
+        ])('%s', (_: string, IDE: string, endpointUri: string, expectedUrl?: string) => {
+            const agentAuthHandler = new AgentAuthHandler(IDE)
+            agentAuthHandler.setTokenCallbackHandler(() => {})
+            if (!expectedUrl) {
+                expect(() => agentAuthHandler.redirectToEndpointLoginPage(endpointUri)).toThrow(
+                    'Failed to construct callback URL'
+                )
+            } else {
+                agentAuthHandler.redirectToEndpointLoginPage(endpointUri)
+                expect(express).toHaveBeenCalled()
+                expect(open).toHaveBeenCalledWith(expectedUrl)
+            }
+        })
+    })
+})
