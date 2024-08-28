42260170d3754d9242d21a4a436acbb3ad3b2ea5
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-08-28 13:52:40 +0200

    Fix crash in autocomplete (wrong index) (fixes #2107) (#2146)
    Fixes https://github.com/sourcegraph/jetbrains/issues/2107.
    
    ## Test plan
    Hard to repro. See the original issue.

--- src/main/kotlin/com/sourcegraph/utils/CodyFormatter.kt
+++ src/main/kotlin/com/sourcegraph/utils/CodyFormatter.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.codeStyle.CodeStyleManager
+import kotlin.math.min
 
 class CodyFormatter {
   companion object {
@@ -41,13 +42,14 @@ class CodyFormatter {
         // Fix for the IJ formatting bug which removes spaces even before the given formatting
         // range.
         val existingStart = appendedString.substring(0, cursor)
-        val formattedStart = psiFile.text.substring(0, cursor)
+        val boundedCursorPosition = min(cursor, psiFile.text.length)
+        val formattedStart = psiFile.text.substring(0, boundedCursorPosition)
         val startOfDiff = existingStart.zip(formattedStart).indexOfFirst { (e, f) -> e != f }
 
         val formattedText =
             if (startOfDiff != -1) {
               val addition = formattedStart.substring(startOfDiff)
-              existingStart + addition + psiFile.text.substring(cursor)
+              existingStart + addition + psiFile.text.substring(boundedCursorPosition)
             } else psiFile.text
         return formattedText.substring(
             range.startOffset, range.endOffset + formattedText.length - document.textLength)
