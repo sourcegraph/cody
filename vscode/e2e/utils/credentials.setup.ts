a4aa1c3554206aab8c1b101a85a27049e0a53792
Rik Nauta <rik.nauta@sourcegraph.com>
2024-08-29 15:18:44 +0200

    Address PR comments

--- vscode/e2e/utils/credentials.setup.ts
+++ vscode/e2e/utils/credentials.setup.ts
@@ -2,17 +2,15 @@ import { test as setup } from '@playwright/test'
 import * as dotenv from 'dotenv'
 import { CREDENTIALS_ENVFILE_PATH, updateEnvFile } from './vscody/credentials-envfile'
 
-// biome-ignore lint/complexity/noBannedTypes: <explanation>
-// biome-ignore lint/correctness/noEmptyPattern: <explanation>
 setup.extend<{}>({})('credentials', async ({}) => {
     try {
         // NOTE: VSCode Playwright UI will abort the running test when it
         // detects a file change. So you'll in some cases have to click the run
-        // test butotn twice. Every other case seems to work fine.
+        // test button twice. Every other case seems to work fine.
         if (updateEnvFile()) {
             dotenv.config({ path: CREDENTIALS_ENVFILE_PATH })
         }
-    } catch {
+    } catch (e) {
         // we ignore error
     }
 })
