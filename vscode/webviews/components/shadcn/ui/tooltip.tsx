958c90e615cbef13038a36dd6aa5474cf0ce6e60
Vova Kulikov <vova@sourcegraph.com>
2024-09-02 13:43:57 -0300

    Render tooltip in portal

--- vscode/webviews/components/shadcn/ui/tooltip.tsx
+++ vscode/webviews/components/shadcn/ui/tooltip.tsx
@@ -15,21 +15,25 @@ interface TooltipContentProps extends React.ComponentPropsWithoutRef<typeof Tool
 const TooltipContent = React.forwardRef<
     React.ElementRef<typeof TooltipPrimitive.Content>,
     TooltipContentProps
->(({ portal, className, sideOffset = 4, ...props }, ref) => (
-    <TooltipPrimitive.Portal>
-        <TooltipPrimitive.Content
-            ref={ref}
-            sideOffset={sideOffset}
-            className={cn(
-                'tw-z-50 tw-overflow-hidden tw-flex tw-items-center tw-rounded-md tw-border tw-border-border tw-leading-tight tw-bg-popover tw-px-3 tw-py-2 tw-text-sm tw-text-popover-foreground tw-max-w-72 tw-text-center tw-whitespace-pre-line tw-shadow-lg [&_kbd]:tw-ml-3 [&_kbd]:-tw-mr-1 [&_kbd]:tw-mt-[-4px] [&_kbd]:tw-mb-[-4px]',
-                className
-            )}
-            {...props}
-        >
-            {props.children}
-        </TooltipPrimitive.Content>
-    </TooltipPrimitive.Portal>
-))
+>(({ portal, className, sideOffset = 4, ...props }, ref) => {
+    const Portal = portal ? TooltipPrimitive.Portal : React.Fragment
+
+    return (
+        <Portal>
+            <TooltipPrimitive.Content
+                ref={ref}
+                sideOffset={sideOffset}
+                className={cn(
+                    'tw-z-50 tw-overflow-hidden tw-flex tw-items-center tw-rounded-md tw-border tw-border-border tw-leading-tight tw-bg-popover tw-px-3 tw-py-2 tw-text-sm tw-text-popover-foreground tw-max-w-72 tw-text-center tw-whitespace-pre-line tw-shadow-lg [&_kbd]:tw-ml-3 [&_kbd]:-tw-mr-1 [&_kbd]:tw-mt-[-4px] [&_kbd]:tw-mb-[-4px]',
+                    className
+                )}
+                {...props}
+            >
+                {props.children}
+            </TooltipPrimitive.Content>
+        </Portal>
+    )
+})
 
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
