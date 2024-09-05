5e8b4650398dd191f70d00db44b5782d9285191c
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-09-05 13:24:30 +0200

    Move updateCodeLenses logic to AgentFixupControls (#5477)
    Fixes https://github.com/sourcegraph/jetbrains/issues/2197.
    
    ## Test plan
    Try to reproduce https://github.com/sourcegraph/jetbrains/issues/2197.
    
    <!-- Required. See
    https://docs-legacy.sourcegraph.com/dev/background-information/testing_principles.
    -->
    
    ## Changelog
    Move updateCodeLenses from agent.ts to AgentFixupControls.ts
    
    <!-- OPTIONAL; info at
    https://www.notion.so/sourcegraph/Writing-a-changelog-entry-dd997f411d524caabf0d8d38a24a878c
    -->

--- vscode/src/non-stop/codelenses/provider.ts
+++ vscode/src/non-stop/codelenses/provider.ts
@@ -187,8 +187,9 @@ export class FixupCodeLenses implements vscode.CodeLensProvider, FixupControlApp
             this.removeLensesFor(task)
             return
         }
-        this.taskLenses.set(task, getLensesForTask(task))
-        this.notifyCodeLensesChanged()
+        const lenses = getLensesForTask(task)
+        this.taskLenses.set(task, lenses)
+        this.notifyCodeLensesChanged(task.document.uri, lenses)
     }
 
     public didDeleteTask(task: FixupTask): void {
@@ -200,7 +201,7 @@ export class FixupCodeLenses implements vscode.CodeLensProvider, FixupControlApp
     private removeLensesFor(task: FixupTask): void {
         if (this.taskLenses.delete(task)) {
             // TODO: Clean up the fixup file when there are no remaining code lenses
-            this.notifyCodeLensesChanged()
+            this.notifyCodeLensesChanged(task.document.uri, [])
         }
     }
 
@@ -224,7 +225,7 @@ export class FixupCodeLenses implements vscode.CodeLensProvider, FixupControlApp
         void vscode.commands.executeCommand('setContext', 'cody.hasActionableEdit', hasActionableEdit)
     }
 
-    private notifyCodeLensesChanged(): void {
+    public notifyCodeLensesChanged(uri: vscode.Uri, codeLenses: vscode.CodeLens[]): void {
         this._onDidChangeCodeLenses.fire()
     }
 
