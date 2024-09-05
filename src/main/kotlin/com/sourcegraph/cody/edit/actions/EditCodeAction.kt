78b09c11873c7a6b2b89a00f0ca4590eee059be3
Piotr Kukiełka <pkukielka@users.noreply.github.com>
2024-09-05 13:24:20 +0200

    Fix shortcuts for Edit Code on Windows (#2223)
    ## Changes
    
    Removes annoying `Alt + Enter` shortcut and unifies both bringing `Edit
    Code` prompt and confirming an edit into a single shortcut.
    
    ## Test plan
    
    1. Select some code in the editor
    2. Hit `Ctrl Enter` to show `Edit Code` prompt
    3. Add some instruction, e.g. to add comments
    4. Hit `Ctrl Enter` again to confirm the changes and do an edit

--- src/main/kotlin/com/sourcegraph/cody/edit/actions/EditCodeAction.kt
+++ src/main/kotlin/com/sourcegraph/cody/edit/actions/EditCodeAction.kt
@@ -1,5 +1,6 @@
 package com.sourcegraph.cody.edit.actions
 
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.Logger
 import com.sourcegraph.cody.agent.protocol_generated.EditTask
 import com.sourcegraph.cody.edit.EditCommandPrompt
@@ -13,6 +14,13 @@ class EditCodeAction :
         logger.warn("EditCodeAction invoked with null project")
       }
     }) {
+
+  override fun update(event: AnActionEvent) {
+    super.update(event)
+    val commandPrompt = EditCommandPrompt.EDIT_COMMAND_PROMPT_KEY.get(event.project)
+    event.presentation.isEnabledAndVisible = commandPrompt?.isVisible != true
+  }
+
   companion object {
     val logger = Logger.getInstance(EditCodeAction::class.java)
     val completedEditTasks = ConcurrentHashMap<String, EditTask>()
