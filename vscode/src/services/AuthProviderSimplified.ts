91696facf50c8f8dd991feb48a4c5425bc49323e
Beatrix <beatrix@sourcegraph.com>
2024-09-04 19:27:23 -0700

    replace getAuthReferralCode with getCodyAuthReferralCode

--- vscode/src/services/AuthProviderSimplified.ts
+++ vscode/src/services/AuthProviderSimplified.ts
@@ -24,19 +24,6 @@ export class AuthProviderSimplified {
     }
 }
 
-/**
- * Returns a known referral code to use based on the current VS Code environment.
- */
-export function getAuthReferralCode(): string {
-    return (
-        {
-            'vscode-insiders': 'CODY_INSIDERS',
-            vscodium: 'CODY_VSCODIUM',
-            cursor: 'CODY_CURSOR',
-        }[vscode.env.uriScheme] || 'CODY'
-    )
-}
-
 // Opens authentication URLs for simplified onboarding.
 function openExternalAuthUrl(
     provider: AuthMethod,
