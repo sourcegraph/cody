5d8495f6092064bfe445401a09e775f809d55af8
Piotr Kukiełka <pkukielka@users.noreply.github.com>
2024-09-05 09:30:38 +0200

    Fix how smart apply handles various absolute paths and spaces in paths (#5466)
    Fixes https://github.com/sourcegraph/jetbrains/issues/2195
    
    ## Changes
    
    Fixes for Smart Apply:
    
    1. Add proper support for paths with spaces
    2. Add support for absolute paths
    
    ## Test plan
    
    * Build with JetBrains plugin and:
    
    1. Create new `quicksort.py` file outside of your workspace, in
    directory with space in name
    3. Open that file and ask Cody to give you an quicksort imlementation
    4. Use Smart Apply
    5. Make sure code was inserted in `quicksort.py` properly

--- agent/src/agent.ts
+++ agent/src/agent.ts
@@ -38,7 +38,7 @@ import type { ExtensionMessage, WebviewMessage } from '../../vscode/src/chat/pro
 import { ProtocolTextDocumentWithUri } from '../../vscode/src/jsonrpc/TextDocumentWithUri'
 import type * as agent_protocol from '../../vscode/src/jsonrpc/agent-protocol'
 
-import { mkdirSync, statSync } from 'node:fs'
+import fs, { mkdirSync, statSync } from 'node:fs'
 import { PassThrough } from 'node:stream'
 import type { Har } from '@pollyjs/persister'
 import { TESTING_TELEMETRY_EXPORTER } from '@sourcegraph/cody-shared/src/telemetry-v2/TelemetryRecorderProvider'
@@ -381,6 +381,12 @@ export class Agent extends MessageHandler implements ExtensionClient {
                       scheme: 'file',
                       path: clientInfo.workspaceRootPath ?? undefined,
                   })
+
+            if (!fs.existsSync(this.workspace.workspaceRootUri.fsPath)) {
+                throw new Error(
+                    `Workspace root '${this.workspace.workspaceRootUri.fsPath}' does not exist`
+                )
+            }
 
             vscode_shim.setWorkspaceDocuments(this.workspace)
             if (clientInfo.capabilities?.codeActions === 'enabled') {
