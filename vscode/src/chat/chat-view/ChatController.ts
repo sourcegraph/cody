7fd12ad3643f94e5534e7c4a45e6cffd7cfb814c
Quinn Slack <quinn@slack.org>
2024-09-07 12:52:05 -0700

    remove needless ChatController.sendConfig call
    None of the values sent by `sendConfig` depend on feature flags anymore, so this is unnecessary.

--- vscode/src/chat/chat-view/ChatController.ts
+++ vscode/src/chat/chat-view/ChatController.ts
@@ -252,13 +252,6 @@ export class ChatController implements vscode.Disposable, vscode.WebviewViewProv
         // Advise local embeddings to start up if necessary.
         void this.retrievers.localEmbeddings?.start()
 
-        // Keep feature flags updated.
-        this.disposables.push({
-            dispose: featureFlagProvider.instance!.onFeatureFlagChanged('', () => {
-                void this.sendConfig()
-            }),
-        })
-
         this.disposables.push(
             startClientStateBroadcaster({
                 useRemoteSearch: this.retrievers.allowRemoteContext,
