5b1ba8f55ca9c0350bb3fe87a31db730aaf19fb0
Beatrix <beatrix@sourcegraph.com>
2024-09-12 00:15:20 -0700

    Update agent bindings

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/ModelTag.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/ModelTag.kt
@@ -1,5 +1,5 @@
 @file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
 package com.sourcegraph.cody.agent.protocol_generated;
 
-typealias ModelTag = String // One of: power, speed, balanced, recommended, deprecated, preview, experimental, pro, free, enterprise, gateway, byok, local, ollama, dev, streamDisabled
+typealias ModelTag = String // One of: power, speed, balanced, recommended, deprecated, experimental, waitlist, on-waitlist, early-access, pro, free, enterprise, gateway, byok, local, ollama, dev, stream-disabled
 
