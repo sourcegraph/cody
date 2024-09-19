324d153b526306f629281c3019e5b241f32ba55a
Enrique Gonzalez <enrique@sourcegraph.com>
2024-09-19 14:37:22 -0400

    add OS info

--- vscode/src/services/utils/issue-reporter.ts
+++ vscode/src/services/utils/issue-reporter.ts
@@ -1,5 +1,16 @@
 import * as vscode from 'vscode'
 import { version } from '../../version'
+import { isdo } from '@sourcegraph/cody-shared'
+
+function getOSName(): string {
+    if (process.platform === 'win32') {
+        return 'Windows'
+    }
+    if (process.platform === 'darwin') {
+        return 'macOS'
+    }
+    return 'Linux'
+}
 
 export function openCodyIssueReporter() {
     const baseUrl = "https://help.sourcegraph.com/hc/en-us/requests/new";
@@ -20,6 +31,7 @@ const issueBody = `
 - Cody Version: ${version}
 - VS Code Version: ${vscode.version}
 - Extension Host: ${vscode.env.appHost}
+- Operating System: ${getOSName()}
 
 Steps to Reproduce
 1.
