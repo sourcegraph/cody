5a6031faa9937db308cf8a1c22c69a2eaaa2a45c
Filip Haftek <filip.haftek@sourcegraph.com>
2024-09-19 13:57:11 -0400

    enable all detectors

--- vscode/src/yoda/detectors/index.ts
+++ vscode/src/yoda/detectors/index.ts
@@ -1,12 +1,12 @@
 import type { Detector } from './Detector'
-// import { OffByOneDetector } from './OffByOneDetector'
-// import { OutdatedSymbolDocumentationDetector } from './OutdatedSymbolDocumentationDetector'
+import { OffByOneDetector } from './OffByOneDetector'
+import { OutdatedSymbolDocumentationDetector } from './OutdatedSymbolDocumentationDetector'
 import { SQLOptimisationDetector } from './SQLOptimisationDetector'
-// import { TestOpportunityDetector } from './TestOppotrunityDetector'
+import { TestOpportunityDetector } from './TestOppotrunityDetector'
 
 export const detectors: Detector<any>[] = [
-    // new OffByOneDetector(),
-    // new OutdatedSymbolDocumentationDetector(),
-    // new TestOpportunityDetector(),
+    new OffByOneDetector(),
+    new OutdatedSymbolDocumentationDetector(),
+    new TestOpportunityDetector(),
     new SQLOptimisationDetector(),
 ] as const
