88cee034ca0b76b774bb85c2b2c6a523e4f17414
Enrique Gonzalez <enrique@sourcegraph.com>
2024-09-19 14:55:58 -0400

    add whether or not they are dotcom user

--- vscode/src/services/utils/issue-reporter.ts
+++ vscode/src/services/utils/issue-reporter.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode'
 import { version } from '../../version'
-import { isdo } from '@sourcegraph/cody-shared'
+import { getConfiguration } from '../../configuration'
+import { isDotComAuthed } from '@sourcegraph/cody-shared'
 
 function getOSName(): string {
     if (process.platform === 'win32') {
@@ -13,13 +14,14 @@ function getOSName(): string {
 }
 
 export function openCodyIssueReporter() {
+    const config = getConfiguration()
     const baseUrl = "https://help.sourcegraph.com/hc/en-us/requests/new";
     const subject = "Cody Issue Report";
 
     const params = new URLSearchParams({
         ticket_form_id: "7300762080909",
         tf_subject: encodeURIComponent(subject),
-        tf_description: encodeURIComponent(issueBody),
+        tf_description: encodeURIComponent(generateIssueBody(config)),
         tf_360041500552: "defect_report"
     });
 
@@ -27,18 +29,28 @@ export function openCodyIssueReporter() {
     vscode.env.openExternal(vscode.Uri.parse(url));
 }
 
-const issueBody = `
-- Cody Version: ${version}
+function generateIssueBody(config: ReturnType<typeof getConfiguration>): string {
+    return `- Cody Version: ${version}
 - VS Code Version: ${vscode.version}
 - Extension Host: ${vscode.env.appHost}
 - Operating System: ${getOSName()}
+- Cody Free/Pro: ${isDotComAuthed()}
+
+## Cody Configuration
+- Server Endpoint: ${config.serverEndpoint}
+- Use Context: ${config.useContext}
+- Autocomplete Enabled: ${config.autocomplete}
+- Code Actions Enabled: ${config.codeActions}
+- Command Hints Enabled: ${config.commandHints}
+- Debug Verbose: ${config.debugVerbose}
+- Telemetry Level: ${config.telemetryLevel}
 
 Steps to Reproduce
 1.
 2.
 3.
 
-## Expected Behaviour
+Expected Behaviour
 
-## Logs
 `
+}
