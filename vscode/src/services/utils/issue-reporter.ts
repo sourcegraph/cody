4ef9d34744a186123a271f4ccb9541ce82d0b6c7
Enrique Gonzalez <enrique@sourcegraph.com>
2024-09-19 11:04:11 -0400

    add issue body template back

--- vscode/src/services/utils/issue-reporter.ts
+++ vscode/src/services/utils/issue-reporter.ts
@@ -1,19 +1,14 @@
 import * as vscode from 'vscode'
-
-// import packageJson from '../../../package.json'
-// import { version } from '../../version'
+import { version } from '../../version'
 
 export function openCodyIssueReporter() {
-    // void vscode.commands.executeCommand('workbench.action.openIssueReporter', {
-    //     extensionId: `${packageJson.publisher}.${packageJson.name}`,
     const baseUrl = "https://help.sourcegraph.com/hc/en-us/requests/new";
-    const subject = "Cody Feedback";
-    const description = "I'd like to provide feedback about Cody";
+    const subject = "Cody Issue Report";
 
     const params = new URLSearchParams({
         ticket_form_id: "7300762080909",
         tf_subject: encodeURIComponent(subject),
-        tf_description: encodeURIComponent(description),
+        tf_description: encodeURIComponent(issueBody),
         tf_360041500552: "defect_report"
     });
 
@@ -21,7 +16,17 @@ export function openCodyIssueReporter() {
     vscode.env.openExternal(vscode.Uri.parse(url));
 }
 
-// const issueBody = `## Extension Information
-// <!-- Do not remove the pre-filled information below -->
-// - lfjsdkljsdflkjsdlfjsdfljsd
-// `
+const issueBody = `
+- Cody Version: ${version}
+- VS Code Version: ${vscode.version}
+- Extension Host: ${vscode.env.appHost}
+
+Steps to Reproduce
+1.
+2.
+3.
+
+## Expected Behaviour
+
+## Logs
+`
