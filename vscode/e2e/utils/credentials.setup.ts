b30daebcbaa185ac1cbbcd92a6b54cc246ef3651
Rik Nauta <rik.nauta@sourcegraph.com>
2024-09-20 17:31:48 -0400

    Add encryption to .credentials.env to mitigate fallout from accidental file leak

--- vscode/e2e/utils/credentials.setup.ts
+++ vscode/e2e/utils/credentials.setup.ts
@@ -3,14 +3,10 @@ import * as dotenv from 'dotenv'
 import { CREDENTIALS_ENVFILE_PATH, updateEnvFile } from './vscody/credentials-envfile'
 
 setup.extend<{}>({})('credentials', async ({}) => {
-    try {
-        // NOTE: VSCode Playwright UI will abort the running test when it
-        // detects a file change. So you'll in some cases have to click the run
-        // test button twice. Every other case seems to work fine.
-        if (updateEnvFile()) {
-            dotenv.config({ path: CREDENTIALS_ENVFILE_PATH })
-        }
-    } catch (e) {
-        // we ignore error
+    // NOTE: VSCode Playwright UI will abort the running test when it
+    // detects a file change. So you'll in some cases have to click the run
+    // test button twice. Every other case seems to work fine.
+    if (await updateEnvFile()) {
+        dotenv.config({ path: CREDENTIALS_ENVFILE_PATH })
     }
 })
