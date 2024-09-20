bd305d5ff6a49e237301520c821229f3c20169ff
Quinn Slack <quinn@slack.org>
2024-09-20 16:37:22 -0400

    kotlin

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/ModelId.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/ModelId.kt
@@ -1,9 +1,5 @@
 @file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
 package com.sourcegraph.cody.agent.protocol_generated;
 
-data class ModelRef(
-  val providerId: ProviderId,
-  val apiVersionId: ApiVersionId,
-  val modelId: ModelId,
-)
+typealias ModelId = String // One of: 
 
