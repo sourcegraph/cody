b43107e421e24a1b9a3201343ec87b3755f9d219
Valery Bugakov <skymk1@gmail.com>
2024-09-24 13:06:37 +0800

    Autocomplete: add `google` provider tests (#5670)
    The problem: currently, we
    [assert](https://github.com/sourcegraph/cody/blob/d10b88f3bcaad1c140352088a15f641f4697c80f/vscode/src/completions/providers/shared/create-provider.test.ts#L218-L219)
    the model ID assigned to the provider instance, but often, it's not used
    at all to make requests, which means this assertion is irrelevant to the
    actual functionality where we bump into bugs.
    
    - This PR adds tests for autocomplete request parameters in all
    supported instance configurations: dotcom/enterprise X
    local-settings/server-side-model-config/site-config for the `google`
    provider.
    - Removes model-specific code by leveraging model helpers we already use
    in other providers.
    - Follow up for https://github.com/sourcegraph/cody/pull/5604.
    - No functional changes.

--- vscode/src/completions/providers/google.ts
+++ vscode/src/completions/providers/google.ts
@@ -1,13 +1,7 @@
-import {
-    type AutocompleteContextSnippet,
-    type CodeCompletionsParams,
-    type Message,
-    PromptString,
-    ps,
-} from '@sourcegraph/cody-shared'
+import { type CodeCompletionsParams, ps } from '@sourcegraph/cody-shared'
 
-import { type PrefixComponents, fixBadCompletionStart, getHeadAndTail } from '../text-processing'
-import { forkSignal, generatorWithTimeout, messagesToText, zipGenerators } from '../utils'
+import { GEMINI_MARKERS } from '../model-helpers/gemini'
+import { forkSignal, generatorWithTimeout, zipGenerators } from '../utils'
 
 import {
     type FetchCompletionResult,
@@ -20,97 +14,26 @@ import {
     type ProviderFactoryParams,
 } from './shared/provider'
 
-const MARKERS = {
-    Prefix: ps`<|prefix|>`,
-    Suffix: ps`<|suffix|>`,
-    Response: ps`<|fim|>`,
-}
-
 class GoogleGeminiProvider extends Provider {
-    public stopSequences = [`${MARKERS.Response}`]
-
-    public emptyPromptLength(options: GenerateCompletionsOptions): number {
-        const { messages } = this.createPrompt(options, [])
-        const promptNoSnippets = messagesToText(messages)
-        return promptNoSnippets.length - 10
-    }
-
-    protected createPrompt(
-        options: GenerateCompletionsOptions,
-        snippets: AutocompleteContextSnippet[]
-    ): {
-        messages: Message[]
-        prefix: PrefixComponents
-    } {
-        const { prefix, suffix } = PromptString.fromAutocompleteDocumentContext(
-            options.docContext,
-            options.document.uri
-        )
-
-        const { head, tail, overlap } = getHeadAndTail(prefix)
-
-        const relativeFilePath = PromptString.fromDisplayPath(options.document.uri)
-
-        let groupedSnippets = ps``
-
-        for (const snippet of snippets) {
-            const { uri } = snippet
-            const { content, symbol } = PromptString.fromAutocompleteContextSnippet(snippet)
-            const contextPrompt = this.createContext(
-                symbol ? ps`symbol` : ps`file`,
-                symbol ? symbol : PromptString.fromDisplayPath(uri),
-                content
-            )
-
-            if (
-                contextPrompt.length + 1 > this.promptChars - this.emptyPromptLength(options) ||
-                !contextPrompt.length
-            ) {
-                break
-            }
-
-            groupedSnippets = groupedSnippets.concat(contextPrompt)
-        }
-
-        if (groupedSnippets.length) {
-            groupedSnippets = ps`Context:\n${groupedSnippets}\n`
-        }
-
-        // See official docs on prompting for Gemini models:
-        // https://ai.google.dev/gemini-api/docs/prompting-intro
-        const fimPrompt = ps`${MARKERS.Prefix}${prefix}${MARKERS.Response}${suffix}${MARKERS.Suffix}`
-
-        const humanText = ps`You are a code completion AI, designed to autofill code enclosed in special markers based on its surrounding context.
-${groupedSnippets}
-
-Code from ${relativeFilePath} file:
-${fimPrompt}
-
-Your mission is to generate completed code that I can replace the ${MARKERS.Response} markers with, ensuring a seamless and syntactically correct result.
-
-Do not repeat code from before and after ${MARKERS.Response} in your output.
-Maintain consistency with the indentation, spacing, and coding style used in the code.
-Leave the output markers empty if no code is required to bridge the gap.
-Your response should contains only the code required to connect the gap, and the code must be enclosed between ${MARKERS.Response} WITHOUT backticks`
-
-        const messages: Message[] = [
-            { speaker: 'human', text: humanText },
-            { speaker: 'assistant', text: ps`${MARKERS.Response}` },
-        ]
-
-        return { messages, prefix: { head, tail, overlap } }
-    }
-
     public getRequestParams(options: GenerateCompletionsOptions): CodeCompletionsParams {
-        const { snippets } = options
-        const { messages } = this.createPrompt(options, snippets)
+        const { snippets, docContext, document } = options
+
+        const prompt = this.modelHelper.getPrompt({
+            snippets,
+            docContext,
+            document,
+            promptChars: this.promptChars,
+        })
 
         return {
             ...this.defaultRequestParams,
-            messages,
             topP: 0.95,
             temperature: 0,
             model: this.legacyModel,
+            messages: [
+                { speaker: 'human', text: prompt },
+                { speaker: 'assistant', text: ps`${GEMINI_MARKERS.Response}` },
+            ],
         }
     }
 
@@ -119,7 +42,7 @@ Your response should contains only the code required to connect the gap, and the
         abortSignal: AbortSignal,
         tracer?: CompletionProviderTracer
     ): Promise<AsyncGenerator<FetchCompletionResult[]>> {
-        const { numberOfCompletionsToGenerate } = generateOptions
+        const { numberOfCompletionsToGenerate, docContext } = generateOptions
 
         const requestParams = this.getRequestParams(generateOptions)
         tracer?.params(requestParams)
@@ -138,28 +61,13 @@ Your response should contains only the code required to connect the gap, and the
                     completionResponseGenerator,
                     abortController,
                     generateOptions,
-                    providerSpecificPostProcess: this.postProcess,
+                    providerSpecificPostProcess: content =>
+                        this.modelHelper.postProcess(content, docContext),
                 })
             }
         )
 
         return zipGenerators(await Promise.all(completionsGenerators))
-    }
-
-    private postProcess = (rawResponse: string): string => {
-        let completion = rawResponse
-
-        // Because the response should be enclosed with RESPONSE_CODE for consistency.
-        completion = completion.replaceAll(`${MARKERS.Response}`, '').replaceAll(`${MARKERS.Suffix}`, '')
-
-        // Remove bad symbols from the start of the completion string.
-        completion = fixBadCompletionStart(completion)
-
-        return completion
-    }
-
-    private createContext(type: PromptString, name: PromptString, content: PromptString) {
-        return ps`\n-TYPE: ${type}\n-NAME: ${name}\n-CONTENT: ${content.trimEnd()}\n---\n`
     }
 }
 
