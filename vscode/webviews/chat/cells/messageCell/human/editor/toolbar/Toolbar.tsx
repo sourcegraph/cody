23d26b8f2e5ade10cbc1170d7a4b417a3fddffb5
Jason Harris <jason.harris@sourcegraph.com>
2024-09-25 11:43:20 -0500

    remove 'prompts' dropdown from chat toolbar

--- vscode/webviews/chat/cells/messageCell/human/editor/toolbar/Toolbar.tsx
+++ vscode/webviews/chat/cells/messageCell/human/editor/toolbar/Toolbar.tsx
@@ -43,77 +43,77 @@ export const Toolbar: FunctionComponent<{
     hidden,
     className,
 }) => {
-    /**
-     * If the user clicks in a gap or on the toolbar outside of any of its buttons, report back to
-     * parent via {@link onGapClick}.
-     */
-    const onMaybeGapClick = useCallback(
-        (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-            const targetIsToolbarButton = event.target !== event.currentTarget
-            if (!targetIsToolbarButton) {
-                event.preventDefault()
-                event.stopPropagation()
-                onGapClick?.()
-            }
-        },
-        [onGapClick]
-    )
+        /**
+         * If the user clicks in a gap or on the toolbar outside of any of its buttons, report back to
+         * parent via {@link onGapClick}.
+         */
+        const onMaybeGapClick = useCallback(
+            (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+                const targetIsToolbarButton = event.target !== event.currentTarget
+                if (!targetIsToolbarButton) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                    onGapClick?.()
+                }
+            },
+            [onGapClick]
+        )
 
-    return (
-        // biome-ignore lint/a11y/useKeyWithClickEvents: only relevant to click areas
-        <menu
-            role="toolbar"
-            aria-hidden={hidden}
-            hidden={hidden}
-            className={clsx(
-                'tw-flex tw-items-center tw-justify-between tw-flex-wrap-reverse tw-border-t tw-border-t-border tw-gap-2 [&_>_*]:tw-flex-shrink-0',
-                className
-            )}
-            onMouseDown={onMaybeGapClick}
-            onClick={onMaybeGapClick}
-        >
-            <div className="tw-flex tw-items-center">
-                {/* Can't use tw-gap-1 because the popover creates an empty element when open. */}
-                {onMentionClick && (
-                    <AddContextButton
-                        onClick={onMentionClick}
-                        className="tw-opacity-60 focus-visible:tw-opacity-100 hover:tw-opacity-100 tw-mr-2"
-                    />
+        return (
+            // biome-ignore lint/a11y/useKeyWithClickEvents: only relevant to click areas
+            <menu
+                role="toolbar"
+                aria-hidden={hidden}
+                hidden={hidden}
+                className={clsx(
+                    'tw-flex tw-items-center tw-justify-between tw-flex-wrap-reverse tw-border-t tw-border-t-border tw-gap-2 [&_>_*]:tw-flex-shrink-0',
+                    className
                 )}
-                <PromptSelectFieldToolbarItem focusEditor={focusEditor} className="tw-ml-1 tw-mr-1" />
-                <ModelSelectFieldToolbarItem
-                    userInfo={userInfo}
-                    focusEditor={focusEditor}
-                    className="tw-mr-1"
-                />
-            </div>
-            <div className="tw-flex-1 tw-flex tw-justify-end">
-                <SubmitButton
-                    onClick={onSubmitClick}
-                    isEditorFocused={isEditorFocused}
-                    state={submitState}
-                />
-            </div>
-        </menu>
-    )
-}
+                onMouseDown={onMaybeGapClick}
+                onClick={onMaybeGapClick}
+            >
+                <div className="tw-flex tw-items-center">
+                    {/* Can't use tw-gap-1 because the popover creates an empty element when open. */}
+                    {onMentionClick && (
+                        <AddContextButton
+                            onClick={onMentionClick}
+                            className="tw-opacity-60 focus-visible:tw-opacity-100 hover:tw-opacity-100 tw-mr-2"
+                        />
+                    )}
+                    {/*<PromptSelectFieldToolbarItem focusEditor={focusEditor} className="tw-ml-1 tw-mr-1" />*/}
+                    <ModelSelectFieldToolbarItem
+                        userInfo={userInfo}
+                        focusEditor={focusEditor}
+                        className="tw-mr-1"
+                    />
+                </div>
+                <div className="tw-flex-1 tw-flex tw-justify-end">
+                    <SubmitButton
+                        onClick={onSubmitClick}
+                        isEditorFocused={isEditorFocused}
+                        state={submitState}
+                    />
+                </div>
+            </menu>
+        )
+    }
 
-const PromptSelectFieldToolbarItem: FunctionComponent<{
-    focusEditor?: () => void
-    className?: string
-}> = ({ focusEditor, className }) => {
-    const dispatchClientAction = useClientActionDispatcher()
+// const PromptSelectFieldToolbarItem: FunctionComponent<{
+//     focusEditor?: () => void
+//     className?: string
+// }> = ({ focusEditor, className }) => {
+//     const dispatchClientAction = useClientActionDispatcher()
 
-    const onSelect = useCallback(
-        (item: PromptOrDeprecatedCommand) => {
-            onPromptSelectInPanel(item, () => {}, dispatchClientAction)
-            focusEditor?.()
-        },
-        [focusEditor, dispatchClientAction]
-    )
+//     const onSelect = useCallback(
+//         (item: PromptOrDeprecatedCommand) => {
+//             onPromptSelectInPanel(item, () => { }, dispatchClientAction)
+//             focusEditor?.()
+//         },
+//         [focusEditor, dispatchClientAction]
+//     )
 
-    return <PromptSelectField onSelect={onSelect} onCloseByEscape={focusEditor} className={className} />
-}
+//     return <PromptSelectField onSelect={onSelect} onCloseByEscape={focusEditor} className={className} />
+// }
 
 const ModelSelectFieldToolbarItem: FunctionComponent<{
     userInfo: UserAccountInfo
