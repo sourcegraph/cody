1da2704d4e4fcf1484aa92782584ffd07e9ccd55
Beatrix <beatrix@sourcegraph.com>
2024-09-26 09:05:30 -0700

    update test snippets

--- vscode/src/chat/chat-view/prompt.test.ts
+++ vscode/src/chat/chat-view/prompt.test.ts
@@ -49,7 +49,8 @@ describe('DefaultPrompter', () => {
           [
             {
               "speaker": "human",
-              "text": "You are Cody, an AI coding assistant from Sourcegraph.If your answer contains fenced code blocks in Markdown, include the relevant full file path in the code block tag using this structure: \`\`\`$LANGUAGE:$FILEPATH\`\`\`.",
+              "text": "You are Cody, an AI coding assistant from Sourcegraph.If your answer contains fenced code blocks in Markdown, include the relevant full file path in the code block tag using this structure: \`\`\`$LANGUAGE:$FILEPATH\`\`\`
+          For executable terminal commands: enclose in a Markdown code block with the language tag "bash".",
             },
             {
               "speaker": "assistant",
@@ -131,7 +132,8 @@ describe('DefaultPrompter', () => {
           [
             {
               "speaker": "human",
-              "text": "You are Cody, an AI coding assistant from Sourcegraph.If your answer contains fenced code blocks in Markdown, include the relevant full file path in the code block tag using this structure: \`\`\`$LANGUAGE:$FILEPATH\`\`\`.
+              "text": "You are Cody, an AI coding assistant from Sourcegraph.If your answer contains fenced code blocks in Markdown, include the relevant full file path in the code block tag using this structure: \`\`\`$LANGUAGE:$FILEPATH\`\`\`
+          For executable terminal commands: enclose in a Markdown code block with the language tag "bash".
 
           Always respond with 🧀 emojis",
             },
