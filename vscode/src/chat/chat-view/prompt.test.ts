801ebb058bf54ea005a9dd900b846b518d83a93f
Beatrix <beatrix@sourcegraph.com>
2024-09-27 12:25:08 -0700

    update snippets

--- vscode/src/chat/chat-view/prompt.test.ts
+++ vscode/src/chat/chat-view/prompt.test.ts
@@ -50,7 +50,7 @@ describe('DefaultPrompter', () => {
             {
               "speaker": "human",
               "text": "You are Cody, an AI coding assistant from Sourcegraph.If your answer contains fenced code blocks in Markdown, include the relevant full file path in the code block tag using this structure: \`\`\`$LANGUAGE:$FILEPATH\`\`\`
-          For executable terminal commands: enclose in a Markdown code block with the language tag "bash".",
+          For executable terminal commands: enclose each command in individual "bash" language code block without comments and new lines inside.",
             },
             {
               "speaker": "assistant",
@@ -133,7 +133,7 @@ describe('DefaultPrompter', () => {
             {
               "speaker": "human",
               "text": "You are Cody, an AI coding assistant from Sourcegraph.If your answer contains fenced code blocks in Markdown, include the relevant full file path in the code block tag using this structure: \`\`\`$LANGUAGE:$FILEPATH\`\`\`
-          For executable terminal commands: enclose in a Markdown code block with the language tag "bash".
+          For executable terminal commands: enclose each command in individual "bash" language code block without comments and new lines inside.
 
           Always respond with 🧀 emojis",
             },
