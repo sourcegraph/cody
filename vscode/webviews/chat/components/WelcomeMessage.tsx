046bfe44e03fb3856e5da525ca97452e7a5b1c66
Jason Harris <jason.harris@sourcegraph.com>
2024-10-02 10:11:14 -0500

    remove unused functions

--- vscode/webviews/chat/components/WelcomeMessage.tsx
+++ vscode/webviews/chat/components/WelcomeMessage.tsx
@@ -1,37 +1,8 @@
-import type { LucideProps } from 'lucide-react'
 import type { FunctionComponent } from 'react'
-import type React from 'react'
 import { PromptList } from '../../components/promptList/PromptList'
 import { Button } from '../../components/shadcn/ui/button'
 import { useActionSelect } from '../../prompts/PromptsTab'
 import { View } from '../../tabs'
-import { useConfig } from '../../utils/useConfig'
-
-const MenuExample: FunctionComponent<{ children: React.ReactNode }> = ({ children }) => (
-    <span className="tw-p-1 tw-rounded tw-text-keybinding-foreground tw-border tw-border-keybinding-border tw-bg-keybinding-background tw-whitespace-nowrap">
-        {children}
-    </span>
-)
-
-type FeatureRowIcon = React.ForwardRefExoticComponent<
-    Omit<LucideProps, 'ref'> & React.RefAttributes<SVGSVGElement>
->
-
-const FeatureRowInlineIcon: FunctionComponent<{
-    Icon: FeatureRowIcon
-}> = ({ Icon }) => (
-    <Icon size={16} strokeWidth={1.25} className="tw-flex-none tw-inline-flex tw-mt-1 tw-opacity-80" />
-)
-
-const FeatureRow: FunctionComponent<{
-    icon: FeatureRowIcon
-    children: React.ReactNode
-}> = ({ icon, children }) => (
-    <div className="tw-py-2 tw-px-4 tw-inline-flex tw-gap-3 tw-text-foreground tw-items-start">
-        <FeatureRowInlineIcon Icon={icon} />
-        <div className="tw-grow">{children}</div>
-    </div>
-)
 
 const localStorageKey = 'chat.welcome-message-dismissed'
 
@@ -43,7 +14,6 @@ export const WelcomeMessage: FunctionComponent<WelcomeMessageProps> = ({ setView
     // Remove the old welcome message dismissal key that is no longer used.
     localStorage.removeItem(localStorageKey)
 
-    const config = useConfig()
     const runAction = useActionSelect()
 
     return (
