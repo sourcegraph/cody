8a8300d7a46d031df53946289678dac351433693
Valery Bugakov <skymk1@gmail.com>
2024-10-04 10:08:22 +0800

    Autocomplete: revert AuthStatus.kt changes

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/AuthStatus.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/AuthStatus.kt
@@ -1,41 +1,11 @@
 @file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
 package com.sourcegraph.cody.agent.protocol_generated;
 
-import com.google.gson.annotations.SerializedName;
-import com.google.gson.Gson;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import java.lang.reflect.Type;
-
-sealed class AuthStatus {
-  companion object {
-    val deserializer: JsonDeserializer<AuthStatus> =
-      JsonDeserializer { element: JsonElement, _: Type, context: JsonDeserializationContext ->
-        when (element.getAsJsonObject().get("endpoint").getAsString()) {
-          "https://example.com" -> context.deserialize<UnauthenticatedAuthStatus>(element, UnauthenticatedAuthStatus::class.java)
-          else -> throw Exception("Unknown discriminator ${element}")
-        }
-      }
-  }
-}
-
-data class UnauthenticatedAuthStatus(
-  val endpoint: EndpointEnum, // Oneof: https://example.com
+data class AuthStatus(
+  val endpoint: String,
   val authenticated: Boolean,
   val showNetworkError: Boolean? = null,
   val showInvalidAccessTokenError: Boolean? = null,
-  val pendingValidation: Boolean,
-) : AuthStatus() {
-
-  enum class EndpointEnum {
-    @SerializedName("https://example.com") `Https-example-com`,
-  }
-}
-
-data class AuthenticatedAuthStatus(
-  val endpoint: EndpointEnum, // Oneof: https://example.com
-  val authenticated: Boolean,
   val username: String,
   val isFireworksTracingEnabled: Boolean? = null,
   val hasVerifiedEmail: Boolean? = null,
@@ -47,11 +17,5 @@ data class AuthenticatedAuthStatus(
   val displayName: String? = null,
   val avatarURL: String? = null,
   val userCanUpgrade: Boolean? = null,
-  val pendingValidation: Boolean,
-) : AuthStatus() {
-
-  enum class EndpointEnum {
-    @SerializedName("https://example.com") `Https-example-com`,
-  }
-}
+)
 
