68d34362ca9c905a2c5ec078959c02f7fee94561
Piotr Kukielka <pkukielka@virtuslab.com>
2024-10-04 13:22:17 +0200

    Add protocol files

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/ModelAvailabilityStatus.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/ModelAvailabilityStatus.kt
@@ -1,7 +1,8 @@
 @file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
 package com.sourcegraph.cody.agent.protocol_generated;
 
-data class Chat_ModelsResult(
-  val models: List<ModelAvailabilityStatus>,
+data class ModelAvailabilityStatus(
+  val model: Model,
+  val isModelAvailable: Boolean,
 )
 
