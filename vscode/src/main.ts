c67d1592d499cf797002d4c37f542924e2ef45ea
Peter Guy <peter.guy@sourcegraph.com>
2024-10-04 11:40:45 -0700

    Move the proxy settings observable from main.ts because of build issues when building for the browser.

--- vscode/src/main.ts
+++ vscode/src/main.ts
@@ -105,10 +105,6 @@ import { SupercompletionProvider } from './supercompletions/supercompletion-prov
 import { parseAllVisibleDocuments, updateParseTreeOnEdit } from './tree-sitter/parse-tree-cache'
 import { version } from './version'
 
-import { proxySettings } from './configuration-proxy'
-
-import { setCustomAgent } from './fetch.node'
-
 /**
  * Start the extension, watching all relevant configuration and secrets for changes.
  */
@@ -161,8 +157,6 @@ export async function start(
             )
         )
     )
-
-    proxySettings.subscribe(setCustomAgent)
 
     setEditorWindowIsFocused(() => vscode.window.state.focused)
 
