dea852bf80b9e8d214afa80b7cfd7ed2a781b1a4
PriNova <info@prinova.de>
2024-10-17 18:05:30 +0100

    fix Biome format

--- vscode/src/chat/protocol.ts
+++ vscode/src/chat/protocol.ts
@@ -239,7 +239,9 @@ export const SG_CHANGELOG_URL = new URL('https://sourcegraph.com/changelog')
 
 // Community and support
 export const DISCORD_URL = new URL('https://discord.gg/s2qDtYGnAE')
-export const CODY_FEEDBACK_URL = new URL('https://community.sourcegraph.com/new-topic?title=&body=&category=cody&tags=feedback,vs-code')
+export const CODY_FEEDBACK_URL = new URL(
+    'https://community.sourcegraph.com/new-topic?title=&body=&category=cody&tags=feedback,vs-code'
+)
 export const CODY_SUPPORT_URL = new URL('https://srcgr.ph/cody-support')
 export const CODY_OLLAMA_DOCS_URL = new URL(
     'https://sourcegraph.com/docs/cody/clients/install-vscode#supported-local-ollama-models-with-cody'
