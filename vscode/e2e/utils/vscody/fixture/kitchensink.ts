35c9898af159576872d484f9e176aeba18f7d4bb
Rik Nauta <rik.nauta@sourcegraph.com>
2024-10-21 12:12:23 +0200

    Fix tests ubuntu

--- vscode/e2e/utils/vscody/fixture/kitchensink.ts
+++ vscode/e2e/utils/vscody/fixture/kitchensink.ts
@@ -1,15 +1,39 @@
 import fs from 'node:fs/promises'
-import { test as _test } from '@playwright/test'
-import { copy as copyExt } from 'fs-extra'
 import 'node:http'
 import 'node:https'
 import path from 'node:path'
+import { test as _test } from '@playwright/test'
+import { copy as copyExt } from 'fs-extra'
 import type { TestContext, WorkerContext } from '.'
 import { CODY_VSCODE_ROOT_DIR, retry } from '../../helpers'
 
 export const kitchensinkFixture = _test.extend<TestContext, WorkerContext>({
+    gitconfigPath: [
+        async ({ validWorkerOptions }, use, testInfo) => {
+            const configPath = path.resolve(
+                validWorkerOptions.globalTmpDir,
+                `${testInfo.parallelIndex}_git_config`
+            )
+            await fs.writeFile(
+                configPath,
+                `
+[user]
+    name = Test User ${testInfo.parallelIndex}
+    email = test_{testInfo.parallelIndex}@sourcegraph.com
+[init]
+    defaultBranch = main
+`
+            )
+
+            use(configPath)
+        },
+        { scope: 'worker' },
+    ],
     workspaceDir: [
-        async ({ validOptions }, use, testInfo) => {
+        async ({ validOptions, gitconfigPath }, use, testInfo) => {
+            process.env.GIT_CONFIG_SYSTEM = '/dev/null'
+            process.env.GIT_CONFIG_GLOBAL = gitconfigPath
+
             const dir = await fs.mkdtemp(path.resolve(validOptions.globalTmpDir, 'test-workspace-'))
 
             await copyExt(path.resolve(CODY_VSCODE_ROOT_DIR, validOptions.templateWorkspaceDir), dir, {
