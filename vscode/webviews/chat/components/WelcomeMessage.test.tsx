f5a6d1172ff3e6c45a4e18a82efaee28d3e70cc5
Vova Kulikov <vova@sourcegraph.com>
2024-10-28 15:29:18 -0300

    Update tests

--- vscode/webviews/chat/components/WelcomeMessage.test.tsx
+++ vscode/webviews/chat/components/WelcomeMessage.test.tsx
@@ -1,7 +1,4 @@
-import {
-    AUTH_STATUS_FIXTURE_AUTHED,
-    type ClientCapabilitiesWithLegacyFields,
-} from '@sourcegraph/cody-shared'
+import { AUTH_STATUS_FIXTURE_AUTHED, type ClientCapabilitiesWithLegacyFields, CodyIDE, } from '@sourcegraph/cody-shared'
 import { fireEvent, render, screen } from '@testing-library/react'
 import { describe, expect, test, vi } from 'vitest'
 import { AppWrapperForTest } from '../../AppWrapperForTest'
@@ -35,7 +32,7 @@ describe('WelcomeMessage', () => {
             } satisfies Partial<ClientCapabilitiesWithLegacyFields> as ClientCapabilitiesWithLegacyFields,
             authStatus: AUTH_STATUS_FIXTURE_AUTHED,
         } satisfies Partial<useConfigModule.Config> as useConfigModule.Config)
-        render(<WelcomeMessage setView={() => {}} />, {
+        render(<WelcomeMessage IDE={CodyIDE.VSCode} setView={() => {}} />, {
             wrapper: AppWrapperForTest,
         })
         openCollapsiblePanels()
@@ -51,7 +48,7 @@ describe('WelcomeMessage', () => {
             } satisfies Partial<ClientCapabilitiesWithLegacyFields> as ClientCapabilitiesWithLegacyFields,
             authStatus: AUTH_STATUS_FIXTURE_AUTHED,
         } satisfies Partial<useConfigModule.Config> as useConfigModule.Config)
-        render(<WelcomeMessage setView={() => {}} />, {
+        render(<WelcomeMessage IDE={CodyIDE.VSCode} setView={() => {}} />, {
             wrapper: AppWrapperForTest,
         })
         openCollapsiblePanels()
