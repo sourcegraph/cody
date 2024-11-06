793f6ac337b80f173c4115dc0e7825688bf2f9c8
Valery Bugakov <skymk1@gmail.com>
2024-11-06 10:55:29 +0800

    Autocomplete: remove the extended language pool option

--- vscode/src/completions/context/utils.test.ts
+++ vscode/src/completions/context/utils.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest'
+
 import { shouldBeUsedAsContext } from './utils'
 
 describe('shouldBeUsedAsContext', () => {
@@ -6,7 +7,6 @@ describe('shouldBeUsedAsContext', () => {
         it('returns true for the same language', () => {
             expect(
                 shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: false,
                     baseLanguageId: 'javascript',
                     languageId: 'javascript',
                 })
@@ -16,14 +16,12 @@ describe('shouldBeUsedAsContext', () => {
         it('allows js as context for jsx and vice versa', () => {
             expect(
                 shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: false,
                     baseLanguageId: 'javascript',
                     languageId: 'javascriptreact',
                 })
             ).toBe(true)
             expect(
                 shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: false,
                     baseLanguageId: 'javascriptreact',
                     languageId: 'javascript',
                 })
@@ -33,66 +31,14 @@ describe('shouldBeUsedAsContext', () => {
         it('allows ts as context for tsx and vice versa', () => {
             expect(
                 shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: false,
                     baseLanguageId: 'typescript',
                     languageId: 'typescriptreact',
                 })
             ).toBe(true)
             expect(
                 shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: false,
                     baseLanguageId: 'typescriptreact',
                     languageId: 'typescript',
-                })
-            ).toBe(true)
-        })
-    })
-
-    describe('with extended language pool', () => {
-        it('allows css files as context for template languages', () => {
-            expect(
-                shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: true,
-                    baseLanguageId: 'typescriptreact',
-                    languageId: 'scss',
-                })
-            ).toBe(true)
-            expect(
-                shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: true,
-                    baseLanguageId: 'javascriptreact',
-                    languageId: 'less',
-                })
-            ).toBe(true)
-            expect(
-                shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: true,
-                    baseLanguageId: 'handlebars',
-                    languageId: 'css',
-                })
-            ).toBe(true)
-        })
-
-        it('allows template languages to be used as context for css files', () => {
-            expect(
-                shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: true,
-                    baseLanguageId: 'scss',
-                    languageId: 'typescriptreact',
-                })
-            ).toBe(true)
-            expect(
-                shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: true,
-                    baseLanguageId: 'less',
-                    languageId: 'javascriptreact',
-                })
-            ).toBe(true)
-            expect(
-                shouldBeUsedAsContext({
-                    enableExtendedLanguagePool: true,
-                    baseLanguageId: 'css',
-                    languageId: 'handlebars',
                 })
             ).toBe(true)
         })
