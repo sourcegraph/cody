5bde0b61f732cd0e00db24482d8f63f2719a7657
arafatkatze <arafat.da.khan@gmail.com>
2024-11-16 06:30:17 +0400

    Integrate OpenTelemetry tracing in Cody Webview

--- vscode/src/chat/chat-view/ChatController.ts
+++ vscode/src/chat/chat-view/ChatController.ts
@@ -16,9 +16,6 @@ import {
     skip,
     skipPendingOperation,
 } from '@sourcegraph/cody-shared'
-import * as uuid from 'uuid'
-import * as vscode from 'vscode'
-
 import {
     type BillingCategory,
     type BillingProduct,
@@ -77,6 +74,8 @@ import {
     truncatePromptString,
     userProductSubscription,
 } from '@sourcegraph/cody-shared'
+import * as uuid from 'uuid'
+import * as vscode from 'vscode'
 
 import type { Span } from '@opentelemetry/api'
 import { captureException } from '@sentry/core'
@@ -106,6 +105,7 @@ import { publicRepoMetadataIfAllWorkspaceReposArePublic } from '../../repository
 import { authProvider } from '../../services/AuthProvider'
 import { AuthProviderSimplified } from '../../services/AuthProviderSimplified'
 import { localStorage } from '../../services/LocalStorageProvider'
+import { TraceSender } from '../../services/open-telemetry/trace-sender'
 import { recordExposedExperimentsToSpan } from '../../services/open-telemetry/utils'
 import {
     handleCodeFromInsertAtCursor,
@@ -324,6 +324,9 @@ export class ChatController implements vscode.Disposable, vscode.WebviewViewProv
                     message.instruction,
                     message.fileName
                 )
+                break
+            case 'trace-export':
+                TraceSender.send(message.traceSpanEncodedJson)
                 break
             case 'smartApplyAccept':
                 await vscode.commands.executeCommand('cody.fixup.codelens.accept', message.id)
