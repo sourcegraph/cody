372bfd4795f5c7217d394c689cee40df59685538
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-11-18 13:26:40 +0900

    Fix a bunch of tests by desugaring using properly.

--- agent/src/esbuild.mjs
+++ agent/src/esbuild.mjs
@@ -52,6 +52,7 @@ async function buildAgent() {
         bundle: true,
         outfile: path.join('dist', 'index.js'),
         platform: 'node',
+        target: 'es2022',
         sourcemap: true,
         logLevel: 'error',
         external: ['typescript'],
