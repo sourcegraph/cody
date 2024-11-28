6f57a472c39d6967fc50eb814b1605fcdbe7b431
Felix Kling <felix@felix-kling.de>
2024-11-28 14:31:07 +0100

    Better encapsulation

---
+++ lib/prompt-editor/src/v2/lexical-interop.ts
@@ -0,0 +1,140 @@
+import { SerializedContextItem, SerializedContextItemMentionNode, SerializedPromptEditorState, SerializedPromptEditorValue } from "@sourcegraph/cody-shared"
+import { SerializedLexicalNode, SerializedParagraphNode, SerializedRootNode, SerializedTextNode } from "lexical"
+import { EditorState } from "prosemirror-state"
+import { Node } from "prosemirror-model"
+
+export function fromSerializedPromptEditorState(
+    state: SerializedPromptEditorState
+): unknown {
+    function fromSerializedLexicalNode(node: SerializedLexicalNode): unknown {
+        switch (node.type) {
+            case 'root': {
+                return {
+                    type: 'doc',
+                    content: (node as SerializedRootNode).children.map(fromSerializedLexicalNode),
+                }
+            }
+            case 'paragraph': {
+                return {
+                    type: 'paragraph',
+                    content: (node as SerializedParagraphNode).children.map(fromSerializedLexicalNode),
+                }
+            }
+            case 'text': {
+                return {
+                    type: 'text',
+                    text: (node as SerializedTextNode).text,
+                }
+            }
+            case 'contextItemMention': {
+                return {
+                    type: 'mention',
+                    attrs: {
+                        item: (node as SerializedContextItemMentionNode).contextItem,
+                    },
+                    content: [
+                        {
+                            type: 'text',
+                            text: (node as SerializedContextItemMentionNode).text,
+                        },
+
+                    ],
+                }
+            }
+        }
+        return undefined
+    }
+    return fromSerializedLexicalNode(state.lexicalEditorState.root)
+}
+
+export function toSerializedPromptEditorValue(
+    state: EditorState
+): SerializedPromptEditorValue {
+    const contextItems: SerializedContextItem[] = []
+    const direction = typeof window !== 'undefined' ? window.getComputedStyle(window.document.body).direction : null
+
+    state.doc.descendants(node => {
+        if (node.type.name === 'mention') {
+            contextItems.push(node.attrs.item)
+            return false
+        }
+        return true
+    })
+
+    function serializeNode(node: Node): SerializedLexicalNode|undefined {
+        switch (node.type.name) {
+            case 'paragraph': {
+                const children: SerializedLexicalNode[] = []
+                node.forEach(child => {
+                    const serializedChild = serializeNode(child)
+                    if (serializedChild) {
+                        children.push(serializedChild)
+                    }
+                })
+                return {
+                    type: 'paragraph',
+                    children,
+                    direction: direction === 'rtl' ? 'rtl' : 'ltr',
+                    format: '',
+                    indent: 0,
+                    version: 1,
+                    textStyle: '',
+                    textFormat: 0,
+                } as SerializedParagraphNode
+            }
+            case 'text': {
+                return {
+                    type: 'text',
+                    text: node.text || '',
+                    detail: 0,
+                    format: 0,
+                    mode: 'normal',
+                    style: '',
+                    version: 1,
+                } as SerializedTextNode
+            }
+            case 'mention': {
+                return {
+                    type: 'contextItemMention',
+                    text: node.textContent,
+                    contextItem: node.attrs.item,
+                    isFromInitialContext: node.attrs.isFromInitialContext,
+                    version: 1,
+                } as SerializedContextItemMentionNode
+            }
+        }
+        return undefined
+    }
+
+    function serializeRoot(root: Node): SerializedRootNode {
+        const children: SerializedLexicalNode[] = []
+        root.forEach(child => {
+            const serializedChild = serializeNode(child)
+            if (serializedChild) {
+                children.push(serializedChild)
+            }
+        })
+
+        return {
+            type: 'root',
+            children,
+            format: '',
+            indent: 0,
+            version: 1,
+            direction: direction === 'rtl' ? 'rtl' : 'ltr',
+        }
+    }
+
+
+    return {
+        text: state.doc.textContent,
+        contextItems,
+        editorState: {
+            v: 'lexical-v1',
+            minReaderV: 'lexical-v1',
+            lexicalEditorState: {
+                root: serializeRoot(state.doc),
+            }
+        },
+    }
+}
