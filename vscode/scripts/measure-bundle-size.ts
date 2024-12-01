b3ecde589402a481b85359283f35e90d361431f4
arafatkatze <arafat.da.khan@gmail.com>
2024-12-01 16:50:45 +0400

    Add the context computation span to the chat trace

--- vscode/scripts/measure-bundle-size.ts
+++ vscode/scripts/measure-bundle-size.ts
@@ -2,7 +2,7 @@ import { promises as fs } from 'node:fs'
 
 const SIZE_LIMITS = {
     extension: 15 * 1024 * 1024, // 15MB
-    webview: 1 * 1024 * 1024, // 10MB
+    webview: 10 * 1024 * 1024, // 10MB
 }
 
 function prettyPrintBytes(bytes: number): string {
