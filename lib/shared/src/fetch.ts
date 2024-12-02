6606abe33730b2bceaed934044c5fe0471fbc305
julialeex <julia.xu@sourcegraph.com>
2024-12-02 13:29:15 -0800

    attempt to fix unit test unauthed.test.ts

--- lib/shared/src/fetch.ts
+++ lib/shared/src/fetch.ts
@@ -2,7 +2,6 @@
  * By hard-requiring isomorphic-fetch, we ensure that even in newer Node environments that include
  * `fetch` by default, we still use the `node-fetch` polyfill and have access to the networking code
  */
-import isomorphicFetch from 'isomorphic-fetch'
 import { globalAgentRef } from './fetch.patch'
 import { addCodyClientIdentificationHeaders } from './sourcegraph-api/client-name-version'
 import type { BrowserOrNodeResponse } from './sourcegraph-api/graphql/client'
@@ -18,5 +17,5 @@ export function fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Bro
         ...init,
         agent: globalAgentRef.agent,
     }
-    return isomorphicFetch(input, initWithAgent)
+    return globalThis.fetch(input, initWithAgent)
 }
