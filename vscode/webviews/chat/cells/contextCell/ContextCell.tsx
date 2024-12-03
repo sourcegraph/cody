4436be32bc14dbce2d5987156cc9d76a376b124c
Beatrix <beatrix@sourcegraph.com>
2024-12-02 19:08:51 -0800

    update styles

--- vscode/webviews/chat/cells/contextCell/ContextCell.tsx
+++ vscode/webviews/chat/cells/contextCell/ContextCell.tsx
@@ -209,7 +209,7 @@ export const ContextCell: FunctionComponent<{
                         >
                             {isContextLoading ? (
                                 isDeepCodyEnabled ? (
-                                    <div className="tw-flex tw-align-middle tw-rounded-md tw-bg-muted-transparent tw-p-4">
+                                    <div className="tw-flex tw-items-center tw-rounded-md tw-bg-muted-transparent tw-p-4">
                                         <LoadingDots />
                                         <div className="tw-ml-4">
                                             May take a few seconds to fetch relevannt context to improve
