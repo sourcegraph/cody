722f36af03b0d6a622570394578511de39f708ca
Beatrix <beatrix@sourcegraph.com>
2024-12-09 18:18:14 -0800

    clean up

--- vscode/src/chat/agentic/DeepCody.ts
+++ vscode/src/chat/agentic/DeepCody.ts
@@ -31,10 +31,7 @@ export class DeepCodyAgent extends CodyChatAgent {
         return CODYAGENT_PROMPTS.review
             .replace('{{CODY_TOOLS_PLACEHOLDER}}', join(toolInstructions))
             .replace('{{CODY_TOOLS_EXAMPLES_PLACEHOLDER}}', join(toolExamples))
-            .replace(
-                '{{CODY_IDE}}',
-                PromptString.fromAgentClientIDE(clientCapabilities().agentIDE || CodyIDE.VSCode)
-            )
+            .replace('{{CODY_IDE}}', fromAgentClientIDE(clientCapabilities().agentIDE || CodyIDE.VSCode))
     }
 
     /**
@@ -129,5 +126,25 @@ export class DeepCodyAgent extends CodyChatAgent {
             })
             return []
         }
+    }
+}
+
+// TODO: move to shared
+function fromAgentClientIDE(client: CodyIDE) {
+    switch (client) {
+        case CodyIDE.Web:
+            return ps`Sourcegraph Web`
+        case CodyIDE.VisualStudio:
+            return ps`Visual Studio`
+        case CodyIDE.JetBrains:
+            return ps`JetBrains`
+        case CodyIDE.Eclipse:
+            return ps`Eclipse`
+        case CodyIDE.Emacs:
+            return ps`Emacs`
+        case CodyIDE.Neovim:
+            return ps`Neovim`
+        default:
+            return ps`VS Code`
     }
 }
