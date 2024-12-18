33bfb9b6520aa519d2a6c5c033a7ef437bdd72c8
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-12-18 16:44:07 +0900

    Give JetBrains time to start

--- ide-test/jetbrains/smoke.test.ts
+++ ide-test/jetbrains/smoke.test.ts
@@ -9,9 +9,12 @@ test(
             cwd: '../jetbrains',
             env: {
                 SKIP_CODE_SEARCH_BUILD: 'true',
+                // TODO: Hermetically seal the right Java to use
+                ...process.env
             },
             stdio: ['ignore', process.stdout, process.stderr],
         })
+        await new Promise(resolve => setTimeout(resolve, 60 * 1000))
         // TODO: Poll the development server until it is up
         const browser = await chromium.connectOverCDP('ws://localhost:8083/jb')
         // TODO: Make some assertions
