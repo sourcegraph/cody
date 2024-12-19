f363cf4d8d3f02b99d18aac5c42641a95dff3470
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-12-19 13:18:34 +0900

    Wait for the test server to be up.

--- ide-test/jetbrains/smoke.test.ts
+++ ide-test/jetbrains/smoke.test.ts
@@ -1,11 +1,33 @@
 import { spawn } from 'node:child_process'
 import { chromium, test } from '@playwright/test'
 
+async function checkTestServerStarted(): Promise<void> {
+    let giveUpDeadline = Date.now() + 60 * 1000
+    let lastError: Error | undefined
+    while (Date.now() < giveUpDeadline) {
+        try {
+            const response = await fetch('http://localhost:8083/healthz')
+            if (response.ok) {
+                return
+            }
+            await new Promise(resolve => setTimeout(resolve, 10))
+        } catch (e: unknown) {
+            if (e instanceof Error) {
+                lastError = e
+            }
+        }
+    }
+    if (lastError) {
+        throw lastError
+    }
+    throw new Error('timed out trying to connect to JetBrains test support server')
+}
+
 test(
     'loads the plugin and displays content in the sidebar',
     async () => {
         // TODO: Refactor this out into a "JetBrains driver"
-        spawn('./gradlew', [':test-support:runIdeForTesting'], {
+        const ideProcess = spawn('./gradlew', [':test-support:runIdeForTesting'], {
             cwd: '../jetbrains',
             env: {
                 SKIP_CODE_SEARCH_BUILD: 'true',
@@ -14,9 +36,13 @@ test(
             },
             stdio: ['ignore', process.stdout, process.stderr],
         })
-        await new Promise(resolve => setTimeout(resolve, 60 * 1000))
-        // TODO: Poll the development server until it is up
-        const browser = await chromium.connectOverCDP('ws://localhost:8083/jb')
-        // TODO: Make some assertions
-        console.log(browser)
+        try {
+            await checkTestServerStarted()
+            // TODO: Poll the development server until it is up
+            const browser = await chromium.connectOverCDP('ws://localhost:8083/jb')
+            // TODO: Make some assertions
+            console.log(browser)
+        } finally {
+            ideProcess.kill()
+        }
     })
