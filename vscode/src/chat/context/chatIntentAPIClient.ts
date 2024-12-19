3bc711e95b9c778ddeb080f2a35b189fb602f839
Naman Kumar <naman@outlook.in>
2024-12-19 12:52:33 +0530

    format

--- vscode/src/chat/context/chatIntentAPIClient.ts
+++ vscode/src/chat/context/chatIntentAPIClient.ts
@@ -1,39 +1,33 @@
 import {
-	FeatureFlag,
-	type SourcegraphGraphQLAPIClient,
-	featureFlagProvider,
-	storeLastValue,
-} from "@sourcegraph/cody-shared";
-import * as vscode from "vscode";
+    FeatureFlag,
+    type SourcegraphGraphQLAPIClient,
+    featureFlagProvider,
+    storeLastValue,
+} from '@sourcegraph/cody-shared'
+import * as vscode from 'vscode'
 
 export class ChatIntentAPIClient {
-	private featureCodyExperimentalOneBox = storeLastValue(
-		featureFlagProvider.evaluatedFeatureFlag(
-			FeatureFlag.CodyExperimentalOneBox,
-		),
-	);
+    private featureCodyExperimentalOneBox = storeLastValue(
+        featureFlagProvider.evaluatedFeatureFlag(FeatureFlag.CodyExperimentalOneBox)
+    )
 
-	constructor(private readonly apiClient: SourcegraphGraphQLAPIClient) {}
+    constructor(private readonly apiClient: SourcegraphGraphQLAPIClient) {}
 
-	public dispose(): void {
-		this.featureCodyExperimentalOneBox.subscription.unsubscribe();
-	}
+    public dispose(): void {
+        this.featureCodyExperimentalOneBox.subscription.unsubscribe()
+    }
 
-	public async detectChatIntent(interactionID: string, query: string) {
-		if (!this.isIntentDetectionAPIEnabled()) {
-			return;
-		}
-		return this.apiClient.chatIntent(interactionID, query);
-	}
+    public async detectChatIntent(interactionID: string, query: string) {
+        if (!this.isIntentDetectionAPIEnabled()) {
+            return
+        }
+        return this.apiClient.chatIntent(interactionID, query)
+    }
 
-	private isIntentDetectionAPIEnabled(): boolean {
-		if (
-			vscode.workspace
-				.getConfiguration()
-				.get<boolean>("cody.internal.intentDetectionAPI")
-		) {
-			return true;
-		}
-		return !!this.featureCodyExperimentalOneBox.value.last;
-	}
+    private isIntentDetectionAPIEnabled(): boolean {
+        if (vscode.workspace.getConfiguration().get<boolean>('cody.internal.intentDetectionAPI')) {
+            return true
+        }
+        return !!this.featureCodyExperimentalOneBox.value.last
+    }
 }
