b701b117efb297a33800c06776bf857197e0d58e
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-12-19 21:33:01 +0900

    Exchanging a few messages.

--- jetbrains/test-support/src/main/kotlin/com/sourcegraph/jetbrains/testing/DebugServerStartupActivity.kt
+++ jetbrains/test-support/src/main/kotlin/com/sourcegraph/jetbrains/testing/DebugServerStartupActivity.kt
@@ -9,16 +9,20 @@ import io.ktor.http.withCharset
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.request.uri
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.websocket.*
 import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.Frame
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class DebugServerStartupActivity : ProjectActivity {
   override suspend fun execute(project: Project) {
     withContext(Dispatchers.IO) {
+      val devToolsUrlPrefix = "http://localhost:8083/cdp/"
       val server = embeddedServer(Netty, port = 8083) {
         install(WebSockets)
         routing {
@@ -26,14 +30,16 @@ class DebugServerStartupActivity : ProjectActivity {
             call.respondText("OK")
           }
           get("/webviews") {
-              val urlPrefix = "http://localhost:8083/cdp/"
-              val webviews = service<ChromeDevToolsProtocolForwarder>().listWebviews(urlPrefix)
+              val webviews = service<ChromeDevToolsProtocolForwarder>().listWebviews(devToolsUrlPrefix)
               // TODO: Replace this with ktor content negotiation and serialization
               // when the core plugin build lets us use the Kotlin serialization plugin
               val gson = com.google.gson.Gson()
               val json = gson.toJson(webviews)
               call.respondText(json, ContentType.Application.Json.withCharset(Charsets.UTF_8))
           }
+            get("/json/version/") {
+                call.respondText(service<ChromeDevToolsProtocolForwarder>().version(devToolsUrlPrefix), ContentType.Application.Json.withCharset(Charsets.UTF_8))
+            }
           webSocket("/cdp/{webviewId}") {
             val webviewId = call.parameters["webviewId"]?.toIntOrNull()
             if (webviewId == null) {
@@ -41,6 +47,10 @@ class DebugServerStartupActivity : ProjectActivity {
                 return@webSocket
             }
             service<ChromeDevToolsProtocolForwarder>().handleSession(webviewId, this)
+          }
+        get {
+            println("got request for ${call.request.uri}")
+            call.respond(HttpStatusCode.NotFound, call.request.uri)
           }
         }
       }
