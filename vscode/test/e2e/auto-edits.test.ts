9cc8ecad1ef4c238c4a47a6cc090b9fce1af45b3
hitesh-1997 <hitesh.sagtani@sourcegraph.com>
2024-12-23 16:59:05 +0530

    try platform specific snapshots

--- vscode/test/e2e/auto-edits.test.ts
+++ vscode/test/e2e/auto-edits.test.ts
@@ -35,6 +35,7 @@ interface AutoeditsTestOptions {
     page: Page
     sidebar: Frame | null
     fileName: string
+    testCaseName: string
     lineOptions: LineOptions[]
 }
 
@@ -42,8 +43,12 @@ const autoeditsTestHelper = async ({
     page,
     sidebar,
     fileName,
+    testCaseName,
     lineOptions,
 }: AutoeditsTestOptions): Promise<void> => {
+    const platform = process.platform
+    const snapshotPlatform = platform === 'darwin' ? 'macos' : platform === 'linux' ? 'linux' : 'windows'
+
     // Use a large number to go the end of the line
     const maxColumnNumber = Number.MAX_SAFE_INTEGER
 
@@ -52,16 +57,14 @@ const autoeditsTestHelper = async ({
         width: 1024,
         height: 741,
     })
-    await page.evaluate(() => {
-        window.devicePixelRatio = 1;
-    });
 
     // In your test setup or beforeAll hook
     await page.evaluate(() => {
-        document.body.style.fontFamily = 'monospace';
-        document.body.style.fontSize = '14px';
-        document.body.style.lineHeight = '1.5';
-    });
+        document.body.style.fontFamily = 'monospace'
+        document.body.style.fontSize = '14px'
+        document.body.style.lineHeight = '1.5'
+        window.devicePixelRatio = 1
+    })
 
     // Sign into Cody
     await sidebarSignin(page, sidebar)
@@ -78,6 +81,7 @@ const autoeditsTestHelper = async ({
     await sidebarExplorer(page).click()
 
     for (const { lineNumber, clip } of lineOptions) {
+        const snapshotName = `${testCaseName}-${lineNumber}.png`
         await executeCommandInPalette(page, 'Go to Line/Column')
         await page.keyboard.type(`${lineNumber}:${maxColumnNumber}`)
         await page.keyboard.press('Enter')
@@ -87,7 +91,9 @@ const autoeditsTestHelper = async ({
         // Wait for the diff view to stabilize - required to reduce flakiness
         await page.waitForTimeout(1000)
 
-        await expect(page).toHaveScreenshot({ clip })
+        await expect(page).toHaveScreenshot([snapshotPlatform, snapshotName], {
+            clip: clip,
+        })
     }
 }
 
@@ -102,5 +108,11 @@ test('autoedits-multi-line-diff-view', async ({ page, sidebar }) => {
             clip: { x: 100, y: 300, width: 500, height: 150 },
         },
     ]
-    await autoeditsTestHelper({ page, sidebar, fileName: 'diff-view-example-1.py', lineOptions })
+    await autoeditsTestHelper({
+        page,
+        sidebar,
+        fileName: 'diff-view-example-1.py',
+        testCaseName: 'autoedits-multi-line-diff-view',
+        lineOptions,
+    })
 })
