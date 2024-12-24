e3ea8cf7e0b52634edfa71078dec8a99e438942a
hitesh-1997 <hitesh.sagtani@sourcegraph.com>
2024-12-24 17:07:29 +0530

    test in zen mode without platform specific diffs

--- vscode/test/e2e/auto-edits.test.ts
+++ vscode/test/e2e/auto-edits.test.ts
@@ -46,8 +46,8 @@ const autoeditsTestHelper = async ({
     testCaseName,
     lineOptions,
 }: AutoeditsTestOptions): Promise<void> => {
-    const platform = process.platform
-    const snapshotPlatform = platform === 'darwin' ? 'macos' : platform === 'linux' ? 'linux' : 'windows'
+    // const platform = process.platform
+    // const snapshotPlatform = platform === 'darwin' ? 'macos' : platform === 'linux' ? 'linux' : 'windows'
 
     // Use a large number to go the end of the line
     const maxColumnNumber = Number.MAX_SAFE_INTEGER
@@ -75,10 +75,13 @@ const autoeditsTestHelper = async ({
     await page.getByRole('treeitem', { name: 'auto-edits' }).locator('a').click()
     await page.getByRole('treeitem', { name: fileName }).locator('a').click()
 
-    await page.waitForTimeout(500)
-
     // Close the explorer view
-    await sidebarExplorer(page).click()
+    // We don't need it when executing in zen mode, keeping the logic commented for now in case need it later.
+    // await page.waitForTimeout(500)
+    // await sidebarExplorer(page).click()
+
+    // Activate the zen mode to remove diffs from any other elements
+    await executeCommandInPalette(page, 'View: Toggle Zen Mode')
 
     for (const { lineNumber, clip } of lineOptions) {
         const snapshotName = `${testCaseName}-${lineNumber}.png`
@@ -86,12 +89,30 @@ const autoeditsTestHelper = async ({
         await page.keyboard.type(`${lineNumber}:${maxColumnNumber}`)
         await page.keyboard.press('Enter')
 
+        // Get the active text editor element and click at current position
+        // This helps make sure that no other element has the focus and snapshot diff is not affected by other elements
+        await page.waitForSelector('.monaco-editor.focused')
+        const editor = await page.locator('.monaco-editor.focused')
+        const editorBounds = await editor.boundingBox()
+        if (editorBounds) {
+            // Click in the middle of the current line
+            await page.mouse.click(
+                editorBounds.x + editorBounds.width / 2,
+                editorBounds.y +
+                    lineNumber *
+                        Number.parseInt(
+                            await page.evaluate(() => getComputedStyle(document.body).lineHeight)
+                        )
+            )
+        }
+
         await executeCommandInPalette(page, 'Cody: Autoedits Manual Trigger')
 
         // Wait for the diff view to stabilize - required to reduce flakiness
         await page.waitForTimeout(1000)
 
-        await expect(page).toHaveScreenshot([snapshotPlatform, snapshotName], {
+        // await expect(page).toHaveScreenshot([snapshotPlatform, snapshotName], {
+        await expect(page).toHaveScreenshot([snapshotName], {
             clip: clip,
         })
     }
@@ -101,11 +122,11 @@ test('autoedits-multi-line-diff-view', async ({ page, sidebar }) => {
     const lineOptions: LineOptions[] = [
         {
             lineNumber: 70,
-            clip: { x: 100, y: 300, width: 700, height: 250 },
+            // clip: { x: 100, y: 300, width: 700, height: 250 },
         },
         {
             lineNumber: 76,
-            clip: { x: 100, y: 300, width: 500, height: 150 },
+            // clip: { x: 100, y: 300, width: 500, height: 150 },
         },
     ]
     await autoeditsTestHelper({
