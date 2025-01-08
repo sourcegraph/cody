d523b941349e41023c391fcc52eb850916f012a9
Beatrix <68532117+abeatrix@users.noreply.github.com>
2025-01-08 15:54:25 -0800

    feat(webview): replace Deep Cody with Agentic Chat (#6561)
    CLOSE
    https://linear.app/sourcegraph/issue/CODY-4638/change-the-daily-hard-limit-to-200
    
    - Updates the rate limit error message in the DeepCodyRateLimiter class
    to use 'Agentic Chat' instead of 'Deep Cody'.
    - This change increases the Deep Cody rate limit multiplier from 2 to 4
    when the feature flag is enabled. This will allow more requests to be
    processed for Deep Cody (200 max), improving the overall user
    experience.
    - Remove Deep Cody client model as we have moved it out of the Model
    dropdown.
    
    ## Test plan
    
    <!-- Required. See
    https://docs-legacy.sourcegraph.com/dev/background-information/testing_principles.
    -->
    
    Green CI - No additional testing required as this is a configuration
    change. All current unit tests should continue to pass.
    
    ## Changelog
    
    <!-- OPTIONAL; info at
    https://www.notion.so/sourcegraph/Writing-a-changelog-entry-dd997f411d524caabf0d8d38a24a878c
    -->

--- lib/shared/src/models/client.ts
+++ lib/shared/src/models/client.ts
@@ -1,4 +1,4 @@
-import { FeatureFlag, type ServerModel } from '..'
+import type { FeatureFlag, ServerModel } from '..'
 import type { ModelTag } from './tags'
 
 /**
@@ -9,27 +9,8 @@ import type { ModelTag } from './tags'
  */
 export function getExperimentalClientModelByFeatureFlag(flag: FeatureFlag): ServerModel | null {
     switch (flag) {
-        case FeatureFlag.DeepCody:
-            return getDeepCodyServerModel()
         default:
             return null
-    }
-}
-
-function getDeepCodyServerModel(): ServerModel {
-    return {
-        // This modelRef does not exist in the backend and is used to identify the model in the client.
-        modelRef: 'sourcegraph::2023-06-01::deep-cody',
-        displayName: 'Deep Cody',
-        modelName: 'deep-cody',
-        capabilities: ['chat'],
-        category: 'accuracy',
-        status: 'experimental' as ModelTag.Experimental,
-        tier: 'pro' as ModelTag.Pro,
-        contextWindow: {
-            maxInputTokens: 45000,
-            maxOutputTokens: 4000,
-        },
     }
 }
 
