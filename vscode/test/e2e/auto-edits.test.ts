d898e40ab37ce6093697e8549013deaca2eabcaf
Tom Ross <tom@umpox.com>
2025-01-10 09:50:29 +0000

    adjust pixel ratio and run 100 times in CI

--- vscode/test/e2e/auto-edits.test.ts
+++ vscode/test/e2e/auto-edits.test.ts
@@ -153,7 +153,7 @@ const autoeditsTestHelper = async ({
 
         await expect(page).toHaveScreenshot([snapshotPlatform, snapshotName], {
             maxDiffPixelRatio: 0.02,
-            maxDiffPixels: 1000,
+            maxDiffPixels: 500,
             clip,
         })
     }
