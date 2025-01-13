4e5952e014a9a88d294c1ccf453d0facec657469
Valery Bugakov <skymk1@gmail.com>
2025-01-13 11:16:52 +0900

    chore(audoedit): simplify output channel logger

--- vscode/src/autoedits/output-channel-logger.ts
+++ vscode/src/autoedits/output-channel-logger.ts
@@ -1,21 +1,15 @@
 import * as vscode from 'vscode'
+
 import { getConfiguration } from '../configuration'
 import { Logger } from '../output-channel-logger'
 
 export class AutoEditsOutputChannelLogger extends Logger {
-    private readonly logger: Logger
-
-    constructor(feature: string) {
-        super(feature)
-        this.logger = new Logger(feature)
-    }
-
     logDebugIfVerbose(filterLabel: string, text: string, ...args: unknown[]): void {
         const workspaceConfig = vscode.workspace.getConfiguration()
         const { debugVerbose } = getConfiguration(workspaceConfig)
 
         if (debugVerbose) {
-            this.logger.logDebug(filterLabel, text, ...args)
+            this.logDebug(filterLabel, text, ...args)
         }
     }
 }
