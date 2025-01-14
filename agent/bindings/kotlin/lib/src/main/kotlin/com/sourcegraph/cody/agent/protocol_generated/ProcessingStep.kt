63059598d30c24cdd05b3574922ea830c8943c83
Beatrix <beatrix@sourcegraph.com>
2025-01-13 18:36:45 -0800

    update agent bindings

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/ProcessingStep.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/ProcessingStep.kt
@@ -4,7 +4,9 @@ package com.sourcegraph.cody.agent.protocol_generated;
 import com.google.gson.annotations.SerializedName;
 
 data class ProcessingStep(
+  val type: ProcessType, // Oneof: confirmation, step
   val id: String,
+  val title: String? = null,
   val content: String,
   val status: StatusEnum, // Oneof: pending, success, error
   val step: Long? = null,
