5a3c4d37e5a4f4fc6e860f6c50055f0c3cee530a
Michael Bahr <michael.bahr@sourcegraph.com>
2025-01-14 15:09:18 +0100

    fix linting errors

--- lib/shared/src/lexicalEditor/atMentionsSerializer.ts
+++ lib/shared/src/lexicalEditor/atMentionsSerializer.ts
@@ -1,11 +1,11 @@
-import {SerializedPromptEditorValue} from "./editorState";
+import {type SerializedPromptEditorValue} from "./editorState";
 
 export function serialize(m: SerializedPromptEditorValue): string {
     return `cody://serialized?data=${btoa(JSON.stringify(m, undefined, 0))}`;
 }
 
 export function deserialize(s: string): SerializedPromptEditorValue | undefined {
-    if (!s.startsWith(`cody://serialized?data=`)) {
+    if (!s.startsWith('cody://serialized?data=')) {
         console.warn('deserialize only accepts cody://serialized?data=... strings')
         return undefined
     }
