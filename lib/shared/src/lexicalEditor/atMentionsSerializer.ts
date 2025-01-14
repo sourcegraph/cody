6b91e2fc8e895b672980a4c4918406e1dede0009
Michael Bahr <michael.bahr@sourcegraph.com>
2025-01-14 16:18:17 +0100

    version for serialized format

--- lib/shared/src/lexicalEditor/atMentionsSerializer.ts
+++ lib/shared/src/lexicalEditor/atMentionsSerializer.ts
@@ -1,14 +1,16 @@
 import type { SerializedPromptEditorValue } from './editorState'
 
+const SERIALIZED_PREFIX = 'cody://serialized.v1'
+
 export function serialize(m: SerializedPromptEditorValue): string {
-    return `cody://serialized?data=${btoa(JSON.stringify(m, undefined, 0))}`
+    return `${SERIALIZED_PREFIX}?data=${btoa(JSON.stringify(m, undefined, 0))}`
 }
 
 export function deserialize(s: string): SerializedPromptEditorValue | undefined {
-    if (!s.startsWith('cody://serialized?data=')) {
-        console.warn('deserialize only accepts cody://serialized?data=... strings')
+    if (!s.startsWith(SERIALIZED_PREFIX)) {
+        console.warn(`deserialize only accepts ${SERIALIZED_PREFIX} strings`)
         return undefined
     }
 
-    return JSON.parse(atob(s.slice('cody://serialized?data='.length))) as SerializedPromptEditorValue
+    return JSON.parse(atob(s.slice(`${SERIALIZED_PREFIX}?data=`.length))) as SerializedPromptEditorValue
 }
