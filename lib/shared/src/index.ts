5317fb94172562b7eebff01edcb1b7432177c8fb
Matthew Manela <mmanela@users.noreply.github.com>
2025-01-15 13:39:13 -0500

    feat(auth): Allow workspace to pre-populate URL for signing after redirect

--- lib/shared/src/index.ts
+++ lib/shared/src/index.ts
@@ -404,5 +404,5 @@ export {
     checkVersion,
 } from './sourcegraph-api/siteVersion'
 export { configOverwrites } from './models/configOverwrites'
-export { isS2 } from './sourcegraph-api/environments'
+export { isS2, isWorkspaceInstance } from './sourcegraph-api/environments'
 export { createGitDiff } from './editor/create-git-diff'
