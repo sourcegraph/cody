a2154ba015850b8fff60df9a4b651de4bf2e07e6
Beyang Liu <beyang@sourcegraph.com>
2025-01-19 10:31:55 +0900

    clean up SubmitButton to remove intent selector

--- vscode/webviews/chat/cells/messageCell/human/editor/toolbar/SubmitButton.tsx
+++ vscode/webviews/chat/cells/messageCell/human/editor/toolbar/SubmitButton.tsx
@@ -1,74 +1,11 @@
 import type { ChatMessage } from '@sourcegraph/cody-shared'
-import { CodyIDE } from '@sourcegraph/cody-shared'
 import clsx from 'clsx'
-import { BadgeCheck, BetweenHorizonalEnd, MessageSquare, Pencil, Search } from 'lucide-react'
 import type { FC } from 'react'
 import { Kbd } from '../../../../../../components/Kbd'
 import { Button } from '../../../../../../components/shadcn/ui/button'
 import { Tooltip, TooltipContent, TooltipTrigger } from '../../../../../../components/shadcn/ui/tooltip'
 
 export type SubmitButtonState = 'submittable' | 'emptyEditorValue' | 'waitingResponseComplete'
-
-interface IntentOption {
-    title: string
-    icon: React.FC<{ className?: string }>
-    intent: ChatMessage['intent']
-    shortcut?: React.ReactNode
-    hidden?: boolean
-}
-
-function getIntentOptions(ide: CodyIDE): IntentOption[] {
-    const standardOneBoxIntents: IntentOption[] = [
-        {
-            title: 'Auto',
-            icon: BadgeCheck,
-            intent: undefined,
-        },
-        {
-            title: 'Chat with code assistant',
-            icon: MessageSquare,
-            intent: 'chat',
-            shortcut: (
-                <>
-                    <Kbd macOS="cmd" linuxAndWindows="ctrl" />
-                    <Kbd macOS="return" linuxAndWindows="return" />
-                </>
-            ),
-        },
-        {
-            title: 'Search Code',
-            icon: Search,
-            intent: 'search',
-            shortcut: (
-                <>
-                    <Kbd macOS="cmd" linuxAndWindows="ctrl" />
-                    <Kbd macOS="opt" linuxAndWindows="alt" />
-                    <Kbd macOS="return" linuxAndWindows="return" />
-                </>
-            ),
-        },
-    ]
-
-    if (ide === CodyIDE.Web) {
-        return standardOneBoxIntents
-    }
-
-    return [
-        ...standardOneBoxIntents,
-        {
-            title: 'Edit Code',
-            icon: Pencil,
-            intent: 'edit',
-            hidden: true,
-        },
-        {
-            title: 'Insert Code',
-            icon: BetweenHorizonalEnd,
-            intent: 'insert',
-            hidden: true,
-        },
-    ]
-}
 
 export const SubmitButton: FC<{
     onClick: (intent?: ChatMessage['intent']) => void
@@ -78,13 +15,6 @@ export const SubmitButton: FC<{
     intent?: ChatMessage['intent']
     onSelectIntent?: (intent: ChatMessage['intent']) => void
 }> = ({ onClick, state = 'submittable', className, intent, onSelectIntent }) => {
-    // const experimentalOneBoxEnabled = useExperimentalOneBox()
-    // const {
-    //     clientCapabilities: { agentIDE },
-    // } = useConfig()
-
-    // const intentOptions = useMemo(() => getIntentOptions(agentIDE), [agentIDE])
-
     if (state === 'waitingResponseComplete') {
         return (
             <Tooltip>
@@ -109,8 +39,6 @@ export const SubmitButton: FC<{
             </Tooltip>
         )
     }
-
-    // const selectedIntent = intentOptions.find(option => option.intent === intent)
 
     return (
         <div className="tw-flex tw-items-center">
@@ -147,49 +75,6 @@ export const SubmitButton: FC<{
                     Send <Kbd macOS="return" linuxAndWindows="return" />
                 </TooltipContent>
             </Tooltip>
-            {/* {experimentalOneBoxEnabled && (
-                <ToolbarPopoverItem
-                    role="combobox"
-                    iconEnd="chevron"
-                    className="tw-justify-between tw-inline-flex"
-                    aria-label="Insert prompt"
-                    popoverContent={close => (
-                        <Command>
-                            <CommandList className="tw-p-2">
-                                {intentOptions.map(option =>
-                                    // biome-ignore lint/correctness/useJsxKeyInIterable: We don't need a key for null
-                                    option.hidden ? null : (
-                                        <CommandItem
-                                            key={option.intent ?? option.title}
-                                            onSelect={() => {
-                                                onSelectIntent?.(option.intent)
-                                                close()
-                                            }}
-                                            className="tw-flex tw-text-left tw-justify-between tw-rounded-sm"
-                                        >
-                                            <div className="tw-flex tw-items-center tw-gap-2">
-                                                <option.icon className="tw-size-8" />
-                                                {option.title}
-                                            </div>
-                                            {option.shortcut && (
-                                                <div className="tw-flex tw-gap-2">{option.shortcut}</div>
-                                            )}
-                                        </CommandItem>
-                                    )
-                                )}
-                            </CommandList>
-                        </Command>
-                    )}
-                    popoverContentProps={{
-                        className: 'tw-w-[250px] !tw-p-0',
-                        onCloseAutoFocus: event => {
-                            // Prevent the popover trigger from stealing focus after the user selects an
-                            // item. We want the focus to return to the editor.
-                            event.preventDefault()
-                        },
-                    }}
-                />
-            )} */}
         </div>
     )
 }
