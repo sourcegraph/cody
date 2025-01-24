70adef1783475d3ba37ce60d776f7995ef1fd636
Tom Ross <tom@umpox.com>
2025-01-24 09:28:18 +0000

    Revert "Improve reporting auth errors (#6639)"
    This reverts commit 1c16f3569c519c3c516a2b9f89cd489b7e5238da.

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/CodyContextFilterItem.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/CodyContextFilterItem.kt
@@ -1,15 +1,8 @@
 @file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
 package com.sourcegraph.cody.agent.protocol_generated;
 
-import com.google.gson.annotations.SerializedName;
-
 data class CodyContextFilterItem(
-  val repoNamePattern: RepoNamePatternEnum, // Oneof: .*
+  val repoNamePattern: String,
   val filePathPatterns: List<String>? = null,
-) {
-
-  enum class RepoNamePatternEnum {
-    @SerializedName(".*") Wildcard,
-  }
-}
+)
 
