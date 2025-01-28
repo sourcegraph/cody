6e8d30d93520a92495a1d07e0fa670bc9144ca98
Michael Bahr <1830132+bahrmichael@users.noreply.github.com>
2025-01-28 15:14:19 +0100

    fix: handle missing spaces around @ mentions in cody chat (#6842)
    This PR updates cody chat to also use proper regex splitting.
    
    Manual verification that it can handle spaces in the chat:
    
    <img width="536" alt="Screenshot 2025-01-28 at 15 09 37"
    src="https://github.com/user-attachments/assets/9bb65e34-533c-41a6-9c5b-f2133f550bc9"
    />
    
    
    ## Test plan
    
    Unit tests + manual testing

--- lib/shared/src/lexicalEditor/editorState.ts
+++ lib/shared/src/lexicalEditor/editorState.ts
@@ -17,7 +17,7 @@ import {
 } from '../context/openctx/api'
 import { displayPath } from '../editor/displayPath'
 import type { PromptString } from '../prompt/prompt-string'
-import { AT_MENTION_SERIALIZED_PREFIX, deserializeContextMentionItem } from './atMentionsSerializer'
+import { AT_MENTION_SERIALIZED_PREFIX, deserializeParagraph } from './atMentionsSerializer'
 import {
     CONTEXT_ITEM_MENTION_NODE_TYPE,
     type SerializedContextItem,
@@ -369,13 +369,13 @@ function lexicalEditorStateFromPromptString(
     const words = input.toString().split(' ')
 
     for (const word of words) {
-        if (word.startsWith(AT_MENTION_SERIALIZED_PREFIX)) {
+        if (word.includes(AT_MENTION_SERIALIZED_PREFIX)) {
             // Save previous last text or mention node before adding new mention
             if (lastTextNode) {
                 children.push(lastTextNode)
                 lastTextNode = undefined
             }
-            children.push(deserializeContextMentionItem(word))
+            children.push(...(deserializeParagraph(word) as SupportedSerializedNodes[]))
             continue
         }
         if (word.startsWith('@')) {
