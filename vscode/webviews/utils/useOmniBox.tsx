47f03f1ffce8855512387cad7b25ff1ce696e475
Tom Ross <tom@umpox.com>
2025-01-28 15:49:24 +0000

    Revert "[JB] Temp add Omnibox FF (#6847)"
    This reverts commit b595dd0863a9b2a161dbc6eeb5f3e3bbea596276.

--- vscode/webviews/utils/useOmniBox.tsx
+++ vscode/webviews/utils/useOmniBox.tsx
@@ -4,10 +4,8 @@ import { useFeatureFlag } from './useFeatureFlags'
 
 export const useOmniBox = (): boolean => {
     const config = useClientConfig()
-    // TODO(naman/tom): Remove this FF check before the Cody release on 29th.
-    const tempFFCheck = useFeatureFlag(FeatureFlag.TempCodyExperimentalOnebox)
 
-    return !!config?.omniBoxEnabled && !!tempFFCheck
+    return !!config?.omniBoxEnabled
 }
 
 export const useOmniBoxDebug = (): boolean | undefined => {
