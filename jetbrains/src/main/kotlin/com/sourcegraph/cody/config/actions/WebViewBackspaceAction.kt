d1f71b9e82afb5af0d2d2fc5d0da41f49031776c
arafatkatze <arafat.da.khan@gmail.com>
2025-01-30 15:11:01 +0000

    fixing key map issues with Jetbrains Rider IDE

--- jetbrains/src/main/kotlin/com/sourcegraph/cody/config/actions/WebViewBackspaceAction.kt
+++ jetbrains/src/main/kotlin/com/sourcegraph/cody/config/actions/WebViewBackspaceAction.kt
@@ -8,17 +8,10 @@ private val logger = Logger.getInstance(WebViewBackspaceAction::class.java)
 
 class WebViewBackspaceAction : DumbAwareEDTAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        // fake print statement to prevent IDE from handling backspace
-        logger.info("CODE222: WebViewBackspaceAction actionPerformed")
-        logger.warn("Backspace Action Triggered") // Warnings are more visible in logs
-                System.out.println("Backspace pressed") // Direct console output
-
     }
 
     override fun update(e: AnActionEvent) {
         // Only enable this in webview context
-        logger.info("CODE222: WebViewBackspaceAction update")
-        logger.warn("Backspace Action Triggered") // Warnings are more visible in logs
         e.presentation.isEnabledAndVisible = false
     }
 }
