16cc4c9840085bc06c6a65420b9b78725542c2cb
arafatkatze <arafat.da.khan@gmail.com>
2025-01-30 15:37:48 +0000

    fixing key map issues with Jetbrains Rider IDE

---
+++ jetbrains/src/main/kotlin/com/sourcegraph/cody/config/actions/WebViewShiftEnterAction.kt
@@ -0,0 +1,17 @@
+package com.sourcegraph.cody.config.actions
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.sourcegraph.common.ui.DumbAwareEDTAction
+import com.intellij.openapi.diagnostic.Logger
+
+private val logger = Logger.getInstance(WebViewShiftEnterAction::class.java)
+
+class WebViewShiftEnterAction : DumbAwareEDTAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+    }
+
+    override fun update(e: AnActionEvent) {
+        // Only enable this in webview context
+        e.presentation.isEnabledAndVisible = false
+    }
+}
