779ede5828989964f1babe5a1aa493234090076e
Tom Ross <tom@umpox.com>
2025-02-10 16:51:18 +0000

    feat(auto-edit): Use the users' font size when generating images

---
+++ vscode/src/autoedits/renderer/image-gen/canvas/render-config.ts
@@ -0,0 +1,54 @@
+import { isMacOS } from '@sourcegraph/cody-shared'
+
+/**
+ * This is the ratio that VS Code uses to automatically determine the line height based on the font size.
+ * It is referenced in the VS Code source code here: https://sourcegraph.com/github.com/microsoft/vscode@bf4c96/-/blob/src/vs/editor/common/config/fontInfo.ts?L10-14
+ */
+const GOLDEN_LINE_HEIGHT_RATIO = isMacOS() ? 1.5 : 1.35
+
+/**
+ * This is the default font size that VS Code uses when rendering text.
+ * It is referenced in the VS Code source code here: https://sourcegraph.com/github.com/microsoft/vscode@bf4c96/-/blob/src/vs/editor/common/config/editorOptions.ts?L5410-5420
+ */
+const DEFAULT_FONT_SIZE = isMacOS() ? 12 : 14
+
+export function getLineHeight(fontSize: number): number {
+    return Math.round(GOLDEN_LINE_HEIGHT_RATIO * fontSize)
+}
+
+/**
+ * Options to render the auto-edit suggestion to the canvas.
+ * This should be configurable by the user and/or the client where suitable.
+ */
+export interface RenderConfig {
+    fontSize: number
+    lineHeight: number
+    padding: { x: number; y: number }
+    maxWidth: number
+    /**
+     * The ratio at which to upscale the canvas. Used to increase resolution of the image.
+     */
+    pixelRatio: number
+    /**
+     * The background color of added characters in the diff.
+     */
+    diffHighlightColor: string
+}
+
+export interface UserProvidedRenderConfig {
+    fontSize?: number
+    lineHeight?: number
+}
+
+export function getRenderConfig(userProvidedConfig: UserProvidedRenderConfig): RenderConfig {
+    const fontSize = userProvidedConfig.fontSize || DEFAULT_FONT_SIZE
+    const lineHeight = getLineHeight(fontSize)
+    return {
+        fontSize,
+        lineHeight,
+        padding: { x: 6, y: 2 },
+        maxWidth: 1200,
+        pixelRatio: 2,
+        diffHighlightColor: 'rgba(35, 134, 54, 0.2)',
+    }
+}
--- vscode/src/autoedits/renderer/image-gen/index.ts
+++ vscode/src/autoedits/renderer/image-gen/index.ts
@@ -1,5 +1,7 @@
+import * as vscode from 'vscode'
 import type { AddedLinesDecorationInfo } from '../decorators/default-decorator'
 import { drawDecorationsToCanvas, initCanvas } from './canvas'
+import type { UserProvidedRenderConfig } from './canvas/render-config'
 import { initSyntaxHighlighter, syntaxHighlightDecorations } from './highlight'
 
 export async function initImageSuggestionService() {
@@ -9,16 +11,26 @@ export async function initImageSuggestionService() {
 interface SuggestionOptions {
     decorations: AddedLinesDecorationInfo[]
     lang: string
+    /**
+     * Note: This is currently only used for test stability, as the default font size / line height will
+     * differ between platforms.
+     */
+    config?: UserProvidedRenderConfig
 }
 
 export function generateSuggestionAsImage(options: SuggestionOptions): { light: string; dark: string } {
     const { decorations, lang } = options
+    const renderConfig: UserProvidedRenderConfig = options.config || {
+        // The image should be rendered using the same font size as the existing text in the editor.
+        // TODO: It should be possible for this value to be set by different clients (e.g. JetBrains)
+        fontSize: vscode.workspace.getConfiguration('editor').get<number>('fontSize'),
+    }
 
     const darkDecorations = syntaxHighlightDecorations(decorations, lang, 'dark')
     const lightDecorations = syntaxHighlightDecorations(decorations, lang, 'light')
 
     return {
-        dark: drawDecorationsToCanvas(darkDecorations, 'dark').toDataURL('image/png'),
-        light: drawDecorationsToCanvas(lightDecorations, 'light').toDataURL('image/png'),
+        dark: drawDecorationsToCanvas(darkDecorations, 'dark', renderConfig).toDataURL('image/png'),
+        light: drawDecorationsToCanvas(lightDecorations, 'light', renderConfig).toDataURL('image/png'),
     }
 }
