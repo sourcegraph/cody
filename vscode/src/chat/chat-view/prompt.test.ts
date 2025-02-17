67b784f13b13fed53f247815e40a9fee39691f8b
David Ichim <ichim.david@gmail.com>
2025-02-17 10:25:14 +0200

    fix(prompt): test to replace hardcoded prompt text with CHAT_PREAMBLE constant
    We need to check for PromptString as that is what prompt contains, not just simple strings

--- vscode/src/chat/chat-view/prompt.test.ts
+++ vscode/src/chat/chat-view/prompt.test.ts
@@ -1,5 +1,6 @@
 import {
     AUTH_STATUS_FIXTURE_AUTHED,
+    CHAT_PREAMBLE,
     CLIENT_CAPABILITIES_FIXTURE,
     type ContextItem,
     ContextItemSource,
@@ -79,26 +80,22 @@ describe('DefaultPrompter', () => {
         chat.addHumanMessage({ text: ps`Hello` })
 
         const { prompt, context } = await new DefaultPrompter([], []).makePrompt(chat, 0)
-        expect(prompt).toMatchInlineSnapshot(`
-          [
+        expect(prompt).toEqual([
             {
-              "speaker": "human",
-              "text": "You are Cody, an AI coding assistant from Sourcegraph.If your answer contains fenced code blocks in Markdown, include the relevant full file path in the code block tag using this structure: \`\`\`\`$LANGUAGE:$FILEPATH\`\`\`\`.
-          If you started a fenced code block, ensure you close it with the same number of backticks before the end of your message.
-          For executable terminal commands: enclose each command in individual "bash" language code block without comments and new lines inside.",
+                speaker: 'human',
+                text: CHAT_PREAMBLE,
             },
             {
-              "speaker": "assistant",
-              "text": "I am Cody, an AI coding assistant from Sourcegraph.",
+                speaker: 'assistant',
+                text: ps`I am Cody, an AI coding assistant from Sourcegraph.`,
             },
             {
-              "contextAlternatives": undefined,
-              "contextFiles": undefined,
-              "speaker": "human",
-              "text": "Hello",
+                contextAlternatives: undefined,
+                contextFiles: undefined,
+                speaker: 'human',
+                text: ps`Hello`,
             },
-          ]
-        `)
+        ])
         expect(context.used).toEqual([])
         expect(context.ignored).toEqual([])
     })
@@ -161,28 +158,22 @@ describe('DefaultPrompter', () => {
         chat.addHumanMessage({ text: ps`Hello` })
 
         const { prompt, context } = await new DefaultPrompter([], []).makePrompt(chat, 0)
-        expect(prompt).toMatchInlineSnapshot(`
-          [
+        expect(prompt).toEqual([
             {
-              "speaker": "human",
-              "text": "You are Cody, an AI coding assistant from Sourcegraph.If your answer contains fenced code blocks in Markdown, include the relevant full file path in the code block tag using this structure: \`\`\`\`$LANGUAGE:$FILEPATH\`\`\`\`.
-          If you started a fenced code block, ensure you close it with the same number of backticks before the end of your message.
-          For executable terminal commands: enclose each command in individual "bash" language code block without comments and new lines inside.
-
-          Always respond with 🧀 emojis",
+                speaker: 'human',
+                text: ps`${CHAT_PREAMBLE}\n\nAlways respond with 🧀 emojis`,
             },
             {
-              "speaker": "assistant",
-              "text": "I am Cody, an AI coding assistant from Sourcegraph.",
+                speaker: 'assistant',
+                text: ps`I am Cody, an AI coding assistant from Sourcegraph.`,
             },
             {
-              "contextAlternatives": undefined,
-              "contextFiles": undefined,
-              "speaker": "human",
-              "text": "Hello",
+                contextAlternatives: undefined,
+                contextFiles: undefined,
+                speaker: 'human',
+                text: ps`Hello`,
             },
-          ]
-        `)
+        ])
         expect(context.used).toEqual([])
         expect(context.ignored).toEqual([])
     })
