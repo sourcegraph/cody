09e42f21885d1b1751acbb249de9c2998afba9bf
Beyang Liu <beyang@sourcegraph.com>
2025-02-17 11:12:19 -0800

    remove options from chat submit button

--- vscode/webviews/chat/cells/messageCell/human/editor/toolbar/SubmitButton.tsx
+++ vscode/webviews/chat/cells/messageCell/human/editor/toolbar/SubmitButton.tsx
@@ -1,27 +1,8 @@
-import {
-    Popover,
-    PopoverContent,
-    type PopoverContentProps,
-    type PopoverProps,
-    PopoverTrigger,
-} from '@radix-ui/react-popover'
 import { type ChatMessage, CodyIDE } from '@sourcegraph/cody-shared'
 import clsx from 'clsx'
-import {
-    BetweenHorizonalEnd,
-    ChevronDown,
-    InfoIcon,
-    MessageSquare,
-    Pencil,
-    Play,
-    Search,
-    Square,
-} from 'lucide-react'
-import type { FC, FunctionComponent, KeyboardEventHandler, PropsWithChildren } from 'react'
-import { useCallback, useMemo, useRef, useState } from 'react'
-import { Kbd } from '../../../../../../components/Kbd'
-import { Badge } from '../../../../../../components/shadcn/ui/badge'
-import { Command, CommandItem, CommandList } from '../../../../../../components/shadcn/ui/command'
+import { BetweenHorizonalEnd, Pencil, Play, Square } from 'lucide-react'
+import type { FC } from 'react'
+import { useMemo } from 'react'
 import { useConfig } from '../../../../../../utils/useConfig'
 import { useOmniBox } from '../../../../../../utils/useOmniBox'
 
@@ -38,46 +19,12 @@ interface IntentOption {
 
 function getIntentOptions({
     ide,
-    isDotComUser,
-    omniBoxEnabled,
 }: {
     ide: CodyIDE
     isDotComUser: boolean
     omniBoxEnabled: boolean
 }): IntentOption[] {
-    const standardOneBoxIntents: IntentOption[] = [
-        {
-            title: 'Run as chat',
-            icon: MessageSquare,
-            intent: 'chat',
-            shortcut: <Kbd macOS="return" linuxAndWindows="return" />,
-        },
-    ]
-
-    if (omniBoxEnabled) {
-        standardOneBoxIntents.push({
-            title: (
-                <span className="tw-inline-flex tw-items-center tw-gap-4">
-                    <span>Run as search</span>
-                    <Badge>Beta</Badge>
-                </span>
-            ),
-            icon: Search,
-            intent: 'search',
-            disabled: isDotComUser,
-            shortcut: isDotComUser ? (
-                <Badge>
-                    Enterprise <InfoIcon className="tw-size-4 tw-ml-1" />
-                </Badge>
-            ) : (
-                <>
-                    <Kbd macOS="cmd" linuxAndWindows="ctrl" />
-                    <Kbd macOS="opt" linuxAndWindows="alt" />
-                    <Kbd macOS="return" linuxAndWindows="return" />
-                </>
-            ),
-        })
-    }
+    const standardOneBoxIntents: IntentOption[] = []
 
     if (ide === CodyIDE.Web) {
         return standardOneBoxIntents
@@ -113,7 +60,7 @@ export const SubmitButton: FC<{
     } = useConfig()
     const omniBoxEnabled = useOmniBox()
 
-    const { intentOptions, availableIntentOptions, disabledIntentOptions } = useMemo(() => {
+    const { intentOptions } = useMemo(() => {
         const intentOptions = getIntentOptions({
             ide: agentIDE,
             isDotComUser,
@@ -168,162 +115,16 @@ export const SubmitButton: FC<{
                 onClick={() => onClick()}
                 className={clsx(
                     'tw-px-3 tw-py-1 md:twpx-4 md:tw-py-2',
-                    'tw-rounded-tl-full tw-rounded-bl-full',
+                    'tw-rounded-full',
                     'tw-w-full tw-relative tw-border tw-border-button-border tw-box-content tw-bg-button-background hover:tw-bg-button-background-hover tw-text-button-foreground',
-
                     'disabled:tw-bg-button-secondary-background disabled:tw-text-muted-foreground'
                 )}
                 title="Send"
             >
-                <Icon className={iconClassName} />
+                <div className="tw-hidden md:tw-inline tw-flex tw-items-center tw-gap-1 tw-whitespace-nowrap tw-flex-nowrap">
+                    <Icon className={iconClassName} />
+                </div>
             </button>
-            <PopoverItem
-                aria-label="Insert prompt"
-                popoverContent={close => (
-                    <Command>
-                        <CommandList className="tw-p-2">
-                            {availableIntentOptions.map(option => (
-                                <CommandItem
-                                    key={option.intent || 'auto'}
-                                    onSelect={() => {
-                                        manuallySelectIntent(option.intent)
-                                        close()
-                                    }}
-                                    className="tw-flex tw-text-left tw-justify-between tw-rounded-sm tw-cursor-pointer tw-px-4"
-                                >
-                                    <div className="tw-flex tw-gap-4">
-                                        <option.icon className="tw-size-8 tw-mt-1" />
-                                        {option.title}
-                                    </div>
-                                    {option.shortcut && (
-                                        <div className="tw-flex tw-gap-2">{option.shortcut}</div>
-                                    )}
-                                </CommandItem>
-                            ))}
-                            {disabledIntentOptions.map(option => (
-                                <CommandItem
-                                    key={option.intent || 'auto'}
-                                    onSelect={() => {
-                                        onClick(option.intent)
-                                        close()
-                                    }}
-                                    disabled={true}
-                                    className="tw-flex tw-text-left tw-justify-between tw-rounded-sm tw-cursor-pointer"
-                                >
-                                    <div className="tw-flex tw-gap-2">
-                                        <option.icon className="tw-size-8 tw-mt-1" />
-                                        {option.title}
-                                    </div>
-                                    {option.shortcut && (
-                                        <div className="tw-flex tw-gap-2">{option.shortcut}</div>
-                                    )}
-                                </CommandItem>
-                            ))}
-                        </CommandList>
-                    </Command>
-                )}
-                popoverContentProps={{
-                    className: 'tw-w-[350px] !tw-p-0 tw-z-10 tw-my-2',
-                    onCloseAutoFocus: event => {
-                        // Prevent the popover trigger from stealing focus after the user selects an
-                        // item. We want the focus to return to the editor.
-                        event.preventDefault()
-                    },
-                }}
-            >
-                <button
-                    type="button"
-                    className={clsx(
-                        'tw-pl-1 tw-pr-2 tw-py-1 md:pl-2 md:tw-pr-3 md:tw-py-2',
-                        'tw-rounded-tr-full tw-rounded-br-full tw-border-l-0',
-                        'tw-w-full tw-relative tw-border tw-border-button-border tw-box-content tw-bg-button-background hover:tw-bg-button-background-hover tw-text-button-foreground',
-                        'disabled:tw-bg-button-secondary-background disabled:tw-text-muted-foreground'
-                    )}
-                    title="Send"
-                >
-                    <ChevronDown className="tw-size-6" />
-                </button>
-            </PopoverItem>
         </div>
-    )
-}
-
-const PopoverItem: FunctionComponent<
-    PropsWithChildren<{
-        popoverContent: (close: () => void) => React.ReactNode
-
-        defaultOpen?: boolean
-
-        onCloseByEscape?: () => void
-
-        popoverRootProps?: Pick<PopoverProps, 'onOpenChange'>
-        popoverContentProps?: Omit<PopoverContentProps, 'align'>
-    }>
-> = ({
-    popoverContent,
-    defaultOpen,
-    onCloseByEscape,
-    popoverRootProps,
-    popoverContentProps,
-    children,
-}) => {
-    const [isOpen, setIsOpen] = useState(defaultOpen)
-    const anchorRef = useRef<HTMLButtonElement>(null)
-
-    const popoverContentRef = useRef<HTMLDivElement>(null)
-
-    const onOpenChange = useCallback(
-        (open: boolean): void => {
-            popoverRootProps?.onOpenChange?.(open)
-
-            setIsOpen(open)
-
-            // Ensure we blur the popover content if it was focused, because React's `onBlur`
-            // doesn't get called when the focused event is unmounted (see
-            // https://github.com/facebook/react/issues/12363#issuecomment-1988608527). This causes
-            // a bug in our HumanMessageEditor where if you interact with any toolbar items that
-            // steal focus for their menu, then the HumanMessageRow stays with partial focus
-            // styling. See the "chat toolbar and row UI" e2e test.
-            if (
-                document.activeElement instanceof HTMLElement &&
-                popoverContentRef.current?.contains(document.activeElement)
-            ) {
-                anchorRef.current?.focus()
-            }
-        },
-        [popoverRootProps?.onOpenChange]
-    )
-
-    const close = useCallback(() => {
-        onOpenChange(false)
-    }, [onOpenChange])
-
-    // After pressing Escape, return focus to the given component.
-    const onKeyDownInPopoverContent = useCallback<KeyboardEventHandler<HTMLDivElement>>(
-        event => {
-            if (event.key === 'Escape') {
-                onCloseByEscape?.()
-            }
-            popoverContentProps?.onKeyDown?.(event)
-        },
-        [onCloseByEscape, popoverContentProps?.onKeyDown]
-    )
-
-    return (
-        <Popover open={isOpen} onOpenChange={onOpenChange} defaultOpen={defaultOpen}>
-            <PopoverTrigger asChild={true}>{children}</PopoverTrigger>
-            <PopoverContent
-                align="end"
-                onKeyDown={onKeyDownInPopoverContent}
-                ref={popoverContentRef}
-                {...popoverContentProps}
-                className={clsx(
-                    'tw-w-[350px] !tw-p-0 tw-z-10 tw-my-2 tw-shadow-lg tw-border tw-border-button-border tw-rounded-md',
-                    popoverContentProps?.className
-                )}
-            >
-                {popoverContent(close)}
-            </PopoverContent>
-        </Popover>
     )
 }
