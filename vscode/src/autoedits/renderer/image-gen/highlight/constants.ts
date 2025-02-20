647a170023ac1b9a68dcd511015d1b3452f5bdb3
Tom Ross <tom@umpox.com>
2025-02-20 14:01:25 +0000

    update themed diff colors

--- vscode/src/autoedits/renderer/image-gen/highlight/constants.ts
+++ vscode/src/autoedits/renderer/image-gen/highlight/constants.ts
@@ -1,6 +1,6 @@
 import type { SYNTAX_HIGHLIGHT_THEME } from './types'
 
 export const DEFAULT_HIGHLIGHT_COLORS: Record<SYNTAX_HIGHLIGHT_THEME, string> = {
-    dark: '#ffffff',
-    light: '#000000',
+    dark: '#f0f0f0',
+    light: '#212121',
 }
