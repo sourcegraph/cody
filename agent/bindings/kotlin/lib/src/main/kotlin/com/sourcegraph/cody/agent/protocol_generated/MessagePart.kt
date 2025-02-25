834938e5c3161f2bb4ac73753fe0768990973cb3
arafatkatze <arafat.da.khan@gmail.com>
2025-02-25 17:38:23 +0400

    Fix Kotlin bindings for image_url support

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/MessagePart.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/MessagePart.kt
@@ -1,9 +1,43 @@
 @file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
 package com.sourcegraph.cody.agent.protocol_generated;
 
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.Gson;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import java.lang.reflect.Type;
+
 sealed class MessagePart {
-  data class Text(val text: String) : MessagePart()
-  data class ImageUrl(val image_url: ImageUrlData) : MessagePart()
+  companion object {
+    val deserializer: JsonDeserializer<MessagePart> =
+      JsonDeserializer { element: JsonElement, _: Type, context: JsonDeserializationContext ->
+        when (element.getAsJsonObject().get("type").getAsString()) {
+          "text" -> context.deserialize<TextMessagePart>(element, TextMessagePart::class.java)
+          "image_url" -> context.deserialize<imageUrlMessagePart>(element, imageUrlMessagePart::class.java)
+          else -> throw Exception("Unknown discriminator ${element}")
+        }
+      }
+  }
 }
 
-data class ImageUrlData(val url: String)
+data class TextMessagePart(
+  val type: TypeEnum, // Oneof: text
+  val text: String,
+) : MessagePart() {
+
+  enum class TypeEnum {
+    @SerializedName("text") Text,
+  }
+}
+
+data class imageUrlMessagePart(
+  val type: TypeEnum, // Oneof: image_url
+  val image_url: Image_urlParams,
+) : MessagePart() {
+
+  enum class TypeEnum {
+    @SerializedName("image_url") imageUrl,
+  }
+}
+
