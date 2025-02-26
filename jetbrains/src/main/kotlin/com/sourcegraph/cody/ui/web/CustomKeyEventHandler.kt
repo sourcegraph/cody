8cffdedd75c11db9ea4c9b5192e3f2b947282200
Piotr Karczmarz <piotr@karczmarz.com>
2025-02-26 12:14:14 +0100

    Intercepting delete key works, but only the first time it's pressed (delete key doesn't work in Rider/Windows)

--- jetbrains/src/main/kotlin/com/sourcegraph/cody/ui/web/CustomKeyEventHandler.kt
+++ jetbrains/src/main/kotlin/com/sourcegraph/cody/ui/web/CustomKeyEventHandler.kt
@@ -3,6 +3,7 @@ package com.sourcegraph.cody.ui.web
 import org.cef.browser.CefBrowser
 import org.cef.handler.CefKeyboardHandler
 import org.cef.misc.BoolRef
+import org.cef.handler.CefKeyboardHandler.CefKeyEvent
 import java.awt.event.KeyEvent
 
 class CustomKeyEventHandler : CefKeyboardHandler {
@@ -12,7 +13,6 @@ class CustomKeyEventHandler : CefKeyboardHandler {
 //        }
 //        return false
 //    }
-
     //    override fun onKeyEvent(browser: CefBrowser?, event: CefKeyboardHandler.CefKeyEvent?): Boolean {
 //        if (event != null && event.windows_key_code.equals(KeyEvent.VK_BACK_SPACE)) {
 //            return false // Let the event pass through to the WebView
@@ -20,10 +20,42 @@ class CustomKeyEventHandler : CefKeyboardHandler {
 //        return true
 //    }
     override fun onPreKeyEvent(p0: CefBrowser?, p1: CefKeyboardHandler.CefKeyEvent?, p2: BoolRef?): Boolean {
-        return true;
+        return true
     }
 
-    override fun onKeyEvent(p0: CefBrowser?, p1: CefKeyboardHandler.CefKeyEvent?): Boolean {
-        return true;
+    override fun onKeyEvent(browser: CefBrowser?, event: CefKeyboardHandler.CefKeyEvent?): Boolean {
+        // Check if the event is not null
+        event?.let {
+            // Check if it's a keypress event (not release)
+            if (it.type == CefKeyEvent.EventType.KEYEVENT_KEYUP) {
+
+                // Check if the key is Delete (typically keyCode 46)
+                if (it.windows_key_code == 46) {
+                    // Handle delete key press here
+                    println("Delete key was pressed!")
+                    // Return true to indicate we've handled this key
+
+//                    browser?.executeJavaScript(
+////                        "document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 46}));",
+//                        "document.activeElement.dispatchEvent(new KeyboardEvent('keyup', {key: 'Delete', code: 'Delete'}));",
+//                        browser.url,
+//                        0
+//                    )
+
+                    browser?.executeJavaScript("""
+                    const el = document.activeElement;
+                    el.dispatchEvent(new KeyboardEvent('keydown', {key: 'Delete', code: 'Delete'}));
+                    el.dispatchEvent(new KeyboardEvent('keyup', {key: 'Delete', code: 'Delete'}));
+                    """,
+                        browser.url, 0)
+
+                    return true
+                }
+            }
+        }
+
+        // Return false to let other keys be processed normally
+        return false
     }
+
 }
