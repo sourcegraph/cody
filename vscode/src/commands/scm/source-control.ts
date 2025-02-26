1fad28aeb18444eb2d5c795535b35f5559c9ddff
David Ichim <ichim.david@gmail.com>
2025-02-26 09:44:05 +0200

    fix(commands): generate commit message to use gemini flash-2.0
    - Flash-lite was a preview model and right now it fails with error from the backend

--- vscode/src/commands/scm/source-control.ts
+++ vscode/src/commands/scm/source-control.ts
@@ -39,7 +39,7 @@ export class CodySourceControl implements vscode.Disposable {
                     .getModels(ModelUsage.Chat)
                     .pipe(skipPendingOperation())
                     .subscribe(models => {
-                        const preferredModel = models.find(p => p.id.includes('gemini-2.0-flash-lite'))
+                        const preferredModel = models.find(p => p.id.endsWith('gemini-2.0-flash'))
                         this.model = preferredModel ?? models.at(0)
                     })
             )
