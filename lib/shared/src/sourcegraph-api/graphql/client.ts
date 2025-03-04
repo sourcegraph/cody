a4f9dd586df517a4e362b2b8cec2e8ca47d63d56
Piotr Kukielka <pkukielka@virtuslab.com>
2025-03-04 13:30:52 +0100

    Set Accept header to 'application/json' for GraphQL queries

--- lib/shared/src/sourcegraph-api/graphql/client.ts
+++ lib/shared/src/sourcegraph-api/graphql/client.ts
@@ -1601,7 +1601,7 @@ export class SourcegraphGraphQLAPIClient {
         signal?.throwIfAborted()
 
         const headers = new Headers(config.configuration?.customHeaders as HeadersInit | undefined)
-        headers.set('Content-Type', 'application/json; charset=utf-8')
+
         if (config.clientState.anonymousUserID && !process.env.CODY_WEB_DONT_SET_SOME_HEADERS) {
             headers.set('X-Sourcegraph-Actor-Anonymous-UID', config.clientState.anonymousUserID)
         }
@@ -1615,6 +1615,7 @@ export class SourcegraphGraphQLAPIClient {
 
         addTraceparent(headers)
         addCodyClientIdentificationHeaders(headers)
+        setJSONAcceptContentTypeHeaders(headers)
 
         try {
             await addAuthHeaders(config.auth, headers, url)
