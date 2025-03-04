8c831bdf59b47cd497cbb3230232981a2f41eab8
Piotr Kukiełka <pkukielka@users.noreply.github.com>
2025-03-04 11:57:40 +0100

    Allow to open devtools as action for an instant debugging caability (#7308)
    ## Changes
    
    Allows to open devtools at will. Sometimes we want to inspect webview
    behaviour in the current IDE without restarting it with special env
    variable. Ability to open devtools easily will be handy.
    
    ## Test plan
    
    1. Hit `Shift shift`
    2. Start typing `cody dev`
    3. Select action `Cody: Open Dev Tools` to open the devtools for the
    Cody webview

--- jetbrains/src/main/kotlin/com/sourcegraph/cody/OpenWebviewDevToolsAction.kt
+++ jetbrains/src/main/kotlin/com/sourcegraph/cody/OpenWebviewDevToolsAction.kt
@@ -3,9 +3,8 @@ package com.sourcegraph.cody
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.sourcegraph.common.ui.DumbAwareEDTAction
 
-class OpenWebviewDevToolsAction(val toolWindowContent: CodyToolWindowContent) :
-    DumbAwareEDTAction("Open WebView DevTools") {
+class OpenWebviewDevToolsAction : DumbAwareEDTAction("Open WebView DevTools") {
   override fun actionPerformed(e: AnActionEvent) {
-    toolWindowContent.openDevTools()
+    CodyToolWindowContent.executeOnInstanceIfNotDisposed(e.project ?: return) { openDevTools() }
   }
 }
