1d1e27f3728654a204451b161270ff75b46285a9
Mikołaj Kondratek <mik.kondratek@gmail.com>
2025-03-04 21:07:23 +0100

    Wire up

--- jetbrains/src/main/kotlin/com/sourcegraph/cody/listeners/CodyElementRefactoringListenerProvider.kt
+++ jetbrains/src/main/kotlin/com/sourcegraph/cody/listeners/CodyElementRefactoringListenerProvider.kt
@@ -4,24 +4,24 @@ import com.intellij.psi.PsiElement
 import com.intellij.refactoring.listeners.RefactoringElementListener
 import com.intellij.refactoring.listeners.RefactoringElementListenerProvider
 import com.sourcegraph.cody.agent.CodyAgentService.Companion.withServer
+import com.sourcegraph.cody.agent.protocol_extensions.ProtocolTextDocumentExt.uriFor
+import com.sourcegraph.cody.agent.protocol_generated.TextDocument_DidRenameParams
 
 class CodyElementListenerProvider : RefactoringElementListenerProvider {
-  override fun getListener(element: PsiElement) =
-      object : RefactoringElementListener {
-        override fun elementMoved(p0: PsiElement) {
-          val virtualFileBefore = element.containingFile.virtualFile
-          val virtualFileAfter = p0.containingFile.virtualFile
-          withServer(element.project) { server ->
-            println("elementMoved from $virtualFileBefore to $virtualFileAfter")
-          }
-        }
+  override fun getListener(element: PsiElement): RefactoringElementListener {
+    val uriBefore = uriFor(element.containingFile.virtualFile)
+    return object : RefactoringElementListener {
+      override fun elementMoved(p0: PsiElement) = notifyAgent(p0)
 
-        override fun elementRenamed(p0: PsiElement) {
-          val virtualFileBefore = element.containingFile.virtualFile
-          val virtualFileAfter = p0.containingFile.virtualFile
-          withServer(element.project) { server ->
-            println("elementRenamed from $virtualFileBefore to $virtualFileAfter")
-          }
+      override fun elementRenamed(p0: PsiElement) = notifyAgent(p0)
+
+      private fun notifyAgent(p0: PsiElement) {
+        val uriAfter = uriFor(p0.containingFile.virtualFile)
+        assert(uriBefore != uriAfter)
+        withServer(element.project) { server ->
+          server.textDocument_didRename(TextDocument_DidRenameParams(uriBefore, uriAfter))
         }
       }
+    }
+  }
 }
