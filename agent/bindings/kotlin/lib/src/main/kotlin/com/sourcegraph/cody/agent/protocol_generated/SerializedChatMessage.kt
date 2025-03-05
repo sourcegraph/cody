e8029f86e843947ddc2b05b31741884b30f22bbc
Beatrix <beatrix@sourcegraph.com>
2025-03-05 09:34:10 -0800

    update agent bindings

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/SerializedChatMessage.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/SerializedChatMessage.kt
@@ -10,8 +10,8 @@ data class SerializedChatMessage(
   val speaker: SpeakerEnum, // Oneof: human, assistant, system
   val text: String? = null,
   val model: String? = null,
-  val intent: IntentEnum? = null, // Oneof: search, chat, edit, insert
-  val manuallySelectedIntent: ManuallySelectedIntentEnum? = null, // Oneof: search, chat, edit, insert
+  val intent: IntentEnum? = null, // Oneof: search, agentic, chat, edit, insert
+  val manuallySelectedIntent: ManuallySelectedIntentEnum? = null, // Oneof: search, agentic, chat, edit, insert
   val search: Any? = null,
   val didYouMeanQuery: String? = null,
   val agent: String? = null,
@@ -28,6 +28,7 @@ data class SerializedChatMessage(
 
   enum class IntentEnum {
     @SerializedName("search") Search,
+    @SerializedName("agentic") Agentic,
     @SerializedName("chat") Chat,
     @SerializedName("edit") Edit,
     @SerializedName("insert") Insert,
@@ -35,6 +36,7 @@ data class SerializedChatMessage(
 
   enum class ManuallySelectedIntentEnum {
     @SerializedName("search") Search,
+    @SerializedName("agentic") Agentic,
     @SerializedName("chat") Chat,
     @SerializedName("edit") Edit,
     @SerializedName("insert") Insert,
