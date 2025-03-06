131cc0fa89e7f8f0778c5f118ec1233efa0bc0ff
Mikołaj Kondratek <mik.kondratek@gmail.com>
2025-03-06 01:13:15 +0100

    Fixes

--- jetbrains/src/main/kotlin/com/sourcegraph/cody/listeners/CodyElementRefactoringListenerProvider.kt
+++ jetbrains/src/main/kotlin/com/sourcegraph/cody/listeners/CodyElementRefactoringListenerProvider.kt
@@ -3,7 +3,7 @@ package com.sourcegraph.cody.listeners
 import com.intellij.psi.PsiElement
 import com.intellij.refactoring.listeners.RefactoringElementListener
 import com.intellij.refactoring.listeners.RefactoringElementListenerProvider
-import com.sourcegraph.cody.agent.CodyAgentService.Companion.withServer
+import com.sourcegraph.cody.agent.CodyAgentService.Companion.withAgent
 import com.sourcegraph.cody.agent.protocol_extensions.ProtocolTextDocumentExt.uriFor
 import com.sourcegraph.cody.agent.protocol_generated.TextDocument_DidRenameParams
 
@@ -11,15 +11,15 @@ class CodyElementListenerProvider : RefactoringElementListenerProvider {
   override fun getListener(element: PsiElement): RefactoringElementListener {
     val uriBefore = uriFor(element.containingFile.virtualFile)
     return object : RefactoringElementListener {
-      override fun elementMoved(p0: PsiElement) = notifyAgent(p0)
+      override fun elementMoved(newPsiElement: PsiElement) = notifyAgent(newPsiElement)
 
-      override fun elementRenamed(p0: PsiElement) = notifyAgent(p0)
+      override fun elementRenamed(newPsiElement: PsiElement) = notifyAgent(newPsiElement)
 
-      private fun notifyAgent(p0: PsiElement) {
-        val uriAfter = uriFor(p0.containingFile.virtualFile)
+      private fun notifyAgent(newPsiElement: PsiElement) {
+        val uriAfter = uriFor(newPsiElement.containingFile.virtualFile)
         assert(uriBefore != uriAfter)
-        withServer(element.project) { server ->
-          server.textDocument_didRename(TextDocument_DidRenameParams(uriBefore, uriAfter))
+        withAgent(element.project) { agent ->
+          agent.server.textDocument_didRename(TextDocument_DidRenameParams(uriBefore, uriAfter))
         }
       }
     }
