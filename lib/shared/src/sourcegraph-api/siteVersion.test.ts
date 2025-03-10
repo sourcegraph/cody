54ae032c26741c63930437d5154482847ccaf32e
Erik Seliger <erikseliger@me.com>
2025-03-10 17:08:15 +0100

    api: Fix version detection for non-main-branch images
    I noticed that some instances were still using api-version 1, and went to dig in why that is. Turns out some edge-case in our versioning scheme wasn't honored (branch name prefix), so I fixed that.
    
    Test plan: Added tests that fail before the code change.

--- lib/shared/src/sourcegraph-api/siteVersion.test.ts
+++ lib/shared/src/sourcegraph-api/siteVersion.test.ts
@@ -11,12 +11,25 @@ describe('inferCodyApiVersion', () => {
         expect(inferCodyApiVersion('5.5.0', false)).toBe(1)
         expect(inferCodyApiVersion('5.6.0', false)).toBe(1)
         expect(inferCodyApiVersion('5.7.0', false)).toBe(1)
+        // branch deployment (2024-09-11 is the cutoff date for 2->8)
+        expect(inferCodyApiVersion('5.11.x_313350_2024-09-10_5.11-63a41475e780', false)).toBe(1)
+        // main deployment (2024-09-11 is the cutoff date for 2->8)
+        expect(inferCodyApiVersion('315302_2024-09-10_5.11-9994f058e2af', false)).toBe(1)
     })
 
     test('returns API version 2 for newer versions', () => {
         expect(inferCodyApiVersion('5.8.0', false)).toBe(2)
         expect(inferCodyApiVersion('5.9.0', false)).toBe(2)
         expect(inferCodyApiVersion('5.10.1', false)).toBe(2)
+    })
+
+    test('API version 8', () => {
+        expect(inferCodyApiVersion('6.1.0', false)).toBe(8)
+        expect(inferCodyApiVersion('6.2.0', false)).toBe(8)
+        // branch deployment
+        expect(inferCodyApiVersion('6.1.x_313350_2025-02-25_6.1-63a41475e780', false)).toBe(8)
+        // main deployment
+        expect(inferCodyApiVersion('315302_2025-03-10_6.1-9994f058e2af', false)).toBe(8)
     })
 
     test('returns API version 8 for dotcom', () => {
