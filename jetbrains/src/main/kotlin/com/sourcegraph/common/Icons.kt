a77bd5aef7d214412bfc30536822a21beb9f119a
Piotr Kukielka <pkukielka@virtuslab.com>
2025-03-10 15:13:01 +0100

    MAke sure icon initialization is lazy

--- jetbrains/src/main/kotlin/com/sourcegraph/common/Icons.kt
+++ jetbrains/src/main/kotlin/com/sourcegraph/common/Icons.kt
@@ -6,25 +6,30 @@ import javax.swing.Icon
 
 object Icons {
   object StatusBar {
-    val CompletionInProgress: Icon = AnimatedIcon.Default.INSTANCE
-    val CodyAvailable: Icon = getIcon("/icons/codyLogoMonochromatic.svg", Icons::class.java)
-    val CodyAutocompleteDisabled: Icon = getIcon("/icons/codyLogoHeavySlash.svg", Icons::class.java)
+    val CompletionInProgress: Icon by lazy { AnimatedIcon.Default.INSTANCE }
+    val CodyAvailable: Icon by lazy {
+      getIcon("/icons/codyLogoMonochromatic.svg", Icons::class.java)
+    }
+    val CodyAutocompleteDisabled: Icon by lazy {
+      getIcon("/icons/codyLogoHeavySlash.svg", Icons::class.java)
+    }
 
-    val CodyUnavailable: Icon = getIcon("/icons/codyLogoMonochromaticUnavailable.svg", Icons::class.java)
+    val CodyUnavailable: Icon by lazy {
+      getIcon("/icons/codyLogoMonochromaticUnavailable.svg", Icons::class.java)
+    }
   }
 
   object LLM {
-    val Anthropic: Icon = getIcon("/icons/chat/llm/anthropic.svg", Icons::class.java)
-    val Google: Icon = getIcon("/icons/chat/llm/google.svg", Icons::class.java)
-    val OpenAI: Icon = getIcon("/icons/chat/llm/openai.svg", Icons::class.java)
-    val Mistral: Icon = getIcon("/icons/chat/llm/mistral.svg", Icons::class.java)
-    val Ollama: Icon = getIcon("/icons/chat/llm/ollama.svg", Icons::class.java)
-    val ProSticker: Icon = getIcon("/icons/chat/llm/proSticker.svg", Icons::class.java)
+    val Anthropic: Icon by lazy { getIcon("/icons/chat/llm/anthropic.svg", Icons::class.java) }
+    val Google: Icon by lazy { getIcon("/icons/chat/llm/google.svg", Icons::class.java) }
+    val OpenAI: Icon by lazy { getIcon("/icons/chat/llm/openai.svg", Icons::class.java) }
+    val Mistral: Icon by lazy { getIcon("/icons/chat/llm/mistral.svg", Icons::class.java) }
+    val Ollama: Icon by lazy { getIcon("/icons/chat/llm/ollama.svg", Icons::class.java) }
+    val ProSticker: Icon by lazy { getIcon("/icons/chat/llm/proSticker.svg", Icons::class.java) }
   }
 
-  val CodyLogo: Icon = getIcon("/icons/codyLogo.svg", Icons::class.java)
-  val SourcegraphLogo: Icon = getIcon("/icons/sourcegraphLogo.svg", Icons::class.java)
-
-  val GearPlain: Icon = getIcon("/icons/gearPlain.svg", Icons::class.java)
-  val CodyLogoSlash: Icon = getIcon("/icons/codyLogoHeavySlash.svg", Icons::class.java)
+  val CodyLogo: Icon by lazy { getIcon("/icons/codyLogo.svg", Icons::class.java) }
+  val CodyLogoSlash: Icon by lazy { getIcon("/icons/codyLogoHeavySlash.svg", Icons::class.java) }
+  val SourcegraphLogo: Icon by lazy { getIcon("/icons/sourcegraphLogo.svg", Icons::class.java) }
+  val GearPlain: Icon by lazy { getIcon("/icons/gearPlain.svg", Icons::class.java) }
 }
