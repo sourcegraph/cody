b63ef8cdbfa7309798a4a6885c1fa9a4466e8bd3
Tom Ross <tom@umpox.com>
2025-03-11 16:41:25 +0000

    Edit: Omit prefixes for reasoning models

--- vscode/src/edit/prompt/models/openai.ts
+++ vscode/src/edit/prompt/models/openai.ts
@@ -1,32 +1,38 @@
 import { ps } from '@sourcegraph/cody-shared'
 import { PROMPT_TOPICS } from '../constants'
 import type { EditLLMInteraction } from '../type'
-import { buildGenericPrompt } from './generic'
+import { buildGenericPrompt, getGenericPrefixes } from './generic'
 
 const RESPONSE_PREFIX = ps`<${PROMPT_TOPICS.OUTPUT}>\n`
 const SHARED_PARAMETERS = {
     responseTopic: PROMPT_TOPICS.OUTPUT,
     stopSequences: [`</${PROMPT_TOPICS.OUTPUT}>`],
+}
+
+const MODEL_PREFIX = {
     assistantText: RESPONSE_PREFIX,
     assistantPrefix: RESPONSE_PREFIX,
-}
+} as const
 
 export const openai: EditLLMInteraction = {
     getEdit(options) {
         return {
             ...SHARED_PARAMETERS,
+            ...getGenericPrefixes(MODEL_PREFIX, options.isReasoningModel),
             prompt: buildGenericPrompt('edit', options),
         }
     },
     getDoc(options) {
         return {
             ...SHARED_PARAMETERS,
+            ...getGenericPrefixes(MODEL_PREFIX, options.isReasoningModel),
             prompt: buildGenericPrompt('doc', options),
         }
     },
     getFix(options) {
         return {
             ...SHARED_PARAMETERS,
+            ...getGenericPrefixes(MODEL_PREFIX, options.isReasoningModel),
             prompt: buildGenericPrompt('fix', options),
         }
     },
@@ -37,13 +43,17 @@ export const openai: EditLLMInteraction = {
         }
         return {
             ...SHARED_PARAMETERS,
-            assistantText: ps`${assistantPreamble}${RESPONSE_PREFIX}`,
+            ...getGenericPrefixes(
+                { ...MODEL_PREFIX, assistantText: ps`${assistantPreamble}${RESPONSE_PREFIX}` },
+                options.isReasoningModel
+            ),
             prompt: buildGenericPrompt('add', options),
         }
     },
     getTest(options) {
         return {
             ...SHARED_PARAMETERS,
+            ...getGenericPrefixes(MODEL_PREFIX, options.isReasoningModel),
             prompt: buildGenericPrompt('test', options),
         }
     },
