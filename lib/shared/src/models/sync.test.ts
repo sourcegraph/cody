3ea946caa9d7937da37ccc980c141d5fe5050a3e
Beatrix <beatrix@sourcegraph.com>
2025-03-12 14:37:17 -0700

    fix test

--- lib/shared/src/models/sync.test.ts
+++ lib/shared/src/models/sync.test.ts
@@ -534,8 +534,7 @@ describe('syncModels', () => {
         vi.spyOn(modelsService, 'modelsChanges', 'get').mockReturnValue(Observable.of(result))
 
         // Check if Deep Cody model is in the primary models list.
-        // NOTE: Deep Cody has been removed from the model list and should not be present in the primary models list.
-        expect(result.primaryModels.some(model => model.id.includes('deep-cody'))).toBe(false)
+        expect(result.primaryModels.some(model => model.id.includes('deep-cody'))).toBe(true)
 
         // preference should not be affected and remains unchanged as this is handled in a later step.
         expect(result.preferences.selected.chat).toBe(undefined)
