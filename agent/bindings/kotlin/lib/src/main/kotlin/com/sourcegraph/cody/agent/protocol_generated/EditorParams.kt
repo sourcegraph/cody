ede32734f325196d1e78c60b2f7c05d40269632d
Tom Ross <tom@umpox.com>
2025-03-17 15:20:01 +0000

    kt bindijngs

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/EditorParams.kt
+++
@@ -1,8 +0,0 @@
-@file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
-package com.sourcegraph.cody.agent.protocol_generated;
-
-data class EditorParams(
-  val fontSize: Long? = null,
-  val lineHeight: Long? = null,
-)
-
