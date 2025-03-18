527d9b8b2f6396c18d8ee19b403aa86944d42b9f
Mikołaj Kondratek <mik.kondratek@gmail.com>
2025-03-18 21:49:02 +0100

    Cleanup AutoeditLineStatusMarkerPopup classes

--- jetbrains/src/main/kotlin/com/sourcegraph/cody/autoedit/AutoeditLineStatusMarkerPopupPanel.kt
+++ jetbrains/src/main/kotlin/com/sourcegraph/cody/autoedit/AutoeditLineStatusMarkerPopupPanel.kt
@@ -10,10 +10,7 @@ import com.intellij.diff.util.DiffDrawUtil.InlineHighlighterBuilder
 import com.intellij.diff.util.DiffDrawUtil.LineHighlighterBuilder
 import com.intellij.diff.util.DiffUtil
 import com.intellij.diff.util.TextDiffType
-import com.intellij.ide.DataManager
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.actionSystem.ActionToolbar
-import com.intellij.openapi.actionSystem.DataProvider
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.LogicalPosition
@@ -21,7 +18,6 @@ import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.editor.markup.RangeHighlighter
 import com.intellij.openapi.util.Disposer
 import com.intellij.ui.EditorTextField
-import com.intellij.ui.ExperimentalUI
 import com.intellij.ui.Gray
 import com.intellij.ui.HintHint
 import com.intellij.ui.HintListener
@@ -30,100 +26,35 @@ import com.intellij.ui.LightweightHint
 import com.intellij.ui.RelativeFont
 import com.intellij.ui.ScreenUtil
 import com.intellij.util.ui.Advertiser
-import com.intellij.util.ui.JBInsets
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.StartupUiUtil.labelFont
-import com.intellij.util.ui.UIUtil
 import com.sourcegraph.Icons
 import java.awt.BorderLayout
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.Font
 import java.awt.Rectangle
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
 import java.util.EventObject
 import java.util.function.Consumer
-import javax.swing.BorderFactory
 import javax.swing.JComponent
 import javax.swing.JPanel
-import javax.swing.SwingUtilities
 
 /** This is a custom impl of [com.intellij.openapi.vcs.ex.LineStatusMarkerPopupPanel] */
 class AutoeditLineStatusMarkerPopupPanel
-private constructor(
-    editor: Editor,
-    toolbar: ActionToolbar,
-    editorComponent: JComponent?,
-    additionalInfo: JComponent?
-) : JPanel(BorderLayout()) {
-  private val myEditorComponent: JComponent?
-  val editor: Editor
+private constructor(val editor: Editor, private val editorComponent: JComponent) :
+    JPanel(BorderLayout()) {
 
   init {
     isOpaque = false
 
-    this.editor = editor
-    myEditorComponent = editorComponent
-    val isEditorVisible = myEditorComponent != null
-
-    val toolbarComponent = toolbar.component
-    toolbarComponent.border = null
-    toolbarComponent.background = TOOLBAR_BACKGROUND_COLOR
-
-    val toolbarPanel: JComponent = JBUI.Panels.simplePanel(toolbarComponent)
-    val outsideToolbarBorder =
-        JBUI.Borders.customLine(borderColor, 1, 1, if (isEditorVisible) 0 else 1, 1)
-    val insets =
-        JBUI.insets(
-            "VersionControl.MarkerPopup.borderInsets",
-            if (ExperimentalUI.isNewUI()) JBUI.insets(6, 8, 6, 10) else JBInsets.create(1, 5))
-    val insideToolbarBorder = JBUI.Borders.empty(insets)
-    toolbarPanel.border =
-        BorderFactory.createCompoundBorder(outsideToolbarBorder, insideToolbarBorder)
-    toolbarPanel.background = TOOLBAR_BACKGROUND_COLOR
-
-    if (additionalInfo != null) {
-      toolbarPanel.add(additionalInfo, BorderLayout.EAST)
-    }
-
-    // 'empty space' to the right of toolbar
-    val emptyPanel = JPanel()
-    emptyPanel.isOpaque = false
-    emptyPanel.preferredSize = Dimension()
-
-    val topPanel = JPanel(BorderLayout())
-    topPanel.isOpaque = false
-    //        topPanel.add(toolbarPanel, BorderLayout.WEST) // todo: toolbar to be considered
-    topPanel.add(emptyPanel, BorderLayout.CENTER)
-
-    add(topPanel, BorderLayout.NORTH)
-    if (myEditorComponent != null) add(myEditorComponent, BorderLayout.CENTER)
-
+    add(editorComponent, BorderLayout.CENTER)
     add(MyAdvertiser().adComponent, BorderLayout.SOUTH)
-
-    // transfer clicks into editor
-    val listener: MouseAdapter =
-        object : MouseAdapter() {
-          override fun mousePressed(e: MouseEvent) {
-            transferEvent(e, editor)
-          }
-
-          override fun mouseClicked(e: MouseEvent) {
-            transferEvent(e, editor)
-          }
-
-          override fun mouseReleased(e: MouseEvent) {
-            transferEvent(e, editor)
-          }
-        }
-    emptyPanel.addMouseListener(listener)
   }
 
   val editorTextOffset: Int
     get() =
         EditorFragmentComponent.createEditorFragmentBorder(editor)
-            .getBorderInsets(myEditorComponent)
+            .getBorderInsets(editorComponent)
             .left
 
   override fun getPreferredSize(): Dimension {
@@ -144,29 +75,14 @@ private constructor(
   }
 
   companion object {
-    private val TOOLBAR_BACKGROUND_COLOR =
-        JBColor.namedColor(
-            "VersionControl.MarkerPopup.Toolbar.background", UIUtil.getPanelBackground())
-
-    private fun transferEvent(e: MouseEvent, editor: Editor) {
-      editor.contentComponent.dispatchEvent(
-          SwingUtilities.convertMouseEvent(e.component, e, editor.contentComponent))
-    }
 
     fun showPopupAt(
         editor: Editor,
-        toolbar: ActionToolbar,
-        editorComponent: JComponent?,
-        additionalInfoPanel: JComponent?,
+        editorComponent: JComponent,
         logicalPosition: LogicalPosition,
-        childDisposable: Disposable,
-        dataProvider: DataProvider?
+        childDisposable: Disposable
     ) {
-      val popupPanel =
-          AutoeditLineStatusMarkerPopupPanel(editor, toolbar, editorComponent, additionalInfoPanel)
-
-      if (dataProvider != null) DataManager.registerDataProvider(popupPanel, dataProvider)
-      toolbar.targetComponent = popupPanel
+      val popupPanel = AutoeditLineStatusMarkerPopupPanel(editor, editorComponent)
 
       val hint = LightweightHint(popupPanel)
       val closeListener = HintListener { _ -> Disposer.dispose(childDisposable) }
@@ -181,7 +97,14 @@ private constructor(
               HintManager.HIDE_BY_TEXT_CHANGE or
               HintManager.UPDATE_BY_SCROLLING
       HintManagerImpl.getInstanceImpl()
-          .showEditorHint(hint, editor, point, flags, -1, false, HintHint(editor, point))
+          .showEditorHint(
+              hint,
+              editor,
+              point,
+              flags,
+              /*timeout =*/ -1,
+              /*reviveOnEditorChange = */ false,
+              HintHint(editor, point))
 
       ApplicationManager.getApplication()
           .messageBus
