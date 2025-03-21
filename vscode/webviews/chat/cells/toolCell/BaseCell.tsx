9075f4924552b5cb0d8e100149fcf2cc649db237
julialeex <julia.xu@sourcegraph.com>
2025-03-21 12:37:04 -0700

    remove the expanded content

--- vscode/webviews/chat/cells/toolCell/BaseCell.tsx
+++ vscode/webviews/chat/cells/toolCell/BaseCell.tsx
@@ -64,11 +64,12 @@ const BaseCellComponent: FC<BaseCellProps> = ({
                         {Icon && <Icon size={16} className="tw-flex-shrink-0 tw-text-zinc-400" />}
                         {headerContent}
                     </div>
-                    {isOpen ? (
+                    {/* Only show chevron icons when there's expandable content */}
+                    {bodyContent && (isOpen ? (
                         <ChevronDown size={16} className="tw-flex-shrink-0 tw-text-zinc-400" />
                     ) : (
                         <ChevronRight size={16} className="tw-flex-shrink-0 tw-text-zinc-400" />
-                    )}
+                    ))}
                 </CollapsibleTrigger>
                 <CollapsibleContent>
                     <div className="tw-overflow-auto tw-bg-zinc-950 tw-p-0 tw-h-auto tw-max-h-[300px]">
--- vscode/webviews/chat/cells/toolCell/FileCell.tsx
+++ vscode/webviews/chat/cells/toolCell/FileCell.tsx
@@ -34,34 +34,13 @@ export const FileCell: FC<FileCellProps> = ({
         </div>
     )
 
-    const renderBodyContent = () => (
-        <div className="tw-overflow-x-auto tw-bg-zinc-950 tw-p-0">
-            <pre className="tw-font-mono tw-text-xs tw-leading-relaxed">
-                <table className="tw-w-full tw-border-collapse">
-                    <tbody>
-                        {result?.content?.split('\n').map((line, index) => (
-                            <tr key={`${index}-${line.substring(0, 10)}`}>
-                                <td className="tw-select-none tw-border-r tw-border-r-zinc-700 tw-px-2 tw-text-right tw-text-zinc-500 tw-w-12">
-                                    {index + 1}
-                                </td>
-                                <td className="tw-px-4 tw-py-0.5 tw-text-zinc-200 tw-whitespace-pre">
-                                    <div className="tw-flex tw-items-center">{line}</div>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </pre>
-        </div>
-    )
-
     return (
         <BaseCell
             icon={FileCode}
             headerContent={renderHeaderContent()}
-            bodyContent={result?.content ? renderBodyContent() : undefined}
+            bodyContent={undefined}
             className={className}
-            defaultOpen={defaultOpen}
+            defaultOpen={false} // Always closed since there's no content to show
         />
     )
 }
