7016b6fad4e3d4201ed1c17d8279a580b74026fc
Piotr Kukielka <pkukielka@virtuslab.com>
2025-03-28 09:53:01 +0100

    Revert "Tests should use its own temp keystore (#7567)"
    This reverts commit 44e30cd382dba0f1891d6d6f68489c0ab96d4dd2.

--- jetbrains/src/main/kotlin/com/sourcegraph/cody/auth/CodySecureStore.kt
+++ jetbrains/src/main/kotlin/com/sourcegraph/cody/auth/CodySecureStore.kt
@@ -6,7 +6,6 @@ import com.intellij.credentialStore.generateServiceName
 import com.intellij.ide.passwordSafe.PasswordSafe
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
-import com.sourcegraph.config.ConfigUtil
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
@@ -16,16 +15,7 @@ import org.apache.commons.lang.RandomStringUtils
 
 @Service(Service.Level.APP)
 class CodySecureStore {
-  private val keyStoreFile =
-      if (!ConfigUtil.isIntegrationTestModeEnabled()) {
-        File(System.getProperty("user.home"), ".sourcegraph/cody.keystore")
-      } else {
-        val file = File.createTempFile("cody", "keystore")
-        // File should be created and initialized in the init block
-        file.delete()
-        file.deleteOnExit()
-        file
-      }
+  private val keyStoreFile = File(System.getProperty("user.home"), ".sourcegraph/cody.keystore")
 
   init {
     if (!keyStoreFile.exists()) {
