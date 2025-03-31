bf07173efb89d809d601a729a8639b532a477759
Dominic Cooney <dominic.cooney@sourcegraph.com>
2025-03-31 17:46:40 +0900

    Stop shimmer effect when Guardrails check stops.

--- vscode/webviews/components/CodeBlockPlaceholder.tsx
+++ vscode/webviews/components/CodeBlockPlaceholder.tsx
@@ -1,3 +1,4 @@
+import { GuardrailsCheckStatus } from '@sourcegraph/cody-shared'
 import { clsx } from 'clsx'
 import type React from 'react'
 import styles from '../chat/ChatMessageContent/ChatMessageContent.module.css'
@@ -5,6 +6,7 @@ import modulestyles from './CodeBlockPlaceholder.module.css'
 
 interface CodeBlockPlaceholderProps {
     text: string
+    status: GuardrailsCheckStatus
     className?: string
 }
 
@@ -14,11 +16,22 @@ interface CodeBlockPlaceholderProps {
  */
 export const CodeBlockPlaceholder: React.FC<CodeBlockPlaceholderProps> = ({
     text,
+    status,
     className,
 }: CodeBlockPlaceholderProps) => {
     const widths = text.split('\n').map(s => s.length)
     return (
-        <div className={clsx(styles.content, 'tw-overflow-hidden tw-p-4', className)}>
+        <div
+            className={clsx(
+                styles.content,
+                'tw-overflow-hidden tw-p-4',
+                className,
+                status === GuardrailsCheckStatus.GeneratingCode ||
+                    status === GuardrailsCheckStatus.Checking
+                    ? modulestyles.guardrailsChecking
+                    : modulestyles.guardrailsChecked
+            )}
+        >
             {widths.map((width, index) => (
                 <div
                     key={`${index}-${width}`}
