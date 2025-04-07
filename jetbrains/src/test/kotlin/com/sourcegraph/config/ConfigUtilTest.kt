2c19f536cc0e1389d4f264e47f945a755d493f94
Mikołaj Kondratek <mik.kondratek@gmail.com>
2025-04-07 20:39:59 +0200

    Fix tests

--- jetbrains/src/test/kotlin/com/sourcegraph/config/ConfigUtilTest.kt
+++ jetbrains/src/test/kotlin/com/sourcegraph/config/ConfigUtilTest.kt
@@ -1,6 +1,7 @@
 import com.google.gson.JsonParser
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.sourcegraph.config.ConfigUtil
+import com.sourcegraph.utils.CodyEditorUtil
 
 class ConfigUtils : BasePlatformTestCase() {
 
@@ -12,7 +13,8 @@ class ConfigUtils : BasePlatformTestCase() {
       "cody.suggestions.mode": "autocomplete"
     }
     """
-    val result = ConfigUtil.getCustomConfiguration(project, input)
+    setCodySettingsJsonContent(input)
+    val result = ConfigUtil.getCustomConfiguration(project)
     val parsed = JsonParser.parseString(result).asJsonObject
 
     assertEquals(
@@ -34,7 +36,8 @@ class ConfigUtils : BasePlatformTestCase() {
       "cody.suggestions.mode": "autocomplete"
     }
     """
-    val result = ConfigUtil.getCustomConfiguration(project, input)
+    setCodySettingsJsonContent(input)
+    val result = ConfigUtil.getCustomConfiguration(project)
     assertNotNull(result)
     val parsed = JsonParser.parseString(result).asJsonObject
     assertEquals(2 + 1, parsed.size()) // +1 for the additional folding property
@@ -49,9 +52,15 @@ class ConfigUtils : BasePlatformTestCase() {
       "cody.suggestions.mode": "autocomplete"
     }
     """
-    val result = ConfigUtil.getCustomConfiguration(project, input)
+    setCodySettingsJsonContent(input)
+    val result = ConfigUtil.getCustomConfiguration(project)
     assertNotNull(result)
     val parsed = JsonParser.parseString(result).asJsonObject
     assertEquals(2 + 1, parsed.size()) // +1 for the additional folding property
+  }
+
+  private fun setCodySettingsJsonContent(codySettingsContent: String) {
+    val uriString = ConfigUtil.getSettingsFile(project).toUri().toString()
+    CodyEditorUtil.createFileOrScratchFromUntitled(project, uriString, codySettingsContent)
   }
 }
