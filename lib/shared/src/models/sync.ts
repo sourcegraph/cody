621615b770422b2383cdfe72dfa70c15d5128526
julialeex <julia.xu@sourcegraph.com>
2025-04-09 13:16:00 -0700

    forceRefresh for EnhancedContextWindow

--- lib/shared/src/models/sync.ts
+++ lib/shared/src/models/sync.ts
@@ -192,7 +192,8 @@ export function syncModels({
                                         ),
                                         enableToolCody,
                                         featureFlagProvider.evaluateFeatureFlag(
-                                            FeatureFlag.EnhancedContextWindow
+                                            FeatureFlag.EnhancedContextWindow,
+                                            true
                                         ),
                                         featureFlagProvider.evaluateFeatureFlag(
                                             FeatureFlag.FallbackToFlash
