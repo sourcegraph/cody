1bced53c817096613f1a732e73f922fcda5ce272
Valery Bugakov <skymk1@gmail.com>
2025-05-06 17:11:46 +0800

    fix(auto-edit): agent bindings

--- agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/Phase.kt
+++ agent/bindings/kotlin/lib/src/main/kotlin/com/sourcegraph/cody/agent/protocol_generated/Phase.kt
@@ -1,5 +1,5 @@
 @file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
 package com.sourcegraph.cody.agent.protocol_generated;
 
-typealias Phase = String // One of: started, contextLoaded, loaded, postProcessed, suggested, read, accepted, rejected, discarded
+typealias Phase = String // One of: started, contextLoaded, loaded, postProcessed, readyToBeRendered, suggested, read, accepted, rejected, discarded
 
