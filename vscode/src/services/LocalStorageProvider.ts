b65a6f1963e30d57495075c12b668b666c7be207
Beatrix <beatrix@sourcegraph.com>
2025-05-19 15:39:00 -0700

    private

--- vscode/src/services/LocalStorageProvider.ts
+++ vscode/src/services/LocalStorageProvider.ts
@@ -235,7 +235,7 @@ class LocalStorage implements LocalStorageForModelPreferences {
         }
     }
 
-    public filterChatHistoryOlderThan(days: Date, history: UserLocalHistory): void {
+    private filterChatHistoryOlderThan(days: Date, history: UserLocalHistory): void {
         const filteredChat = Object.entries(history.chat)
             .filter(
                 ([_, chat]) => new Date(chat.lastInteractionTimestamp) >= new Date(days.toISOString())
