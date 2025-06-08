48e5619ecd8a4f1666e48396e2e518107680e5f3
David Ichim <ichim.david@gmail.com>
2025-06-08 15:39:05 +0300

    fix(tree-sitter): Adjust symbol capture condition in query wrappers
    - Modified the symbol capture condition in `getLanguageSpecificQueryWrappers` to correctly identify symbols based on their position relative to the start position.
    - Changed `node.startPosition.row <= start.row` to `node.startPosition.row < start.row` to refine the symbol matching logic.
    
    This fixes the tests from `vscode/src/tree-sitter/query-tests/documentable-nodes.test.ts`

--- vscode/src/tree-sitter/query-sdk.ts
+++ vscode/src/tree-sitter/query-sdk.ts
@@ -186,7 +186,7 @@ function getLanguageSpecificQueryWrappers(
             const symbol = findLast(symbolCaptures, ({ node }) => {
                 return (
                     node.startPosition.row === start.row &&
-                    (node.startPosition.column <= start.column || node.startPosition.row <= start.row) &&
+                    (node.startPosition.column <= start.column || node.startPosition.row < start.row) &&
                     (start.column <= node.endPosition.column || start.row < node.endPosition.row)
                 )
             })
