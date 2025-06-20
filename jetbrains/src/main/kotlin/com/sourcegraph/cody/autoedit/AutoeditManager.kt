06765b11663f9674f7c3592ea774e5904846b224
Mikołaj Kondratek <mik.kondratek@gmail.com>
2025-06-20 10:44:52 +0200

    Do not hide lookups (#8073)
    This PR is a minor follow up to
    https://github.com/sourcegraph/cody/pull/8071.
    Accidentally I made lookups hint disappear just after they are shown.
    That is clearly wrong. This PR makes sure that we dispose hints only if
    there is an active autoedit.
    
    Also, the only reasonable way to dismiss the auto edit popup is to use
    the message bus - this PR does this change.
    
    ## Test plan
    - lookups work
    - lookup hint does not show with auto edit popup
    
    
    <!-- Required. See
    https://docs-legacy.sourcegraph.com/dev/background-information/testing_principles.
    -->

--- jetbrains/src/main/kotlin/com/sourcegraph/cody/autoedit/AutoeditManager.kt
+++ jetbrains/src/main/kotlin/com/sourcegraph/cody/autoedit/AutoeditManager.kt
@@ -1,8 +1,7 @@
 package com.sourcegraph.cody.autoedit
 
-import com.intellij.codeInsight.hint.HintManagerImpl
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Document
@@ -10,6 +9,7 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.ex.Range
+import com.intellij.util.messages.Topic
 import com.sourcegraph.cody.agent.protocol_generated.AutocompleteEditItem
 import kotlin.math.abs
 
@@ -91,13 +91,15 @@ class AutoeditManager(private val project: Project) {
   }
 
   fun hide() {
-    runInEdt {
-      HintManagerImpl.getInstanceImpl().hideAllHints()
-      disposable.dispose()
-    }
+    ApplicationManager.getApplication().messageBus.syncPublisher(TOPIC).run()
+    disposable.dispose()
   }
 
   companion object {
+
+    val TOPIC: Topic<Runnable> =
+        Topic<Runnable>(Runnable::class.java, Topic.BroadcastDirection.TO_DIRECT_CHILDREN)
+
     @JvmStatic fun getInstance(project: Project): AutoeditManager = project.service()
   }
 }
