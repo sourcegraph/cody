bdba122bdd0dd8b3542c880c56c25494560cc84e
Mikołaj Kondratek <mik.kondratek@gmail.com>
2025-06-25 09:27:53 +0200

    Add a test for `extensionConfiguration/didUpdate`

---
+++ agent/src/configuration.test.ts
@@ -0,0 +1,36 @@
+import path from 'node:path'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { TESTING_CREDENTIALS } from '../../vscode/src/testutils/testing-credentials'
+import { TestClient } from './TestClient'
+import { TestWorkspace } from './TestWorkspace'
+import * as vscode from './vscode-shim'
+
+describe('Configuration', () => {
+    const workspace = new TestWorkspace(path.join(__dirname, '__tests__', 'configuration'))
+    const client = TestClient.create({
+        workspaceRootUri: workspace.rootUri,
+        name: 'configuration',
+        credentials: TESTING_CREDENTIALS.dotcom,
+    })
+
+    beforeAll(async () => {
+        await workspace.beforeAll()
+        await client.beforeAll()
+    })
+
+    afterAll(async () => {
+        await workspace.afterAll()
+        await client.afterAll()
+    })
+
+    it('extensionConfiguration/didUpdate', async () => {
+        const configuration = vscode.workspace.getConfiguration()
+        configuration.update('cody.dummy.setting', 'random')
+
+        expect(client.extensionConfigurationUpdates.length).toBe(1)
+        expect(client.extensionConfigurationUpdates[0]).toBe({
+            key: 'cody.dummy.setting',
+            value: 'random',
+        })
+    }, 10_000_000)
+})
