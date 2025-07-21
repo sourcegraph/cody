2e5b3abc4d5dc8d0dec61e5afd8469a5c8bdeb13
Mikołaj Kondratek <mik.kondratek@gmail.com>
2025-07-21 11:26:07 +0200

    [JetBrains] Fix an issue with exiting the multi-caret mode (#8179)
    My [fix](https://github.com/sourcegraph/cody/pull/8071) broke exiting
    from multiline caret mode 🥲
    
    This PR fixes the issue by using the built-in chain of responsibility
    responsibly 😅
    
    ## Test Plan
    
    ### Test Case 1: Multiline Mode + Auto-edit Dismissal
    **Setup:**
    - No Vim extension enabled
    - Open any file in editor
    - Add multiple cursors (Ctrl/Cmd + Alt + Click or similar)
    - Enter multiline editing mode
    
    **Steps:**
    1. Trigger auto-edit suggestion (type code that generates autocomplete)
    2. Verify popup/inlay appears
    3. Press `Escape` key
    
    **Expected Result:**
    - Auto-edit popup/inlay is dismissed
    - Multiline cursor mode is exited (returns to single cursor)
    - Editor returns to normal single-cursor state
    
    ### Test Case 2: Vim Insert Mode + Auto-edit Dismissal  
    **Setup:**
    - Vim extension (IdeaVim) enabled
    - Open any file in editor
    - Enter Vim insert mode (`i`, `a`, `o`, etc.)
    
    **Steps:**
    1. While in insert mode, trigger auto-edit suggestion
    2. Verify popup/inlay appears
    3. Press `Escape` key
    
    **Expected Result:**
    - Auto-edit popup/inlay is dismissed
    - Vim exits insert mode and returns to normal mode
    - Cursor behavior follows Vim normal mode conventions
    
    ### Test Case 3: Baseline - No Special Mode
    **Setup:**
    - No Vim extension
    - Single cursor, normal editing mode
    
    **Steps:**
    1. Trigger auto-edit suggestion
    2. Verify popup/inlay appears  
    3. Press `Escape` key
    
    **Expected Result:**
    - Auto-edit popup/inlay is dismissed
    - Editor remains in normal editing state
    - No unexpected mode changes
    
    ---
    
    **Note:** These test cases verify that the handler properly participates
    in IntelliJ's action chain, allowing both autocomplete dismissal AND the
    underlying editor action (multiline exit, vim mode change) to execute
    correctly.
    
    
    
    <!-- Required. See
    https://docs-legacy.sourcegraph.com/dev/background-information/testing_principles.
    -->

--- jetbrains/src/main/kotlin/com/sourcegraph/cody/autocomplete/action/DisposeAutocompleteSuggestionActionHandler.kt
+++ jetbrains/src/main/kotlin/com/sourcegraph/cody/autocomplete/action/DisposeAutocompleteSuggestionActionHandler.kt
@@ -3,13 +3,22 @@ package com.sourcegraph.cody.autocomplete.action
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Caret
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler
 import com.sourcegraph.cody.autocomplete.CodyAutocompleteManager
 import com.sourcegraph.cody.autoedit.AutoeditManager
 
-class DisposeAutocompleteSuggestionActionHandler : AutocompleteActionHandler() {
+class DisposeAutocompleteSuggestionActionHandler(private val originalHandler: EditorActionHandler) :
+    AutocompleteActionHandler() {
   override fun doExecute(editor: Editor, caret: Caret?, dataContext: DataContext?) {
-    val project = editor.project ?: return
-    CodyAutocompleteManager.getInstance(project).disposeInlays(editor)
-    AutoeditManager.getInstance(project).hide()
+    editor.project?.let {
+      CodyAutocompleteManager.getInstance(it).disposeInlays(editor)
+      AutoeditManager.getInstance(it).hide()
+    }
+
+    originalHandler.execute(editor, caret, dataContext)
   }
+
+  override fun isEnabledForCaret(editor: Editor, caret: Caret, dataContext: DataContext?) =
+      if (super.isEnabledForCaret(editor, caret, dataContext)) true
+      else originalHandler.isEnabled(editor, caret, dataContext)
 }
