a022afdd7ac0277d88729590ef4bf9b1511add0f
Naman Kumar <naman@outlook.in>
2025-07-22 17:17:09 +0530

    Implement exclude patterns from workspace settings

--- vscode/src/editor/utils/findWorkspaceFiles.ts
+++ vscode/src/editor/utils/findWorkspaceFiles.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode'
+import { getExcludePattern } from '../../cody-ignore/context-filter'
 
 /**
  * Find all files in all workspace folders, respecting the user's `files.exclude`, `search.exclude`,
@@ -13,62 +14,4 @@ export async function findWorkspaceFiles(): Promise<ReadonlyArray<vscode.Uri>> {
     )
 
     return vscode.workspace.findFiles('**/*', `{${excludePatterns.join(',')}}`)
-}
-
-type IgnoreRecord = Record<string, boolean>
-
-async function getExcludePattern(workspaceFolder: vscode.WorkspaceFolder | null): Promise<string[]> {
-    const config = vscode.workspace.getConfiguration('', workspaceFolder)
-    const filesExclude = config.get<IgnoreRecord>('files.exclude', {})
-    const searchExclude = config.get<IgnoreRecord>('search.exclude', {})
-    const useIgnoreFiles = config.get<boolean>('search.useIgnoreFiles')
-    const gitignoreExclude =
-        useIgnoreFiles && workspaceFolder
-            ? await readIgnoreFile(vscode.Uri.joinPath(workspaceFolder.uri, '.gitignore'))
-            : {}
-    const ignoreExclude =
-        useIgnoreFiles && workspaceFolder
-            ? await readIgnoreFile(vscode.Uri.joinPath(workspaceFolder.uri, '.ignore'))
-            : {}
-    const mergedExclude: IgnoreRecord = {
-        ...filesExclude,
-        ...searchExclude,
-        ...gitignoreExclude,
-        ...ignoreExclude,
-    }
-    return Object.keys(mergedExclude).filter(key => mergedExclude[key] === true)
-}
-
-export async function readIgnoreFile(uri: vscode.Uri): Promise<IgnoreRecord> {
-    const ignore: IgnoreRecord = {}
-    try {
-        const data = await vscode.workspace.fs.readFile(uri)
-        for (let line of Buffer.from(data).toString('utf-8').split('\n')) {
-            if (line.startsWith('!')) {
-                continue
-            }
-
-            // Strip comment and whitespace.
-            line = line.replace(/\s*(#.*)?$/, '').trim()
-
-            if (line === '') {
-                continue
-            }
-
-            // Replace , with . that contain commas to avoid typos for entries such as
-            // *,something
-            if (line.includes(',')) {
-                line = line.replace(',', '.')
-            }
-
-            if (line.endsWith('/')) {
-                line = line.slice(0, -1)
-            }
-            if (!line.startsWith('/') && !line.startsWith('**/')) {
-                line = `**/${line}`
-            }
-            ignore[line] = true
-        }
-    } catch {}
-    return ignore
 }
