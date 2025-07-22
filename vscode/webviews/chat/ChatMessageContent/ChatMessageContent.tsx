bad155132724c53eb77b4d73b425259e0feafd40
Piotr Kukiełka <pkukielka@users.noreply.github.com>
2025-07-22 11:52:39 +0200

    Improve markdown component caching (#8183)
    This PR is not fixing a memory leak in the DOM highlighter code but
    significantly reduces amount of re-computation which is done. With
    mocked LLM response of length of 7528 characters I get:
    
    **Before my changes:**
    
    Execution time: 1 min 25 s ec
    Peak memory used: over 2GB
    Stable memory after final GC: 833MB 
    
    **After my changes:**
    
    Execution time: **45 sec**
    Peak memory used: over 1GB
    Stable memory after final GC: 719MB 
    
    Sop while memory usage drop was not significant there is very visible
    speedup in the execution speed.
    Also memory usage swings were subjectively less significant.
    
    ## Test plan
    
    I do not have good testing procedure on `main`, but I di testing using
    my testing PR, with some custom logging:
    https://github.com/sourcegraph/cody/pull/8167

--- vscode/webviews/chat/ChatMessageContent/ChatMessageContent.tsx
+++ vscode/webviews/chat/ChatMessageContent/ChatMessageContent.tsx
@@ -1,7 +1,7 @@
 import type { Guardrails, PromptString } from '@sourcegraph/cody-shared'
 import { clsx } from 'clsx'
 import type React from 'react'
-import { useCallback, useMemo } from 'react'
+import { memo, useCallback, useMemo } from 'react'
 import { RichMarkdown } from '../../components/RichMarkdown'
 import { getVSCodeAPI } from '../../utils/VSCodeApi'
 import { useConfig } from '../../utils/useConfig'
@@ -51,71 +51,79 @@ interface ChatMessageContentProps {
 /**
  * A component presenting the content of a chat message.
  */
-export const ChatMessageContent: React.FunctionComponent<ChatMessageContentProps> = ({
-    displayMarkdown,
-    isMessageLoading,
-    humanMessage,
-    copyButtonOnSubmit,
-    insertButtonOnSubmit,
-    onRegenerate,
-    regeneratingCodeBlocks,
-    guardrails,
-    className,
-    smartApply,
-    isThoughtProcessOpened,
-    setThoughtProcessOpened,
-}) => {
-    const config = useConfig()
+export const ChatMessageContent: React.FunctionComponent<ChatMessageContentProps> = memo(
+    ({
+        displayMarkdown,
+        isMessageLoading,
+        humanMessage,
+        copyButtonOnSubmit,
+        insertButtonOnSubmit,
+        onRegenerate,
+        regeneratingCodeBlocks,
+        guardrails,
+        className,
+        smartApply,
+        isThoughtProcessOpened,
+        setThoughtProcessOpened,
+    }) => {
+        const config = useConfig()
 
-    const { displayContent, thinkContent, isThinking } = useMemo(
-        () => extractThinkContent(displayMarkdown),
-        [displayMarkdown]
-    )
+        const { displayContent, thinkContent, isThinking } = useMemo(
+            () => extractThinkContent(displayMarkdown),
+            [displayMarkdown]
+        )
 
-    const onInsert = config.config.hasEditCapability ? insertButtonOnSubmit : undefined
+        const onInsert = useMemo(
+            () => (config.config.hasEditCapability ? insertButtonOnSubmit : undefined),
+            [config.config.hasEditCapability, insertButtonOnSubmit]
+        )
 
-    let onExecute: ((command: string) => void) | undefined = useCallback((command: string) => {
-        // Execute command in terminal
-        const vscodeAPI = getVSCodeAPI()
-        vscodeAPI.postMessage({
-            command: 'command',
-            id: 'cody.terminal.execute',
-            arg: command.trim(),
-        })
-    }, [])
+        let onExecute: ((command: string) => void) | undefined = useCallback((command: string) => {
+            // Execute command in terminal
+            const vscodeAPI = getVSCodeAPI()
+            vscodeAPI.postMessage({
+                command: 'command',
+                id: 'cody.terminal.execute',
+                arg: command.trim(),
+            })
+        }, [])
 
-    // TODO: Replace this isVSCode check with a client capability check for
-    // terminal execution when agent/src/vscode-shim.ts implements `terminal()`
-    onExecute = config.clientCapabilities.isVSCode ? onExecute : undefined
+        // TODO: Replace this isVSCode check with a client capability check for
+        // terminal execution when agent/src/vscode-shim.ts implements `terminal()`
+        onExecute = useMemo(
+            () => (config.clientCapabilities.isVSCode ? onExecute : undefined),
+            [config.clientCapabilities.isVSCode, onExecute]
+        )
 
-    const onCopy = useCallback(
-        (code: string) => copyButtonOnSubmit?.(code, 'Button'),
-        [copyButtonOnSubmit]
-    )
+        const onCopy = useCallback(
+            (code: string) => copyButtonOnSubmit?.(code, 'Button'),
+            [copyButtonOnSubmit]
+        )
 
-    return (
-        <div data-testid="chat-message-content">
-            {setThoughtProcessOpened && thinkContent.length > 0 && (
-                <ThinkingCell
-                    isOpen={!!isThoughtProcessOpened}
-                    setIsOpen={setThoughtProcessOpened}
-                    isThinking={isMessageLoading && isThinking}
-                    thought={thinkContent}
+        return (
+            <div data-testid="chat-message-content">
+                {setThoughtProcessOpened && thinkContent.length > 0 && (
+                    <ThinkingCell
+                        isOpen={!!isThoughtProcessOpened}
+                        setIsOpen={setThoughtProcessOpened}
+                        isThinking={isMessageLoading && isThinking}
+                        thought={thinkContent}
+                    />
+                )}
+                <RichMarkdown
+                    markdown={displayContent}
+                    isMessageLoading={isMessageLoading}
+                    guardrails={guardrails}
+                    onCopy={onCopy}
+                    onInsert={onInsert}
+                    onExecute={onExecute}
+                    onRegenerate={onRegenerate}
+                    regeneratingCodeBlocks={regeneratingCodeBlocks}
+                    smartApply={smartApply}
+                    className={clsx(styles.content, className)}
+                    hasEditIntent={humanMessage?.intent === 'edit'}
                 />
-            )}
-            <RichMarkdown
-                markdown={displayContent}
-                isMessageLoading={isMessageLoading}
-                guardrails={guardrails}
-                onCopy={onCopy}
-                onInsert={onInsert}
-                onExecute={onExecute}
-                onRegenerate={onRegenerate}
-                regeneratingCodeBlocks={regeneratingCodeBlocks}
-                smartApply={smartApply}
-                className={clsx(styles.content, className)}
-                hasEditIntent={humanMessage?.intent === 'edit'}
-            />
-        </div>
-    )
-}
+            </div>
+        )
+    }
+)
